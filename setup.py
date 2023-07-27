1672392e03c27973557f1538eb70c97e73100088
ishaan-jaff <ishaanjaffer0324@gmail.com>
2023-07-26 17:36:28 -0700

    now a py package

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='litellm',
+    version='0.1.00',
+    description='Library to easily interface with LLM API providers',
+    author='Ishaan Jaffer',
+    packages=[
+        'litellm'
+    ],
+    install_requires=[
+        'openai',
+        'cohere',
+        'os'
+    ],
+)
