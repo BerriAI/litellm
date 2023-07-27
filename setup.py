36eaaa7d367ac2812cfe9c4fb3d2c3207944cec9
ishaan-jaff <ishaanjaffer0324@gmail.com>
2023-07-26 22:31:21 -0700

    fixed a pip install bug

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='litellm',
-    version='0.1.00',
+    version='0.1.01',
     description='Library to easily interface with LLM API providers',
     author='Ishaan Jaffer',
     packages=[
