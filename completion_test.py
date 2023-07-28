00c2384a2da9bf6cc2b78b71ba186d966b64e919
Zakhar Kogan <36503576+zaharkogan@users.noreply.github.com>
2023-07-28 11:44:08 +0300

    Added openrouter.ai support

--- completion_test.py
+++ completion_test.py
@@ -1,4 +1,4 @@
-from main import completion
+from litellm.main import completion
 import os
 
 ## Configs for Models ##
@@ -8,23 +8,27 @@ import os
 
 
 messages = [{ "content": "Hello, how are you?","role": "user"}]
-# openai call
-response = completion(model="gpt-3.5-turbo", messages=messages)
-print("\nOpenAI call")
-print(response)
+# # openai call
+# response = completion(model="gpt-3.5-turbo", messages=messages)
+# print("\nOpenAI call")
+# print(response)
 
-# azure openai call
-response = completion("chatgpt-test", messages, azure=True)
-print("\nAzure call")
-print(response)
+# # azure openai call
+# response = completion("chatgpt-test", messages, azure=True)
+# print("\nAzure call")
+# print(response)
 
-# text davinci openai call
-response = completion("text-davinci-003", messages)
-print("\nDavinci call")
-print(response)
+# # text davinci openai call
+# response = completion("text-davinci-003", messages)
+# print("\nDavinci call")
+# print(response)
 
-# cohere call
-response = completion("command-nightly", messages)
-print("\nCohere call")
-print(response)
+# # cohere call
+# response = completion("command-nightly", messages)
+# print("\nCohere call")
+# print(response)
 
+# openrouter call
+response = completion("google/palm-2-codechat-bison", messages)
+print("\OpenRouter call")
+print(response)
