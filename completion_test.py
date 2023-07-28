761bf819779b018f27f5a913c00cd4043b52e9bb
Zakhar Kogan <36503576+zaharkogan@users.noreply.github.com>
2023-07-28 19:49:40 +0300

    Uncommented the tests

--- completion_test.py
+++ completion_test.py
@@ -8,25 +8,25 @@ import os
 
 
 messages = [{ "content": "Hello, how are you?","role": "user"}]
-# # openai call
-# response = completion(model="gpt-3.5-turbo", messages=messages)
-# print("\nOpenAI call")
-# print(response)
+# openai call
+response = completion(model="gpt-3.5-turbo", messages=messages)
+print("\nOpenAI call")
+print(response)
 
-# # azure openai call
-# response = completion("chatgpt-test", messages, azure=True)
-# print("\nAzure call")
-# print(response)
+# azure openai call
+response = completion("chatgpt-test", messages, azure=True)
+print("\nAzure call")
+print(response)
 
-# # text davinci openai call
-# response = completion("text-davinci-003", messages)
-# print("\nDavinci call")
-# print(response)
+# text davinci openai call
+response = completion("text-davinci-003", messages)
+print("\nDavinci call")
+print(response)
 
-# # cohere call
-# response = completion("command-nightly", messages)
-# print("\nCohere call")
-# print(response)
+# cohere call
+response = completion("command-nightly", messages)
+print("\nCohere call")
+print(response)
 
 # openrouter call
 response = completion("google/palm-2-codechat-bison", messages)
