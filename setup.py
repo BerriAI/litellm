7890915676866a2c1fe23edca0c6d7a86c29e5d7
Krrish Dholakia <krrishdholakia@gmail.com>
2023-08-01 11:35:11 -0700

    updating version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='litellm',
-    version='0.1.205',
+    version='0.1.206',
     description='Library to easily interface with LLM API providers',
     author='BerriAI',
     packages=[
