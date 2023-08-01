1b6793164109b7abb92a94ae5e5ee91463f298ff
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2023-07-31 17:48:15 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='litellm',
-    version='0.1.203',
+    version='0.1.204',
     description='Library to easily interface with LLM API providers',
     author='BerriAI',
     packages=[
