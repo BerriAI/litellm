7b2901be9ef77aefabd2eb6d247798f1e62ea30b
Krrish Dholakia <krrishdholakia@gmail.com>
2023-08-01 12:20:25 -0700

    custom timeout decorator

---
+++ litellm/tests/test_timeout.py
@@ -0,0 +1,26 @@
+#### What this tests ####
+#    This tests the timeout decorator
+
+import sys, os
+import traceback
+sys.path.insert(0, os.path.abspath('../..'))  # Adds the parent directory to the system path
+import time
+from litellm import timeout
+
+@timeout(10)
+def stop_after_10_s(force_timeout=60):
+    print("Stopping after 10 seconds")
+    time.sleep(10)
+    return
+
+
+start_time = time.time() 
+
+try:
+  stop_after_10_s(force_timeout=1)
+except:
+  pass
+
+end_time = time.time()
+
+print(f"total time: {end_time-start_time}")
