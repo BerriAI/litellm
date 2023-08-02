9d9c87db0565ae49771a7d00989ddfacafdc45c1
Krrish Dholakia <krrishdholakia@gmail.com>
2023-08-01 22:29:04 -0700

    update setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='litellm',
-    version='0.1.207',
+    version='0.1.208',
     description='Library to easily interface with LLM API providers',
     author='BerriAI',
     packages=[
@@ -16,5 +16,6 @@ setup(
         'replicate',
         'python-dotenv',
         'openai[datalib]',
+        'google.generativeai'
     ],
 )
