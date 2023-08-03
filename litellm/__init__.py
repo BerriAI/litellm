16966e61b9d642d779e0686bd67de33205174317
ishaan-jaff <ishaanjaffer0324@gmail.com>
2023-08-03 10:46:05 -0700

    new models

--- litellm/__init__.py
+++ litellm/__init__.py
@@ -9,8 +9,16 @@ api_base = None
 headers = None
 ####### COMPLETION MODELS ###################
 open_ai_chat_completion_models = [
+  "gpt-4",
+  "gpt-4-0613",
+  "gpt-4-32k",
+  "gpt-4-32k-0613",
+  #################
+  "gpt-3.5-turbo",
+  "gpt-3.5-turbo-16k",
+  "gpt-3.5-turbo-0613",
+  "gpt-3.5-turbo-16k-0613",
   'gpt-3.5-turbo', 
-  'gpt-4',
   'gpt-3.5-turbo-16k-0613',
   'gpt-3.5-turbo-16k'
 ]
