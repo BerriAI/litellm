a5e1d3a15ab2abde2f7dbbac8e0ddd41d4277c9e
Krrish Dholakia <krrishdholakia@gmail.com>
2023-08-02 17:22:34 -0700

    exposing a model list

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='litellm',
-    version='0.1.218',
+    version='0.1.219',
     description='Library to easily interface with LLM API providers',
     author='BerriAI',
     packages=[
