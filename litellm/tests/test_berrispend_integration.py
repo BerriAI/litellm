d1c42360354f034dc11c494a4eb7898c1163dd04
Krrish Dholakia <krrishdholakia@gmail.com>
2023-08-05 16:27:56 -0700

    update

--- litellm/tests/test_berrispend_integration.py
+++ litellm/tests/test_berrispend_integration.py
@@ -1,25 +1,25 @@
 #### What this tests ####
 #    This tests if logging to the helicone integration actually works
 
-import sys, os
-import traceback
-import pytest
+# import sys, os
+# import traceback
+# import pytest
 
-sys.path.insert(0, os.path.abspath('../..'))  # Adds the parent directory to the system path
-import litellm
-from litellm import embedding, completion
+# sys.path.insert(0, os.path.abspath('../..'))  # Adds the parent directory to the system path
+# import litellm
+# from litellm import embedding, completion
 
-litellm.success_callback = ["berrispend"]
-litellm.failure_callback = ["berrispend"]
+# litellm.success_callback = ["berrispend"]
+# litellm.failure_callback = ["berrispend"]
 
-litellm.set_verbose = True
+# litellm.set_verbose = True
 
-user_message = "Hello, how are you?"
-messages = [{ "content": user_message,"role": "user"}]
+# user_message = "Hello, how are you?"
+# messages = [{ "content": user_message,"role": "user"}]
 
 
-#openai call
-response = completion(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Hi 👋 - i'm openai"}]) 
+# #openai call
+# response = completion(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Hi 👋 - i'm openai"}]) 
 
-#bad request call
-response = completion(model="chatgpt-test", messages=[{"role": "user", "content": "Hi 👋 - i'm a bad request"}]) 
+# #bad request call
+# response = completion(model="chatgpt-test", messages=[{"role": "user", "content": "Hi 👋 - i'm a bad request"}]) 
