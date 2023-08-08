f3f7b755f425476844d693d548afda8db95ffe03
Zakhar Kogan <36503576+zaharkogan@users.noreply.github.com>
2023-08-08 08:22:03 +0300

    Added Openrouter support back

--- litellm/tests/test_completion.py
+++ litellm/tests/test_completion.py
@@ -32,6 +32,14 @@ def test_completion_openai_with_optional_params():
     except Exception as e:
         pytest.fail(f"Error occurred: {e}")
 
+def test_completion_openrouter():
+    try:
+        response = completion(model="google/palm-2-chat-bison", messages=messages, temperature=0.5, top_p=0.1, user="ishaan_dev@berri.ai")
+        # Add any assertions here to check the response
+        print(response)
+    except Exception as e:
+        pytest.fail(f"Error occurred: {e}")
+
 def test_completion_openai_with_more_optional_params():
     try:
         response = completion(model="gpt-3.5-turbo", messages=messages, temperature=0.5, top_p=0.1, n=2, max_tokens=150, presence_penalty=0.5, frequency_penalty=-0.5, logit_bias={123: 5}, user="ishaan_dev@berri.ai")
@@ -99,6 +107,7 @@ def test_completion_cohere():
         print(response)
     except Exception as e:
         pytest.fail(f"Error occurred: {e}")
+
 
 # Replicate API endpoints are unstable -> throw random CUDA errors -> this means our tests can fail even if our tests weren't incorrect. 
 # [TODO] improve our try-except block to handle for these
