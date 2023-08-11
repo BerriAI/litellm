0dc9c2a4c3cf266d0395e7932f42f3eb293459ac
Krrish Dholakia <krrishdholakia@gmail.com>
2023-08-11 09:51:14 -0700

    update

---
+++ litellm/tests/test_batch_completions.py
@@ -0,0 +1,17 @@
+#### What this tests ####
+#    This tests calling batch_completions by running 100 messages together
+
+import sys, os
+import traceback
+sys.path.insert(0, os.path.abspath('../..'))  # Adds the parent directory to the system path
+import litellm
+from litellm import batch_completion
+
+messages = [[{"role": "user", "content": "Hey, how's it going"}] for _ in range(100)]
+print(messages[0:5])
+print(len(messages))
+model = "gpt-3.5-turbo"
+
+result = batch_completion(model=model, messages=messages)
+print(result)
+print(len(result))
