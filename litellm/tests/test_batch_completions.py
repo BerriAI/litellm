a2efd32f5c7e2b40d7e758dcf418946f4ddcd9a6
Krrish Dholakia <krrishdholakia@gmail.com>
2023-08-11 17:12:44 -0700

    fixing test for circle ci

--- litellm/tests/test_batch_completions.py
+++ litellm/tests/test_batch_completions.py
@@ -7,7 +7,7 @@ sys.path.insert(0, os.path.abspath('../..'))  # Adds the parent directory to the
 import litellm
 from litellm import batch_completion
 
-messages = [[{"role": "user", "content": "Hey, how's it going"}] for _ in range(100)]
+messages = [[{"role": "user", "content": "Hey, how's it going"}] for _ in range(5)]
 print(messages[0:5])
 print(len(messages))
 model = "gpt-3.5-turbo"
