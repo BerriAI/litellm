a2ea39818d062137c542aeccb1b3c2ae7e72abd4
ishaan-jaff <ishaanjaffer0324@gmail.com>
2023-08-17 07:54:56 -0700

    test_proxy_stream

--- cookbook/proxy-server/test_proxy_stream.py
+++ cookbook/proxy-server/test_proxy_stream.py
@@ -1,21 +1,21 @@
-import openai
-import os
+# import openai
+# import os
 
-os.environ["OPENAI_API_KEY"] = ""
+# os.environ["OPENAI_API_KEY"] = ""
 
-openai.api_key = os.environ["OPENAI_API_KEY"]
-openai.api_base ="http://localhost:5000"
+# openai.api_key = os.environ["OPENAI_API_KEY"]
+# openai.api_base ="http://localhost:5000"
 
-messages = [
-    {
-        "role": "user",
-        "content": "write a 1 pg essay in liteLLM"
-    }
-]
+# messages = [
+#     {
+#         "role": "user",
+#         "content": "write a 1 pg essay in liteLLM"
+#     }
+# ]
 
-response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages, stream=True)
-print("got response", response)
-# response is a generator
+# response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages, stream=True)
+# print("got response", response)
+# # response is a generator
 
-for chunk in response:
-    print(chunk)
+# for chunk in response:
+#     print(chunk)
