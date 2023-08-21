61afceece1e3509a868e11258271020f96eb815d
Vince Lwt <vince@lyser.io>
2023-08-21 21:31:09 +0200

    fix error tracking

--- litellm/tests/test_llmonitor_integration.py
+++ litellm/tests/test_llmonitor_integration.py
@@ -9,37 +9,16 @@ sys.path.insert(0, os.path.abspath('../..'))
 from litellm import completion
 import litellm
 
-litellm.input_callback = ["llmonitor"]
 litellm.success_callback = ["llmonitor"]
-litellm.error_callback = ["llmonitor"]
+litellm.failure_callback = ["llmonitor"]
 
 litellm.set_verbose = True
 
-os.environ[
-    "OPENAI_API_KEY"] = "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"
+# openai call
+first_success_test = completion(model="gpt-3.5-turbo",
+                                messages=[{
+                                    "role": "user",
+                                    "content": "Hi 👋 - i'm openai"
+                                }])
 
-print(os.environ["OPENAI_API_KEY"])
-
-# def my_custom_logging_fn(model_call_dict):
-#     print(f"model call details: {model_call_dict}")
-
-# # openai call
-# response = completion(model="gpt-3.5-turbo",
-#                       messages=[{
-#                           "role": "user",
-#                           "content": "Hi 👋 - i'm openai"
-#                       }],
-#                       logger_fn=my_custom_logging_fn)
-
-# print(response)
-
-# #bad request call
-# response = completion(model="chatgpt-test", messages=[{"role": "user", "content": "Hi 👋 - i'm a bad request"}])
-
-# cohere call
-response = completion(model="command-nightly",
-                      messages=[{
-                          "role": "user",
-                          "content": "Hi 👋 - i'm cohere"
-                      }])
-print(response)
+print(first_success_test)
