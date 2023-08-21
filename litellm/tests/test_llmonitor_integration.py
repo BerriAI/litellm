647df997743b482e397be520a04b084f8777ded3
Vince Lwt <vince@lyser.io>
2023-08-21 16:26:47 +0200

    almost working llmonitor

--- litellm/tests/test_llmonitor_integration.py
+++ litellm/tests/test_llmonitor_integration.py
@@ -1,28 +1,36 @@
 #### What this tests ####
-#    This tests if logging to the helicone integration actually works
-
-from litellm import embedding, completion
-import litellm
+#    This tests if logging to the llmonitor integration actually works
+# Adds the parent directory to the system path
 import sys
 import os
-import traceback
-import pytest
 
-# Adds the parent directory to the system path
 sys.path.insert(0, os.path.abspath('../..'))
 
+from litellm import completion
+import litellm
+
+litellm.input_callback = ["llmonitor"]
 litellm.success_callback = ["llmonitor"]
+litellm.error_callback = ["llmonitor"]
 
 litellm.set_verbose = True
 
-user_message = "Hello, how are you?"
-messages = [{"content": user_message, "role": "user"}]
-
-
 # openai call
-response = completion(model="gpt-3.5-turbo",
-                      messages=[{"role": "user", "content": "Hi 👋 - i'm openai"}])
+# response = completion(model="gpt-3.5-turbo",
+#                       messages=[{
+#                           "role": "user",
+#                           "content": "Hi 👋 - i'm openai"
+#                       }])
+
+# print(response)
+
+# #bad request call
+# response = completion(model="chatgpt-test", messages=[{"role": "user", "content": "Hi 👋 - i'm a bad request"}])
 
 # cohere call
-# response = completion(model="command-nightly",
-# messages=[{"role": "user", "content": "Hi 👋 - i'm cohere"}])
+response = completion(model="command-nightly",
+                      messages=[{
+                          "role": "user",
+                          "content": "Hi 👋 - i'm cohere"
+                      }])
+print(response)
