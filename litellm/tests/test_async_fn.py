3d9518abbb447a3875f78c34d0944016e6905b7d
Krrish Dholakia <krrishdholakia@gmail.com>
2023-08-21 16:04:15 -0700

    acreate thin-client

--- litellm/tests/test_async_fn.py
+++ litellm/tests/test_async_fn.py
@@ -9,7 +9,7 @@ import asyncio
 sys.path.insert(
     0, os.path.abspath("../..")
 )  # Adds the parent directory to the system path
-from litellm import acompletion
+from litellm import acompletion, acreate
 
 
 async def test_get_response():
@@ -24,3 +24,16 @@ async def test_get_response():
 
 response = asyncio.run(test_get_response())
 print(response)
+
+# async def test_get_response():
+#     user_message = "Hello, how are you?"
+#     messages = [{"content": user_message, "role": "user"}]
+#     try:
+#         response = await acreate(model="gpt-3.5-turbo", messages=messages)
+#     except Exception as e:
+#         pytest.fail(f"error occurred: {e}")
+#     return response
+
+
+# response = asyncio.run(test_get_response())
+# print(response)
