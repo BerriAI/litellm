f8ea4f4f9e34ef4c385508aea1d2b3ccb2faad95
NANDINI-star <bagganandini8@gmail.com>
2023-08-24 03:22:59 +0530

    modified the image extension to .png

--- docs/my-website/docusaurus.config.js
+++ docs/my-website/docusaurus.config.js
@@ -60,7 +60,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/docusaurus-social-card.png',
       navbar: {
         title: '🚅 LiteLLM',
         items: [
