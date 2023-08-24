3b9ea1d1c6303cc38baff8fab84ed74ef0f6a238
Krrish Dholakia <krrishdholakia@gmail.com>
2023-08-24 15:39:41 -0700

    update dashboard tutorial

--- docs/my-website/docusaurus.config.js
+++ docs/my-website/docusaurus.config.js
@@ -31,8 +31,8 @@ const config = {
     [
       '@docusaurus/plugin-ideal-image',
       {
-        quality: 70,
-        max: 1030, // max resized image's size.
+        quality: 100,
+        max: 1920, // max resized image's size.
         min: 640, // min resized image's size. if original is lower, use that size.
         steps: 2, // the max number of images generated between min and max (inclusive)
         disableInDev: false,
