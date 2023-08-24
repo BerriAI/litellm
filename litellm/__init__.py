c4e869a15e3106a052b3766bdfa6e8c12ba541d8
Krrish Dholakia <krrishdholakia@gmail.com>
2023-08-24 08:26:00 -0700

    fixing mypy linting errors

--- litellm/__init__.py
+++ litellm/__init__.py
@@ -1,5 +1,5 @@
 import threading
-from typing import Callable, List, Optional
+from typing import Callable, List, Optional, Dict
 
 input_callback: List[str] = []
 success_callback: List[str] = []
@@ -23,7 +23,7 @@ vertex_location: Optional[str] = None
 togetherai_api_key: Optional[str] = None
 caching = False
 caching_with_models = False  # if you want the caching key to be model + prompt
-model_alias_map = {}
+model_alias_map: Dict[str, str] = {}
 debugger = False
 model_cost = {
     "babbage-002": {
