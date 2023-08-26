cb14701bc44c28482d4de7512829ac6dd4628afa
Krrish Dholakia <krrishdholakia@gmail.com>
2023-08-26 10:39:50 -0700

    update model alias map

--- litellm/tests/test_model_alias_map.py
+++ litellm/tests/test_model_alias_map.py
@@ -14,4 +14,4 @@ litellm.set_verbose = True
 
 # Test: Check if the alias created via LiteDebugger is mapped correctly
 {"top_p": 0.75, "prompt": "What's the meaning of life?", "num_beams": 4, "temperature": 0.1}
-print(completion("llama-7b", messages=[{"role": "user", "content": "Hey, how's it going?"}], top_p=0.1, temperature=0, num_beams=4, max_tokens=60))
+print(completion("llama2", messages=[{"role": "user", "content": "Hey, how's it going?"}], top_p=0.1, temperature=0, num_beams=4, max_tokens=60))
