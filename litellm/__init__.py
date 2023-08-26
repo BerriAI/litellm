ff7b4ffcf1df641df23b775a1ca0b2e22b46dd06
Krrish Dholakia <krrishdholakia@gmail.com>
2023-08-26 10:38:39 -0700

    use client opt-in

--- litellm/__init__.py
+++ litellm/__init__.py
@@ -22,6 +22,7 @@ vertex_project: Optional[str] = None
 vertex_location: Optional[str] = None
 togetherai_api_key: Optional[str] = None
 baseten_key: Optional[str] = None
+use_client = False
 caching = False
 caching_with_models = False  # if you want the caching key to be model + prompt
 model_alias_map: Dict[str, str] = {}
