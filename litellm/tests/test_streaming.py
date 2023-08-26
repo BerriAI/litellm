1192640fb9e01553235be176e94dc0586d324ac4
Krrish Dholakia <krrishdholakia@gmail.com>
2023-08-26 10:28:27 -0700

    show litedebugger for streaming objects

--- litellm/tests/test_streaming.py
+++ litellm/tests/test_streaming.py
@@ -3,14 +3,14 @@
 
 import sys, os
 import traceback
-
+import time 
 sys.path.insert(
     0, os.path.abspath("../..")
 )  # Adds the parent directory to the system path
 import litellm
 from litellm import completion
 
-litellm.set_verbose = False
+litellm.set_verbose = True
 
 score = 0
 
@@ -22,54 +22,25 @@ def logger_fn(model_call_object: dict):
 user_message = "Hello, how are you?"
 messages = [{"content": user_message, "role": "user"}]
 
-# test on openai completion call
-try:
-    response = completion(
-        model="gpt-3.5-turbo", messages=messages, stream=True, logger_fn=logger_fn
-    )
-    for chunk in response:
-        print(chunk["choices"][0]["delta"])
-    score += 1
-except:
-    print(f"error occurred: {traceback.format_exc()}")
-    pass
-
-
-# test on azure completion call
-try:
-    response = completion(
-        model="azure/chatgpt-test", messages=messages, stream=True, logger_fn=logger_fn
-    )
-    for chunk in response:
-        print(chunk["choices"][0]["delta"])
-    score += 1
-except:
-    print(f"error occurred: {traceback.format_exc()}")
-    pass
-
-
 # test on anthropic completion call
 try:
+    start_time = time.time()
     response = completion(
         model="claude-instant-1", messages=messages, stream=True, logger_fn=logger_fn
     )
     for chunk in response:
+        chunk_time = time.time() 
+        print(f"time since initial request: {chunk_time - start_time:.2f}")
         print(chunk["choices"][0]["delta"])
     score += 1
 except:
     print(f"error occurred: {traceback.format_exc()}")
     pass
 
-
-# # test on huggingface completion call
+# # test on openai completion call
 # try:
 #     response = completion(
-#         model="meta-llama/Llama-2-7b-chat-hf",
-#         messages=messages,
-#         custom_llm_provider="huggingface",
-#         custom_api_base="https://s7c7gytn18vnu4tw.us-east-1.aws.endpoints.huggingface.cloud",
-#         stream=True,
-#         logger_fn=logger_fn,
+#         model="gpt-3.5-turbo", messages=messages, stream=True, logger_fn=logger_fn
 #     )
 #     for chunk in response:
 #         print(chunk["choices"][0]["delta"])
@@ -77,3 +48,47 @@ except:
 # except:
 #     print(f"error occurred: {traceback.format_exc()}")
 #     pass
+
+
+# # test on azure completion call
+# try:
+#     response = completion(
+#         model="azure/chatgpt-test", messages=messages, stream=True, logger_fn=logger_fn
+#     )
+#     for chunk in response:
+#         print(chunk["choices"][0]["delta"])
+#     score += 1
+# except:
+#     print(f"error occurred: {traceback.format_exc()}")
+#     pass
+
+
+# # test on anthropic completion call
+# try:
+#     response = completion(
+#         model="claude-instant-1", messages=messages, stream=True, logger_fn=logger_fn
+#     )
+#     for chunk in response:
+#         print(chunk["choices"][0]["delta"])
+#     score += 1
+# except:
+#     print(f"error occurred: {traceback.format_exc()}")
+#     pass
+
+
+# # # test on huggingface completion call
+# # try:
+# #     response = completion(
+# #         model="meta-llama/Llama-2-7b-chat-hf",
+# #         messages=messages,
+# #         custom_llm_provider="huggingface",
+# #         custom_api_base="https://s7c7gytn18vnu4tw.us-east-1.aws.endpoints.huggingface.cloud",
+# #         stream=True,
+# #         logger_fn=logger_fn,
+# #     )
+# #     for chunk in response:
+# #         print(chunk["choices"][0]["delta"])
+# #     score += 1
+# # except:
+# #     print(f"error occurred: {traceback.format_exc()}")
+# #     pass
