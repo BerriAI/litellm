1d782a11538da5303d1e86c44c3568b2df4b6e1a
ishaan-jaff <ishaanjaffer0324@gmail.com>
2023-08-29 09:34:16 -0700

    remove traceloop as a dependency

--- litellm/integrations/traceloop.py
+++ litellm/integrations/traceloop.py
@@ -1,7 +1,6 @@
 import sys
-from traceloop.tracing import Tracer
-
 
 class TraceloopLogger:
     def __init__(self):
+        from traceloop.tracing import Tracer
         self.tracer = Tracer.init(app_name=sys.argv[0])
