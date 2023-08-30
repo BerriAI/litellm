8c993456ed06ba356184687a436cf2106a56332d
Nir Gazit <nirga@users.noreply.github.com>
2023-08-30 12:08:53 +0200

    Added traceloop to sidebar

--- docs/my-website/sidebars.js
+++ docs/my-website/sidebars.js
@@ -54,6 +54,7 @@ const sidebars = {
         "observability/callbacks",
         "observability/integrations",
         "observability/promptlayer_integration",
+        "observability/traceloop_integration",
         "observability/llmonitor_integration",
         "observability/helicone_integration",
         "observability/supabase_integration",
