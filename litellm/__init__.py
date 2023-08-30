04455d8ea1d77c9ef071651b7e7961b1470608e8
Krrish Dholakia <krrishdholakia@gmail.com>
2023-08-30 08:23:32 -0700

    adding new openai models

--- litellm/__init__.py
+++ litellm/__init__.py
@@ -150,10 +150,13 @@ secret_manager_client = None
 open_ai_chat_completion_models = [
     "gpt-4",
     "gpt-4-0613",
+    "gpt-4-0314",
     "gpt-4-32k",
+    "gpt-4-32k-0314",
     "gpt-4-32k-0613",
     #################
     "gpt-3.5-turbo",
+    "gpt-3.5-turbo-0301",
     "gpt-3.5-turbo-16k",
     "gpt-3.5-turbo-0613",
     "gpt-3.5-turbo-16k-0613",
