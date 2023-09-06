e30a70f0d026dd389ab54dd9bce976318e128b62
ishaan-jaff <ishaanjaffer0324@gmail.com>
2023-09-06 16:08:44 -0700

    add experimental together_computer cost calc

---
+++ litellm/tests/test_completion_cost.py
@@ -0,0 +1,22 @@
+
+# import sys, os
+# import traceback
+# from dotenv import load_dotenv
+
+# load_dotenv()
+# import os
+
+# sys.path.insert(
+#     0, os.path.abspath("../..")
+# )  # Adds the parent directory to the system path
+# import pytest
+# import litellm
+# from litellm import embedding, completion, text_completion
+# from litellm.utils import completion_cost
+
+
+# print(completion_cost(
+#         model="togethercomputer/llama-2-70b-chat", 
+#         prompt="gm", 
+#         completion="hello"
+#     ))
