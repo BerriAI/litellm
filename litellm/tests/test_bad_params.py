f7dbdb52bdc2c75750481d9115363fe91d197e01
Krrish Dholakia <krrishdholakia@gmail.com>
2023-09-09 11:18:10 -0700

    improve error message returned if model not passed in

--- litellm/tests/test_bad_params.py
+++ litellm/tests/test_bad_params.py
@@ -4,31 +4,23 @@
 
 import sys, os
 import traceback
-from dotenv import load_dotenv
-
-load_dotenv()
-# Get the current directory of the script
-current_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Get the parent directory by joining the current directory with '..'
-parent_dir = os.path.join(current_dir, "../..")
-
-# Add the parent directory to the system path
-sys.path.append(parent_dir)
-
-
+import pytest
+sys.path.insert(
+    0, os.path.abspath("../..")
+)  # Adds the parent directory to the system path
 import litellm
 from litellm import embedding, completion
 
 
 litellm.set_verbose = True
-litellm.success_callback = ["posthog"]
-litellm.failure_callback = ["slack", "sentry", "posthog"]
-
-
 user_message = "Hello, how are you?"
 messages = [{"content": user_message, "role": "user"}]
 model_val = None
+
+def test_completion_with_no_model():
+    # test on empty
+    with pytest.raises(ValueError):
+        response = completion(messages=messages)
 
 
 def test_completion_with_empty_model():
@@ -40,14 +32,14 @@ def test_completion_with_empty_model():
         pass
 
 
-# bad key
-temp_key = os.environ.get("OPENAI_API_KEY")
-os.environ["OPENAI_API_KEY"] = "bad-key"
-# test on openai completion call
-try:
-    response = completion(model="gpt-3.5-turbo", messages=messages)
-    print(f"response: {response}")
-except:
-    print(f"error occurred: {traceback.format_exc()}")
-    pass
-os.environ["OPENAI_API_KEY"] = str(temp_key)  # this passes linting#5
+# # bad key
+# temp_key = os.environ.get("OPENAI_API_KEY")
+# os.environ["OPENAI_API_KEY"] = "bad-key"
+# # test on openai completion call
+# try:
+#     response = completion(model="gpt-3.5-turbo", messages=messages)
+#     print(f"response: {response}")
+# except:
+#     print(f"error occurred: {traceback.format_exc()}")
+#     pass
+# os.environ["OPENAI_API_KEY"] = str(temp_key)  # this passes linting#5
