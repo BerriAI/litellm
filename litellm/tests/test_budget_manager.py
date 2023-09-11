e8e65805427bcf6d0cd7ad978dd13539d0d07103
Krrish Dholakia <krrishdholakia@gmail.com>
2023-09-11 13:46:24 -0700

    update budget manager with hosted client

--- litellm/tests/test_budget_manager.py
+++ litellm/tests/test_budget_manager.py
@@ -1,8 +1,9 @@
 #### What this tests ####
 #    This tests calling batch_completions by running 100 messages together
 
-import sys, os
+import sys, os, json
 import traceback
+import pytest 
 
 sys.path.insert(
     0, os.path.abspath("../..")
@@ -10,54 +11,60 @@ sys.path.insert(
 import litellm 
 from litellm import BudgetManager, completion 
 
-budget_manager = BudgetManager(type="local")
+budget_manager = BudgetManager(project_name="test_project")
 
 ## Scenario 1: User budget enough to make call
 def test_user_budget_enough():
-    user = "1234"
-    # create a budget for a user
-    budget_manager.create_budget(total_budget=10, user=user)
+    try: 
+        user = "1234"
+        # create a budget for a user
+        budget_manager.create_budget(total_budget=10, user=user)
 
-    # check if a given call can be made
-    data = {
-        "model": "gpt-3.5-turbo",
-        "messages": [{"role": "user", "content": "Hey, how's it going?"}]
-    }
-    if budget_manager.get_current_cost(user=user) <= budget_manager.get_total_budget(user):
-        response = completion(**data)
-        print(budget_manager.update_cost(completion_obj=response, user=user))
-    else:
-        response = "Sorry - no budget!"
+        # check if a given call can be made
+        data = {
+            "model": "gpt-3.5-turbo",
+            "messages": [{"role": "user", "content": "Hey, how's it going?"}]
+        }
+        if budget_manager.get_current_cost(user=user) <= budget_manager.get_total_budget(user):
+            response = completion(**data)
+            print(budget_manager.update_cost(completion_obj=response, user=user))
+        else:
+            response = "Sorry - no budget!"
 
-    print(f"response: {response}")
+        print(f"response: {response}")
+    except:
+        pytest.fail(f"An error occurred")
 
 ## Scenario 2: User budget not enough to make call
 def test_user_budget_not_enough():
-    user = "12345"
-    # create a budget for a user
-    budget_manager.create_budget(total_budget=0, user=user)
+    try: 
+        user = "12345"
+        # create a budget for a user
+        budget_manager.create_budget(total_budget=0, user=user)
 
-    # check if a given call can be made
-    data = {
-        "model": "gpt-3.5-turbo",
-        "messages": [{"role": "user", "content": "Hey, how's it going?"}]
-    }
-    model = data["model"]
-    messages = data["messages"]
-    if budget_manager.get_current_cost(user=user) < budget_manager.get_total_budget(user=user):
-        response = completion(**data)
-        print(budget_manager.update_cost(completion_obj=response, user=user))
-    else:
-        response = "Sorry - no budget!"
+        # check if a given call can be made
+        data = {
+            "model": "gpt-3.5-turbo",
+            "messages": [{"role": "user", "content": "Hey, how's it going?"}]
+        }
+        model = data["model"]
+        messages = data["messages"]
+        if budget_manager.get_current_cost(user=user) < budget_manager.get_total_budget(user=user):
+            response = completion(**data)
+            print(budget_manager.update_cost(completion_obj=response, user=user))
+        else:
+            response = "Sorry - no budget!"
 
-    print(f"response: {response}")
+        print(f"response: {response}")
+    except:
+        pytest.fail(f"An error occurred")
 
-## Scenario 3: Saving budget to disk 
-def test_budget_save_to_disk():
-    budget_manager.save_data()
-
-## Scenario 4: Loading budget from disk
-def test_budget_load_from_disk():
-    budget_manager_2 = BudgetManager(type="local") 
-
-## Scenario 5: Test get model cost from user dict 
+## Scenario 3: Saving budget to client 
+def test_save_user_budget():
+    try:
+        response = budget_manager.save_data()
+        if response["status"] == "error":
+            raise Exception(f"An error occurred - {json.dumps(response)}")
+        print(response)
+    except:
+        pytest.fail(f"An error occurred")
