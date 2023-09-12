d097b1c6c20385fdc4b3b2788f77515f9007fa94
henry-pulver <henrypulver@hotmail.co.uk>
2023-09-12 12:12:11 +0100

    Fix proxy server railway deployment
    On Railway you need to use the PORT env var for the port

--- proxy-server/main.py
+++ proxy-server/main.py
@@ -61,7 +61,7 @@ def get_models():
 
 if __name__ == "__main__":
   from waitress import serve
-  serve(app, host="0.0.0.0", port=5000, threads=500)
+  serve(app, host="0.0.0.0", port=os.environ.get("PORT", 5000), threads=500)
 
 ############### Advanced ##########################
 
