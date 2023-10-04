1423e07aabfce664aa83460f9df7872d308fd09a
ishaan-jaff <ishaanjaffer0324@gmail.com>
2023-10-03 20:56:23 -0700

    test langfuse added

---
+++ litellm/tests/test_langfuse.py
@@ -0,0 +1,29 @@
+import sys
+import os
+import io
+
+sys.path.insert(0, os.path.abspath('../..'))
+
+from litellm import completion
+import litellm
+
+litellm.success_callback = ["langfuse"]
+# litellm.set_verbose = True
+import time
+
+
+
+def test_langfuse_logging():
+    try:
+        response = completion(model="claude-instant-1.2",
+                              messages=[{
+                                  "role": "user",
+                                  "content": "Hi 👋 - i'm claude"
+                              }])
+    except Exception as e:
+        print(e)
+
+test_langfuse_logging()
+
+
+
