5a39a3554d1fd5295228b79ecf819ec105c9125a
ishaan-jaff <ishaanjaffer0324@gmail.com>
2023-10-13 08:45:17 -0700

    (docs) litellm ollama docker image

---
+++ cookbook/litellm-ollama-docker-image/test.py
@@ -0,0 +1,36 @@
+
+import openai
+
+api_base = f"http://0.0.0.0:8000"
+
+openai.api_base = api_base
+openai.api_key = "temp-key"
+print(openai.api_base)
+
+
+print(f'LiteLLM: response from proxy with streaming')
+response = openai.ChatCompletion.create(
+    model="ollama/llama2", 
+    messages = [
+        {
+            "role": "user",
+            "content": "this is a test request, acknowledge that you got it"
+        }
+    ],
+    stream=True
+)
+
+for chunk in response:
+    print(f'LiteLLM: streaming response from proxy {chunk}')
+
+response = openai.ChatCompletion.create(
+    model="ollama/llama2", 
+    messages = [
+        {
+            "role": "user",
+            "content": "this is a test request, acknowledge that you got it"
+        }
+    ]
+)
+
+print(f'LiteLLM: response from proxy {response}')
