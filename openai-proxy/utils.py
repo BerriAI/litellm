5455e75a361d6e0c2ff05869565ec95162111de2
ishaan-jaff <ishaanjaffer0324@gmail.com>
2023-10-23 14:56:27 -0700

    (fix) setting langfuse callbacks

--- openai-proxy/utils.py
+++ openai-proxy/utils.py
@@ -3,5 +3,5 @@ import dotenv
 dotenv.load_dotenv() # load env variables
 
 def set_callbacks():
-    if ("LANGUFSE_PUBLIC_KEY" in os.environ and "LANGUFSE_SECRET_KEY" in os.environ) or "LANGFUSE_HOST" in os.environ: 
+    if ("LANGFUSE_PUBLIC_KEY" in os.environ and "LANGFUSE_SECRET_KEY" in os.environ) or "LANGFUSE_HOST" in os.environ: 
         litellm.success_callback = ["langfuse"] 
