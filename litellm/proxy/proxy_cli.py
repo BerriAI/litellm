20353db8c429355890988acb023c7bab251f9090
Krrish Dholakia <krrishdholakia@gmail.com>
2023-11-04 22:24:23 -0700

    bump: version 0.13.1.dev1 → 0.13.1.dev2

--- litellm/proxy/proxy_cli.py
+++ litellm/proxy/proxy_cli.py
@@ -176,6 +176,7 @@ def run_server(host, port, api_base, api_version, model, alias, add_key, headers
         
         if port == 8000 and is_port_in_use(port):
             port = random.randint(1024, 49152)
+        print(os.listdir(os.getcwd()))
         uvicorn.run("proxy_server:app", host=host, port=port, workers=num_workers)
 
 
