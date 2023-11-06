b8cc981db50b6bc20b6cfc09572d5d7ab8c4d191
Krrish Dholakia <krrishdholakia@gmail.com>
2023-11-06 06:34:16 -0800

    fix(utils.py): better exception raising if logging object is not able to get set

--- litellm/tests/test_langfuse.py
+++ litellm/tests/test_langfuse.py
@@ -1,7 +1,8 @@
 import sys
 import os
-import io
-
+import io, asyncio
+import logging
+logging.basicConfig(level=logging.DEBUG)
 sys.path.insert(0, os.path.abspath('../..'))
 
 from litellm import completion
@@ -11,7 +12,18 @@ litellm.success_callback = ["langfuse"]
 # litellm.set_verbose = True
 import time
 
+def test_langfuse_logging_async(): 
+    async def _test_langfuse():
+        await litellm.acompletion(
+            model="gpt-3.5-turbo",
+            prompt="This is a test",
+            max_tokens=1000,
+            temperature=0.7,
+        )
+    response = asyncio.run(_test_langfuse())
+    print(f"response: {response}")
 
+test_langfuse_logging_async()
 
 def test_langfuse_logging():
     try:
@@ -27,7 +39,7 @@ def test_langfuse_logging():
     except Exception as e:
         print(e)
 
-test_langfuse_logging()
+# test_langfuse_logging()
 
 def test_langfuse_logging_custom_generation_name():
     try:
@@ -47,7 +59,7 @@ def test_langfuse_logging_custom_generation_name():
     except Exception as e:
         print(e)
 
-test_langfuse_logging_custom_generation_name()
+# test_langfuse_logging_custom_generation_name()
 
 def test_langfuse_logging_function_calling():
     function1 = [
@@ -80,7 +92,7 @@ def test_langfuse_logging_function_calling():
     except Exception as e:
         print(e)
 
-test_langfuse_logging_function_calling()
+# test_langfuse_logging_function_calling()
 
 
 
