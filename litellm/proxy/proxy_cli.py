547f41071e86905dec997ca82b9f2861714e0350
ishaan-jaff <ishaanjaffer0324@gmail.com>
2023-11-08 12:00:13 -0800

    (fix) proxy cli --test

--- litellm/proxy/proxy_cli.py
+++ litellm/proxy/proxy_cli.py
@@ -157,11 +157,7 @@ def run_server(host, port, api_base, api_version, model, alias, add_key, headers
         )
         for chunk in response:
             click.echo(f'LiteLLM: streaming response from proxy {chunk}')
-        
-        # response = openai.Completion.create(model="gpt-3.5-turbo", prompt='this is a test request, write a short poem', stream=True)
 
-        for chunk in response:
-            click.echo(f'LiteLLM: streaming response from proxy {chunk}')
         return
     else:
         if headers:
