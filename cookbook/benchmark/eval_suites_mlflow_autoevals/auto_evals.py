e221761001ba3639ea61ab0b55e67fd4fdd6748d
ishaan-jaff <ishaanjaffer0324@gmail.com>
2023-11-13 20:35:23 -0800

    (docs) cook book autoevals

---
+++ cookbook/benchmark/eval_suites_mlflow_autoevals/auto_evals.py
@@ -0,0 +1,35 @@
+import sys, os
+import traceback
+from dotenv import load_dotenv
+load_dotenv()
+
+import litellm
+from litellm import embedding, completion, completion_cost
+
+from autoevals.llm import *
+###################
+import litellm
+
+# litellm completion call
+question = "which country has the highest population"
+response = litellm.completion(
+    model = "gpt-3.5-turbo",
+    messages = [
+        {
+            "role": "user",
+            "content": question
+        }
+    ],
+)
+print(response)
+# use the auto eval Factuality() evaluator
+
+print("calling evaluator")
+evaluator = Factuality()
+result = evaluator(
+    output=response.choices[0]["message"]["content"],       # response from litellm.completion()
+    expected="India",                                       # expected output
+    input=question                                          # question passed to litellm.completion
+)
+
+print(result)
