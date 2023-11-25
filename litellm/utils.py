1be0b4040a822982e0c4c3960ce3d0f847ae2f75
Krrish Dholakia <krrishdholakia@gmail.com>
2023-11-24 16:05:51 -0800

    fix(main.py): logit bias mapping for batch_completions

--- litellm/utils.py
+++ litellm/utils.py
@@ -104,6 +104,8 @@ class UnsupportedParamsError(Exception):
     def __init__(self, status_code, message):
         self.status_code = status_code
         self.message = message
+        self.request = httpx.Request(method="POST", url=" https://openai.api.com/v1/")
+        self.response = httpx.Response(status_code=status_code, request=self.request)
         super().__init__(
             self.message
         )  # Call the base class constructor with the parameters it needs
