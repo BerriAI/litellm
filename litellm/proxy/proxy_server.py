fbdcde1a54e8be1a9f8817e79be7b739875b0010
Krrish Dholakia <krrishdholakia@gmail.com>
2023-12-01 19:45:09 -0800

    fix(proxy_server.py): fix linting errors

--- litellm/proxy/proxy_server.py
+++ litellm/proxy/proxy_server.py
@@ -295,32 +295,24 @@ def load_from_azure_key_vault(use_azure_key_vault: bool = False):
         from azure.identity import ClientSecretCredential
 
         # Set your Azure Key Vault URI
-        KVUri = os.getenv("AZURE_KEY_VAULT_URI")
+        KVUri = os.getenv("AZURE_KEY_VAULT_URI", None)
 
         # Set your Azure AD application/client ID, client secret, and tenant ID
-        client_id = os.getenv("AZURE_CLIENT_ID")
-        client_secret = os.getenv("AZURE_CLIENT_SECRET")
-        tenant_id = os.getenv("AZURE_TENANT_ID") 
+        client_id = os.getenv("AZURE_CLIENT_ID", None)
+        client_secret = os.getenv("AZURE_CLIENT_SECRET", None)
+        tenant_id = os.getenv("AZURE_TENANT_ID", None) 
 
-        # Initialize the ClientSecretCredential
-        credential = ClientSecretCredential(client_id=client_id, client_secret=client_secret, tenant_id=tenant_id)
+        if KVUri is not None and client_id is not None and client_secret is not None and tenant_id is not None: 
+            # Initialize the ClientSecretCredential
+            credential = ClientSecretCredential(client_id=client_id, client_secret=client_secret, tenant_id=tenant_id)
 
-        # Create the SecretClient using the credential
-        client = SecretClient(vault_url=KVUri, credential=credential)
+            # Create the SecretClient using the credential
+            client = SecretClient(vault_url=KVUri, credential=credential)
         
-        litellm.secret_manager_client = client
-        # # Retrieve all secrets
-        # secrets = client.get_secrets()
-
-        # # Load secrets into environment variables
-        # for secret in secrets:
-        #     secret_name = secret.name
-        #     secret_value = client.get_secret(secret_name).value
-        #     os.environ[secret_name] = secret_value
-
-        print(f"test key - : {litellm.get_secret('test-3')}")
+            litellm.secret_manager_client = client
+        else: 
+            raise Exception(f"Missing KVUri or client_id or client_secret or tenant_id from environment")
     except Exception as e: 
-        print(e)
         print("Error when loading keys from Azure Key Vault. Ensure you run `pip install azure-identity azure-keyvault-secrets`")
 
 
