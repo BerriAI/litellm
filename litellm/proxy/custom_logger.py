92df0418e77174b437cbcc30d3f8ae6d4e337f9a
ishaan-jaff <ishaanjaffer0324@gmail.com>
2023-12-04 09:23:55 -0800

    (feat) proxy:custom_logger

--- litellm/proxy/custom_logger.py
+++ litellm/proxy/custom_logger.py
@@ -5,24 +5,35 @@ class MyCustomHandler(CustomLogger):
         print(f"Pre-API Call")
     
     def log_post_api_call(self, kwargs, response_obj, start_time, end_time): 
-        # log: key, user, model, prompt, response, tokens, cost
         print(f"Post-API Call")
-        print("\n kwargs\n")
-        print(kwargs)
-        model = kwargs["model"]
-        messages = kwargs["messages"]
-        cost = litellm.completion_cost(completion_response=response_obj)
 
-        # tokens used in response 
-        usage = response_obj.usage
-        print(usage)
-
-    
     def log_stream_event(self, kwargs, response_obj, start_time, end_time):
         print(f"On Stream")
         
     def log_success_event(self, kwargs, response_obj, start_time, end_time): 
         print(f"On Success")
+        # log: key, user, model, prompt, response, tokens, cost
+        print("\n kwargs\n")
+        print(kwargs)
+        model = kwargs["model"]
+        messages = kwargs["messages"]
+        cost = litellm.completion_cost(completion_response=response_obj)
+        response = response_obj
+        # tokens used in response 
+        usage = response_obj["usage"]
+
+        print(
+            f"""
+                Model: {model},
+                Messages: {messages},
+                Usage: {usage},
+                Cost: {cost},
+                Response: {response}
+            """
+        )
+
+        print(usage)
+
 
     def log_failure_event(self, kwargs, response_obj, start_time, end_time): 
         print(f"On Failure")
