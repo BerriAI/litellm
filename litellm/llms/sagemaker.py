976e1f9f66a38284df0b4c1aa0faff5ad2197fd4
Krrish Dholakia <krrishdholakia@gmail.com>
2023-12-05 16:19:28 -0800

    fix(sagemaker.py): accept all amazon neuron llama2 models

--- litellm/llms/sagemaker.py
+++ litellm/llms/sagemaker.py
@@ -120,7 +120,7 @@ def completion(
         )
     else:
         hf_model_name = model
-        if "jumpstart-dft-meta-textgeneration-llama" in model or "meta-textgenerationneuron-llama-2-7b" in model: # llama2 model 
+        if "jumpstart-dft-meta-textgeneration-llama" in model or "meta-textgenerationneuron-llama-2" in model: # llama2 model 
             if model.endswith("-f") or "-f-" in model or "chat" in model: # sagemaker default for a chat model
                 hf_model_name = "meta-llama/Llama-2-7b-chat" # apply the prompt template for a llama2 chat model 
             else:
