f5dbddef39a9a9978dbed3c453d414c9cfe3eac6
Krrish Dholakia <krrishdholakia@gmail.com>
2023-12-09 12:28:51 -0800

    test(test_router.py): fix init issue

--- litellm/tests/test_router.py
+++ litellm/tests/test_router.py
@@ -228,7 +228,7 @@ def test_call_one_endpoint():
 			print("\n response", response)
 		asyncio.run(call_azure_completion())
 		asyncio.run(call_bedrock_claude())
-		asyncio.run(call_azure_embedding()
+		asyncio.run(call_azure_embedding())
 		
 		os.environ["AZURE_API_BASE"] = old_api_base
 		os.environ["AZURE_API_KEY"] = old_api_key
