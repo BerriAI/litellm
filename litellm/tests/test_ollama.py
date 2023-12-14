47574a0d6491f2cf7774d478035a0fdaaf3c9d5f
ishaan-jaff <ishaanjaffer0324@gmail.com>
2023-12-14 19:48:18 +0530

    (test) ollama test

--- litellm/tests/test_ollama.py
+++ litellm/tests/test_ollama.py
@@ -16,15 +16,22 @@ import litellm
 from litellm.utils import get_optional_params, get_llm_provider
 
 def test_get_ollama_params():
-    converted_params = get_optional_params(custom_llm_provider="ollama", model="llama2", max_tokens=20, temperature=0.5, stream=True)
-    print("Converted params", converted_params)
-    assert converted_params == {'num_predict': 20, 'stream': True, 'temperature': 0.5}, f"{converted_params} != {'num_predict': 20, 'stream': True, 'temperature': 0.5}"
+    try:
+        converted_params = get_optional_params(custom_llm_provider="ollama", model="llama2", max_tokens=20, temperature=0.5, stream=True)
+        print("Converted params", converted_params)
+        assert converted_params == {'num_predict': 20, 'stream': True, 'temperature': 0.5}, f"{converted_params} != {'num_predict': 20, 'stream': True, 'temperature': 0.5}"
+    except Exception as e:
+        pytest.fail(f"Error occurred: {e}")
 # test_get_ollama_params()
 
 
 def test_get_ollama_model():
-    model, custom_llm_provider, _, _ = get_llm_provider("ollama/code-llama-22")
-    print("Model", "custom_llm_provider", model, custom_llm_provider)
-    assert custom_llm_provider == "ollama", f"{custom_llm_provider} != ollama"
-    assert model == "code-llama-22", f"{model} != code-llama-22"
+    try:
+        model, custom_llm_provider, _, _ = get_llm_provider("ollama/code-llama-22")
+        print("Model", "custom_llm_provider", model, custom_llm_provider)
+        assert custom_llm_provider == "ollama", f"{custom_llm_provider} != ollama"
+        assert model == "code-llama-22", f"{model} != code-llama-22"
+    except Exception as e:
+        pytest.fail(f"Error occurred: {e}")
+
 # test_get_ollama_model()
