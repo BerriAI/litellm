9bdd6e73bbd5fad68a52844ba6fb5fa793b93968
ishaan-jaff <ishaanjaffer0324@gmail.com>
2023-12-15 21:38:44 +0530

    (feat) proxy logs: dynamodb - set table name

--- litellm/__init__.py
+++ litellm/__init__.py
@@ -58,6 +58,7 @@ aclient_session: Optional[httpx.AsyncClient] = None
 model_fallbacks: Optional[List] = None # Deprecated for 'litellm.fallbacks'
 model_cost_map_url: str = "https://raw.githubusercontent.com/BerriAI/litellm/main/model_prices_and_context_window.json"
 suppress_debug_info = False
+dynamodb_table_name: Optional[str] = None
 #### RELIABILITY ####
 request_timeout: Optional[float] = 6000
 num_retries: Optional[int] = None
