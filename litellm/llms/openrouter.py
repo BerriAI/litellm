6b272076d7e411220ddc5f989fa7a5015b19b687
ishaan-jaff <ishaanjaffer0324@gmail.com>
2023-12-18 09:16:33 +0530

    (feat) openrouter set `transforms=[]` default

--- litellm/llms/openrouter.py
+++ litellm/llms/openrouter.py
@@ -1,4 +1,5 @@
-from typing import List
+from typing import List, Dict
+import types
 
 class OpenrouterConfig():
     """
@@ -6,6 +7,24 @@ class OpenrouterConfig():
 
     """
     # OpenRouter-only parameters
-    transforms: List[str] = []
-    models: List[str] = []
-    route: str = ''
+    extra_body: Dict[str, List[str]] = {
+        'transforms': [] # default transforms to []
+    }
+
+
+    def __init__(self, 
+                 transforms: List[str] = [],
+                 models: List[str] = [],
+                 route: str = '',
+                ) -> None:
+        locals_ = locals()
+        for key, value in locals_.items():
+            if key != 'self' and value is not None:
+                setattr(self.__class__, key, value)
+
+    @classmethod
+    def get_config(cls):
+        return {k: v for k, v in cls.__dict__.items() 
+                if not k.startswith('__') 
+                and not isinstance(v, (types.FunctionType, types.BuiltinFunctionType, classmethod, staticmethod)) 
+                and v is not None}
