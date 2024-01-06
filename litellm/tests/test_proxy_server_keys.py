0e1b123a5916414c97fbefbb6f4daf1fc241f706
ishaan-jaff <ishaanjaffer0324@gmail.com>
2024-01-06 12:30:43 +0530

    (ci/cd) fix event loop bug proxy_test

--- litellm/tests/test_proxy_server_keys.py
+++ litellm/tests/test_proxy_server_keys.py
@@ -58,18 +58,18 @@ save_worker_config(
 import asyncio
 
 
-@pytest.fixture
-def event_loop():
-    """Create an instance of the default event loop for each test case."""
-    policy = asyncio.WindowsSelectorEventLoopPolicy()
-    res = policy.new_event_loop()
-    asyncio.set_event_loop(res)
-    res._close = res.close
-    res.close = lambda: None
+# @pytest.fixture
+# def event_loop():
+#     """Create an instance of the default event loop for each test case."""
+#     policy = asyncio.WindowsSelectorEventLoopPolicy()
+#     res = policy.new_event_loop()
+#     asyncio.set_event_loop(res)
+#     res._close = res.close
+#     res.close = lambda: None
 
-    yield res
+#     yield res
 
-    res._close()
+#     res._close()
 
 
 # Here you create a fixture that will be used by your tests
