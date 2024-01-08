a9194f8f1380b5fdb1d31ff8c8a65982abdf210d
ishaan-jaff <ishaanjaffer0324@gmail.com>
2024-01-08 10:35:49 +0530

    (feat) use '-debug' with proxy logger

--- litellm/_logging.py
+++ litellm/_logging.py
@@ -2,6 +2,14 @@ import logging
 
 set_verbose = False
 
+# Create a handler for the logger (you may need to adapt this based on your needs)
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+
+# Create a formatter and set it for the handler
+formatter = logging.Formatter()
+handler.setFormatter(formatter)
+
 
 def print_verbose(print_statement):
     try:
@@ -14,3 +22,6 @@ def print_verbose(print_statement):
 verbose_proxy_logger = logging.getLogger("LiteLLM Proxy")
 verbose_router_logger = logging.getLogger("LiteLLM Router")
 verbose_logger = logging.getLogger("LiteLLM")
+
+# Add the handler to the logger
+verbose_router_logger.addHandler(handler)
