5b201bbb4df64779ddcb1d987f80ba3e1cc161b0
ishaan-jaff <ishaanjaffer0324@gmail.com>
2024-01-16 16:20:29 -0800

    (test) add new key gen tests

---
+++ litellm/tests/test_key_generate_dynamodb.py
@@ -0,0 +1,9 @@
+# Test the following scenarios:
+# 1. Generate a Key, and use it to make a call
+# 2. Make a call with invalid key, expect it to fail
+# 3. Make a call to a key with invalid model - expect to fail
+# 4. Make a call to a key with valid model - expect to pass
+# 5. Make a call with expired key - expect to fail
+# 6. Make a call with unexpired key - expect to pass
+# 7. Make a call with key under budget, expect to pass
+# 8. Make a call with key over budget, expect to fail
