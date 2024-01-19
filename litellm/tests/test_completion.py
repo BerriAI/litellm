1ecda73c5536428739d8322340db70e5112d31f1
Krrish Dholakia <krrishdholakia@gmail.com>
2024-01-18 17:54:16 -0800

    test(test_completion.py): handle together ai timeout

--- litellm/tests/test_completion.py
+++ litellm/tests/test_completion.py
@@ -1269,6 +1269,8 @@ def test_completion_together_ai():
             "Cost for completion call together-computer/llama-2-70b: ",
             f"${float(cost):.10f}",
         )
+    except litellm.Timeout as e:
+        pass
     except Exception as e:
         pytest.fail(f"Error occurred: {e}")
 
