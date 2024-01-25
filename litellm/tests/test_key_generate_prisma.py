62fdfc627c35c5e21dafe2fe0bc0ab68b8f3767f
ishaan-jaff <ishaanjaffer0324@gmail.com>
2024-01-24 18:54:23 -0800

    (test) logging cache_key in spendLogs

--- litellm/tests/test_key_generate_prisma.py
+++ litellm/tests/test_key_generate_prisma.py
@@ -716,6 +716,9 @@ def test_call_with_key_over_budget(prisma_client):
             # update spend using track_cost callback, make 2nd request, it should fail
             from litellm.proxy.proxy_server import track_cost_callback
             from litellm import ModelResponse, Choices, Message, Usage
+            from litellm.caching import Cache
+
+            litellm.cache = Cache()
             import time
 
             request_id = f"chatcmpl-e41836bb-bb8b-4df2-8e70-8f3e160155ac{time.time()}"
