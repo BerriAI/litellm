2f3765a03f5ff659c8292edf3e0491297e477fe7
ishaan-jaff <ishaanjaffer0324@gmail.com>
2024-01-24 18:51:39 -0800

    (fix) log cache hits on SpendLogs table

--- litellm/proxy/proxy_server.py
+++ litellm/proxy/proxy_server.py
@@ -593,6 +593,12 @@ async def track_cost_callback(
                 "user_api_key_user_id", None
             )
 
+            if kwargs.get("cache_hit", False) == True:
+                response_cost = 0.0
+                verbose_proxy_logger.info(
+                    f"Cache Hit: response_cost {response_cost}, for user_id {user_id}"
+                )
+
             verbose_proxy_logger.info(
                 f"response_cost {response_cost}, for user_id {user_id}"
             )
