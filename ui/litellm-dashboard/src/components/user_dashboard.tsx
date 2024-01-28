5ac449a5181745e0fd17f97deeec85ea69fc7a21
ishaan-jaff <ishaanjaffer0324@gmail.com>
2024-01-27 17:31:59 -0800

    (feat) add enter proxy url screen

--- ui/litellm-dashboard/src/components/user_dashboard.tsx
+++ ui/litellm-dashboard/src/components/user_dashboard.tsx
@@ -3,7 +3,10 @@ import React from "react";
 import { Grid, Col, Card, Text } from "@tremor/react";
 import CreateKey from "./create_key_button";
 import ViewKeyTable from "./view_key_table";
+import EnterProxyUrl from "./enter_proxy_url";
 import { useSearchParams } from "next/navigation";
+
+
 
 export default function UserDashboard() {
   const searchParams = useSearchParams();
@@ -11,6 +14,18 @@ export default function UserDashboard() {
   const accessToken = searchParams.get("accessToken");
   const proxyBaseUrl = searchParams.get("proxyBaseUrl");
 
+  const handleProxyUrlChange = (url: string) => {
+    // Do something with the entered proxy URL, e.g., save it in the state
+    console.log('Entered Proxy URL:', url);
+  };
+
+  if (proxyBaseUrl == null) {
+    return (
+      <div>
+        <EnterProxyUrl onUrlChange={handleProxyUrlChange} />
+      </div>
+    );
+  }
   if (userID == null || accessToken == null || proxyBaseUrl == null) {
     return (
       <Card
