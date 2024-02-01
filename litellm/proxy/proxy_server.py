fc60b752a4c9a549ab599f7413814d9189652b87
ishaan-jaff <ishaanjaffer0324@gmail.com>
2024-02-01 13:37:14 -0800

    (fix) user_custom_auth

--- litellm/proxy/proxy_server.py
+++ litellm/proxy/proxy_server.py
@@ -252,12 +252,13 @@ async def user_api_key_auth(
 ) -> UserAPIKeyAuth:
     global master_key, prisma_client, llm_model_list, user_custom_auth, custom_db_client
     try:
-        if isinstance(api_key, str):
-            api_key = _get_bearer_token(api_key=api_key)
-        ### USER-DEFINED AUTH FUNCTION ###
+        ### USER-DEFINED AUTH FUNCTION -> This should always be run first if a user has defined it ###
         if user_custom_auth is not None:
             response = await user_custom_auth(request=request, api_key=api_key)
             return UserAPIKeyAuth.model_validate(response)
+
+        if isinstance(api_key, str):
+            api_key = _get_bearer_token(api_key=api_key)
         ### LITELLM-DEFINED AUTH FUNCTION ###
         if master_key is None:
             if isinstance(api_key, str):
