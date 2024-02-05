f0dc0876f8322a45263105de6140c52098ebbc1d
ishaan-jaff <ishaanjaffer0324@gmail.com>
2024-02-05 15:00:13 -0800

    (feat) add pre-commit hook to check  model_prices_and_context_window.json litellm/model_prices_and_context_window_backup.json

--- ci_cd/check_files_match.py
+++ ci_cd/check_files_match.py
@@ -1,23 +1,37 @@
 import sys
 import filecmp
+import difflib
+
+
+def show_diff(file1, file2):
+    with open(file1, "r") as f1, open(file2, "r") as f2:
+        lines1 = f1.readlines()
+        lines2 = f2.readlines()
+
+    diff = difflib.unified_diff(lines1, lines2, lineterm="")
+
+    for line in diff:
+        print(line)
 
 
 def main(argv=None):
-    if argv is None:
-        argv = sys.argv[1:]
+    print(
+        "comparing model_prices_and_context_window, and litellm/model_prices_and_context_window_backup.json files.......... checking they match",
+        argv,
+    )
 
-    if len(argv) != 2:
-        print("Usage: python check_files_match.py <file1> <file2>")
-        return 1
-
-    file1 = argv[0]
-    file2 = argv[1]
-
-    if filecmp.cmp(file1, file2, shallow=False):
-        print(f"Files {file1} and {file2} match.")
+    file1 = "model_prices_and_context_window.json"
+    file2 = "litellm/model_prices_and_context_window_backup.json"
+    cmp_result = filecmp.cmp(file1, file2, shallow=False)
+    if cmp_result:
+        print(f"Passed ! Files {file1} and {file2} match.")
         return 0
     else:
-        print(f"Files {file1} and {file2} do not match.")
+        # show the diff
+        print(f"Failed ! Files {file1} and {file2} do not match.")
+        print("\nDiff")
+        show_diff(file1, file2)
+
         return 1
 
 
