52b864976bf41870f38ae33ac5714de03a6606a7
ishaan-jaff <ishaanjaffer0324@gmail.com>
2024-02-06 15:19:36 -0800

    (feat) support max_user_budget

--- litellm/proxy/utils.py
+++ litellm/proxy/utils.py
@@ -1213,6 +1213,28 @@ async def reset_budget(prisma_client: PrismaClient):
             )
 
 
+async def _read_request_body(request):
+    """
+    Asynchronous function to read the request body and parse it as JSON or literal data.
+
+    Parameters:
+    - request: The request object to read the body from
+
+    Returns:
+    - dict: Parsed request data as a dictionary
+    """
+    import ast, json
+
+    request_data = {}
+    body = await request.body()
+    body_str = body.decode()
+    try:
+        request_data = ast.literal_eval(body_str)
+    except:
+        request_data = json.loads(body_str)
+    return request_data
+
+
 # LiteLLM Admin UI - Non SSO Login
 html_form = """
 <!DOCTYPE html>
