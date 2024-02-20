3a67e2ca47770b07a50439a9d818d3bbfebf3269
Krrish Dholakia <krrishdholakia@gmail.com>
2024-02-19 22:41:36 -0800

    fix(gemini.py): fix async streaming + add native async completions

--- litellm/main.py
+++ litellm/main.py
@@ -263,6 +263,7 @@ async def acompletion(
             or custom_llm_provider == "ollama"
             or custom_llm_provider == "ollama_chat"
             or custom_llm_provider == "vertex_ai"
+            or custom_llm_provider == "gemini"
             or custom_llm_provider == "sagemaker"
             or custom_llm_provider in litellm.openai_compatible_providers
         ):  # currently implemented aiohttp calls for just azure, openai, hf, ollama, vertex ai soon all.
