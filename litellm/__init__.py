b4d7151e94fb513452438ac1812512cc32ec1122
Krrish Dholakia <krrishdholakia@gmail.com>
2024-02-20 15:19:31 -0800

    fix(presidio_pii_masking.py): enable user to pass their own ad hoc recognizers to presidio

--- litellm/__init__.py
+++ litellm/__init__.py
@@ -55,9 +55,12 @@ baseten_key: Optional[str] = None
 aleph_alpha_key: Optional[str] = None
 nlp_cloud_key: Optional[str] = None
 use_client: bool = False
+### GUARDRAILS ###
 llamaguard_model_name: Optional[str] = None
+presidio_ad_hoc_recognizers: Optional[str] = None
 google_moderation_confidence_threshold: Optional[float] = None
 llamaguard_unsafe_content_categories: Optional[str] = None
+##################
 logging: bool = True
 caching: bool = (
     False  # Not used anymore, will be removed in next MAJOR release - https://github.com/BerriAI/litellm/discussions/648
