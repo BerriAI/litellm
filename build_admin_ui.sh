cb3c7c44505edbf18065f0c57aebe94f1dbcaee8
ishaan-jaff <ishaanjaffer0324@gmail.com>
2024-02-21 21:09:15 -0800

    (fix) build command

--- build_admin_ui.sh
+++ build_admin_ui.sh
@@ -9,7 +9,7 @@ pwd
 
 
 # only run this step for litellm enterprise, we run this if enterprise/enterprise_ui/_enterprise.json exists
-if [ ! -f "enterprise/enterprise_ui/_enterprise_colors.json" ]; then
+if [ ! -f "enterprise/enterprise_ui/enterprise_colors.json" ]; then
     echo "Admin UI - using default LiteLLM UI"
     exit 0
 fi
@@ -46,8 +46,8 @@ nvm install v18.17.0
 nvm use v18.17.0
 npm install -g npm
 
-# copy ui_colors.json from this directory to /ui/litellm-dashboard, replace any existing ui_colors.json
-cp enterprise/enterprise_ui/_enterprise.json ui/litellm-dashboard/ui_colors.json
+# copy _enterprise.json from this directory to /ui/litellm-dashboard, and rename it to ui_colors.json
+cp enterprise/enterprise_ui/enterprise_colors.json ui/litellm-dashboard/ui_colors.json
 
 # cd in to /ui/litellm-dashboard
 cd ui/litellm-dashboard
