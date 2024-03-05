02bd749aa042f0f46f39cff0b169af111d00f3cf
Krrish Dholakia <krrishdholakia@gmail.com>
2024-03-04 17:15:35 -0800

    fix(bedrock.py): support anthropic messages api on bedrock (claude-3)

--- litellm/tests/test_router_timeout.py
+++ litellm/tests/test_router_timeout.py
@@ -87,7 +87,6 @@ def test_router_timeouts():
         print("********** TOKENS USED SO FAR = ", total_tokens_used)
 
 
-@pytest.mark.skip(reason="AWS Suspended Account")
 @pytest.mark.asyncio
 async def test_router_timeouts_bedrock():
     import openai
