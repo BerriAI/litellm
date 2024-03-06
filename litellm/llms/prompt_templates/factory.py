45957bdba4430f05f2710881d8179310c17c7739
J <cyberpenguin1@protonmail.com>
2024-03-06 12:44:48 -0800

    minor fix: prompt_templates/factory.py

--- litellm/llms/prompt_templates/factory.py
+++ litellm/llms/prompt_templates/factory.py
@@ -637,9 +637,9 @@ def prompt_factory(
         else:
             return gemini_text_image_pt(messages=messages)
     try:
-        if "mistral" in model and "instruct" in model:
+        if ("mistral" in model or "mixtral" in model) and "instruct" in model:
             return mistral_instruct_pt(messages=messages)
-        elif "meta-llama/llama-2" in model and "chat" in model:
+        elif "llama-2" in model and "chat" in model:
             return llama_2_chat_pt(messages=messages)
         elif (
             "tiiuae/falcon" in model
