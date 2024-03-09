32f6bcd953fa97321bbf302b8480325c2d98e624
Krrish Dholakia <krrishdholakia@gmail.com>
2024-03-08 23:33:54 -0800

    fix(openai.py): fix async audio transcription

--- litellm/llms/openai.py
+++ litellm/llms/openai.py
@@ -862,7 +862,7 @@ class OpenAIChatCompletion(BaseLLM):
                 additional_args={"complete_input_dict": data},
                 original_response=stringified_response,
             )
-            return convert_to_model_response_object(response_object=stringified_response, model_response_object=model_response, response_type="image_generation")  # type: ignore
+            return convert_to_model_response_object(response_object=stringified_response, model_response_object=model_response, response_type="audio_transcription")  # type: ignore
         except Exception as e:
             ## LOGGING
             logging_obj.post_call(
