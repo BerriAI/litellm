445564a99bdfdd6791e0799e5f4abbf364d93cbb
Jakob <haruhunab1320@gmail.com>
2024-03-13 13:41:11 -0700

    commit

--- litellm/utils.py
+++ litellm/utils.py
@@ -3583,7 +3583,7 @@ def cost_per_token(
     model_cost_ref = litellm.model_cost
     model_with_provider = model
     if custom_llm_provider is not None:
-        model_with_provider = model
+        model_with_provider = custom_llm_provider + "/" + model
         if region_name is not None:
             model_with_provider_and_region = (
                 f"{custom_llm_provider}/{region_name}/{model}"
@@ -3852,6 +3852,7 @@ def completion_cost(
         if (
             "togethercomputer" in model
             or "together_ai" in model
+            or "together" in model
             or custom_llm_provider == "together_ai"
         ):
             # together ai prices based on size of llm
