c0af9dc4f68d60f59818ec4f7b0950045da26004
ishaan-jaff <ishaanjaffer0324@gmail.com>
2024-03-12 18:52:49 -0700

    (fix) locust load test

--- litellm/proxy/proxy_load_test/openai_endpoint.py
+++ litellm/proxy/proxy_load_test/openai_endpoint.py
@@ -6,6 +6,7 @@ from fastapi import FastAPI, Request, status, HTTPException, Depends
 from fastapi.responses import StreamingResponse
 from fastapi.security import OAuth2PasswordBearer
 from fastapi.middleware.cors import CORSMiddleware
+import uuid
 
 app = FastAPI()
 
@@ -23,7 +24,7 @@ app.add_middleware(
 @app.post("/v1/chat/completions")
 async def completion(request: Request):
     return {
-        "id": "chatcmpl-123",
+        "id": f"chatcmpl-{uuid.uuid4().hex}",
         "object": "chat.completion",
         "created": 1677652288,
         "model": "gpt-3.5-turbo-0125",
