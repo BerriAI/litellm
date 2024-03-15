0b1dbc30a07ecffaeb5d7eb5a1b65fad1f292f2a
ishaan-jaff <ishaanjaffer0324@gmail.com>
2024-03-15 11:00:46 -0700

    (ci/cd) interpret load test results

--- .github/workflows/interpret_load_test.py
+++ .github/workflows/interpret_load_test.py
@@ -42,6 +42,7 @@ def interpret_results(csv_file):
             print(result_str)
         interpreted_results_str += result_str
     print(interpreted_results_str)
+    return interpreted_results_str
 
 
 if __name__ == "__main__":
