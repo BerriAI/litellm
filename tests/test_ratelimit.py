75d029a91d575a3d28ac16c51f69ac791ca9cb25
CLARKBENHAM <clark.benham@gmail.com>
2024-04-02 17:18:03 -0700

    comments

--- tests/test_ratelimit.py
+++ tests/test_ratelimit.py
@@ -42,6 +42,9 @@ def router_factory():
 
 
 def generate_list_of_messages(num_messages):
+    """
+    create num_messages new chat conversations
+    """
     return [
         [{"role": "user", "content": f"{i}. Hey, how's it going? {random.random()}"}]
         for i in range(num_messages)
@@ -49,6 +52,9 @@ def generate_list_of_messages(num_messages):
 
 
 def calculate_limits(list_of_messages):
+    """
+    Return the min rpm and tpm level that would let all messages in list_of_messages be sent this minute
+    """
     rpm = len(list_of_messages)
     tpm = sum((utils.token_counter(messages=m) + COMPLETION_TOKENS for m in list_of_messages))
     return rpm, tpm
@@ -68,36 +74,48 @@ class ExpectNoException(Exception):
 
 
 @pytest.mark.parametrize(
-    "num_messages, num_rate_limits",
+    "num_try_send, num_allowed_send",
     [
-        # (2, 30),  # No exception expected
-        # (2, 3),  # No exception expected
         (2, 2),  # No exception expected
+        (10, 10),  # No exception expected
         (3, 2),  # Expect ValueError
-        # (6, 5),  # Expect ValueError
+        (10, 9),  # Expect ValueError
     ],
 )
 @pytest.mark.parametrize("sync_mode", [True, False])  # Use parametrization for sync/async
-def test_rate_limit(router_factory, num_messages, num_rate_limits, sync_mode):
-    expected_exception = ExpectNoException if num_messages <= num_rate_limits else ValueError
+def test_rate_limit(router_factory, num_try_send, num_allowed_send, sync_mode):
+    """
+    Check if router.completion and router.acompletion can send more messages than they've been limited to.
+    Args:
+        router_factory: makes new router object, without any shared Global state
+        num_try_send (int): number of messages to try to send
+        num_allowed_send (int): max number of messages allowed to be sent in 1 minute
+        sync_mode (bool): if making sync (router.completion) or async (router.acompletion)
+    Raises:
+        ValueError: Error router throws when it hits rate limits
+        ExpectNoException: Signfies that no other error has happened. A NOP
+    """
+    # Can send more messages then we're going to; so don't expect a rate limit error
+    expected_exception = ExpectNoException if num_try_send <= num_allowed_send else ValueError
 
-    list_of_messages = generate_list_of_messages(max(num_messages, num_rate_limits))
-    rpm, tpm = calculate_limits(list_of_messages[:num_rate_limits])
-    list_of_messages = list_of_messages[:num_messages]
+    list_of_messages = generate_list_of_messages(max(num_try_send, num_allowed_send))
+    rpm, tpm = calculate_limits(list_of_messages[:num_allowed_send])
+    list_of_messages = list_of_messages[:num_try_send]
     router = router_factory(rpm, tpm)
 
-    with pytest.raises(expected_exception) as excinfo:
+    with pytest.raises(expected_exception) as excinfo:  # asserts correct type raised
         if sync_mode:
             results = sync_call(router, list_of_messages)
         else:
             results = asyncio.run(async_call(router, list_of_messages))
-            if len([i for i in results if i is not None]) != num_messages:
-                raise ValueError(
-                    "No deployments available for selected model"
-                )  # not all results got returned
+        print(results)
+        if len([i for i in results if i is not None]) != num_try_send:
+            # since not all results got returned, raise rate limit error
+            raise ValueError("No deployments available for selected model")
         raise ExpectNoException
 
-    if expected_exception is not ExpectNoException:
+    print(expected_exception, excinfo)
+    if expected_exception is ValueError:
         assert "No deployments available for selected model" in str(excinfo.value)
     else:
-        assert len([i for i in results if i is not None]) == num_messages
+        assert len([i for i in results if i is not None]) == num_try_send
