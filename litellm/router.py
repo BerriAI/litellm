0d7e2aaab7478668ef4eff3e63ed26e57c475ff0
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-04-06 11:28:25 -0700

    feat - re-use openai client for text completion

--- litellm/router.py
+++ litellm/router.py
@@ -1769,6 +1769,7 @@ class Router:
             or custom_llm_provider == "azure"
             or custom_llm_provider == "custom_openai"
             or custom_llm_provider == "openai"
+            or custom_llm_provider == "text-completion-openai"
             or "ft:gpt-3.5-turbo" in model_name
             or model_name in litellm.open_ai_embedding_models
         ):
