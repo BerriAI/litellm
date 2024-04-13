60d502436f9235d9aab8e7e0b6a6c382e99d2056
zJ <19760191+zJuuu@users.noreply.github.com>
2024-04-14 01:44:14 +0200

    additional type check

--- ui/litellm-dashboard/src/components/model_dashboard.tsx
+++ ui/litellm-dashboard/src/components/model_dashboard.tsx
@@ -258,7 +258,7 @@ const ModelDashboard: React.FC<ModelDashboardProps> = ({
       // get the list of deployments
       let deployments: Array<string> = [];
       // This is required because the formValues["model"] can be of type string or array depending on the input type (input field or dropdown selection)
-      if (formValues["model"] instanceof String) {
+      if (formValues["model"] instanceof String || typeof formValues["model"] === "string") {
         // user has entered one or multiple model names separated by comma in to the input field
         deployments = formValues["model"].split(",");
       } else if (formValues["model"] instanceof Array) {
