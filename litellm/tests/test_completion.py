26286a54b8438f5f4b4645e5465efe3884d00789
Krrish Dholakia <krrishdholakia@gmail.com>
2024-04-15 08:15:00 -0700

    fix(anthropic_text.py): add support for async text completion calls

--- litellm/tests/test_completion.py
+++ litellm/tests/test_completion.py
@@ -492,6 +492,31 @@ def test_completion_claude2_1():
 
 # test_completion_claude2_1()
 
+
+@pytest.mark.asyncio
+async def test_acompletion_claude2_1():
+    try:
+        litellm.set_verbose = True
+        print("claude2.1 test request")
+        messages = [
+            {
+                "role": "system",
+                "content": "Your goal is generate a joke on the topic user gives.",
+            },
+            {"role": "user", "content": "Generate a 3 liner joke for me"},
+        ]
+        # test without max tokens
+        response = await litellm.acompletion(model="claude-2.1", messages=messages)
+        # Add any assertions here to check the response
+        print(response)
+        print(response.usage)
+        print(response.usage.completion_tokens)
+        print(response["usage"]["completion_tokens"])
+        # print("new cost tracking")
+    except Exception as e:
+        pytest.fail(f"Error occurred: {e}")
+
+
 # def test_completion_oobabooga():
 #     try:
 #         response = completion(
