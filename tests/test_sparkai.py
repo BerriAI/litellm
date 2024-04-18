9d2245bb49507e4d06e1535f8fe6f05557f1e666
bwl <bwl15698296860@126.com>
2024-04-18 17:34:18 +0800

    The Spark API supports the completion method from the Litellm
    Signed-off-by: bwl <bwl15698296860@126.com>

--- tests/test_sparkai.py
+++ tests/test_sparkai.py
@@ -1,17 +1,11 @@
 import os
 import litellm
-'''
 os.environ['SPARKAI_APP_ID']='your_id'
 os.environ['SPARKAI_API_KEY']='your_key'
 os.environ['SPARKAI_API_SECRET']='your_secret_key'
 os.environ['SPARKAI_DOMAIN']='your_domain_name'
 os.environ['SPARKAI_URL']="your_url"
-'''
-os.environ['SPARKAI_APP_ID']='21628c31'
-os.environ['SPARKAI_API_KEY']='b54eb223efc235aef46f061e5fd752ea'
-os.environ['SPARKAI_API_SECRET']='MDIwNzM5MzVjOTcwMDI3MTdjYWNlZjE3'
-os.environ['SPARKAI_DOMAIN']='generalv3.5'
-os.environ['SPARKAI_URL']="wss://spark-openapi.cn-huabei-1.xf-yun.com/v3.2/chat"
+
 def test_sparkai():
     response_1 = litellm.completion(
         model="spark_ai",
