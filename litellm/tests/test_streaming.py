de120eb6d91dc353c8cc7e68972eaaeb44a2c5dc
Krrish Dholakia <krrishdholakia@gmail.com>
2024-04-18 07:08:36 -0700

    fix(utils.py): fix azure streaming logic

--- litellm/tests/test_streaming.py
+++ litellm/tests/test_streaming.py
@@ -222,14 +222,12 @@ tools_schema = [
 
 def test_completion_azure_stream_special_char():
     litellm.set_verbose = True
-    messages = [
-        {"role": "user", "content": "Respond with the '<' sign and nothing else."}
-    ]
+    messages = [{"role": "user", "content": "hi. respond with the <xml> tag only"}]
     response = completion(model="azure/chatgpt-v-2", messages=messages, stream=True)
     response_str = ""
     for part in response:
         response_str += part.choices[0].delta.content or ""
-
+    print(f"response_str: {response_str}")
     assert len(response_str) > 0
 
 
