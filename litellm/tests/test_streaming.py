7d0086d742ed6666ea7b8251a3dcef49dbafe069
Krrish Dholakia <krrishdholakia@gmail.com>
2024-04-17 17:43:41 -0700

    fix(utils.py): ensure streaming output parsing only applied for hf / sagemaker models
    selectively applies the <s>
    </s> checking

--- litellm/tests/test_streaming.py
+++ litellm/tests/test_streaming.py
@@ -220,6 +220,20 @@ tools_schema = [
 # test_completion_cohere_stream()
 
 
+def test_completion_azure_stream_special_char():
+    messages = [
+        {"role": "user", "content": "Respond with the '<' sign and nothing else."}
+    ]
+    response = completion(model="azure/chatgpt-v-2", messages=messages, stream=True)
+    response_str = ""
+    for part in response:
+        response_str += part.choices[0].delta.content or ""
+
+    print(f"response_str: {response_str}")
+    assert len(response_str) > 0
+    raise Exception("it worked")
+
+
 def test_completion_cohere_stream_bad_key():
     try:
         litellm.cache = None
