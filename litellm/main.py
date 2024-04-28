eec4f3bda7c78e308e36dd15b85e2ea6e3c9a75a
jinno <klvx01@gmail.com>
2024-04-28 21:06:37 +0900

    fix(main.py): use model_api_key determined from get_api_key

--- litellm/main.py
+++ litellm/main.py
@@ -775,6 +775,12 @@ def completion(
 
         if dynamic_api_key is not None:
             api_key = dynamic_api_key
+        if api_key is None:
+            # Priority order
+            #   1. dynamic_api_key of get_llm_provider()
+            #   2. api_key of Parameters
+            #   3. model_api_key of get_api_key()
+            api_key = model_api_key
         # check if user passed in any of the OpenAI optional params
         optional_params = get_optional_params(
             functions=functions,
