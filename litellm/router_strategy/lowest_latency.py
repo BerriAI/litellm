3b0aa0537854dbb53dffe402569b4fe3722ce42c
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-04-29 15:51:52 -0700

    fix lowest latency - routing

--- litellm/router_strategy/lowest_latency.py
+++ litellm/router_strategy/lowest_latency.py
@@ -312,6 +312,8 @@ class LowestLatencyLoggingHandler(CustomLogger):
         except:
             input_tokens = 0
 
+        all_deployments = random.sample(all_deployments.items(), len(all_deployments))
+        all_deployments = dict(all_deployments)
         for item, item_map in all_deployments.items():
             ## get the item from model list
             _deployment = None
