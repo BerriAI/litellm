b46db8b89135a6b17f5b0797fdf20ec34735f8b0
Krrish Dholakia <krrishdholakia@gmail.com>
2024-04-29 19:21:19 -0700

    feat(utils.py): json logs for raw request sent by litellm
    make it easier to view verbose logs in datadog

--- litellm/__init__.py
+++ litellm/__init__.py
@@ -2,7 +2,7 @@
 import threading, requests, os
 from typing import Callable, List, Optional, Dict, Union, Any, Literal
 from litellm.caching import Cache
-from litellm._logging import set_verbose, _turn_on_debug, verbose_logger
+from litellm._logging import set_verbose, _turn_on_debug, verbose_logger, json_logs
 from litellm.proxy._types import (
     KeyManagementSystem,
     KeyManagementSettings,
