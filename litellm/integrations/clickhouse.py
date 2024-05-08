6575143460e3dcebc40ff1d5e4006ef0780841b5
Krrish Dholakia <krrishdholakia@gmail.com>
2024-05-08 16:00:08 -0700

    feat(proxy_server.py): return litellm version in response headers

--- litellm/integrations/clickhouse.py
+++ litellm/integrations/clickhouse.py
@@ -3,7 +3,6 @@
 #### What this does ####
 #    On success, logs events to Promptlayer
 import dotenv, os
-import requests
 
 from litellm.proxy._types import UserAPIKeyAuth
 from litellm.caching import DualCache
