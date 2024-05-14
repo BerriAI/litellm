8033bf343f2bc597e4a8d93924961fe9f65b1a4d
Krrish Dholakia <krrishdholakia@gmail.com>
2024-05-13 18:40:51 -0700

    test(test_completion.py): handle async watsonx call fail

--- litellm/main.py
+++ litellm/main.py
@@ -15,7 +15,6 @@ import dotenv, traceback, random, asyncio, time, contextvars
 from copy import deepcopy
 import httpx
 import litellm
-
 from ._logging import verbose_logger
 from litellm import (  # type: ignore
     client,
