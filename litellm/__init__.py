d2229dcd21deb4ac4c5f79fce59abc1936b8ce4a
Krrish Dholakia <krrishdholakia@gmail.com>
2024-05-23 16:29:46 -0700

    feat(databricks.py): adds databricks support - completion, async, streaming
    Closes https://github.com/BerriAI/litellm/issues/2160

--- litellm/__init__.py
+++ litellm/__init__.py
@@ -69,6 +69,7 @@ retry = True
 ### AUTH ###
 api_key: Optional[str] = None
 openai_key: Optional[str] = None
+databricks_key: Optional[str] = None
 azure_key: Optional[str] = None
 anthropic_key: Optional[str] = None
 replicate_key: Optional[str] = None
@@ -615,6 +616,7 @@ provider_list: List = [
     "watsonx",
     "triton",
     "predibase",
+    "databricks",
     "custom",  # custom apis
 ]
 
@@ -730,6 +732,7 @@ from .utils import (
 )
 from .llms.huggingface_restapi import HuggingfaceConfig
 from .llms.anthropic import AnthropicConfig
+from .llms.databricks import DatabricksConfig
 from .llms.predibase import PredibaseConfig
 from .llms.anthropic_text import AnthropicTextConfig
 from .llms.replicate import ReplicateConfig
