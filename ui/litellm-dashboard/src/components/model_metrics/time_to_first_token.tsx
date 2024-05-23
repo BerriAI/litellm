06606dc01bf38708784a6a821c1a9dd8aa68d5de
Krrish Dholakia <krrishdholakia@gmail.com>
2024-05-22 18:09:53 -0700

    feat(ui/time_to_first_token.tsx): add time to first token card to model metrics

--- ui/litellm-dashboard/src/components/model_metrics/time_to_first_token.tsx
+++ ui/litellm-dashboard/src/components/model_metrics/time_to_first_token.tsx
@@ -1,82 +1,27 @@
+import React from "react";
 import { LineChart } from "@tremor/react";
+interface TimeToFirstTokenProps {
+  modelMetrics: any[];
+  modelMetricsCategories: string[];
+  customTooltip: any;
+}
 
-const chartdata = [
-  {
-    date: "Jan 22",
-    SolarPanels: 2890,
-    Inverters: 2338,
-  },
-  {
-    date: "Feb 22",
-    SolarPanels: 2756,
-    Inverters: 2103,
-  },
-  {
-    date: "Mar 22",
-    SolarPanels: 3322,
-    Inverters: 2194,
-  },
-  {
-    date: "Apr 22",
-    SolarPanels: 3470,
-    Inverters: 2108,
-  },
-  {
-    date: "May 22",
-    SolarPanels: 3475,
-    Inverters: 1812,
-  },
-  {
-    date: "Jun 22",
-    SolarPanels: 3129,
-    Inverters: 1726,
-  },
-  {
-    date: "Jul 22",
-    SolarPanels: 3490,
-    Inverters: 1982,
-  },
-  {
-    date: "Aug 22",
-    SolarPanels: 2903,
-    Inverters: 2012,
-  },
-  {
-    date: "Sep 22",
-    SolarPanels: 2643,
-    Inverters: 2342,
-  },
-  {
-    date: "Oct 22",
-    SolarPanels: 2837,
-    Inverters: 2473,
-  },
-  {
-    date: "Nov 22",
-    SolarPanels: 2954,
-    Inverters: 3848,
-  },
-  {
-    date: "Dec 22",
-    SolarPanels: 3239,
-    Inverters: 3736,
-  },
-];
-
-const dataFormatter = (number: number) =>
-  `$${Intl.NumberFormat("us").format(number).toString()}`;
-
-const TimeToFirstToken = () => {
+const TimeToFirstToken: React.FC<TimeToFirstTokenProps> = ({
+  modelMetrics,
+  modelMetricsCategories,
+  customTooltip,
+}) => {
   return (
     <LineChart
-      className="h-80"
-      data={chartdata}
+      title="Time to First token (s)"
+      className="h-72"
+      data={modelMetrics}
       index="date"
-      categories={["SolarPanels", "Inverters"]}
+      showLegend={false}
+      categories={modelMetricsCategories}
       colors={["indigo", "rose"]}
-      valueFormatter={dataFormatter}
-      yAxisWidth={60}
-      onValueChange={(v) => console.log(v)}
+      connectNulls={true}
+      customTooltip={customTooltip}
     />
   );
 };
