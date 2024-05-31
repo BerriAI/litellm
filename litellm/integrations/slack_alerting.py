0d44b1581a5a2b9c1f85424724c948029ef29322
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-05-31 10:37:56 -0700

    fix - move email templates

--- litellm/integrations/slack_alerting.py
+++ litellm/integrations/slack_alerting.py
@@ -18,6 +18,7 @@ from litellm.proxy._types import WebhookEvent
 import random
 from typing import TypedDict
 from openai import APIError
+from .email_templates.templates import *
 
 import litellm.types
 from litellm.types.router import LiteLLM_Params
@@ -66,67 +67,6 @@ class SlackAlertingArgsEnum(Enum):
     minor_outage_alert_threshold: int = 1 * 5
     major_outage_alert_threshold: int = 1 * 10
     max_outage_alert_list_size: int = 1 * 10
-
-
-KEY_CREATED_EMAIL_TEMPLATE = """
-                    <img src="{email_logo_url}" alt="LiteLLM Logo" width="150" height="50" />
-
-                    <p> Hi {recipient_email}, <br/>
-        
-                    I'm happy to provide you with an OpenAI Proxy API Key, loaded with ${key_budget} per month. <br /> <br />
-
-                    <b>
-                    Key: <pre>{key_token}</pre> <br>
-                    </b>
-
-                    <h2>Usage Example</h2>
-
-                    Detailed Documentation on <a href="https://docs.litellm.ai/docs/proxy/user_keys">Usage with OpenAI Python SDK, Langchain, LlamaIndex, Curl</a>
-
-                    <pre>
-
-                    import openai
-                    client = openai.OpenAI(
-                        api_key="{key_token}",
-                        base_url={{base_url}}
-                    )
-
-                    response = client.chat.completions.create(
-                        model="gpt-3.5-turbo", # model to send to the proxy
-                        messages = [
-                            {{
-                                "role": "user",
-                                "content": "this is a test request, write a short poem"
-                            }}
-                        ]
-                    )
-
-                    </pre>
-
-
-                    If you have any questions, please send an email to {email_support_contact} <br /> <br />
-
-                    Best, <br />
-                    The LiteLLM team <br />
-"""
-
-
-USER_INVITED_EMAIL_TEMPLATE = """
-                    <img src="{email_logo_url}" alt="LiteLLM Logo" width="150" height="50" />
-
-                    <p> Hi {recipient_email}, <br/>
-
-                    You were invited to use OpenAI Proxy API for team {team_name}  <br /> <br />
-
-                    <button><a href="{base_url}">Get Started here</a></button> <br /> <br />
-
-
-                    
-                    If you have any questions, please send an email to {email_support_contact} <br /> <br />
-
-                    Best, <br />
-                    The LiteLLM team <br />
-"""
 
 
 class SlackAlertingArgs(LiteLLMBase):
