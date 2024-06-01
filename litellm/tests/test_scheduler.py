4ffbd80584f7cdcd577364fc4e88cc8ebe625fcd
Krrish Dholakia <krrishdholakia@gmail.com>
2024-06-01 16:09:41 -0700

    fix(router.py): simplify scheduler
    move the scheduler poll queuing logic into the router class, making it easier to use

--- litellm/tests/test_scheduler.py
+++ litellm/tests/test_scheduler.py
@@ -4,12 +4,14 @@
 import sys, os, time, openai, uuid
 import traceback, asyncio
 import pytest
+from typing import List
 
 sys.path.insert(
     0, os.path.abspath("../..")
 )  # Adds the parent directory to the system path
 from litellm import Router
 from litellm.scheduler import FlowItem, Scheduler
+from litellm import ModelResponse
 
 
 @pytest.mark.asyncio
@@ -171,4 +173,61 @@ async def test_aascheduler_prioritized_requests_mock_response(p0, p1):
     )  # assert higher priority request got done first
     assert (
         completed_responses[0][2] < completed_responses[1][2]
+    )  # higher priority request tried first
+
+
+@pytest.mark.parametrize("p0, p1", [(0, 1), (0, 0)])  #
+@pytest.mark.asyncio
+async def test_aascheduler_prioritized_requests_mock_response_simplified(p0, p1):
+    """
+    2 requests for same model group
+
+    if model is at rate limit, ensure the higher priority request gets done first
+    """
+
+    router = Router(
+        model_list=[
+            {
+                "model_name": "gpt-3.5-turbo",
+                "litellm_params": {
+                    "model": "gpt-3.5-turbo",
+                    "mock_response": "Hello world this is Macintosh!",
+                    "rpm": 0,
+                },
+            },
+        ],
+        timeout=10,
+        num_retries=3,
+        cooldown_time=5,
+        routing_strategy="usage-based-routing-v2",
+    )
+
+    tasks = []
+
+    data = {
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role": "user", "content": "Hey, how's it going?"}],
+    }
+
+    tasks.append(router.schedule_acompletion(**data, priority=p0))
+    tasks.append(router.schedule_acompletion(**data, priority=p1))
+
+    # Running the tasks and getting responses in order of completion
+    completed_responses: List[dict] = []
+    for task in asyncio.as_completed(tasks):
+        try:
+            result = await task
+        except Exception as e:
+            result = {"priority": e.priority, "response_completed_at": time.time()}
+            completed_responses.append(result)
+            print(f"Received response: {result}")
+
+    print(f"responses: {completed_responses}")
+
+    assert (
+        completed_responses[0]["priority"] == 0
+    )  # assert higher priority request got done first
+    assert (
+        completed_responses[0]["response_completed_at"]
+        < completed_responses[1]["response_completed_at"]
     )  # higher priority request tried first
