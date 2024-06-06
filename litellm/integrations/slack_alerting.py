99429457976c0852bf26997d60bfb16639dba2d9
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-06-06 12:30:56 -0700

    docs - redacting messages from slack alerting

--- litellm/integrations/slack_alerting.py
+++ litellm/integrations/slack_alerting.py
@@ -327,7 +327,7 @@ class SlackAlerting(CustomLogger):
             )
         )
         if litellm.turn_off_message_logging or litellm.redact_messages_in_exceptions:
-            messages = "Message not logged. `litellm.turn_off_message_logging=True`."
+            messages = "Message not logged. litellm.redact_messages_in_exceptions=True"
         request_info = f"\nRequest Model: `{model}`\nAPI Base: `{api_base}`\nMessages: `{messages}`"
         slow_message = f"`Responses are slow - {round(time_difference_float,2)}s response time > Alerting threshold: {self.alerting_threshold}s`"
         if time_difference_float > self.alerting_threshold:
@@ -572,7 +572,7 @@ class SlackAlerting(CustomLogger):
                 or litellm.redact_messages_in_exceptions
             ):
                 messages = (
-                    "Message not logged. `litellm.turn_off_message_logging=True`."
+                    "Message not logged. litellm.redact_messages_in_exceptions=True"
                 )
             request_info = f"\nRequest Model: `{model}`\nMessages: `{messages}`"
         else:
