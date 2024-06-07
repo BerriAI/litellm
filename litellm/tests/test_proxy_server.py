8106a6dc9b3ce0c5b16be6133041bb5f8e2f1c53
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-06-07 13:48:21 -0700

    fix simplify - pass litellm_parent_otel_span

--- litellm/tests/test_proxy_server.py
+++ litellm/tests/test_proxy_server.py
@@ -152,7 +152,6 @@ def test_chat_completion(mock_acompletion, client_no_auth):
             specific_deployment=True,
             metadata=mock.ANY,
             proxy_server_request=mock.ANY,
-            litellm_parent_otel_span=mock.ANY,
         )
         print(f"response - {response.text}")
         assert response.status_code == 200
