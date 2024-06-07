8c8413131c920fdd8078d2f1560c1077845252f5
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-06-07 14:23:06 -0700

    fix type error on python 3.8

--- litellm/types/files.py
+++ litellm/types/files.py
@@ -1,10 +1,11 @@
 from enum import Enum
-from types import MappingProxyType
-from typing import List, Set
+from typing import List, Set, Dict
 
 """
 Base Enums/Consts
 """
+
+
 class FileType(Enum):
     AAC = "AAC"
     CSV = "CSV"
@@ -49,99 +50,106 @@ class FileType(Enum):
     XLS = "XLS"
     XLSX = "XLSX"
 
-FILE_EXTENSIONS: MappingProxyType[FileType, List[str]] = MappingProxyType({
-    FileType.AAC: ["aac"],
-    FileType.CSV: ["csv"],
-    FileType.DOC: ["doc"],
-    FileType.DOCX: ["docx"],
-    FileType.FLAC: ["flac"],
-    FileType.FLV: ["flv"],
-    FileType.GIF: ["gif"],
-    FileType.GOOGLE_DOC: ["gdoc"],
-    FileType.GOOGLE_DRAWINGS: ["gdraw"],
-    FileType.GOOGLE_SHEETS: ["gsheet"],
-    FileType.GOOGLE_SLIDES: ["gslides"],
-    FileType.HEIC: ["heic"],
-    FileType.HEIF: ["heif"],
-    FileType.HTML: ["html", "htm"],
-    FileType.JPEG: ["jpeg", "jpg"],
-    FileType.JSON: ["json"],
-    FileType.M4A: ["m4a"],
-    FileType.M4V: ["m4v"],
-    FileType.MOV: ["mov"],
-    FileType.MP3: ["mp3"],
-    FileType.MP4: ["mp4"],
-    FileType.MPEG: ["mpeg"],
-    FileType.MPEGPS: ["mpegps"],
-    FileType.MPG: ["mpg"],
-    FileType.MPA: ["mpa"],
-    FileType.MPGA: ["mpga"],
-    FileType.OGG: ["ogg"],
-    FileType.OPUS: ["opus"],
-    FileType.PDF: ["pdf"],
-    FileType.PCM: ["pcm"],
-    FileType.PNG: ["png"],
-    FileType.PPT: ["ppt"],
-    FileType.PPTX: ["pptx"],
-    FileType.RTF: ["rtf"],
-    FileType.THREE_GPP: ["3gpp"],
-    FileType.TXT: ["txt"],
-    FileType.WAV: ["wav"],
-    FileType.WEBM: ["webm"],
-    FileType.WEBP: ["webp"],
-    FileType.WMV: ["wmv"],
-    FileType.XLS: ["xls"],
-    FileType.XLSX: ["xlsx"],
-})
 
-FILE_MIME_TYPES: MappingProxyType[FileType, str] = MappingProxyType({
-    FileType.AAC: "audio/aac",
-    FileType.CSV: "text/csv",
-    FileType.DOC: "application/msword",
-    FileType.DOCX: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-    FileType.FLAC: "audio/flac",
-    FileType.FLV: "video/x-flv",
-    FileType.GIF: "image/gif",
-    FileType.GOOGLE_DOC: "application/vnd.google-apps.document",
-    FileType.GOOGLE_DRAWINGS: "application/vnd.google-apps.drawing",
-    FileType.GOOGLE_SHEETS: "application/vnd.google-apps.spreadsheet",
-    FileType.GOOGLE_SLIDES: "application/vnd.google-apps.presentation",
-    FileType.HEIC: "image/heic",
-    FileType.HEIF: "image/heif",
-    FileType.HTML: "text/html",
-    FileType.JPEG: "image/jpeg",
-    FileType.JSON: "application/json",
-    FileType.M4A: "audio/x-m4a",
-    FileType.M4V: "video/x-m4v",
-    FileType.MOV: "video/quicktime",
-    FileType.MP3: "audio/mpeg",
-    FileType.MP4: "video/mp4",
-    FileType.MPEG: "video/mpeg",
-    FileType.MPEGPS: "video/mpegps",
-    FileType.MPG: "video/mpg",
-    FileType.MPA: "audio/m4a",
-    FileType.MPGA: "audio/mpga",
-    FileType.OGG: "audio/ogg",
-    FileType.OPUS: "audio/opus",
-    FileType.PDF: "application/pdf",
-    FileType.PCM: "audio/pcm",
-    FileType.PNG: "image/png",
-    FileType.PPT: "application/vnd.ms-powerpoint",
-    FileType.PPTX: "application/vnd.openxmlformats-officedocument.presentationml.presentation",
-    FileType.RTF: "application/rtf",
-    FileType.THREE_GPP: "video/3gpp",
-    FileType.TXT: "text/plain",
-    FileType.WAV: "audio/wav",
-    FileType.WEBM: "video/webm",
-    FileType.WEBP: "image/webp",
-    FileType.WMV: "video/wmv",
-    FileType.XLS: "application/vnd.ms-excel",
-    FileType.XLSX: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-})
+FILE_EXTENSIONS: Dict[FileType, List[str]] = Dict(
+    {
+        FileType.AAC: ["aac"],
+        FileType.CSV: ["csv"],
+        FileType.DOC: ["doc"],
+        FileType.DOCX: ["docx"],
+        FileType.FLAC: ["flac"],
+        FileType.FLV: ["flv"],
+        FileType.GIF: ["gif"],
+        FileType.GOOGLE_DOC: ["gdoc"],
+        FileType.GOOGLE_DRAWINGS: ["gdraw"],
+        FileType.GOOGLE_SHEETS: ["gsheet"],
+        FileType.GOOGLE_SLIDES: ["gslides"],
+        FileType.HEIC: ["heic"],
+        FileType.HEIF: ["heif"],
+        FileType.HTML: ["html", "htm"],
+        FileType.JPEG: ["jpeg", "jpg"],
+        FileType.JSON: ["json"],
+        FileType.M4A: ["m4a"],
+        FileType.M4V: ["m4v"],
+        FileType.MOV: ["mov"],
+        FileType.MP3: ["mp3"],
+        FileType.MP4: ["mp4"],
+        FileType.MPEG: ["mpeg"],
+        FileType.MPEGPS: ["mpegps"],
+        FileType.MPG: ["mpg"],
+        FileType.MPA: ["mpa"],
+        FileType.MPGA: ["mpga"],
+        FileType.OGG: ["ogg"],
+        FileType.OPUS: ["opus"],
+        FileType.PDF: ["pdf"],
+        FileType.PCM: ["pcm"],
+        FileType.PNG: ["png"],
+        FileType.PPT: ["ppt"],
+        FileType.PPTX: ["pptx"],
+        FileType.RTF: ["rtf"],
+        FileType.THREE_GPP: ["3gpp"],
+        FileType.TXT: ["txt"],
+        FileType.WAV: ["wav"],
+        FileType.WEBM: ["webm"],
+        FileType.WEBP: ["webp"],
+        FileType.WMV: ["wmv"],
+        FileType.XLS: ["xls"],
+        FileType.XLSX: ["xlsx"],
+    }
+)
+
+FILE_MIME_TYPES: Dict[FileType, str] = Dict(
+    {
+        FileType.AAC: "audio/aac",
+        FileType.CSV: "text/csv",
+        FileType.DOC: "application/msword",
+        FileType.DOCX: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        FileType.FLAC: "audio/flac",
+        FileType.FLV: "video/x-flv",
+        FileType.GIF: "image/gif",
+        FileType.GOOGLE_DOC: "application/vnd.google-apps.document",
+        FileType.GOOGLE_DRAWINGS: "application/vnd.google-apps.drawing",
+        FileType.GOOGLE_SHEETS: "application/vnd.google-apps.spreadsheet",
+        FileType.GOOGLE_SLIDES: "application/vnd.google-apps.presentation",
+        FileType.HEIC: "image/heic",
+        FileType.HEIF: "image/heif",
+        FileType.HTML: "text/html",
+        FileType.JPEG: "image/jpeg",
+        FileType.JSON: "application/json",
+        FileType.M4A: "audio/x-m4a",
+        FileType.M4V: "video/x-m4v",
+        FileType.MOV: "video/quicktime",
+        FileType.MP3: "audio/mpeg",
+        FileType.MP4: "video/mp4",
+        FileType.MPEG: "video/mpeg",
+        FileType.MPEGPS: "video/mpegps",
+        FileType.MPG: "video/mpg",
+        FileType.MPA: "audio/m4a",
+        FileType.MPGA: "audio/mpga",
+        FileType.OGG: "audio/ogg",
+        FileType.OPUS: "audio/opus",
+        FileType.PDF: "application/pdf",
+        FileType.PCM: "audio/pcm",
+        FileType.PNG: "image/png",
+        FileType.PPT: "application/vnd.ms-powerpoint",
+        FileType.PPTX: "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+        FileType.RTF: "application/rtf",
+        FileType.THREE_GPP: "video/3gpp",
+        FileType.TXT: "text/plain",
+        FileType.WAV: "audio/wav",
+        FileType.WEBM: "video/webm",
+        FileType.WEBP: "image/webp",
+        FileType.WMV: "video/wmv",
+        FileType.XLS: "application/vnd.ms-excel",
+        FileType.XLSX: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    }
+)
 
 """
 Util Functions
 """
+
+
 def get_file_mime_type_from_extension(extension: str) -> str:
     for file_type, extensions in FILE_EXTENSIONS.items():
         if extension in extensions:
@@ -167,6 +175,7 @@ def get_file_type_from_extension(extension: str) -> FileType:
 def get_file_extension_for_file_type(file_type: FileType) -> str:
     return FILE_EXTENSIONS[file_type][0]
 
+
 def get_file_mime_type_for_file_type(file_type: FileType) -> str:
     return FILE_MIME_TYPES[file_type]
 
@@ -182,11 +191,13 @@ IMAGE_FILE_TYPES = {
     FileType.GIF,
     FileType.WEBP,
     FileType.HEIC,
-    FileType.HEIF
+    FileType.HEIF,
 }
+
 
 def is_image_file_type(file_type):
     return file_type in IMAGE_FILE_TYPES
+
 
 # Videos
 VIDEO_FILE_TYPES = {
@@ -199,11 +210,13 @@ VIDEO_FILE_TYPES = {
     FileType.MPG,
     FileType.WEBM,
     FileType.WMV,
-    FileType.THREE_GPP
+    FileType.THREE_GPP,
 }
+
 
 def is_video_file_type(file_type):
     return file_type in VIDEO_FILE_TYPES
+
 
 # Audio
 AUDIO_FILE_TYPES = {
@@ -217,19 +230,18 @@ AUDIO_FILE_TYPES = {
     FileType.WAV,
 }
 
+
 def is_audio_file_type(file_type):
     return file_type in AUDIO_FILE_TYPES
 
+
 # Text
-TEXT_FILE_TYPES = {
-    FileType.CSV,
-    FileType.HTML,
-    FileType.RTF,
-    FileType.TXT
-}
+TEXT_FILE_TYPES = {FileType.CSV, FileType.HTML, FileType.RTF, FileType.TXT}
+
 
 def is_text_file_type(file_type):
     return file_type in TEXT_FILE_TYPES
+
 
 """
 Other FileType Groupings
@@ -262,6 +274,7 @@ GEMINI_1_5_ACCEPTED_FILE_TYPES: Set[FileType] = {
     # PDF
     FileType.PDF,
 }
+
 
 def is_gemini_1_5_accepted_file_type(file_type: FileType) -> bool:
     return file_type in GEMINI_1_5_ACCEPTED_FILE_TYPES
