b2c9b5bbf4bca6640b82b97dddffbb90422ee47f
Krrish Dholakia <krrishdholakia@gmail.com>
2024-06-10 17:33:43 -0700

    feat(proxy/_logging.py): support controlling log level when json logs=True
    allow user to control log level when json_logs=True

--- litellm/proxy/_logging.py
+++ litellm/proxy/_logging.py
@@ -1,7 +1,12 @@
 import json
 import logging
 from logging import Formatter
-import sys
+import os
+from litellm import json_logs
+
+# Set default log level to INFO
+log_level = os.getenv("LITELLM_LOG", "INFO")
+numeric_level: str = getattr(logging, log_level.upper())
 
 
 class JsonFormatter(Formatter):
@@ -16,6 +21,14 @@ class JsonFormatter(Formatter):
 
 logger = logging.root
 handler = logging.StreamHandler()
-handler.setFormatter(JsonFormatter())
+if json_logs:
+    handler.setFormatter(JsonFormatter())
+else:
+    formatter = logging.Formatter(
+        "\033[92m%(asctime)s - %(name)s:%(levelname)s\033[0m: %(filename)s:%(lineno)s - %(message)s",
+        datefmt="%H:%M:%S",
+    )
+
+    handler.setFormatter(formatter)
 logger.handlers = [handler]
-logger.setLevel(logging.INFO)
+logger.setLevel(numeric_level)
