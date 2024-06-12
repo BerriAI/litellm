43e85a3993d27159f5b94264aaab742da87b497c
Krrish Dholakia <krrishdholakia@gmail.com>
2024-06-12 08:42:35 -0700

    fix(proxy/_types.py): support logging k,v pairs to spend logs with `spend_logs_metadata` param

--- litellm/proxy/utils.py
+++ litellm/proxy/utils.py
@@ -2018,6 +2018,7 @@ def get_logging_payload(
         user_api_key_team_id=None,
         user_api_key_user_id=None,
         user_api_key_team_alias=None,
+        spend_logs_metadata=None,
     )
     if isinstance(metadata, dict):
         verbose_proxy_logger.debug(
