7d5e27d6a1c1c784dc9dc655bc1acf4cc52e2091
Krrish Dholakia <krrishdholakia@gmail.com>
2024-06-12 08:42:35 -0700

    fix(proxy/_types.py): support logging k,v pairs to spend logs with `spend_logs_metadata` param

--- litellm/proxy/_types.py
+++ litellm/proxy/_types.py
@@ -1489,6 +1489,9 @@ class SpendLogsMetadata(TypedDict):
     user_api_key_team_id: Optional[str]
     user_api_key_user_id: Optional[str]
     user_api_key_team_alias: Optional[str]
+    spend_logs_metadata: Optional[
+        dict
+    ]  # special param to log k,v pairs to spendlogs for a call
 
 
 class SpendLogsPayload(TypedDict):
