fbc0393f25633e2a468f449494e8cfb056dd9113
Krrish Dholakia <krrishdholakia@gmail.com>
2024-06-19 11:19:30 -0700

    docs(docusaurus.config.js): fix docusaurus base url

--- docs/my-website/docusaurus.config.js
+++ docs/my-website/docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: '/img/favicon.ico', 
 
   // Set the production url of your site here
-  url: 'https://litellm.vercel.app/',
+  url: 'https://docs.litellm.ai/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
