46da517cdff99aef0d6f6de522d2d5e104378111
Krrish Dholakia <krrishdholakia@gmail.com>
2024-06-20 20:42:32 -0700

    test(test_python_38.py): add longer delay

--- litellm/tests/test_python_38.py
+++ litellm/tests/test_python_38.py
@@ -64,7 +64,7 @@ def test_litellm_proxy_server_config_no_general_settings():
         )
 
         # Allow some time for the server to start
-        time.sleep(5)  # Adjust the sleep time if necessary
+        time.sleep(20)  # Adjust the sleep time if necessary
 
         # Send a request to the /health/liveliness endpoint
         response = requests.get("http://localhost:4000/health/liveliness")
