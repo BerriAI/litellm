08a0dbaba2dc70e4bb4f6d1b818d4c61233ae329
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-06-22 13:40:34 -0700

    testing - use in memory langfuse client cache

--- litellm/tests/test_alangfuse.py
+++ litellm/tests/test_alangfuse.py
@@ -4,6 +4,7 @@ import json
 import logging
 import os
 import sys
+from typing import Any
 from unittest.mock import MagicMock, patch
 
 logging.basicConfig(level=logging.DEBUG)
@@ -19,17 +20,28 @@ import time
 
 import pytest
 
+in_memory_langfuse_client: dict[str, Any] = {}
+
 
 @pytest.fixture
 def langfuse_client():
     import langfuse
 
-    langfuse_client = langfuse.Langfuse(
-        public_key=os.environ["LANGFUSE_PUBLIC_KEY"],
-        secret_key=os.environ["LANGFUSE_SECRET_KEY"],
-        host=None,
+    _langfuse_cache_key = (
+        f"{os.environ['LANGFUSE_PUBLIC_KEY']}-{os.environ['LANGFUSE_SECRET_KEY']}"
     )
-    print("NEW LANGFUSE CLIENT")
+    # use a in memory langfuse client for testing, RAM util on ci/cd gets too high when we init many langfuse clients
+    if _langfuse_cache_key in in_memory_langfuse_clients:
+        langfuse_client = in_memory_langfuse_clients[_langfuse_cache_key]
+    else:
+        langfuse_client = langfuse.Langfuse(
+            public_key=os.environ["LANGFUSE_PUBLIC_KEY"],
+            secret_key=os.environ["LANGFUSE_SECRET_KEY"],
+            host=None,
+        )
+        in_memory_langfuse_clients[_langfuse_cache_key] = langfuse_client
+
+        print("NEW LANGFUSE CLIENT")
 
     with patch(
         "langfuse.Langfuse", MagicMock(return_value=langfuse_client)
