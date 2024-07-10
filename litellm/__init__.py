4da866e81d9d61a7c33210b43d74dac97055ee19
Krrish Dholakia <krrishdholakia@gmail.com>
2024-07-10 00:32:28 -0700

    feat(anthropic_adapter.py): support for translating anthropic params to openai format

--- litellm/__init__.py
+++ litellm/__init__.py
@@ -364,7 +364,7 @@ for key, value in model_cost.items():
     elif value.get("litellm_provider") == "mistral":
         mistral_chat_models.append(key)
     elif value.get("litellm_provider") == "anthropic":
-        anthropic_models.append(key)    
+        anthropic_models.append(key)
     elif value.get("litellm_provider") == "empower":
         empower_models.append(key)
     elif value.get("litellm_provider") == "openrouter":
@@ -881,3 +881,8 @@ from .assistants.main import *
 from .batches.main import *
 from .scheduler import *
 from .cost_calculator import response_cost_calculator, cost_per_token
+
+### ADAPTERS ###
+from .types.adapter import AdapterItem
+
+adapters: List[AdapterItem] = []
