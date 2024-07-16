c0bd3076fbdb8616bd6fc50c0f88d447caf72da8
Raymond Huang <1415176717@qq.com>
2024-07-15 19:02:09 -0700

    test case for key overriding

--- litellm/tests/test_api_key_override_dynamic_key.py
+++ litellm/tests/test_api_key_override_dynamic_key.py
@@ -9,3 +9,11 @@ try:
     
 except Exception as e:
     print(e)
+
+print("==============try original===============")
+response = completion(
+    model="groq/mixtral-8x7b-32768",
+    messages=[{"role": "user", "content": "hey, how's it going?"}],
+)
+
+print(response)
