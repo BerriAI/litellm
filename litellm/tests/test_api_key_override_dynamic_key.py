663520f58ce6950987db205d26af136a363965ef
Raymond Huang <1415176717@qq.com>
2024-07-15 21:52:40 -0700

    added test cases

--- litellm/tests/test_api_key_override_dynamic_key.py
+++ litellm/tests/test_api_key_override_dynamic_key.py
@@ -1,8 +1,12 @@
 from litellm import completion
 
+model = "perplexity/llama-3-sonar-large-32k-online"
+# model = "groq/mixtral-8x7b-32768"
+# model = "together_ai/Qwen/Qwen1.5-32B-Chat"
+# model = "fireworks_ai/llama-v3-8b-instruct"
 try:
     response = completion(
-        model="groq/mixtral-8x7b-32768",
+        model=model,
         messages=[{"role": "user", "content": "hey, how's it going?"}],
         api_key="invalid_key"
     )
@@ -12,7 +16,7 @@ except Exception as e:
 
 print("==============try original===============")
 response = completion(
-    model="gpt-3.5-turbo",
+    model=model,
     messages=[{"role": "user", "content": "hey, how's it going?"}],
 )
 
