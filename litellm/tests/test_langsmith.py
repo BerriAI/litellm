0890299f655bcc915138831c1983c46ee1761a29
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-07-17 10:08:29 -0700

    test langmsith logging

--- litellm/tests/test_langsmith.py
+++ litellm/tests/test_langsmith.py
@@ -1,19 +1,20 @@
-import sys
-import os
 import io
+import os
+import sys
 
 sys.path.insert(0, os.path.abspath("../.."))
 
-from litellm import completion
 import litellm
+from litellm import completion
 
-litellm.success_callback = ["langsmith"]
 litellm.set_verbose = True
 import time
 
 
 def test_langsmith_logging():
     try:
+        litellm.set_verbose = True
+        litellm.success_callback = ["langsmith"]
         response = completion(
             model="claude-instant-1.2",
             messages=[{"role": "user", "content": "what llm are u"}],
@@ -21,6 +22,7 @@ def test_langsmith_logging():
             temperature=0.2,
         )
         print(response)
+        time.sleep(3)
     except Exception as e:
         print(e)
 
@@ -30,17 +32,15 @@ def test_langsmith_logging():
 
 def test_langsmith_logging_with_metadata():
     try:
+        litellm.success_callback = ["langsmith"]
         response = completion(
             model="gpt-3.5-turbo",
             messages=[{"role": "user", "content": "what llm are u"}],
             max_tokens=10,
             temperature=0.2,
-            metadata={
-                "run_name": "litellmRUN",
-                "project_name": "litellm-completion",
-            },
         )
         print(response)
+        time.sleep(3)
     except Exception as e:
         print(e)
 
@@ -50,21 +50,19 @@ def test_langsmith_logging_with_metadata():
 
 def test_langsmith_logging_with_streaming_and_metadata():
     try:
+        litellm.success_callback = ["langsmith"]
         response = completion(
             model="gpt-3.5-turbo",
             messages=[{"role": "user", "content": "what llm are u"}],
             max_tokens=10,
             temperature=0.2,
-            metadata={
-                "run_name": "litellmRUN",
-                "project_name": "litellm-completion",
-            },
             stream=True,
         )
         for chunk in response:
             continue
+        time.sleep(3)
     except Exception as e:
         print(e)
 
 
-test_langsmith_logging_with_streaming_and_metadata()
+# test_langsmith_logging_with_streaming_and_metadata()
