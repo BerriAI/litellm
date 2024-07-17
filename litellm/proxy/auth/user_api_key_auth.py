4e9d278b5c3557f84991eadd6e3bfc1a6e6fc7c9
John Walz <john@validmind.ai>
2024-07-16 21:38:26 -0400

    fix: remove debug messages

--- litellm/proxy/auth/user_api_key_auth.py
+++ litellm/proxy/auth/user_api_key_auth.py
@@ -964,10 +964,6 @@ async def user_api_key_auth(
             _user_role = _get_user_role(user_id_information=user_id_information)
 
             if not _is_user_proxy_admin(user_id_information):  # if non-admin
-                print("#### ROUTE ####")
-                print(route)
-                print(request["route"].name)
-                print()
                 if is_openai_route(route=route):
                     pass
                 elif is_openai_route(route=request["route"].name):
