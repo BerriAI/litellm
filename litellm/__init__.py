8c005d8134fda9cceeb03ab0b1886eb31130da3a
Krrish Dholakia <krrishdholakia@gmail.com>
2024-07-22 20:58:02 -0700

    feat(redact_messages.py): allow remove sensitive key information before passing to logging integration

--- litellm/__init__.py
+++ litellm/__init__.py
@@ -76,6 +76,7 @@ post_call_rules: List[Callable] = []
 turn_off_message_logging: Optional[bool] = False
 log_raw_request_response: bool = False
 redact_messages_in_exceptions: Optional[bool] = False
+redact_user_api_key_info: Optional[bool] = False
 store_audit_logs = False  # Enterprise feature, allow users to see audit logs
 ## end of callbacks #############
 
