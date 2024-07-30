4849df03ff551f38653e0704a9bc5ac9dec8e879
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-07-29 19:46:55 -0700

    add list fine tune endpoints

--- litellm/llms/openai_fine_tuning/openai.py
+++ litellm/llms/openai_fine_tuning/openai.py
@@ -2,6 +2,7 @@ from typing import Any, Coroutine, Optional, Union
 
 import httpx
 from openai import AsyncOpenAI, OpenAI
+from openai.pagination import AsyncCursorPage
 from openai.types.fine_tuning import FineTuningJob
 
 from litellm._logging import verbose_logger
@@ -144,3 +145,53 @@ class OpenAIFineTuningAPI(BaseLLM):
             fine_tuning_job_id=fine_tuning_job_id
         )
         return response
+
+    async def alist_fine_tuning_jobs(
+        self,
+        openai_client: AsyncOpenAI,
+        after: Optional[str] = None,
+        limit: Optional[int] = None,
+    ):
+        response = await openai_client.fine_tuning.jobs.list(after=after, limit=limit)
+        return response
+
+    def list_fine_tuning_jobs(
+        self,
+        _is_async: bool,
+        api_key: Optional[str],
+        api_base: Optional[str],
+        timeout: Union[float, httpx.Timeout],
+        max_retries: Optional[int],
+        organization: Optional[str],
+        client: Optional[Union[OpenAI, AsyncOpenAI]] = None,
+        after: Optional[str] = None,
+        limit: Optional[int] = None,
+    ):
+        openai_client: Optional[Union[OpenAI, AsyncOpenAI]] = self.get_openai_client(
+            api_key=api_key,
+            api_base=api_base,
+            timeout=timeout,
+            max_retries=max_retries,
+            organization=organization,
+            client=client,
+            _is_async=_is_async,
+        )
+        if openai_client is None:
+            raise ValueError(
+                "OpenAI client is not initialized. Make sure api_key is passed or OPENAI_API_KEY is set in the environment."
+            )
+
+        if _is_async is True:
+            if not isinstance(openai_client, AsyncOpenAI):
+                raise ValueError(
+                    "OpenAI client is not an instance of AsyncOpenAI. Make sure you passed an AsyncOpenAI client."
+                )
+            return self.alist_fine_tuning_jobs(  # type: ignore
+                after=after,
+                limit=limit,
+                openai_client=openai_client,
+            )
+        verbose_logger.debug("list fine tuning job, after= %s, limit= %s", after, limit)
+        response = openai_client.fine_tuning.jobs.list(after=after, limit=limit)
+        return response
+        pass
