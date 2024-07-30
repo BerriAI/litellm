8e6df89f8aaac34a5da293bcee42c9d84affadeb
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-07-29 19:23:35 -0700

    fix doc string

--- litellm/fine_tuning/main.py
+++ litellm/fine_tuning/main.py
@@ -190,7 +190,7 @@ def cancel_fine_tuning_job(
     **kwargs,
 ) -> Union[FineTuningJob, Coroutine[Any, Any, FineTuningJob]]:
     """
-    Creates a fine-tuning job which begins the process of creating a new model from a given dataset.
+    Immediately cancel a fine-tune job.
 
     Response includes details of the enqueued job including job status and the name of the fine-tuned models once complete
 
