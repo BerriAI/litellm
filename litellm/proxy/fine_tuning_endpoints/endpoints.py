9b6231810bff35d40e0f8b6522ba3019c681e137
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-07-31 15:58:35 -0700

    add GET fine_tuning/jobs

--- litellm/proxy/fine_tuning_endpoints/endpoints.py
+++ litellm/proxy/fine_tuning_endpoints/endpoints.py
@@ -138,7 +138,6 @@ async def create_fine_tuning_job(
         # add llm_provider_config to data
         data.update(llm_provider_config)
 
-        # For now, use custom_llm_provider=="openai" -> this will change as LiteLLM adds more providers for fine-tuning
         response = await litellm.acreate_fine_tuning_job(**data)
 
         ### ALERTING ###
@@ -172,6 +171,108 @@ async def create_fine_tuning_job(
         )
         verbose_proxy_logger.error(
             "litellm.proxy.proxy_server.create_fine_tuning_job(): Exception occurred - {}".format(
+                str(e)
+            )
+        )
+        verbose_proxy_logger.debug(traceback.format_exc())
+        if isinstance(e, HTTPException):
+            raise ProxyException(
+                message=getattr(e, "message", str(e.detail)),
+                type=getattr(e, "type", "None"),
+                param=getattr(e, "param", "None"),
+                code=getattr(e, "status_code", status.HTTP_400_BAD_REQUEST),
+            )
+        else:
+            error_msg = f"{str(e)}"
+            raise ProxyException(
+                message=getattr(e, "message", error_msg),
+                type=getattr(e, "type", "None"),
+                param=getattr(e, "param", "None"),
+                code=getattr(e, "status_code", 500),
+            )
+
+
+@router.get(
+    "/v1/fine_tuning/jobs",
+    dependencies=[Depends(user_api_key_auth)],
+    tags=["fine-tuning"],
+)
+async def list_fine_tuning_jobs(
+    request: Request,
+    fastapi_response: Response,
+    custom_llm_provider: Literal["openai", "azure"],
+    after: Optional[str] = None,
+    limit: Optional[int] = None,
+    user_api_key_dict: dict = Depends(user_api_key_auth),
+):
+    """
+    Lists fine-tuning jobs for the organization.
+    This is the equivalent of GET https://api.openai.com/v1/fine_tuning/jobs
+
+    Supported Query Params:
+    - `custom_llm_provider`: Name of the LiteLLM provider
+    - `after`: Identifier for the last job from the previous pagination request.
+    - `limit`: Number of fine-tuning jobs to retrieve (default is 20).
+    """
+    from litellm.proxy.proxy_server import (
+        add_litellm_data_to_request,
+        general_settings,
+        get_custom_headers,
+        proxy_config,
+        proxy_logging_obj,
+        version,
+    )
+
+    data: dict = {}
+    try:
+        # Include original request and headers in the data
+        data = await add_litellm_data_to_request(
+            data=data,
+            request=request,
+            general_settings=general_settings,
+            user_api_key_dict=user_api_key_dict,
+            version=version,
+            proxy_config=proxy_config,
+        )
+
+        # get configs for custom_llm_provider
+        llm_provider_config = get_fine_tuning_provider_config(
+            custom_llm_provider=custom_llm_provider
+        )
+
+        data.update(llm_provider_config)
+
+        response = await litellm.alist_fine_tuning_jobs(
+            **data,
+            after=after,
+            limit=limit,
+        )
+
+        ### RESPONSE HEADERS ###
+        hidden_params = getattr(response, "_hidden_params", {}) or {}
+        model_id = hidden_params.get("model_id", None) or ""
+        cache_key = hidden_params.get("cache_key", None) or ""
+        api_base = hidden_params.get("api_base", None) or ""
+
+        fastapi_response.headers.update(
+            get_custom_headers(
+                user_api_key_dict=user_api_key_dict,
+                model_id=model_id,
+                cache_key=cache_key,
+                api_base=api_base,
+                version=version,
+                model_region=getattr(user_api_key_dict, "allowed_model_region", ""),
+            )
+        )
+
+        return response
+
+    except Exception as e:
+        await proxy_logging_obj.post_call_failure_hook(
+            user_api_key_dict=user_api_key_dict, original_exception=e, request_data=data
+        )
+        verbose_proxy_logger.error(
+            "litellm.proxy.proxy_server.list_fine_tuning_jobs(): Exception occurred - {}".format(
                 str(e)
             )
         )
