1965f1f90c3e89b284caf3f7534f1050e6bbfd7a
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-07-31 13:50:51 -0700

    add /fine_tuning/jobs routes

--- litellm/proxy/_types.py
+++ litellm/proxy/_types.py
@@ -208,6 +208,8 @@ class LiteLLMRoutes(enum.Enum):
         "/files/{file_id}",
         "/v1/files/{file_id}/content",
         "/files/{file_id}/content",
+        # fine_tuning
+        "/fine_tuning/jobs",
         # assistants-related routes
         "/assistants",
         "/v1/assistants",
