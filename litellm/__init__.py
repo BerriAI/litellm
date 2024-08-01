84036ea70910e34c256e64823370009788e637c9
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-08-01 15:25:19 -0700

    init gcs using gcs_bucket

--- litellm/__init__.py
+++ litellm/__init__.py
@@ -46,6 +46,7 @@ _custom_logger_compatible_callbacks_literal = Literal[
     "galileo",
     "braintrust",
     "arize",
+    "gcs_bucket",
 ]
 _known_custom_logger_compatible_callbacks: List = list(
     get_args(_custom_logger_compatible_callbacks_literal)
