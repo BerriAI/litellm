77e5cb89edb8fd41d22b583a94d8ee1882d8df99
Krrish Dholakia <krrishdholakia@gmail.com>
2024-08-01 15:22:32 -0700

    fix(google.py): fix cost tracking for vertex ai mistral models

--- litellm/litellm_core_utils/llm_cost_calc/google.py
+++ litellm/litellm_core_utils/llm_cost_calc/google.py
@@ -44,7 +44,9 @@ def cost_router(
     Returns
         - str, the specific google cost calc function it should route to.
     """
-    if custom_llm_provider == "vertex_ai" and ("claude" in model or "llama" in model):
+    if custom_llm_provider == "vertex_ai" and (
+        "claude" in model or "llama" in model or "mistral" in model
+    ):
         return "cost_per_token"
     elif custom_llm_provider == "gemini":
         return "cost_per_token"
