22e2840daabf24aef0fcad3e1b2286e081b5c256
Krrish Dholakia <krrishdholakia@gmail.com>
2024-08-12 15:45:21 -0700

    fix(cost_calculator.py): handle openai usage pydantic object
    Fixes https://github.com/BerriAI/litellm/issues/5165

--- litellm/cost_calculator.py
+++ litellm/cost_calculator.py
@@ -490,6 +490,10 @@ def completion_cost(
             isinstance(completion_response, BaseModel)
             or isinstance(completion_response, dict)
         ):  # tts returns a custom class
+            if isinstance(completion_response, BaseModel) and not isinstance(
+                completion_response, litellm.Usage
+            ):
+                completion_response = litellm.Usage(**completion_response.model_dump())
             # get input/output tokens from completion_response
             prompt_tokens = completion_response.get("usage", {}).get("prompt_tokens", 0)
             completion_tokens = completion_response.get("usage", {}).get(
