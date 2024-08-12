8a7571ad7250bbc8a419db6c1b3a4c895087af3f
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-08-12 16:33:35 -0700

    tes logging to gcs buckets

--- litellm/tests/test_gcs_bucket.py
+++ litellm/tests/test_gcs_bucket.py
@@ -9,6 +9,7 @@ import json
 import logging
 import tempfile
 import uuid
+from datetime import datetime
 
 import pytest
 
@@ -116,8 +117,17 @@ async def test_basic_gcs_logger():
 
     await asyncio.sleep(5)
 
+    # Get the current date
+    # Get the current date
+    current_date = datetime.now().strftime("%Y-%m-%d")
+
+    # Modify the object_name to include the date-based folder
+    object_name = f"{current_date}%2F{response.id}"
+
+    print("object_name", object_name)
+
     # Check if object landed on GCS
-    object_from_gcs = await gcs_logger.download_gcs_object(object_name=response.id)
+    object_from_gcs = await gcs_logger.download_gcs_object(object_name=object_name)
     print("object from gcs=", object_from_gcs)
     # convert object_from_gcs from bytes to DICT
     parsed_data = json.loads(object_from_gcs)
@@ -150,4 +160,4 @@ async def test_basic_gcs_logger():
 
     # Delete Object from GCS
     print("deleting object from GCS")
-    await gcs_logger.delete_gcs_object(object_name=response.id)
+    # await gcs_logger.delete_gcs_object(object_name=response.id)
