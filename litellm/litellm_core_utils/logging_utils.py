8cb62213e1f4ad3b86c0f3fc193e9bf79bc041f7
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-08-19 11:25:31 -0700

    feat run aporia as post call success hook

--- litellm/litellm_core_utils/logging_utils.py
+++ litellm/litellm_core_utils/logging_utils.py
@@ -1,4 +1,12 @@
-from typing import Any
+from typing import TYPE_CHECKING, Any, Optional, Union
+
+if TYPE_CHECKING:
+    from litellm import ModelResponse as _ModelResponse
+
+    LiteLLMModelResponse = _ModelResponse
+else:
+    LiteLLMModelResponse = Any
+
 
 import litellm
 
@@ -20,3 +28,21 @@ def convert_litellm_response_object_to_dict(response_obj: Any) -> dict:
 
     # If it's not a LiteLLM type, return the object as is
     return dict(response_obj)
+
+
+def convert_litellm_response_object_to_str(
+    response_obj: Union[Any, LiteLLMModelResponse]
+) -> Optional[str]:
+    """
+    Get the string of the response object from LiteLLM
+
+    """
+    if isinstance(response_obj, litellm.ModelResponse):
+        response_str = ""
+        for choice in response_obj.choices:
+            if isinstance(choice, litellm.Choices):
+                if choice.message.content and isinstance(choice.message.content, str):
+                    response_str += choice.message.content
+        return response_str
+
+    return None
