6237183493779ce80a81043c5953ea29bd103233
Kamil Zych <kzych@inpost.pl>
2024-08-20 10:34:18 +0200

    Use `ClientSecretCredential` instead of `DefaultAzureCredential`.

--- litellm/proxy/secret_managers/get_azure_ad_token_provider.py
+++ litellm/proxy/secret_managers/get_azure_ad_token_provider.py
@@ -1,7 +1,7 @@
 import os
 from typing import Callable
 
-from azure.identity import DefaultAzureCredential
+from azure.identity import ClientSecretCredential
 from azure.identity import get_bearer_token_provider
 
 
@@ -10,15 +10,23 @@ def get_azure_ad_token_provider() -> Callable[[], str]:
     Get Azure AD token provider based on Service Principal with Secret workflow.
 
     Based on: https://github.com/openai/openai-python/blob/main/examples/azure_ad.py
-    See Also: https://learn.microsoft.com/en-us/python/api/overview/azure/identity-readme?view=azure-python#service-principal-with-secret.
+    See Also:
+        https://learn.microsoft.com/en-us/python/api/overview/azure/identity-readme?view=azure-python#service-principal-with-secret;
+        https://learn.microsoft.com/en-us/python/api/azure-identity/azure.identity.clientsecretcredential?view=azure-python.
+
+    Returns:
+        Callable that returns a temporary authentication token.
     """
-    required_keys = {"AZURE_CLIENT_ID", "AZURE_TENANT_ID", "AZURE_CLIENT_SECRET"}
-    missing_keys = required_keys.difference(os.environ)
-    if missing_keys:
-        raise ValueError(f"Missing environment variables required by Azure AD workflow: {missing_keys}.")
+    try:
+        credential = ClientSecretCredential(
+            client_id=os.environ["AZURE_CLIENT_ID"],
+            client_secret=os.environ["AZURE_CLIENT_SECRET"],
+            tenant_id=os.environ["AZURE_TENANT_ID"],
+        )
+    except KeyError as e:
+        raise ValueError("Missing environment variable required by Azure AD workflow.") from e
+
     return get_bearer_token_provider(
-        # DefaultAzureCredential access environment variables:
-        # AZURE_CLIENT_ID, AZURE_TENANT_ID, AZURE_CLIENT_SECRET
-        DefaultAzureCredential(),
+        credential,
         "https://cognitiveservices.azure.com/.default",
     )
