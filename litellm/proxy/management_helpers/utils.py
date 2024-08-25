6de3a00102fca7e399b319a191a6d2d9f5a9a8bd
Krrish Dholakia <krrishdholakia@gmail.com>
2024-08-24 18:00:36 -0700

    fix(utils.py): fix linting errors

--- litellm/proxy/management_helpers/utils.py
+++ litellm/proxy/management_helpers/utils.py
@@ -105,7 +105,7 @@ async def add_new_member(
                 returned_user = LiteLLM_UserTable(**_returned_user.model_dump())
         elif len(existing_user_row) == 1:
             user_info = existing_user_row[0]
-            _returned_user = await prisma_client.db.litellm_usertable.update(
+            _returned_user = await prisma_client.db.litellm_usertable.update(  # type: ignore
                 where={"user_id": user_info.user_id},
                 data={"teams": {"push": [team_id]}},
             )
