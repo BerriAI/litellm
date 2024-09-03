bae916e6a7f749b1773bd06206c96af3eab0672e
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-09-02 19:29:32 -0700

    fix linting

--- litellm/proxy/pass_through_endpoints/success_handler.py
+++ litellm/proxy/pass_through_endpoints/success_handler.py
@@ -111,18 +111,20 @@ class PassThroughEndpointLogging:
             model = self.extract_model_from_url(url_route)
             _json_response = httpx_response.json()
 
-            litellm_model_response = await transform_vertex_response_to_openai(
-                response=_json_response,
-                model=model,
-                model_response=litellm.EmbeddingResponse(),
+            litellm_embedding_response: litellm.EmbeddingResponse = (
+                await transform_vertex_response_to_openai(
+                    response=_json_response,
+                    model=model,
+                    model_response=litellm.EmbeddingResponse(),
+                )
             )
 
-            litellm_model_response.model = model
-            logging_obj.model = litellm_model_response.model
+            litellm_embedding_response.model = model
+            logging_obj.model = litellm_embedding_response.model
             logging_obj.model_call_details["model"] = logging_obj.model
 
             await logging_obj.async_success_handler(
-                result=litellm_model_response,
+                result=litellm_embedding_response,
                 start_time=start_time,
                 end_time=end_time,
                 cache_hit=cache_hit,
