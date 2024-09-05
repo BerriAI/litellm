cce9b2d19343fc82077211080a9c9468894ff308
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-09-05 15:39:30 -0700

    run ci/cd on main

--- litellm/tests/test_completion.py
+++ litellm/tests/test_completion.py
@@ -11,8 +11,7 @@ import os
 
 sys.path.insert(
     0, os.path.abspath("../..")
-)
-# Adds the parent directory to the system-path
+)  # Adds the parent directory to the system-path
 
 import os
 from unittest.mock import AsyncMock, MagicMock, patch
