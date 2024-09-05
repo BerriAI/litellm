3e6eaa9cb95fe6ec709d9515d934831159ca92ff
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-09-05 13:13:58 -0700

    add ui testing folder

---
+++ tests/proxy_admin_ui_tests/test_usage_endpoints.py
@@ -0,0 +1,14 @@
+"""
+Tests the following endpoints used by the UI 
+
+/global/spend/logs
+/global/spend/keys
+/global/spend/models
+/global/activity
+/global/activity/model
+
+
+For all tests - test the following:
+- Response is valid 
+- Response for Admin User is different from response from Internal User
+"""
