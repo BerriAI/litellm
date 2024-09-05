e48bfd7e6f562b1d30fdf8a04ce92660a28fe058
Krrish Dholakia <krrishdholakia@gmail.com>
2024-09-05 16:24:41 -0700

    fix(proxy_server.py): fix read for max_internal_user_budget

--- litellm/proxy/proxy_server.py
+++ litellm/proxy/proxy_server.py
@@ -1645,6 +1645,8 @@ class ProxyConfig:
                     verbose_proxy_logger.debug(
                         f"litellm.post_call_rules: {litellm.post_call_rules}"
                     )
+                elif key == "max_internal_user_budget":
+                    litellm.max_internal_user_budget = float(value)  # type: ignore
                 elif key == "default_max_internal_user_budget":
                     litellm.default_max_internal_user_budget = float(value)
                     if litellm.max_internal_user_budget is None:
