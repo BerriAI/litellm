90c0e31d9f0d901db01e02730150c8c11b6462e5
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-09-07 09:25:13 -0700

    allow setting password for redis cluster

--- litellm/_redis.py
+++ litellm/_redis.py
@@ -67,6 +67,7 @@ def _get_redis_cluster_kwargs(client=None):
     exclude_args = {"self", "connection_pool", "retry", "host", "port", "startup_nodes"}
 
     available_args = [x for x in arg_spec.args if x not in exclude_args]
+    available_args.append("password")
 
     return available_args
 
