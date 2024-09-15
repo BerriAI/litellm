6ea06a3482f834224acae08a47959c59248fedf5
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-09-14 17:25:03 -0700

    track num cooldowns on prometheus

--- litellm/router_utils/cooldown_callbacks.py
+++ litellm/router_utils/cooldown_callbacks.py
@@ -31,14 +31,14 @@ async def router_cooldown_handler(
     _litellm_params = _deployment["litellm_params"]
     temp_litellm_params = copy.deepcopy(_litellm_params)
     temp_litellm_params = dict(temp_litellm_params)
-    _model_name = _deployment.get("model_name", None)
+    _model_name = _deployment.get("model_name", None) or ""
     _api_base = litellm.get_api_base(
         model=_model_name, optional_params=temp_litellm_params
     )
     model_info = _deployment["model_info"]
     model_id = model_info.id
 
-    litellm_model_name = temp_litellm_params.get("model")
+    litellm_model_name = temp_litellm_params.get("model") or ""
     llm_provider = ""
     try:
 
@@ -53,10 +53,22 @@ async def router_cooldown_handler(
     from litellm.litellm_core_utils.litellm_logging import prometheusLogger
 
     if prometheusLogger is not None:
-        prometheusLogger.set_deployment_complete_outage(
-            litellm_model_name=_model_name,
-            model_id=model_id,
-            api_base=_api_base,
-            api_provider=llm_provider,
-        )
+        from litellm.integrations.prometheus import PrometheusLogger
+
+        if isinstance(prometheusLogger, PrometheusLogger):
+            prometheusLogger.set_deployment_complete_outage(
+                litellm_model_name=_model_name,
+                model_id=model_id,
+                api_base=_api_base,
+                api_provider=llm_provider,
+            )
+
+            prometheusLogger.increment_deployment_cooled_down(
+                litellm_model_name=_model_name,
+                model_id=model_id,
+                api_base=_api_base,
+                api_provider=llm_provider,
+                cooldown_time=cooldown_time,
+            )
+
     return
