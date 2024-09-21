b01de44d299cb5cbc33a55e359b74316c61a9a84
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-09-20 22:23:28 -0700

    [Feat] Add fireworks AI embedding (#5812)
    * add fireworks embedding models
    
    * add fireworks ai
    
    * fireworks ai embeddings support
    
    * is_fireworks_embedding_model
    
    * working fireworks embeddings
    
    * fix health check * models
    
    * fix embedding get optional params
    
    * fix linting errors
    
    * fix pick_cheapest_chat_model_from_llm_provider
    
    * add fireworks ai litellm provider
    
    * docs fireworks embedding models
    
    * fixes for when azure ad token  is passed

--- litellm/__init__.py
+++ litellm/__init__.py
@@ -378,6 +378,7 @@ nlp_cloud_models: List = []
 aleph_alpha_models: List = []
 bedrock_models: List = []
 fireworks_ai_models: List = []
+fireworks_ai_embedding_models: List = []
 deepinfra_models: List = []
 perplexity_models: List = []
 watsonx_models: List = []
@@ -454,6 +455,10 @@ def add_known_models():
             # ignore the 'up-to', '-to-' model names -> not real models. just for cost tracking based on model params.
             if "-to-" not in key:
                 fireworks_ai_models.append(key)
+        elif value.get("litellm_provider") == "fireworks_ai-embedding-models":
+            # ignore the 'up-to', '-to-' model names -> not real models. just for cost tracking based on model params.
+            if "-to-" not in key:
+                fireworks_ai_embedding_models.append(key)
 
 
 add_known_models()
@@ -779,7 +784,7 @@ models_by_provider: dict = {
     "maritalk": maritalk_models,
     "watsonx": watsonx_models,
     "gemini": gemini_models,
-    "fireworks_ai": fireworks_ai_models,
+    "fireworks_ai": fireworks_ai_models + fireworks_ai_embedding_models,
 }
 
 # mapping for those models which have larger equivalents
@@ -825,6 +830,7 @@ all_embedding_models = (
     + cohere_embedding_models
     + bedrock_embedding_models
     + vertex_embedding_models
+    + fireworks_ai_embedding_models
 )
 
 ####### IMAGE GENERATION MODELS ###################
@@ -971,6 +977,9 @@ from .llms.cerebras.chat import CerebrasConfig
 from .llms.sambanova.chat import SambanovaConfig
 from .llms.AI21.chat import AI21ChatConfig
 from .llms.fireworks_ai.chat.fireworks_ai_transformation import FireworksAIConfig
+from .llms.fireworks_ai.embed.fireworks_ai_transformation import (
+    FireworksAIEmbeddingConfig,
+)
 from .llms.volcengine import VolcEngineConfig
 from .llms.text_completion_codestral import MistralTextCompletionConfig
 from .llms.AzureOpenAI.azure import (
--- litellm/litellm_core_utils/get_llm_provider_logic.py
+++ litellm/litellm_core_utils/get_llm_provider_logic.py
@@ -216,7 +216,12 @@ def get_llm_provider(
                 dynamic_api_key = api_key or get_secret("DEEPSEEK_API_KEY")
             elif custom_llm_provider == "fireworks_ai":
                 # fireworks is openai compatible, we just need to set this to custom_openai and have the api_base be https://api.fireworks.ai/inference/v1
-                if not model.startswith("accounts/"):
+                if litellm.FireworksAIEmbeddingConfig().is_fireworks_embedding_model(
+                    model=model
+                ):
+                    # fireworks embeddings models do no require accounts/fireworks prefix https://docs.fireworks.ai/api-reference/creates-an-embedding-vector-representing-the-input-text
+                    pass
+                elif not model.startswith("accounts/"):
                     model = f"accounts/fireworks/models/{model}"
                 api_base = (
                     api_base
