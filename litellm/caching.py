53ab65aa98aa8771912a8f2c3617865dc6bfbc52
Krrish Dholakia <krrishdholakia@gmail.com>
2024-09-28 18:40:17 -0700

    fix(router.py): skip setting model_group response headers for now
    current implementation increases redis cache calls by 3x

--- litellm/caching.py
+++ litellm/caching.py
@@ -1860,6 +1860,7 @@ class DualCache(BaseCache):
 
         Returns - int - the incremented value
         """
+        traceback.print_stack()
         try:
             result: int = value
             if self.in_memory_cache is not None:
