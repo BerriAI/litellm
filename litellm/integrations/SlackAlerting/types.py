845e3bac262902d073d9e3884b14f5d017f209e2
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-09-30 14:06:54 -0700

    define all slack alert types

--- litellm/integrations/SlackAlerting/types.py
+++ litellm/integrations/SlackAlerting/types.py
@@ -32,7 +32,7 @@ class LiteLLMBase(BaseModel):
     Implements default functions, all pydantic objects should have.
     """
 
-    def json(self, **kwargs):
+    def json(self, **kwargs):  # type: ignore
         try:
             return self.model_dump()  # noqa
         except:
@@ -41,14 +41,14 @@ class LiteLLMBase(BaseModel):
 
 
 class SlackAlertingArgsEnum(Enum):
-    daily_report_frequency: int = 12 * 60 * 60
-    report_check_interval: int = 5 * 60
-    budget_alert_ttl: int = 24 * 60 * 60
-    outage_alert_ttl: int = 1 * 60
-    region_outage_alert_ttl: int = 1 * 60
-    minor_outage_alert_threshold: int = 1 * 5
-    major_outage_alert_threshold: int = 1 * 10
-    max_outage_alert_list_size: int = 1 * 10
+    daily_report_frequency = 12 * 60 * 60
+    report_check_interval = 5 * 60
+    budget_alert_ttl = 24 * 60 * 60
+    outage_alert_ttl = 1 * 60
+    region_outage_alert_ttl = 1 * 60
+    minor_outage_alert_threshold = 1 * 5
+    major_outage_alert_threshold = 1 * 10
+    max_outage_alert_list_size = 1 * 10
 
 
 class SlackAlertingArgs(LiteLLMBase):
@@ -56,37 +56,37 @@ class SlackAlertingArgs(LiteLLMBase):
         default=int(
             os.getenv(
                 "SLACK_DAILY_REPORT_FREQUENCY",
-                SlackAlertingArgsEnum.daily_report_frequency.value,
+                int(SlackAlertingArgsEnum.daily_report_frequency.value),
             )
         ),
         description="Frequency of receiving deployment latency/failure reports. Default is 12hours. Value is in seconds.",
     )
     report_check_interval: int = Field(
-        default=SlackAlertingArgsEnum.report_check_interval.value,
+        default=SlackAlertingArgsEnum.report_check_interval,
         description="Frequency of checking cache if report should be sent. Background process. Default is once per hour. Value is in seconds.",
     )  # 5 minutes
     budget_alert_ttl: int = Field(
-        default=SlackAlertingArgsEnum.budget_alert_ttl.value,
+        default=SlackAlertingArgsEnum.budget_alert_ttl,
         description="Cache ttl for budgets alerts. Prevents spamming same alert, each time budget is crossed. Value is in seconds.",
     )  # 24 hours
     outage_alert_ttl: int = Field(
-        default=SlackAlertingArgsEnum.outage_alert_ttl.value,
+        default=SlackAlertingArgsEnum.outage_alert_ttl,
         description="Cache ttl for model outage alerts. Sets time-window for errors. Default is 1 minute. Value is in seconds.",
     )  # 1 minute ttl
     region_outage_alert_ttl: int = Field(
-        default=SlackAlertingArgsEnum.region_outage_alert_ttl.value,
+        default=SlackAlertingArgsEnum.region_outage_alert_ttl,
         description="Cache ttl for provider-region based outage alerts. Alert sent if 2+ models in same region report errors. Sets time-window for errors. Default is 1 minute. Value is in seconds.",
     )  # 1 minute ttl
     minor_outage_alert_threshold: int = Field(
-        default=SlackAlertingArgsEnum.minor_outage_alert_threshold.value,
+        default=SlackAlertingArgsEnum.minor_outage_alert_threshold,
         description="The number of errors that count as a model/region minor outage. ('400' error code is not counted).",
     )
     major_outage_alert_threshold: int = Field(
-        default=SlackAlertingArgsEnum.major_outage_alert_threshold.value,
+        default=SlackAlertingArgsEnum.major_outage_alert_threshold,
         description="The number of errors that countas a model/region major outage. ('400' error code is not counted).",
     )
     max_outage_alert_list_size: int = Field(
-        default=SlackAlertingArgsEnum.max_outage_alert_list_size.value,
+        default=SlackAlertingArgsEnum.max_outage_alert_list_size,
         description="Maximum number of errors to store in cache. For a given model/region. Prevents memory leaks.",
     )  # prevent memory leak
 
@@ -119,3 +119,51 @@ class SlackAlertingCacheKeys(Enum):
     failed_requests_key = "failed_requests_daily_metrics"
     latency_key = "latency_daily_metrics"
     report_sent_key = "daily_metrics_report_sent"
+
+
+class AlertType(str, Enum):
+    """
+    Enum for alert types and management event types
+    """
+
+    # LLM-related alerts
+    llm_exceptions = "llm_exceptions"
+    llm_too_slow = "llm_too_slow"
+    llm_requests_hanging = "llm_requests_hanging"
+
+    # Budget and spend alerts
+    budget_alerts = "budget_alerts"
+    spend_reports = "spend_reports"
+    failed_tracking_spend = "failed_tracking_spend"
+
+    # Database alerts
+    db_exceptions = "db_exceptions"
+
+    # Report alerts
+    daily_reports = "daily_reports"
+
+    # Deployment alerts
+    cooldown_deployment = "cooldown_deployment"
+    new_model_added = "new_model_added"
+
+    # Outage alerts
+    outage_alerts = "outage_alerts"
+    region_outage_alerts = "region_outage_alerts"
+
+    # Fallback alerts
+    fallback_reports = "fallback_reports"
+
+    # Virtual Key Events
+    new_virtual_key_created = "new_virtual_key_created"
+    virtual_key_updated = "virtual_key_updated"
+    virtual_key_deleted = "virtual_key_deleted"
+
+    # Team Events
+    new_team_created = "new_team_created"
+    team_updated = "team_updated"
+    team_deleted = "team_deleted"
+
+    # Internal User Events
+    new_internal_user_created = "new_internal_user_created"
+    internal_user_updated = "internal_user_updated"
+    internal_user_deleted = "internal_user_deleted"
