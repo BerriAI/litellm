2d0621ddbe6f795e0cb64534d5f46b2bb5d19ea6
Doug Blank <doug.blank@gmail.com>
2024-10-01 13:35:52 -0400

    WIP

--- litellm/integrations/opik/opik.py
+++ litellm/integrations/opik/opik.py
@@ -1,94 +1,26 @@
-from typing import Any, Dict, Callable
-import litellm
-from litellm.types.utils import ModelResponse
-import datetime
-
 """
 Opik Logger that logs LLM events to an Opik server
 """
 
-BLUE = "\033[94m"
-RED = "\033[91m"
-RESET = "\033[0m"
+from typing import Any, Dict, Callable
+import datetime
 
-def pformat(text: str, level: str="info") -> str:
-    """
-    Format the text with colors.
+import litellm
+from litellm.types.utils import ModelResponse
+from litellm.integrations.custom_logger import CustomLogger
 
-    Args:
-        text: the text to format
-        info: the mode ("input" or "error")
+from .utils import (
+    pformat,
+    get_current_span_id,
+    get_current_trace_id,
+    model_response_to_dict,
+    redact_secrets,
+)
 
-    Returns a formatted string
-    """
-    return "%s%s%s" % (BLUE if level == "info" else RED, text, RESET)
-
-def get_current_span_id(metadata: Dict[str, Any]) -> str:
-    from opik.opik_context import get_current_span_data
-
-    if metadata.get("current_span_data"):
-        current_span_data = metadata.get("current_span_data")
-    else:
-        current_span_data = get_current_span_data()
-
-    if current_span_data:
-        return current_span_data.id
-    else:
-        return None
-
-
-def get_current_trace_id(metadata: Dict[str, Any]) -> str:
-    from opik.opik_context import get_current_trace_data
-
-    if metadata.get("current_trace_data"):
-        current_trace_data = metadata.get("current_trace_data")
-    else:
-        current_trace_data = get_current_trace_data()
-
-    if current_trace_data:
-        return current_trace_data.id
-    else:
-        return None
-
-
-def model_response_to_dict(response_obj: ModelResponse) -> Dict:
-    """
-    Convert the ModelResponse to a dictionary.
-
-    Args:
-        response_obj: the ModelResponse from the model vendor, standardized
-
-    Returns a dictionary
-    """
-    return response_obj.to_dict()
-
-def redact_secrets(item):
-    """
-    Recursively redact sensitive information
-    """
-    if isinstance(item, dict):
-        redacted_dict = {}
-        for key, value in item.items():
-            value = redact_secrets(value)
-            if key == "api_key":
-                value = "***REDACTED***"
-            redacted_dict[key] = value
-        return redacted_dict
-    else:
-        return item
-
-
-class OpikLogger():
+class OpikLogger(CustomLogger):
     """
     Opik Logger for logging events to an Opik Server
     """
-    def __init__(self):
-        try:
-            import opik
-            self.opik = opik
-        except ImportError:
-            self.opik = None
-
     def log_event(
         self,
         kwargs: Dict[str, Any],
