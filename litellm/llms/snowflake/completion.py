704a9789585808d6241cabe5f79bcc6256012cb1
alejandro <alejandro.herrera@snowflake.com>
2024-10-02 00:30:00 -0400

    cleanup to use openai-like handler

--- litellm/llms/snowflake/completion.py
+++ litellm/llms/snowflake/completion.py
@@ -2,19 +2,12 @@
 Handler for Snowflake's Cortex Complete endpoint
 """
 
-import json
-import time
-from typing import Callable, Optional
-
+from typing import Callable
 import httpx
-
-from litellm.llms.OpenAI.openai import OpenAIConfig
-from litellm.llms.snowflake.snowflake_utils import ModelResponseIterator
-from litellm.llms.custom_httpx.http_handler import AsyncHTTPHandler, HTTPHandler
-from litellm.utils import CustomStreamWrapper, ModelResponse, Usage
-
+from litellm.utils import ModelResponse
+from litellm.llms.databricks.chat import DatabricksChatCompletion
+from litellm.types.utils import Choices, Message
 from ..base import BaseLLM
-
 
 
 class SnowflakeError(Exception):
@@ -27,6 +20,7 @@ class SnowflakeError(Exception):
             self.message
         )  # Call the base class constructor with the parameters it needs
 
+
 # makes headers for API call
 def validate_environment(api_key, user_headers):
     if api_key is None:
@@ -35,7 +29,7 @@ def validate_environment(api_key, user_headers):
         )
 
     headers = {
-        "Accept": "application/json,text/stream",
+        "Accept": "text/stream",
         "Content-Type": "application/json",
         "Authorization": f'Snowflake Token="{api_key}"',
     }
@@ -46,159 +40,9 @@ def validate_environment(api_key, user_headers):
 
 
 class SnowflakeTextCompletion(BaseLLM):
+
     def __init__(self) -> None:
         super().__init__()
-
-    def _process_response(
-        self, model_response: ModelResponse, response, encoding, prompt: str, model: str
-    ):
-        ## RESPONSE OBJECT
-        try:
-            completion_response = self._process_snowflake_complete_response(
-                response.content
-            )
-        except:
-            raise SnowflakeError(
-                message=response.text, status_code=response.status_code
-            )
-        if "error" in completion_response:
-            raise SnowflakeError(
-                message=str(completion_response["error"]),
-                status_code=response.status_code,
-            )
-        else:
-            if len(completion_response["completion"]) > 0:
-                model_response.choices[0].message.content = completion_response[
-                    "completion"
-                ]
-
-            model_response.choices[0].finish_reason = completion_response["stop_reason"]
-
-        ## CALCULATING USAGE
-        prompt_tokens = len(encoding.encode(prompt[0]["content"]))
-        completion_tokens = len(encoding.encode(completion_response["completion"]))
-
-        model_response.created = int(time.time())
-        model_response.model = model
-        usage = Usage(
-            prompt_tokens=prompt_tokens,
-            completion_tokens=completion_tokens,
-            total_tokens=prompt_tokens + completion_tokens,
-        )
-
-        setattr(model_response, "usage", usage)
-
-        return model_response
-
-    def _process_snowflake_complete_response(self, data: bytes):
-
-        # Decode the bytes object to a string
-        data_str = data.decode("utf-8")
-
-        # Split the data into individual JSON objects
-        json_objects = data_str.split("\ndata: ")
-
-        # Initialize an empty list to store the JSON objects
-        json_list = []
-
-        # Iterate over each JSON object
-        for obj in json_objects:
-            obj = obj.strip()
-            if obj:
-                # Remove the 'data: ' prefix if it exists
-                if obj.startswith("data: "):
-                    obj = obj[6:]
-                # Load the JSON object into a Python dictionary
-                json_dict = json.loads(str(obj))
-                # Append the JSON dictionary to the list
-                json_list.append(json_dict)
-
-        completion = ""
-        model = ""
-        choices = {}
-        for chunk in json_list:
-
-            model = chunk["model"]
-            choices = chunk["choices"][0]
-
-            if "content" in choices["delta"].keys():
-
-                completion += choices["delta"]["content"]
-
-        processed_response = {
-            "model": model,
-            "completion": completion,
-            "stop_reason": choices["finish_reason"],
-        }
-
-        return ModelResponse(**processed_response)
-
-    async def async_completion(
-        self,
-        model: str,
-        model_response: ModelResponse,
-        api_base: str,
-        logging_obj,
-        encoding,
-        headers: dict,
-        data: dict,
-        client=None,
-    ):
-        if client is None:
-            client = AsyncHTTPHandler(timeout=httpx.Timeout(timeout=600.0, connect=5.0))
-
-        response = await client.post(api_base, headers=headers, data=json.dumps(data))
-
-        if response.status_code != 200:
-            raise SnowflakeError(
-                status_code=response.status_code, message=response.text
-            )
-
-        ## LOGGING
-        logging_obj.post_call(
-            input=data["messages"],
-            api_key=headers.get("Authorization"),
-            original_response=response.text,
-            additional_args={"complete_input_dict": data},
-        )
-
-        response = self._process_response(
-            model_response=model_response,
-            response=response,
-            encoding=encoding,
-            prompt=data["messages"],
-            model=model,
-        )
-        return response
-
-    async def async_streaming(
-        self,
-        model: str,
-        api_base: str,
-        logging_obj,
-        headers: dict,
-        data: Optional[dict],
-        client=None,
-    ):
-        if client is None:
-            client = AsyncHTTPHandler(timeout=httpx.Timeout(timeout=600.0, connect=5.0))
-
-        response = await client.post(api_base, headers=headers, data=json.dumps(data))
-
-        if response.status_code != 200:
-            raise SnowflakeError(
-                status_code=response.status_code, message=response.text
-            )
-
-        completion_stream = response.aiter_lines()
-
-        streamwrapper = CustomStreamWrapper(
-            completion_stream=completion_stream,
-            model=model,
-            custom_llm_provider="snowflake",
-            logging_obj=logging_obj,
-        )
-        return streamwrapper
 
     def completion(
         self,
@@ -219,103 +63,39 @@ class SnowflakeTextCompletion(BaseLLM):
         client=None,
     ):
         headers = validate_environment(api_key, headers)
+        openai_like_chat_completions = DatabricksChatCompletion()
 
-        ## Load Config
-        # config = SnowflakeTextConfig.get_config()
-        config = OpenAIConfig.get_config()
-        for k, v in config.items():
-             if k not in optional_params:
-                 optional_params[k] = v
+        # Non Streaming Mode Currently Not Enabled in Preview
+        streaming = optional_params["stream"] if "stream" in optional_params else False
+        optional_params["stream"] = True
 
-        data = {
-            "model": model,
-            "messages": messages,
-            **optional_params,
-        }
-
-        ## LOGGING
-        logging_obj.pre_call(
-            input=messages[0]['content'],
-            api_key=api_key,
-            additional_args={
-                "complete_input_dict": data,
-                "api_base": api_base,
-                "headers": headers,
-            },
+        response = openai_like_chat_completions.completion(
+            model=model,
+            messages=messages,
+            api_base=api_base,
+            api_key=None,
+            headers=headers,
+            custom_prompt_dict=custom_prompt_dict,
+            model_response=model_response,
+            print_verbose=print_verbose,
+            logging_obj=logging_obj,
+            optional_params=optional_params,
+            acompletion=acompletion,
+            litellm_params=litellm_params,
+            logger_fn=logger_fn,
+            client=client,
+            encoding=encoding,
+            custom_llm_provider="snowflake",
+            custom_endpoint=True,
         )
 
-        ## COMPLETION CALL
-        if "stream" in optional_params and optional_params["stream"] == True:
-            if acompletion == True:
-                return self.async_streaming(
-                    model=model,
-                    api_base=api_base,
-                    logging_obj=logging_obj,
-                    headers=headers,
-                    data=data,
-                    client=None,
-                )
-
-            if client is None:
-                client = HTTPHandler(timeout=httpx.Timeout(timeout=600.0, connect=5.0))
-
-            response = client.post(api_base, headers=headers, data=json.dumps(data))
-
-            if response.status_code != 200:
-                raise SnowflakeError(
-                    status_code=response.status_code, message=response.text
-                )
-
-            completion_stream = ModelResponseIterator(
-                streaming_response=response.iter_lines(), sync_stream=True
-            )
-
-            stream_response = CustomStreamWrapper(
-                completion_stream=completion_stream,
-                model=model,
-                custom_llm_provider="snowflake",
-                logging_obj=logging_obj,
-            )
-            return stream_response
-        elif acompletion == True:
-            return self.async_completion(
-                model=model,
-                model_response=model_response,
-                api_base=api_base,
-                logging_obj=logging_obj,
-                encoding=encoding,
-                headers=headers,
-                data=data,
-                client=client,
-            )
-        else:
-            if client is None:
-                client = HTTPHandler(timeout=httpx.Timeout(timeout=600.0, connect=5.0))
-            response = client.post(api_base, headers=headers, data=json.dumps(data))
-
-            if response.status_code != 200:
-                raise SnowflakeError(
-                    status_code=response.status_code, message=response.text
-                )
-
-            ## LOGGING
-            logging_obj.post_call(
-                input=data["messages"],
-                api_key=api_key,
-                original_response=response.text,
-                additional_args={"complete_input_dict": data},
-            )
-            print_verbose(f"raw model_response: {response.text}")
-
-            response = self._process_response(
-                model_response=model_response,
-                response=response,
-                encoding=encoding,
-                prompt=data["messages"],
-                model=model,
-            )
+        if streaming:
             return response
+        else:
 
-    def embedding(self):
-        # logic for parsing in - calling - parsing out model embedding calls
-        pass
+            completion = ""
+            for part in response:
+                completion += part.choices[0]["delta"]["content"] or ""
+            choices = Choices(message=Message(content=completion))
+
+            return ModelResponse(choices=[choices])
