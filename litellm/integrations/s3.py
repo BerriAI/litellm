bb062ab2c68d0fbcb9739f4fd2a93f91aaacdb9b
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-10-11 19:08:18 +0530

    fix s3 type errors

--- litellm/integrations/s3.py
+++ litellm/integrations/s3.py
@@ -9,12 +9,14 @@ NOTE 2: We create a httpx client with a concurrent limit of 1 to upload to s3. F
 
 import asyncio
 import json
+from datetime import datetime
 from typing import Dict, List, Optional
 
 import litellm
 from litellm._logging import print_verbose, verbose_logger
 from litellm.llms.base_aws_llm import BaseAWSLLM
 from litellm.llms.custom_httpx.http_handler import (
+    _get_httpx_client,
     get_async_httpx_client,
     httpxSpecialProvider,
 )
@@ -135,60 +137,14 @@ class S3Logger(CustomBatchLogger, BaseAWSLLM):
                 f"s3 Logging - Enters logging function for model {kwargs}"
             )
 
-            # construct payload to send to s3
-            # follows the same params as langfuse.py
-            litellm_params = kwargs.get("litellm_params", {})
-            metadata = (
-                litellm_params.get("metadata", {}) or {}
-            )  # if litellm_params['metadata'] == None
-
-            # Clean Metadata before logging - never log raw metadata
-            # the raw metadata can contain circular references which leads to infinite recursion
-            # we clean out all extra litellm metadata params before logging
-            clean_metadata = {}
-            if isinstance(metadata, dict):
-                for key, value in metadata.items():
-                    # clean litellm metadata before logging
-                    if key in [
-                        "headers",
-                        "endpoint",
-                        "caching_groups",
-                        "previous_models",
-                    ]:
-                        continue
-                    else:
-                        clean_metadata[key] = value
-
-            # Ensure everything in the payload is converted to str
-            payload: Optional[StandardLoggingPayload] = kwargs.get(
-                "standard_logging_object", None
+            s3_batch_logging_element = self.create_s3_batch_logging_element(
+                start_time=start_time,
+                standard_logging_payload=kwargs.get("standard_logging_object", None),
+                s3_path=self.s3_path,
             )
 
-            if payload is None:
-                return
-
-            s3_file_name = litellm.utils.get_logging_id(start_time, payload) or ""
-            s3_object_key = (
-                (self.s3_path.rstrip("/") + "/" if self.s3_path else "")
-                + start_time.strftime("%Y-%m-%d")
-                + "/"
-                + s3_file_name
-            )  # we need the s3 key to include the time, so we log cache hits too
-            s3_object_key += ".json"
-
-            s3_object_download_filename = (
-                "time-"
-                + start_time.strftime("%Y-%m-%dT%H-%M-%S-%f")
-                + "_"
-                + payload["id"]
-                + ".json"
-            )
-
-            s3_batch_logging_element = s3BatchLoggingElement(
-                payload=payload,  # type: ignore
-                s3_object_key=s3_object_key,
-                s3_object_download_filename=s3_object_download_filename,
-            )
+            if s3_batch_logging_element is None:
+                raise ValueError("s3_batch_logging_element is None")
 
             verbose_logger.debug(
                 "\ns3 Logger - Logging payload = %s", s3_batch_logging_element
@@ -206,7 +162,35 @@ class S3Logger(CustomBatchLogger, BaseAWSLLM):
             verbose_logger.exception(f"s3 Layer Error - {str(e)}")
             pass
 
-    async def upload_data_to_s3(self, batch_logging_element: s3BatchLoggingElement):
+    def log_success_event(self, kwargs, response_obj, start_time, end_time):
+        """
+        Synchronous logging function to log to s3
+
+        Does not batch logging requests, instantly logs on s3 Bucket
+        """
+        try:
+            s3_batch_logging_element = self.create_s3_batch_logging_element(
+                start_time=start_time,
+                standard_logging_payload=kwargs.get("standard_logging_object", None),
+                s3_path=self.s3_path,
+            )
+
+            if s3_batch_logging_element is None:
+                raise ValueError("s3_batch_logging_element is None")
+
+            verbose_logger.debug(
+                "\ns3 Logger - Logging payload = %s", s3_batch_logging_element
+            )
+
+            # log the element sync httpx client
+            self.upload_data_to_s3(s3_batch_logging_element)
+        except Exception as e:
+            verbose_logger.exception(f"s3 Layer Error - {str(e)}")
+            pass
+
+    async def async_upload_data_to_s3(
+        self, batch_logging_element: s3BatchLoggingElement
+    ):
         try:
             import hashlib
 
@@ -281,4 +265,104 @@ class S3Logger(CustomBatchLogger, BaseAWSLLM):
             return
 
         for payload in self.log_queue:
-            asyncio.create_task(self.upload_data_to_s3(payload))
+            asyncio.create_task(self.async_upload_data_to_s3(payload))
+
+    def create_s3_batch_logging_element(
+        self,
+        start_time: datetime,
+        standard_logging_payload: Optional[StandardLoggingPayload],
+        s3_path: Optional[str],
+    ) -> Optional[s3BatchLoggingElement]:
+        """
+        Helper function to create an s3BatchLoggingElement.
+
+        Args:
+            start_time (datetime): The start time of the logging event.
+            standard_logging_payload (Optional[StandardLoggingPayload]): The payload to be logged.
+            s3_path (Optional[str]): The S3 path prefix.
+
+        Returns:
+            Optional[s3BatchLoggingElement]: The created s3BatchLoggingElement, or None if payload is None.
+        """
+        if standard_logging_payload is None:
+            return None
+
+        s3_file_name = (
+            litellm.utils.get_logging_id(start_time, standard_logging_payload) or ""
+        )
+        s3_object_key = (
+            (s3_path.rstrip("/") + "/" if s3_path else "")
+            + start_time.strftime("%Y-%m-%d")
+            + "/"
+            + s3_file_name
+            + ".json"
+        )
+
+        s3_object_download_filename = f"time-{start_time.strftime('%Y-%m-%dT%H-%M-%S-%f')}_{standard_logging_payload['id']}.json"
+
+        return s3BatchLoggingElement(
+            payload=standard_logging_payload,  # type: ignore
+            s3_object_key=s3_object_key,
+            s3_object_download_filename=s3_object_download_filename,
+        )
+
+    def upload_data_to_s3(self, batch_logging_element: s3BatchLoggingElement):
+        try:
+            import hashlib
+
+            import boto3
+            import requests
+            from botocore.auth import SigV4Auth
+            from botocore.awsrequest import AWSRequest
+            from botocore.credentials import Credentials
+        except ImportError:
+            raise ImportError("Missing boto3 to call bedrock. Run 'pip install boto3'.")
+        try:
+            credentials: Credentials = self.get_credentials(
+                aws_access_key_id=self.s3_aws_access_key_id,
+                aws_secret_access_key=self.s3_aws_secret_access_key,
+                aws_session_token=self.s3_aws_session_token,
+                aws_region_name=self.s3_region_name,
+            )
+
+            # Prepare the URL
+            url = f"https://{self.bucket_name}.s3.{self.s3_region_name}.amazonaws.com/{batch_logging_element.s3_object_key}"
+
+            if self.s3_endpoint_url:
+                url = self.s3_endpoint_url + "/" + batch_logging_element.s3_object_key
+
+            # Convert JSON to string
+            json_string = json.dumps(batch_logging_element.payload)
+
+            # Calculate SHA256 hash of the content
+            content_hash = hashlib.sha256(json_string.encode("utf-8")).hexdigest()
+
+            # Prepare the request
+            headers = {
+                "Content-Type": "application/json",
+                "x-amz-content-sha256": content_hash,
+                "Content-Language": "en",
+                "Content-Disposition": f'inline; filename="{batch_logging_element.s3_object_download_filename}"',
+                "Cache-Control": "private, immutable, max-age=31536000, s-maxage=0",
+            }
+            req = requests.Request("PUT", url, data=json_string, headers=headers)
+            prepped = req.prepare()
+
+            # Sign the request
+            aws_request = AWSRequest(
+                method=prepped.method,
+                url=prepped.url,
+                data=prepped.body,
+                headers=prepped.headers,
+            )
+            SigV4Auth(credentials, "s3", self.s3_region_name).add_auth(aws_request)
+
+            # Prepare the signed headers
+            signed_headers = dict(aws_request.headers.items())
+
+            httpx_client = _get_httpx_client()
+            # Make the request
+            response = httpx_client.put(url, data=json_string, headers=signed_headers)
+            response.raise_for_status()
+        except Exception as e:
+            verbose_logger.exception(f"Error uploading to s3: {str(e)}")
