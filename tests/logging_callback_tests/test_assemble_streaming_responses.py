073f95a8d73cda453926642acc3359c2a836c030
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-10-15 09:50:39 +0530

    test test_assemble_complete_response_from_streaming_chunks_3

--- tests/logging_callback_tests/test_assemble_streaming_responses.py
+++ tests/logging_callback_tests/test_assemble_streaming_responses.py
@@ -132,5 +132,72 @@ def test_assemble_complete_response_from_streaming_chunks_2():
     pass
 
 
-def test_assemble_complete_response_from_streaming_chunks_3():
-    pass
+@pytest.mark.parametrize("is_async", [True, False])
+def test_assemble_complete_response_from_streaming_chunks_3(is_async):
+
+    request_kwargs = {
+        "model": "test_model",
+        "messages": [{"role": "user", "content": "Hello, world!"}],
+    }
+
+    list_streaming_chunks_1 = []
+    list_streaming_chunks_2 = []
+
+    chunk = {
+        "id": "chatcmpl-9mWtyDnikZZoB75DyfUzWUxiiE2Pi",
+        "choices": [
+            litellm.utils.StreamingChoices(
+                delta=litellm.utils.Delta(
+                    content="hello in response",
+                    function_call=None,
+                    role=None,
+                    tool_calls=None,
+                ),
+                index=0,
+                logprobs=None,
+            )
+        ],
+        "created": 1721353246,
+        "model": "gpt-3.5-turbo",
+        "object": "chat.completion.chunk",
+        "system_fingerprint": None,
+        "usage": None,
+    }
+    chunk = litellm.ModelResponse(**chunk, stream=True)
+    complete_streaming_response = _assemble_complete_response_from_streaming_chunks(
+        result=chunk,
+        start_time=datetime.now(),
+        end_time=datetime.now(),
+        request_kwargs=request_kwargs,
+        streaming_chunks=list_streaming_chunks_1,
+        is_async=is_async,
+    )
+
+    # this is the 1st chunk - complete_streaming_response should be None
+
+    print("list_streaming_chunks_1", list_streaming_chunks_1)
+    print("complete_streaming_response", complete_streaming_response)
+    assert complete_streaming_response is None
+    assert len(list_streaming_chunks_1) == 1
+    assert list_streaming_chunks_1[0] == chunk
+    assert len(list_streaming_chunks_2) == 0
+
+    # now add a chunk to the 2nd list
+
+    complete_streaming_response = _assemble_complete_response_from_streaming_chunks(
+        result=chunk,
+        start_time=datetime.now(),
+        end_time=datetime.now(),
+        request_kwargs=request_kwargs,
+        streaming_chunks=list_streaming_chunks_2,
+        is_async=is_async,
+    )
+
+    print("list_streaming_chunks_2", list_streaming_chunks_2)
+    print("complete_streaming_response", complete_streaming_response)
+    assert complete_streaming_response is None
+    assert len(list_streaming_chunks_2) == 1
+    assert list_streaming_chunks_2[0] == chunk
+    assert len(list_streaming_chunks_1) == 1
+
+    # now add a chunk to the 1st list
