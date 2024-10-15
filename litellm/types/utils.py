8705f1af921653d9b6e5534a77fd85c2f51bbc9c
Krish Dholakia <krrishdholakia@gmail.com>
2024-10-14 22:44:00 -0700

    test(router_code_coverage.py): check if all router functions are dire… (#6186)
    * test(router_code_coverage.py): check if all router functions are directly tested
    
    prevent regressions
    
    * docs(configs.md): document all environment variables (#6185)
    
    * docs: make it easier to find anthropic/openai prompt caching doc
    
    * aded codecov yml (#6207)
    
    * fix codecov.yaml
    
    * run ci/cd again
    
    * (refactor) caching use LLMCachingHandler for async_get_cache and set_cache  (#6208)
    
    * use folder for caching
    
    * fix importing caching
    
    * fix clickhouse pyright
    
    * fix linting
    
    * fix correctly pass kwargs and args
    
    * fix test case for embedding
    
    * fix linting
    
    * fix embedding caching logic
    
    * fix refactor handle utils.py
    
    * fix test_embedding_caching_azure_individual_items_reordered
    
    * (feat) prometheus have well defined latency buckets (#6211)
    
    * fix prometheus have well defined latency buckets
    
    * use a well define latency bucket
    
    * use types file for prometheus logging
    
    * add test for LATENCY_BUCKETS
    
    * fix prom testing
    
    * fix config.yml
    
    * (refactor caching) use LLMCachingHandler for caching streaming responses  (#6210)
    
    * use folder for caching
    
    * fix importing caching
    
    * fix clickhouse pyright
    
    * fix linting
    
    * fix correctly pass kwargs and args
    
    * fix test case for embedding
    
    * fix linting
    
    * fix embedding caching logic
    
    * fix refactor handle utils.py
    
    * refactor async set stream cache
    
    * fix linting
    
    * bump (#6187)
    
    * update code cov yaml
    
    * fix config.yml
    
    * add caching component to code cov
    
    * fix config.yml ci/cd
    
    * add coverage for proxy auth
    
    * (refactor caching) use common `_retrieve_from_cache` helper  (#6212)
    
    * use folder for caching
    
    * fix importing caching
    
    * fix clickhouse pyright
    
    * fix linting
    
    * fix correctly pass kwargs and args
    
    * fix test case for embedding
    
    * fix linting
    
    * fix embedding caching logic
    
    * fix refactor handle utils.py
    
    * refactor async set stream cache
    
    * fix linting
    
    * refactor - use _retrieve_from_cache
    
    * refactor use _convert_cached_result_to_model_response
    
    * fix linting errors
    
    * bump: version 1.49.2 → 1.49.3
    
    * fix code cov components
    
    * test(test_router_helpers.py): add router component unit tests
    
    * test: add additional router tests
    
    * test: add more router testing
    
    * test: add more router testing + more mock functions
    
    * ci(router_code_coverage.py): fix check
    
    ---------
    
    Co-authored-by: Ishaan Jaff <ishaanjaffer0324@gmail.com>
    Co-authored-by: yujonglee <yujonglee.dev@gmail.com>

--- litellm/types/utils.py
+++ litellm/types/utils.py
@@ -11,6 +11,12 @@ from openai.types.completion_usage import (
     CompletionUsage,
     PromptTokensDetails,
 )
+from openai.types.moderation import (
+    Categories,
+    CategoryAppliedInputTypes,
+    CategoryScores,
+)
+from openai.types.moderation_create_response import Moderation, ModerationCreateResponse
 from pydantic import BaseModel, ConfigDict, PrivateAttr
 from typing_extensions import Callable, Dict, Required, TypedDict, override
 
@@ -20,6 +26,7 @@ from .llms.openai import (
     ChatCompletionUsageBlock,
     OpenAIChatCompletionChunk,
 )
+from .rerank import RerankResponse
 
 
 def _generate_id():  # private helper function
@@ -811,7 +818,7 @@ class EmbeddingResponse(OpenAIObject):
         model: Optional[str] = None,
         usage: Optional[Usage] = None,
         response_ms=None,
-        data: Optional[List] = None,
+        data: Optional[Union[List, List[Embedding]]] = None,
         hidden_params=None,
         _response_headers=None,
         **params,
