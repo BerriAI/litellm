f3926f667284603c46a055bbc62e8d58f54be6b8
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-10-16 17:10:40 +0530

    add testing for test_should_run_cooldown_logic

--- tests/local_testing/test_router_cooldown_utils.py
+++ tests/local_testing/test_router_cooldown_utils.py
@@ -14,7 +14,15 @@ from dotenv import load_dotenv
 from unittest.mock import AsyncMock, MagicMock
 from litellm.integrations.prometheus import PrometheusLogger
 from litellm.router_utils.cooldown_callbacks import router_cooldown_event_callback
-
+from litellm.router_utils.cooldown_handlers import (
+    _should_run_cooldown_logic,
+    _should_cooldown_deployment,
+    cast_exception_status_to_int,
+)
+from litellm.router_utils.router_callbacks.track_deployment_metrics import (
+    increment_deployment_failures_for_current_minute,
+    increment_deployment_successes_for_current_minute,
+)
 
 load_dotenv()
 
@@ -158,3 +166,115 @@ async def test_router_cooldown_event_callback_no_deployment():
 
     # Assert that the router's get_deployment method was called
     mock_router.get_deployment.assert_called_once_with(model_id="test-deployment")
+
+
+@pytest.fixture
+def testing_litellm_router():
+    return Router(
+        model_list=[
+            {
+                "model_name": "gpt-3.5-turbo",
+                "litellm_params": {"model": "gpt-3.5-turbo"},
+                "model_id": "test_deployment",
+            },
+        ]
+    )
+
+
+def test_should_run_cooldown_logic(testing_litellm_router):
+    testing_litellm_router.disable_cooldowns = True
+    # don't run cooldown logic if disable_cooldowns is True
+    assert (
+        _should_run_cooldown_logic(
+            testing_litellm_router, "test_deployment", 500, Exception("Test")
+        )
+        is False
+    )
+
+    # don't cooldown if deployment is None
+    testing_litellm_router.disable_cooldowns = False
+    assert (
+        _should_run_cooldown_logic(testing_litellm_router, None, 500, Exception("Test"))
+        is False
+    )
+
+    # don't cooldown if it's a provider default deployment
+    testing_litellm_router.provider_default_deployment_ids = ["test_deployment"]
+    assert (
+        _should_run_cooldown_logic(
+            testing_litellm_router, "test_deployment", 500, Exception("Test")
+        )
+        is False
+    )
+
+
+@pytest.mark.asyncio
+async def test_should_cooldown_deployment(testing_litellm_router):
+    """
+    Test the _should_cooldown_deployment function
+    """
+    # Test 429 error (rate limit) -> always cooldown a deployment returning 429s
+    assert (
+        _should_cooldown_deployment(
+            testing_litellm_router, "test_deployment", 429, Exception("Rate limit")
+        )
+        is True
+    )
+
+    # cooldown a deployment if it fails 60% of requests in 1 minute
+    # threshold is 50%
+    for _ in range(60):
+        increment_deployment_failures_for_current_minute(
+            litellm_router_instance=testing_litellm_router,
+            deployment_id="test_deployment",
+        )
+    for _ in range(40):
+        increment_deployment_successes_for_current_minute(
+            litellm_router_instance=testing_litellm_router,
+            deployment_id="test_deployment",
+        )
+    await asyncio.sleep(0.5)
+    assert (
+        _should_cooldown_deployment(
+            testing_litellm_router, "test_deployment", 500, Exception("Test")
+        )
+        is True
+    )
+
+    # don't cooldown a deployment if it fails around 10% of requests in 1 minute
+    for _ in range(10):
+        increment_deployment_failures_for_current_minute(
+            litellm_router_instance=testing_litellm_router,
+            deployment_id="test_deployment-1",
+        )
+    for _ in range(90):
+        increment_deployment_successes_for_current_minute(
+            litellm_router_instance=testing_litellm_router,
+            deployment_id="test_deployment-1",
+        )
+    await asyncio.sleep(0.5)
+    assert (
+        _should_cooldown_deployment(
+            testing_litellm_router, "test_deployment-1", 500, Exception("Test")
+        )
+        is False
+    )
+
+    # Test authentication error
+    assert (
+        _should_cooldown_deployment(
+            testing_litellm_router,
+            "test_deployment-2",
+            401,
+            litellm.exceptions.AuthenticationError(
+                "Auth failed", "openai", "gpt-3.5-turbo"
+            ),
+        )
+        is True
+    )
+
+
+def test_cast_exception_status_to_int():
+    assert cast_exception_status_to_int(200) == 200
+    assert cast_exception_status_to_int("404") == 404
+    assert cast_exception_status_to_int("invalid") == 500
