c104057663180f078215c150371f9a27767c4068
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-10-19 17:36:24 +0530

    refactor convert_dict_to_chat_completion_response

--- litellm/litellm_core_utils/llm_response_utils/convert_dict_to_chat_completion_response.py
+++ litellm/litellm_core_utils/llm_response_utils/convert_dict_to_chat_completion_response.py
@@ -9,13 +9,14 @@ from litellm.types.utils import (
     Choices,
     Message,
     ModelResponse,
+    StreamingChoices,
 )
 
 from .convert_dict_to_streaming_response import convert_to_streaming_response
 from .handle_parallel_tool_calls import _handle_invalid_parallel_tool_calls
 
 
-def convert_dict_to_chat_completion_response(  # noqa: PLR0915
+def convert_dict_to_chat_completion_response(
     model_response_object: Optional[ModelResponse],
     response_object: Optional[Dict],
     stream: bool,
@@ -31,12 +32,78 @@ def convert_dict_to_chat_completion_response(  # noqa: PLR0915
     if stream is True:
         # for returning cached responses, we need to yield a generator
         return convert_to_streaming_response(response_object=response_object)
-    choice_list = []
 
     assert response_object["choices"] is not None and isinstance(
         response_object["choices"], Iterable
     )
+    choice_list: List[Union[Choices, StreamingChoices]] = _process_choices_in_response(
+        response_object=response_object,
+        convert_tool_call_to_json_mode=convert_tool_call_to_json_mode,
+    )
+    model_response_object.choices = choice_list
 
+    if "usage" in response_object and response_object["usage"] is not None:
+        usage_object = litellm.Usage(**response_object["usage"])
+        setattr(model_response_object, "usage", usage_object)
+    if "created" in response_object:
+        model_response_object.created = response_object["created"] or int(time.time())
+
+    if "id" in response_object:
+        model_response_object.id = response_object["id"] or str(uuid.uuid4())
+
+    if "system_fingerprint" in response_object:
+        model_response_object.system_fingerprint = response_object["system_fingerprint"]
+
+    if "model" in response_object:
+        if model_response_object.model is None:
+            model_response_object.model = response_object["model"]
+        elif (
+            "/" in model_response_object.model and response_object["model"] is not None
+        ):
+            openai_compatible_provider = model_response_object.model.split("/")[0]
+            model_response_object.model = (
+                openai_compatible_provider + "/" + response_object["model"]
+            )
+
+    if start_time is not None and end_time is not None:
+        if isinstance(start_time, type(end_time)):
+            model_response_object._response_ms = (  # type: ignore
+                end_time - start_time
+            ).total_seconds() * 1000
+
+    if hidden_params is not None:
+        if model_response_object._hidden_params is None:
+            model_response_object._hidden_params = {}
+        model_response_object._hidden_params.update(hidden_params)
+
+    if _response_headers is not None:
+        model_response_object._response_headers = _response_headers
+
+    special_keys = list(litellm.ModelResponse.model_fields.keys())
+    special_keys.append("usage")
+    for k, v in response_object.items():
+        if k not in special_keys:
+            setattr(model_response_object, k, v)
+
+    return model_response_object
+
+
+def _process_choices_in_response(
+    response_object: Dict,
+    convert_tool_call_to_json_mode: Optional[bool],
+) -> List[Union[Choices, StreamingChoices]]:
+    """
+    Process the choices in the response object.
+
+    Args:
+        response_object (Dict): The API response object to process.
+        convert_tool_call_to_json_mode (Optional[bool]): Whether to convert tool calls to JSON mode.
+
+    Returns:
+        List[Choices]: The processed choices.
+    """
+
+    choice_list: List[Union[Choices, StreamingChoices]] = []
     for idx, choice in enumerate(response_object["choices"]):
         ## HANDLE JSON MODE - anthropic returns single function call]
         tool_calls = choice["message"].get("tool_calls", None)
@@ -86,49 +153,4 @@ def convert_dict_to_chat_completion_response(  # noqa: PLR0915
             enhancements=enhancements,
         )
         choice_list.append(choice)
-    model_response_object.choices = choice_list
-
-    if "usage" in response_object and response_object["usage"] is not None:
-        usage_object = litellm.Usage(**response_object["usage"])
-        setattr(model_response_object, "usage", usage_object)
-    if "created" in response_object:
-        model_response_object.created = response_object["created"] or int(time.time())
-
-    if "id" in response_object:
-        model_response_object.id = response_object["id"] or str(uuid.uuid4())
-
-    if "system_fingerprint" in response_object:
-        model_response_object.system_fingerprint = response_object["system_fingerprint"]
-
-    if "model" in response_object:
-        if model_response_object.model is None:
-            model_response_object.model = response_object["model"]
-        elif (
-            "/" in model_response_object.model and response_object["model"] is not None
-        ):
-            openai_compatible_provider = model_response_object.model.split("/")[0]
-            model_response_object.model = (
-                openai_compatible_provider + "/" + response_object["model"]
-            )
-
-    if start_time is not None and end_time is not None:
-        if isinstance(start_time, type(end_time)):
-            model_response_object._response_ms = (  # type: ignore
-                end_time - start_time
-            ).total_seconds() * 1000
-
-    if hidden_params is not None:
-        if model_response_object._hidden_params is None:
-            model_response_object._hidden_params = {}
-        model_response_object._hidden_params.update(hidden_params)
-
-    if _response_headers is not None:
-        model_response_object._response_headers = _response_headers
-
-    special_keys = list(litellm.ModelResponse.model_fields.keys())
-    special_keys.append("usage")
-    for k, v in response_object.items():
-        if k not in special_keys:
-            setattr(model_response_object, k, v)
-
-    return model_response_object
+    return choice_list
