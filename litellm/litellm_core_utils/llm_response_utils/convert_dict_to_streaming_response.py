237848a2c358f248a4a818c0302d3f389202285f
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-10-19 22:35:11 +0530

    refactor convert_to_streaming_response_async

--- litellm/litellm_core_utils/llm_response_utils/convert_dict_to_streaming_response.py
+++ litellm/litellm_core_utils/llm_response_utils/convert_dict_to_streaming_response.py
@@ -1,5 +1,5 @@
 import asyncio
-from typing import Optional
+from typing import AsyncGenerator, Generator, Optional
 
 from litellm.types.utils import (
     ChatCompletionDeltaToolCall,
@@ -10,7 +10,9 @@ from litellm.types.utils import (
 )
 
 
-async def convert_to_streaming_response_async(response_object: Optional[dict] = None):
+async def convert_to_streaming_response_async(
+    response_object: Optional[dict] = None,
+) -> AsyncGenerator:
     """
     Asynchronously converts a response object to a streaming response.
 
@@ -25,6 +27,29 @@ async def convert_to_streaming_response_async(response_object: Optional[dict] = 
 
     Returns:
         None
+    """
+    model_response_object = _get_streaming_respons_object(response_object)
+    yield model_response_object
+    await asyncio.sleep(0)
+
+
+def convert_to_streaming_response(response_object: Optional[dict] = None) -> Generator:
+    # used for yielding Cache hits when stream == True
+    model_response_object = _get_streaming_respons_object(response_object)
+    yield model_response_object
+
+
+def _get_streaming_respons_object(
+    response_object: Optional[dict] = None,
+) -> ModelResponse:
+    """
+    Helper function to get the streaming `litellm.ModelResponse` object
+
+    Args:
+        response_object (Optional[dict]): The Dictionary response object to be converted. Defaults to None.
+
+    Raises:
+        Exception: If the response object is None.
     """
     if response_object is None:
         raise Exception("Error in response object format")
@@ -92,56 +117,4 @@ async def convert_to_streaming_response_async(response_object: Optional[dict] = 
     if "model" in response_object:
         model_response_object.model = response_object["model"]
 
-    yield model_response_object
-    await asyncio.sleep(0)
-
-
-def convert_to_streaming_response(response_object: Optional[dict] = None):
-    # used for yielding Cache hits when stream == True
-    if response_object is None:
-        raise Exception("Error in response object format")
-
-    model_response_object = ModelResponse(stream=True)
-    choice_list = []
-    for idx, choice in enumerate(response_object["choices"]):
-        delta = Delta(
-            content=choice["message"].get("content", None),
-            role=choice["message"]["role"],
-            function_call=choice["message"].get("function_call", None),
-            tool_calls=choice["message"].get("tool_calls", None),
-        )
-        finish_reason = choice.get("finish_reason", None)
-        if finish_reason is None:
-            # gpt-4 vision can return 'finish_reason' or 'finish_details'
-            finish_reason = choice.get("finish_details")
-        logprobs = choice.get("logprobs", None)
-        enhancements = choice.get("enhancements", None)
-        choice = StreamingChoices(
-            finish_reason=finish_reason,
-            index=idx,
-            delta=delta,
-            logprobs=logprobs,
-            enhancements=enhancements,
-        )
-
-        choice_list.append(choice)
-    model_response_object.choices = choice_list
-
-    if "usage" in response_object and response_object["usage"] is not None:
-        setattr(model_response_object, "usage", Usage())
-        model_response_object.usage.completion_tokens = response_object["usage"].get("completion_tokens", 0)  # type: ignore
-        model_response_object.usage.prompt_tokens = response_object["usage"].get("prompt_tokens", 0)  # type: ignore
-        model_response_object.usage.total_tokens = response_object["usage"].get("total_tokens", 0)  # type: ignore
-
-    if "id" in response_object:
-        model_response_object.id = response_object["id"]
-
-    if "created" in response_object:
-        model_response_object.created = response_object["created"]
-
-    if "system_fingerprint" in response_object:
-        model_response_object.system_fingerprint = response_object["system_fingerprint"]
-
-    if "model" in response_object:
-        model_response_object.model = response_object["model"]
-    yield model_response_object
+    return model_response_object
