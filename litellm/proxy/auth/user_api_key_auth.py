1908419059fe15d14f45c0ef036fb1a95fa91c68
Krrish Dholakia <krrishdholakia@gmail.com>
2024-10-19 22:17:34 -0700

    fix(user_api_key_auth.py): fix exception string

--- litellm/proxy/auth/user_api_key_auth.py
+++ litellm/proxy/auth/user_api_key_auth.py
@@ -147,9 +147,7 @@ def _is_api_route_allowed(
     _user_role = _get_user_role(user_obj=user_obj)
 
     if valid_token is None:
-        raise HTTPException(
-            status_code=401, detail="Unable to retrieve key from DB. valid_token=None."
-        )
+        raise HTTPException(status_code=401, detail="Invalid proxy server token passed")
 
     if not _is_user_proxy_admin(user_obj=user_obj):  # if non-admin
         non_proxy_admin_allowed_routes_check(
