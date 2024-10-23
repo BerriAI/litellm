7af1a4e2e35b5c418930016a7fe79f03709d1daa
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-10-23 10:08:35 +0530

    fix linting

--- litellm/integrations/langfuse/langfuse_handler.py
+++ litellm/integrations/langfuse/langfuse_handler.py
@@ -87,7 +87,7 @@ class LangFuseHandler:
         if globalLangfuseLogger is not None:
             return globalLangfuseLogger
 
-        credentials_dict = (
+        credentials_dict: Dict[str, Any] = (
             {}
         )  # the global langfuse logger uses Environment Variables, there are no dynamic credentials
         globalLangfuseLogger = in_memory_dynamic_logger_cache.get_cache(
