50d2824bf15f05f609c1294fc895169408ae1c9d
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-10-25 16:47:35 +0400

    fix StandardLoggingMetadata with user_api_key_org_id

--- litellm/litellm_core_utils/litellm_logging.py
+++ litellm/litellm_core_utils/litellm_logging.py
@@ -2515,6 +2515,7 @@ class StandardLoggingPayloadSetup:
             user_api_key_hash=None,
             user_api_key_alias=None,
             user_api_key_team_id=None,
+            user_api_key_org_id=None,
             user_api_key_user_id=None,
             user_api_key_team_alias=None,
             spend_logs_metadata=None,
