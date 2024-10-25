c991a42b88732fefbf92eda4d044aeb8ea5b379e
Krish Dholakia <krrishdholakia@gmail.com>
2024-10-24 22:03:16 -0700

    feat(litellm_pre_call_utils.py): support 'add_user_information_to_llm… (#6390)
    * feat(litellm_pre_call_utils.py): support 'add_user_information_to_llm_headers' param
    
    enables passing user info to backend llm (user request for custom vllm server)
    
    * fix(litellm_logging.py): fix linting error

--- litellm/__init__.py
+++ litellm/__init__.py
@@ -80,6 +80,9 @@ turn_off_message_logging: Optional[bool] = False
 log_raw_request_response: bool = False
 redact_messages_in_exceptions: Optional[bool] = False
 redact_user_api_key_info: Optional[bool] = False
+add_user_information_to_llm_headers: Optional[bool] = (
+    None  # adds user_id, team_id, token hash (params from StandardLoggingMetadata) to request headers
+)
 store_audit_logs = False  # Enterprise feature, allow users to see audit logs
 ## end of callbacks #############
 
