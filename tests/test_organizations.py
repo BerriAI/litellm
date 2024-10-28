f3160fc9043ede1042655014cd53002c77b872a1
superpoussin22 <vincent.nadal@orange.fr>
2024-10-28 18:19:43 +0100

    Update test_organizations.py

--- tests/test_organizations.py
+++ tests/test_organizations.py
@@ -29,6 +29,22 @@ async def new_organization(session, i, organization_alias, max_budget=None):
 
         return await response.json()
 
+async def list_organization(session, i):
+    url = "http://0.0.0.0:4000/organization/list"
+    headers = {"Authorization": "Bearer sk-1234", "Content-Type": "application/json"}
+
+    async with session.post(url, headers=headers) as response:
+        status = response.status
+        response_json = await response.json()
+
+        print(f"Response {i} (Status code: {status}):")
+        print(response_json)
+        print()
+
+        if status != 200:
+            raise Exception(f"Request {i} did not return a 200 status code: {status}")
+
+        return await response.json()
 
 @pytest.mark.asyncio
 async def test_organization_new():
@@ -44,3 +60,15 @@ async def test_organization_new():
             for i in range(1, 20)
         ]
         await asyncio.gather(*tasks)
+
+@pytest.mark.asyncio
+async def test_organization_list():
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            status = response.status
+            response_json = await response.json()
+
+            print(response_json)
+
+            if status != 200:
+                raise Exception(f"Request did not return a 200 status code: {status}")
