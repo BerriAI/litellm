030ece8c3fc0216dc39fd915e551e04ecdc1e2b0
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-10-28 22:01:32 +0530

    (Feat) New Logging integration - add Datadog LLM Observability support  (#6449)
    * add type for dd llm obs request ob
    
    * working dd llm obs
    
    * datadog use well defined type
    
    * clean up
    
    * unit test test_create_llm_obs_payload
    
    * fix linting
    
    * add datadog_llm_observability
    
    * add datadog_llm_observability
    
    * docs DD LLM obs
    
    * run testing again
    
    * document DD_ENV
    
    * test_create_llm_obs_payload

---
+++ litellm/types/integrations/datadog_llm_obs.py
@@ -0,0 +1,52 @@
+"""
+Payloads for Datadog LLM Observability Service (LLMObs)
+
+API Reference: https://docs.datadoghq.com/llm_observability/setup/api/?tab=example#api-standards
+"""
+
+from typing import Any, List, Literal, Optional, TypedDict
+
+
+class InputMeta(TypedDict):
+    messages: List[Any]
+
+
+class OutputMeta(TypedDict):
+    messages: List[Any]
+
+
+class Meta(TypedDict):
+    # The span kind: "agent", "workflow", "llm", "tool", "task", "embedding", or "retrieval".
+    kind: Literal["llm", "tool", "task", "embedding", "retrieval"]
+    input: InputMeta  # The span’s input information.
+    output: OutputMeta  # The span’s output information.
+
+
+class LLMMetrics(TypedDict, total=False):
+    input_tokens: float
+    output_tokens: float
+    total_tokens: float
+    time_to_first_token: float
+    time_per_output_token: float
+
+
+class LLMObsPayload(TypedDict):
+    parent_id: str
+    trace_id: str
+    span_id: str
+    name: str
+    meta: Meta
+    start_ns: int
+    duration: int
+    metrics: LLMMetrics
+
+
+class DDSpanAttributes(TypedDict):
+    ml_app: str
+    tags: List[str]
+    spans: List[LLMObsPayload]
+
+
+class DDIntakePayload(TypedDict):
+    type: str
+    attributes: DDSpanAttributes
