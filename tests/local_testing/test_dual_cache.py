1f4b39e71eafa7cd18606b5eb67047c858e9d0bc
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-10-29 14:46:29 +0530

    fix batch_get_cache

--- tests/local_testing/test_dual_cache.py
+++ tests/local_testing/test_dual_cache.py
@@ -158,7 +158,7 @@ async def test_dual_cache_batch_operations(is_async):
         if is_async:
             results = await dual_cache.async_batch_get_cache(test_keys)
         else:
-            results = dual_cache.batch_get_cache(test_keys)
+            results = dual_cache.batch_get_cache(test_keys, parent_otel_span=None)
 
         assert results == test_values
         mock_redis_get.assert_not_called()
