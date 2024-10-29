4fa2304fa356f5d16ce563957475b36019d4e46f
ebisuke <fungi@rect.sakura.ne.jp>
2024-10-29 13:55:36 +0900

    add: tests for tool calls with/without args in Gemini

--- tests/local_testing/test_gemini_no_arg_function_call.py
+++ tests/local_testing/test_gemini_no_arg_function_call.py
@@ -81,3 +81,65 @@ def test_gemini_function_call_without_args():
     ]
     r = litellm.completion(messages=messages, model="gemini/gemini-1.5-flash-002")
     assert len(r.choices) > 0
+def test_gemini_function_call_without_args_enabled_tool_calls():
+    messages=[
+        {
+            "role":"user",
+            "content":"invoke tool call"
+        }
+    ]
+    tools=[
+        {
+            "type": "function",
+            "function": {
+                "name": "test",
+                "description": "invoke test",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                    },
+                    "required": []
+                }
+            }
+        },
+    ]
+    r = litellm.completion(messages=messages, model="gemini/gemini-1.5-flash-002",tools=tools,temperature=0.0,tool_choice="required")
+
+    assert len(r.choices) > 0,"choices not found"
+    assert len(r.choices[0].message.tool_calls) > 0, "tool calls not found"
+    assert r.choices[0].message.tool_calls[0].function.name=="test","tool call name is not 'test'"
+    assert r.choices[0].message.tool_calls[0].function.arguments == "{}" ,"arguments is not empty"
+def test_gemini_function_call_with_args_enabled_tool_calls():
+    messages=[
+        {
+            "role":"user",
+            "content":"invoke tool call with arg1(42)"
+        }
+    ]
+    tools=[
+        {
+            "type": "function",
+            "function": {
+                "name": "test2",
+                "description": "invoke test",
+                "parameters": {
+                    "type": "object",
+
+                    "properties": {
+                        "arg1": {
+                            "type": "string",
+                            "description": "arg1"
+                        },
+                    },
+
+                    "required": ["arg1"]
+                }
+            }
+        },
+    ]
+    r = litellm.completion(messages=messages, model="gemini/gemini-1.5-flash-002",tools=tools,temperature=0.0,tool_choice="required")
+
+    assert len(r.choices) > 0,"choices not found"
+    assert len(r.choices[0].message.tool_calls) > 0, "tool calls not found"
+    assert r.choices[0].message.tool_calls[0].function.name=="test2","tool call name is not 'test'"
+    assert r.choices[0].message.tool_calls[0].function.arguments == '{"arg1": "42"}' ,"arguments is not match"
