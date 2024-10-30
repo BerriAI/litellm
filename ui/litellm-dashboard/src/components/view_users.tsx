1c07403f60784d6eea364cd37b10db1e09c367db
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-10-30 15:47:43 +0530

    fix view internal user key count

--- ui/litellm-dashboard/src/components/view_users.tsx
+++ ui/litellm-dashboard/src/components/view_users.tsx
@@ -271,23 +271,10 @@ const ViewUserDashboard: React.FC<ViewUserDashboardProps> = ({
                         </TableCell>
                         <TableCell>
                           <Grid numItems={2}>
-                            {user && user.key_aliases ? (
-                              user.key_aliases.filter(
-                                (key: any) => key !== null
-                              ).length > 0 ? (
-                                <Badge size={"xs"} color={"indigo"}>
-                                  {
-                                    user.key_aliases.filter(
-                                      (key: any) => key !== null
-                                    ).length
-                                  }
-                                  &nbsp;Keys
-                                </Badge>
-                              ) : (
-                                <Badge size={"xs"} color={"gray"}>
-                                  No Keys
-                                </Badge>
-                              )
+                            {user.key_count > 0 ? (
+                              <Badge size={"xs"} color={"indigo"}>
+                                {user.key_count} Keys
+                              </Badge>
                             ) : (
                               <Badge size={"xs"} color={"gray"}>
                                 No Keys
