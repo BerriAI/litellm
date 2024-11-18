add0cf7da921db6b4e739d08f9279db1b786178c
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-11-17 18:17:14 -0800

    vertex_ai/codestral@2405 is very unstable - handle their instability in our tests

--- tests/local_testing/test_amazing_vertex_completion.py
+++ tests/local_testing/test_amazing_vertex_completion.py
@@ -995,6 +995,8 @@ async def test_partner_models_httpx(model, sync_mode):
         pass
     except litellm.APIConnectionError as e:
         pass
+    except litellm.ServiceUnavailableError as e:
+        pass
     except Exception as e:
         if "429 Quota exceeded" in str(e):
             pass
