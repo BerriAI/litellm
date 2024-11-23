3ebc685f9a7d60d6d79d164f6427546c0de47935
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-11-23 08:34:55 -0800

    fix playwright e2e ui test

--- tests/proxy_admin_ui_tests/playwright.config.ts
+++ tests/proxy_admin_ui_tests/playwright.config.ts
@@ -13,6 +13,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './e2e_ui_tests',
+  testIgnore: '**/tests/pass_through_tests/**',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
