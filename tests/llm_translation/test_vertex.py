05b5a21014048b8f71a428d2722b827fcdf17ba7
Krrish Dholakia <krrishdholakia@gmail.com>
2024-11-25 21:15:19 +0530

    fix(gemini/): fix image_url handling for gemini
    Fixes https://github.com/BerriAI/litellm/issues/6897

--- tests/llm_translation/test_vertex.py
+++ tests/llm_translation/test_vertex.py
@@ -1298,20 +1298,3 @@ def test_vertex_embedding_url(model, expected_url):
 
     assert url == expected_url
     assert endpoint == "predict"
-
-
-from base_llm_unit_tests import BaseLLMChatTest
-
-
-class TestVertexGemini(BaseLLMChatTest):
-    def get_base_completion_call_args(self) -> dict:
-        return {"model": "gemini/gemini-1.5-flash"}
-
-    def test_tool_call_no_arguments(self, tool_call_no_arguments):
-        """Test that tool calls with no arguments is translated correctly. Relevant issue: https://github.com/BerriAI/litellm/issues/6833"""
-        from litellm.llms.prompt_templates.factory import (
-            convert_to_gemini_tool_call_invoke,
-        )
-
-        result = convert_to_gemini_tool_call_invoke(tool_call_no_arguments)
-        print(result)
