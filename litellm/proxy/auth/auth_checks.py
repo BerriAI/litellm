a4491658e3e04fcf00e492b62ce6ae5065487dfe
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-11-25 15:50:58 -0800

    add "openai/*" in allowed models check

--- litellm/proxy/auth/auth_checks.py
+++ litellm/proxy/auth/auth_checks.py
@@ -908,6 +908,9 @@ def _model_is_within_list_of_allowed_models(
         return True
     if "all-proxy-models" in allowed_models:
         return True
+    # Note: This is here to maintain backwards compatibility. This Used to be in our code and removing could impact existing customer code
+    if "openai/*" in allowed_models:
+        return True
     if model in allowed_models:
         return True
     if _model_matches_patterns(model=model, allowed_models=allowed_models) is True:
