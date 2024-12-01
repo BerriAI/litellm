a2351fcb595b75e36a4aedf2ba396fadc0cca92e
Krish Dholakia <krrishdholakia@gmail.com>
2024-12-01 05:24:11 -0800

    LiteLLM Minor Fixes & Improvements (11/29/2024)  (#6965)
    * fix(factory.py): ensure tool call converts image url
    
    Fixes https://github.com/BerriAI/litellm/issues/6953
    
    * fix(transformation.py): support mp4 + pdf url's for vertex ai
    
    Fixes https://github.com/BerriAI/litellm/issues/6936
    
    * fix(http_handler.py): mask gemini api key in error logs
    
    Fixes https://github.com/BerriAI/litellm/issues/6963
    
    * docs(prometheus.md): update prometheus FAQs
    
    * feat(auth_checks.py): ensure specific model access > wildcard model access
    
    if wildcard model is in access group, but specific model is not - deny access
    
    * fix(auth_checks.py): handle auth checks for team based model access groups
    
    handles scenario where model access group used for wildcard models
    
    * fix(internal_user_endpoints.py): support adding guardrails on `/user/update`
    
    Fixes https://github.com/BerriAI/litellm/issues/6942
    
    * fix(key_management_endpoints.py): fix prepare_metadata_fields helper
    
    * fix: fix tests
    
    * build(requirements.txt): bump openai dep version
    
    fixes proxies argument
    
    * test: fix tests
    
    * fix(http_handler.py): fix error message masking
    
    * fix(bedrock_guardrails.py): pass in prepped data
    
    * test: fix test
    
    * test: fix nvidia nim test
    
    * fix(http_handler.py): return original response headers
    
    * fix: revert maskedhttpstatuserror
    
    * test: update tests
    
    * test: cleanup test
    
    * fix(key_management_endpoints.py): fix metadata field update logic
    
    * fix(key_management_endpoints.py): maintain initial order of guardrails in key update
    
    * fix(key_management_endpoints.py): handle prepare metadata
    
    * fix: fix linting errors
    
    * fix: fix linting errors
    
    * fix: fix linting errors
    
    * fix: fix key management errors
    
    * fix(key_management_endpoints.py): update metadata
    
    * test: update test
    
    * refactor: add more debug statements
    
    * test: skip flaky test
    
    * test: fix test
    
    * fix: fix test
    
    * fix: fix update metadata logic
    
    * fix: fix test
    
    * ci(config.yml): change db url for e2e ui testing

--- litellm/proxy/management_endpoints/internal_user_endpoints.py
+++ litellm/proxy/management_endpoints/internal_user_endpoints.py
@@ -32,6 +32,7 @@ from litellm.proxy.auth.user_api_key_auth import user_api_key_auth
 from litellm.proxy.management_endpoints.key_management_endpoints import (
     duration_in_seconds,
     generate_key_helper_fn,
+    prepare_metadata_fields,
 )
 from litellm.proxy.management_helpers.utils import (
     add_new_member,
@@ -42,7 +43,7 @@ from litellm.proxy.utils import handle_exception_on_proxy
 router = APIRouter()
 
 
-def _update_internal_user_params(data_json: dict, data: NewUserRequest) -> dict:
+def _update_internal_new_user_params(data_json: dict, data: NewUserRequest) -> dict:
     if "user_id" in data_json and data_json["user_id"] is None:
         data_json["user_id"] = str(uuid.uuid4())
     auto_create_key = data_json.pop("auto_create_key", True)
@@ -145,7 +146,7 @@ async def new_user(
     from litellm.proxy.proxy_server import general_settings, proxy_logging_obj
 
     data_json = data.json()  # type: ignore
-    data_json = _update_internal_user_params(data_json, data)
+    data_json = _update_internal_new_user_params(data_json, data)
     response = await generate_key_helper_fn(request_type="user", **data_json)
 
     # Admin UI Logic
@@ -438,6 +439,52 @@ async def user_info(  # noqa: PLR0915
         raise handle_exception_on_proxy(e)
 
 
+def _update_internal_user_params(data_json: dict, data: UpdateUserRequest) -> dict:
+    non_default_values = {}
+    for k, v in data_json.items():
+        if (
+            v is not None
+            and v
+            not in (
+                [],
+                {},
+                0,
+            )
+            and k not in LiteLLM_ManagementEndpoint_MetadataFields
+        ):  # models default to [], spend defaults to 0, we should not reset these values
+            non_default_values[k] = v
+
+    is_internal_user = False
+    if data.user_role == LitellmUserRoles.INTERNAL_USER:
+        is_internal_user = True
+
+    if "budget_duration" in non_default_values:
+        duration_s = duration_in_seconds(duration=non_default_values["budget_duration"])
+        user_reset_at = datetime.now(timezone.utc) + timedelta(seconds=duration_s)
+        non_default_values["budget_reset_at"] = user_reset_at
+
+    if "max_budget" not in non_default_values:
+        if (
+            is_internal_user and litellm.max_internal_user_budget is not None
+        ):  # applies internal user limits, if user role updated
+            non_default_values["max_budget"] = litellm.max_internal_user_budget
+
+    if (
+        "budget_duration" not in non_default_values
+    ):  # applies internal user limits, if user role updated
+        if is_internal_user and litellm.internal_user_budget_duration is not None:
+            non_default_values["budget_duration"] = (
+                litellm.internal_user_budget_duration
+            )
+            duration_s = duration_in_seconds(
+                duration=non_default_values["budget_duration"]
+            )
+            user_reset_at = datetime.now(timezone.utc) + timedelta(seconds=duration_s)
+            non_default_values["budget_reset_at"] = user_reset_at
+
+    return non_default_values
+
+
 @router.post(
     "/user/update",
     tags=["Internal User management"],
@@ -459,7 +506,8 @@ async def user_update(
         "user_id": "test-litellm-user-4",
         "user_role": "proxy_admin_viewer"
     }'
-
+    ```
+    
     Parameters:
         - user_id: Optional[str] - Specify a user id. If not set, a unique id will be generated.
         - user_email: Optional[str] - Specify a user email.
@@ -491,7 +539,7 @@ async def user_update(
         - duration: Optional[str] - [NOT IMPLEMENTED].
         - key_alias: Optional[str] - [NOT IMPLEMENTED].
             
-    ```
+    
     """
     from litellm.proxy.proxy_server import prisma_client
 
@@ -502,46 +550,21 @@ async def user_update(
             raise Exception("Not connected to DB!")
 
         # get non default values for key
-        non_default_values = {}
-        for k, v in data_json.items():
-            if v is not None and v not in (
-                [],
-                {},
-                0,
-            ):  # models default to [], spend defaults to 0, we should not reset these values
-                non_default_values[k] = v
+        non_default_values = _update_internal_user_params(
+            data_json=data_json, data=data
+        )
 
-        is_internal_user = False
-        if data.user_role == LitellmUserRoles.INTERNAL_USER:
-            is_internal_user = True
+        existing_user_row = await prisma_client.get_data(
+            user_id=data.user_id, table_name="user", query_type="find_unique"
+        )
 
-        if "budget_duration" in non_default_values:
-            duration_s = duration_in_seconds(
-                duration=non_default_values["budget_duration"]
-            )
-            user_reset_at = datetime.now(timezone.utc) + timedelta(seconds=duration_s)
-            non_default_values["budget_reset_at"] = user_reset_at
+        existing_metadata = existing_user_row.metadata if existing_user_row else {}
 
-        if "max_budget" not in non_default_values:
-            if (
-                is_internal_user and litellm.max_internal_user_budget is not None
-            ):  # applies internal user limits, if user role updated
-                non_default_values["max_budget"] = litellm.max_internal_user_budget
-
-        if (
-            "budget_duration" not in non_default_values
-        ):  # applies internal user limits, if user role updated
-            if is_internal_user and litellm.internal_user_budget_duration is not None:
-                non_default_values["budget_duration"] = (
-                    litellm.internal_user_budget_duration
-                )
-                duration_s = duration_in_seconds(
-                    duration=non_default_values["budget_duration"]
-                )
-                user_reset_at = datetime.now(timezone.utc) + timedelta(
-                    seconds=duration_s
-                )
-                non_default_values["budget_reset_at"] = user_reset_at
+        non_default_values = prepare_metadata_fields(
+            data=data,
+            non_default_values=non_default_values,
+            existing_metadata=existing_metadata or {},
+        )
 
         ## ADD USER, IF NEW ##
         verbose_proxy_logger.debug("/user/update: Received data = %s", data)
