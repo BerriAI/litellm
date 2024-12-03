1c8010bf5749ceed3816776a1202bfb6ebce6992
Krish Dholakia <krrishdholakia@gmail.com>
2024-12-02 21:03:33 -0800

    Litellm dev 11 30 2024 (#6974)
    * feat(cohere/chat.py): return citations in model response
    
    Closes https://github.com/BerriAI/litellm/issues/6814
    
    * fix(cohere/chat.py): fix linting errors
    
    * fix(langsmith.py): support 'run_id' for langsmith
    
    Fixes https://github.com/BerriAI/litellm/issues/6862
    
    * fix(langsmith.py): fix langsmith quickstart
    
    Fixes https://github.com/BerriAI/litellm/issues/6861
    
    * fix: suppress linting error
    
    * LiteLLM Minor Fixes & Improvements (11/29/2024)  (#6965)
    
    * fix(factory.py): ensure tool call converts image url
    
    Fixes https://github.com/BerriAI/litellm/issues/6953
    
    * fix(transformation.py): support mp4 + pdf url's for vertex ai
    
    Fixes https://github.com/BerriAI/litellm/issues/6936
    
    * fix(http_handler.py): mask gemini api key in error logs
    
    Fixes https://github.com/BerriAI/litellm/issues/6963
    
    * docs(prometheus.md): update prometheus FAQs
    
    * feat(auth_checks.py): ensure specific model access > wildcard model access
    
    if wildcard model is in access group, but specific model is not - deny access
    
    * fix(auth_checks.py): handle auth checks for team based model access groups
    
    handles scenario where model access group used for wildcard models
    
    * fix(internal_user_endpoints.py): support adding guardrails on `/user/update`
    
    Fixes https://github.com/BerriAI/litellm/issues/6942
    
    * fix(key_management_endpoints.py): fix prepare_metadata_fields helper
    
    * fix: fix tests
    
    * build(requirements.txt): bump openai dep version
    
    fixes proxies argument
    
    * test: fix tests
    
    * fix(http_handler.py): fix error message masking
    
    * fix(bedrock_guardrails.py): pass in prepped data
    
    * test: fix test
    
    * test: fix nvidia nim test
    
    * fix(http_handler.py): return original response headers
    
    * fix: revert maskedhttpstatuserror
    
    * test: update tests
    
    * test: cleanup test
    
    * fix(key_management_endpoints.py): fix metadata field update logic
    
    * fix(key_management_endpoints.py): maintain initial order of guardrails in key update
    
    * fix(key_management_endpoints.py): handle prepare metadata
    
    * fix: fix linting errors
    
    * fix: fix linting errors
    
    * fix: fix linting errors
    
    * fix: fix key management errors
    
    * fix(key_management_endpoints.py): update metadata
    
    * test: update test
    
    * refactor: add more debug statements
    
    * test: skip flaky test
    
    * test: fix test
    
    * fix: fix test
    
    * fix: fix update metadata logic
    
    * fix: fix test
    
    * ci(config.yml): change db url for e2e ui testing
    
    * test: add more debug logs to langsmith
    
    * fix: test change
    
    * build(config.yml): fix db url
    
    '

--- tests/local_testing/test_function_calling.py
+++ tests/local_testing/test_function_calling.py
@@ -46,11 +46,12 @@ def get_current_weather(location, unit="fahrenheit"):
     "model",
     [
         "gpt-3.5-turbo-1106",
-        # "mistral/mistral-large-latest",
+        "mistral/mistral-large-latest",
         "claude-3-haiku-20240307",
         "gemini/gemini-1.5-pro",
         "anthropic.claude-3-sonnet-20240229-v1:0",
-        # "groq/llama3-8b-8192",
+        "groq/llama3-8b-8192",
+        "cohere_chat/command-r",
     ],
 )
 @pytest.mark.flaky(retries=3, delay=1)
