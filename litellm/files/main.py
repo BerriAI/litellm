c2234fb9033c733e9c840579298e416bbf2f1819
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-12-04 19:40:28 -0800

    (feat) add Vertex Batches API support in OpenAI format  (#7032)
    * working request
    
    * working transform
    
    * working request
    
    * transform vertex batch response
    
    * add _async_create_batch
    
    * move gcs functions to base
    
    * fix _get_content_from_openai_file
    
    * transform_openai_file_content_to_vertex_ai_file_content
    
    * fix transform vertex gcs bucket upload to OAI files format
    
    * working e2e test
    
    * _get_gcs_object_name
    
    * fix linting
    
    * add doc string
    
    * fix transform_gcs_bucket_response_to_openai_file_object
    
    * use vertex for batch endpoints
    
    * add batches support for vertex
    
    * test_vertex_batches_endpoint
    
    * test_vertex_batch_prediction
    
    * fix gcs bucket base auth
    
    * docs clean up batches
    
    * docs Batch API
    
    * docs vertex batches api
    
    * test_get_gcs_logging_config_without_service_account
    
    * undo change
    
    * fix vertex md
    
    * test_get_gcs_logging_config_without_service_account
    
    * ci/cd run again

--- litellm/files/main.py
+++ litellm/files/main.py
@@ -17,6 +17,9 @@ import litellm
 from litellm import client, get_secret_str
 from litellm.llms.files_apis.azure import AzureOpenAIFilesAPI
 from litellm.llms.OpenAI.openai import FileDeleted, FileObject, OpenAIFilesAPI
+from litellm.llms.vertex_ai_and_google_ai_studio.files.handler import (
+    VertexAIFilesHandler,
+)
 from litellm.types.llms.openai import (
     Batch,
     CreateFileRequest,
@@ -30,6 +33,7 @@ from litellm.utils import supports_httpx_timeout
 ####### ENVIRONMENT VARIABLES ###################
 openai_files_instance = OpenAIFilesAPI()
 azure_files_instance = AzureOpenAIFilesAPI()
+vertex_ai_files_instance = VertexAIFilesHandler()
 #################################################
 
 
@@ -490,7 +494,7 @@ def file_list(
 async def acreate_file(
     file: FileTypes,
     purpose: Literal["assistants", "batch", "fine-tune"],
-    custom_llm_provider: Literal["openai", "azure"] = "openai",
+    custom_llm_provider: Literal["openai", "azure", "vertex_ai"] = "openai",
     extra_headers: Optional[Dict[str, str]] = None,
     extra_body: Optional[Dict[str, str]] = None,
     **kwargs,
@@ -532,7 +536,7 @@ async def acreate_file(
 def create_file(
     file: FileTypes,
     purpose: Literal["assistants", "batch", "fine-tune"],
-    custom_llm_provider: Literal["openai", "azure"] = "openai",
+    custom_llm_provider: Literal["openai", "azure", "vertex_ai"] = "openai",
     extra_headers: Optional[Dict[str, str]] = None,
     extra_body: Optional[Dict[str, str]] = None,
     **kwargs,
@@ -626,6 +630,32 @@ def create_file(
                 api_base=api_base,
                 api_key=api_key,
                 api_version=api_version,
+                timeout=timeout,
+                max_retries=optional_params.max_retries,
+                create_file_data=_create_file_request,
+            )
+        elif custom_llm_provider == "vertex_ai":
+            api_base = optional_params.api_base or ""
+            vertex_ai_project = (
+                optional_params.vertex_project
+                or litellm.vertex_project
+                or get_secret_str("VERTEXAI_PROJECT")
+            )
+            vertex_ai_location = (
+                optional_params.vertex_location
+                or litellm.vertex_location
+                or get_secret_str("VERTEXAI_LOCATION")
+            )
+            vertex_credentials = optional_params.vertex_credentials or get_secret_str(
+                "VERTEXAI_CREDENTIALS"
+            )
+
+            response = vertex_ai_files_instance.create_file(
+                _is_async=_is_async,
+                api_base=api_base,
+                vertex_project=vertex_ai_project,
+                vertex_location=vertex_ai_location,
+                vertex_credentials=vertex_credentials,
                 timeout=timeout,
                 max_retries=optional_params.max_retries,
                 create_file_data=_create_file_request,
