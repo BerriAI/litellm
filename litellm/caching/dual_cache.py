35e5ce857b0b421876c807f63d34b4ab8ee01873
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-12-06 21:14:12 -0800

    Provider Budget Routing - Get Budget, Spend Details (#7063)
    * add async_get_ttl to dual cache
    
    * add ProviderBudgetResponse
    
    * add provider_budgets
    
    * test_redis_get_ttl
    
    * _init_or_get_provider_budget_in_cache
    
    * test_init_or_get_provider_budget_in_cache
    
    * use _init_provider_budget_in_cache
    
    * test_get_current_provider_budget_reset_at
    
    * doc Get Budget, Spend Details
    
    * doc Provider Budget Routing

--- litellm/caching/dual_cache.py
+++ litellm/caching/dual_cache.py
@@ -423,3 +423,12 @@ class DualCache(BaseCache):
             self.in_memory_cache.delete_cache(key)
         if self.redis_cache is not None:
             await self.redis_cache.async_delete_cache(key)
+
+    async def async_get_ttl(self, key: str) -> Optional[int]:
+        """
+        Get the remaining TTL of a key in in-memory cache or redis
+        """
+        ttl = await self.in_memory_cache.async_get_ttl(key)
+        if ttl is None and self.redis_cache is not None:
+            ttl = await self.redis_cache.async_get_ttl(key)
+        return ttl
