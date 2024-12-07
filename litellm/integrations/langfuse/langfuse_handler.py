20e8dc35e1e10e00d62ede87dc1997545d333546
Krish Dholakia <krrishdholakia@gmail.com>
2024-12-06 23:10:22 -0800

    feat(langfuse/): support langfuse prompt management (#7073)
    * feat(langfuse/): support langfuse prompt management
    
    Initial working commit for langfuse prompt management support
    
    Closes https://github.com/BerriAI/litellm/issues/6269
    
    * test: update test
    
    * fix(litellm_logging.py): suppress linting error

--- litellm/integrations/langfuse/langfuse_handler.py
+++ litellm/integrations/langfuse/langfuse_handler.py
@@ -6,7 +6,10 @@ Used to get the LangFuseLogger for a given request
 Handles Key/Team Based Langfuse Logging
 """
 
+import os
 from typing import TYPE_CHECKING, Any, Dict, Optional
+
+from packaging.version import Version
 
 from litellm.litellm_core_utils.litellm_logging import StandardCallbackDynamicParams
 
