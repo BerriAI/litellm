02d5c06e09a0a605085215ffebdebae7afe548bb
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-12-07 20:49:43 -0800

    fix refactor - use consistent file naming convention

--- tests/llm_translation/test_max_completion_tokens.py
+++ tests/llm_translation/test_max_completion_tokens.py
@@ -220,7 +220,7 @@ def test_all_model_configs():
         optional_params={},
     ) == {"max_tokens": 10}
 
-    from litellm.llms.AI21.chat import AI21ChatConfig
+    from litellm.llms.ai21.chat import AI21ChatConfig
 
     assert "max_completion_tokens" in AI21ChatConfig().get_supported_openai_params(
         "jamba-1.5-mini@001"
