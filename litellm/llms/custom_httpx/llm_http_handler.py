30d984918cf36e065bf8ae0281c8ab7a3a0d5a5f
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-12-09 13:51:13 -0800

    feat  cohere using base llm class

--- litellm/llms/custom_httpx/llm_http_handler.py
+++ litellm/llms/custom_httpx/llm_http_handler.py
@@ -129,21 +129,36 @@ class BaseLLMHTTPHandler:
         )
 
         if acompletion is True:
-            return self.async_completion(
-                custom_llm_provider=custom_llm_provider,
-                provider_config=provider_config,
-                api_base=api_base,
-                headers=headers,
-                data=data,
-                timeout=timeout,
-                model=model,
-                model_response=model_response,
-                logging_obj=logging_obj,
-                api_key=api_key,
-                messages=messages,
-                optional_params=optional_params,
-                encoding=encoding,
-            )
+            if stream is True:
+                data["stream"] = stream
+                return self.acompletion_stream_function(
+                    model=model,
+                    messages=messages,
+                    api_base=api_base,
+                    headers=headers,
+                    custom_llm_provider=custom_llm_provider,
+                    provider_config=provider_config,
+                    timeout=timeout,
+                    logging_obj=logging_obj,
+                    data=data,
+                )
+
+            else:
+                return self.async_completion(
+                    custom_llm_provider=custom_llm_provider,
+                    provider_config=provider_config,
+                    api_base=api_base,
+                    headers=headers,
+                    data=data,
+                    timeout=timeout,
+                    model=model,
+                    model_response=model_response,
+                    logging_obj=logging_obj,
+                    api_key=api_key,
+                    messages=messages,
+                    optional_params=optional_params,
+                    encoding=encoding,
+                )
 
         if stream is True:
             data["stream"] = stream
@@ -230,6 +245,88 @@ class BaseLLMHTTPHandler:
             streaming_response=response.iter_lines(), sync_stream=True
         )
 
+        # LOGGING
+        logging_obj.post_call(
+            input=messages,
+            api_key="",
+            original_response="first stream response received",
+            additional_args={"complete_input_dict": data},
+        )
+
+        return completion_stream, response.headers
+
+    async def acompletion_stream_function(
+        self,
+        model: str,
+        messages: list,
+        api_base: str,
+        custom_llm_provider: str,
+        headers: dict,
+        provider_config: BaseConfig,
+        timeout: Union[float, httpx.Timeout],
+        logging_obj: LiteLLMLoggingObj,
+        data: dict,
+    ):
+        data["stream"] = True
+        completion_stream, _response_headers = await self.make_async_call(
+            custom_llm_provider=custom_llm_provider,
+            provider_config=provider_config,
+            api_base=api_base,
+            headers=headers,
+            data=json.dumps(data),
+            messages=messages,
+            logging_obj=logging_obj,
+            timeout=timeout,
+        )
+        streamwrapper = CustomStreamWrapper(
+            completion_stream=completion_stream,
+            model=model,
+            custom_llm_provider=custom_llm_provider,
+            logging_obj=logging_obj,
+        )
+        return streamwrapper
+
+    async def make_async_call(
+        self,
+        custom_llm_provider: str,
+        provider_config: BaseConfig,
+        api_base: str,
+        headers: dict,
+        data: str,
+        messages: list,
+        logging_obj: LiteLLMLoggingObj,
+        timeout: Optional[Union[float, httpx.Timeout]],
+    ) -> Tuple[Any, httpx.Headers]:
+        async_httpx_client = get_async_httpx_client(
+            llm_provider=litellm.LlmProviders(custom_llm_provider)
+        )
+        try:
+            response = await async_httpx_client.post(
+                api_base, headers=headers, data=data, stream=True, timeout=timeout
+            )
+        except httpx.HTTPStatusError as e:
+            raise self._handle_error(
+                e=e,
+                provider_config=provider_config,
+            )
+        except Exception as e:
+            for exception in litellm.LITELLM_EXCEPTION_TYPES:
+                if isinstance(e, exception):
+                    raise e
+            raise self._handle_error(
+                e=e,
+                provider_config=provider_config,
+            )
+
+        if response.status_code != 200:
+            raise BaseLLMException(
+                status_code=response.status_code,
+                message=str(response.read()),
+            )
+
+        completion_stream = provider_config.get_model_response_iterator(
+            streaming_response=response.aiter_lines(), sync_stream=False
+        )
         # LOGGING
         logging_obj.post_call(
             input=messages,
