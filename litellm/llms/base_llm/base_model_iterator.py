922c76cc8b703652bdb92c7532c4c81a89e16adc
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-12-09 20:12:12 -0800

    fix base model iterator

--- litellm/llms/base_llm/base_model_iterator.py
+++ litellm/llms/base_llm/base_model_iterator.py
@@ -22,63 +22,9 @@ class FakeStreamResponseIterator:
     def __iter__(self):
         return self
 
-    def _handle_json_mode_chunk(
-        self, text: str, tool_calls: Optional[List[ChatCompletionToolCallChunk]]
-    ) -> Tuple[str, Optional[ChatCompletionToolCallChunk]]:
-        """
-        If JSON mode is enabled, convert the tool call to a message.
-
-        Bedrock returns the JSON schema as part of the tool call
-        OpenAI returns the JSON schema as part of the content, this handles placing it in the content
-
-        Args:
-            text: str
-            tool_use: Optional[ChatCompletionToolCallChunk]
-        Returns:
-            Tuple[str, Optional[ChatCompletionToolCallChunk]]
-
-            text: The text to use in the content
-            tool_use: The ChatCompletionToolCallChunk to use in the chunk response
-        """
-        tool_use: Optional[ChatCompletionToolCallChunk] = None
-        if self.json_mode is True and tool_calls is not None:
-            message = litellm.AnthropicConfig()._convert_tool_response_to_message(
-                tool_calls=tool_calls
-            )
-            if message is not None:
-                text = message.content or ""
-                tool_use = None
-        elif tool_calls is not None and len(tool_calls) > 0:
-            tool_use = tool_calls[0]
-        return text, tool_use
-
-    def chunk_parser(self, chunk: ModelResponse) -> GenericStreamingChunk:
-        try:
-            chunk_usage: litellm.Usage = getattr(chunk, "usage")
-            text = chunk.choices[0].message.content or ""  # type: ignore
-            tool_use = None
-            if self.json_mode is True:
-                text, tool_use = self._handle_json_mode_chunk(
-                    text=text,
-                    tool_calls=chunk.choices[0].message.tool_calls,  # type: ignore
-                )
-            processed_chunk = GenericStreamingChunk(
-                text=text,
-                tool_use=tool_use,
-                is_finished=True,
-                finish_reason=map_finish_reason(
-                    finish_reason=chunk.choices[0].finish_reason or ""
-                ),
-                usage=ChatCompletionUsageBlock(
-                    prompt_tokens=chunk_usage.prompt_tokens,
-                    completion_tokens=chunk_usage.completion_tokens,
-                    total_tokens=chunk_usage.total_tokens,
-                ),
-                index=0,
-            )
-            return processed_chunk
-        except Exception as e:
-            raise ValueError(f"Failed to decode chunk: {chunk}. Error: {e}")
+    @abstractmethod
+    def chunk_parser(self, chunk: dict) -> GenericStreamingChunk:
+        pass
 
     def __next__(self):
         if self.is_done:
