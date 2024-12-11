1d8956a3d46bfc98df98d2a89496f1efd92b0ff2
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-12-10 21:44:25 -0800

    Code Quality Improvement - remove `file_apis`, `fine_tuning_apis`  from `/llms`  (#7156)
    * remove files_apis from /llms
    
    * fix imports
    
    * move fine tuning api from /llms
    
    * fix importing fine tuning handlers
    
    * fix imports

--- litellm/files/main.py
+++ litellm/files/main.py
@@ -15,7 +15,7 @@ import httpx
 
 import litellm
 from litellm import client, get_secret_str
-from litellm.llms.files_apis.azure import AzureOpenAIFilesAPI
+from litellm.llms.azure.files.handler import AzureOpenAIFilesAPI
 from litellm.llms.openai.openai import FileDeleted, FileObject, OpenAIFilesAPI
 from litellm.llms.vertex_ai_and_google_ai_studio.files.handler import (
     VertexAIFilesHandler,
