d97cc153fd85d659ff2cb8a52a876ab10ec131c6
Krish Dholakia <krrishdholakia@gmail.com>
2024-12-10 16:15:19 -0800

    refactor(fireworks_ai/): inherit from openai like base config (#7146)
    * refactor(fireworks_ai/): inherit from openai like base config
    
    refactors fireworks ai to use a common config
    
    * test: fix import in test
    
    * refactor(watsonx/): refactor watsonx to use llm base config
    
    refactors chat + completion routes to base config path
    
    * fix: fix linting error
    
    * test: fix test
    
    * fix: fix test

--- tests/llm_translation/test_max_completion_tokens.py
+++ tests/llm_translation/test_max_completion_tokens.py
@@ -154,13 +154,18 @@ def test_all_model_configs():
         {"max_completion_tokens": 10}, {}, "llama3", drop_params=False
     ) == {"max_tokens": 10}
 
-    from litellm.llms.fireworks_ai.chat.fireworks_ai_transformation import (
+    from litellm.llms.fireworks_ai.chat.transformation import (
         FireworksAIConfig,
     )
 
-    assert "max_completion_tokens" in FireworksAIConfig().get_supported_openai_params()
+    assert "max_completion_tokens" in FireworksAIConfig().get_supported_openai_params(
+        model="llama3"
+    )
     assert FireworksAIConfig().map_openai_params(
-        {"max_completion_tokens": 10}, {}, "llama3"
+        model="llama3",
+        non_default_params={"max_completion_tokens": 10},
+        optional_params={},
+        drop_params=False,
     ) == {"max_tokens": 10}
 
     from litellm.llms.huggingface_restapi import HuggingfaceConfig
