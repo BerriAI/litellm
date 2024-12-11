bfccf4ec2d156b0abf7d9c5dec76f21c0ed13bfa
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-12-11 12:07:22 -0800

    fix test_vertexai_model_garden_model_completion

--- litellm/llms/vertex_ai/vertex_model_garden/main.py
+++ litellm/llms/vertex_ai/vertex_model_garden/main.py
@@ -76,7 +76,7 @@ class VertexAIModelGardenModels(VertexBase):
             from google.cloud import aiplatform
 
             from litellm.llms.openai_like.chat.handler import OpenAILikeChatHandler
-            from litellm.llms.vertex_ai_and_google_ai_studio.gemini.vertex_and_google_ai_studio_gemini import (
+            from litellm.llms.vertex_ai.gemini.vertex_and_google_ai_studio_gemini import (
                 VertexLLM,
             )
         except Exception:
