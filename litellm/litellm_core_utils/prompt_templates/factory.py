6b7c350f4848018e50028d45ace0b8c7b412c1d5
Krrish Dholakia <krrishdholakia@gmail.com>
2024-12-12 11:09:08 -0800

    fix(factory.py): fix linting error

--- litellm/litellm_core_utils/prompt_templates/factory.py
+++ litellm/litellm_core_utils/prompt_templates/factory.py
@@ -38,6 +38,7 @@ from litellm.types.llms.openai import (
     ChatCompletionToolCallFunctionChunk,
     ChatCompletionToolMessage,
     ChatCompletionUserMessage,
+    OpenAIMessageContentListBlock,
 )
 from litellm.types.utils import GenericImageParsingChunk
 
@@ -2688,7 +2689,10 @@ def _bedrock_converse_messages_pt(  # noqa: PLR0915
                             _parts.append(_part)  # type: ignore
                         _cache_point_block = (
                             litellm.AmazonConverseConfig()._get_cache_point_block(
-                                message_block=element, block_type="content_block"
+                                message_block=cast(
+                                    OpenAIMessageContentListBlock, element
+                                ),
+                                block_type="content_block",
                             )
                         )
                         if _cache_point_block is not None:
