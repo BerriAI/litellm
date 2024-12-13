388d638c00680213caf37c3ee5e9571d47610368
cailingxiao <cailingxiao2013@qq.com>
2024-12-13 22:18:46 +0800

    add models

--- litellm/proxy/proxy_server.py
+++ litellm/proxy/proxy_server.py
@@ -225,6 +225,13 @@ from litellm.proxy.spend_tracking.spend_tracking_utils import get_logging_payloa
 from litellm.proxy.ui_crud_endpoints.proxy_setting_endpoints import (
     router as ui_crud_endpoints_router,
 )
+from litellm.proxy.wuban.models import (
+    set_model_list_def as set_model_list
+)
+from litellm.proxy.wuban.models import (
+    router as wuban_router
+)
+
 from litellm.proxy.utils import (
     PrismaClient,
     ProxyLogging,
@@ -9150,6 +9157,7 @@ def cleanup_router_config_variables():
     health_check_interval = None
     prisma_client = None
 
+set_model_list(model_list)
 
 app.include_router(router)
 app.include_router(rerank_router)
@@ -9172,3 +9180,4 @@ app.include_router(debugging_endpoints_router)
 app.include_router(ui_crud_endpoints_router)
 app.include_router(openai_files_router)
 app.include_router(team_callback_router)
+app.include_router(wuban_router)
--- litellm/proxy/wuban/models.py
+++ litellm/proxy/wuban/models.py
@@ -1,9 +1,5 @@
-import asyncio
-import copy
-import os
-import traceback
-from datetime import datetime, timedelta
-from typing import Literal, Optional, Union
+from json import dumps
+from typing import Callable
 
 import fastapi
 from fastapi import APIRouter, Depends, Header, HTTPException, Request, Response, status
@@ -19,12 +15,14 @@ from litellm.proxy._types import (
     WebhookEvent,
 )
 from litellm.proxy.auth.user_api_key_auth import user_api_key_auth
-from litellm.proxy.health_check import perform_health_check
-from litellm.proxy.proxy_server import model_list
-#### Health ENDPOINTS ####
+
 
 router = APIRouter()
+model_list: Callable
 
+def set_model_list_def(model_list_from_proxy: Callable):
+    global model_list
+    model_list = model_list_from_proxy
 
 @router.get(
     "/api/models",
@@ -32,11 +30,31 @@ router = APIRouter()
     tags=["model management"]
 )
 async def models(user_api_key_dict: UserAPIKeyAuth = Depends(user_api_key_auth)):
-    originData = model_list(user_api_key_dict)
+    originData = await model_list(user_api_key_dict)
     ## todo transform data
     print(originData)
-    # outData = []
-    return originData
+    # "data": [
+    #     {"id": "deepseek/deepseek-chat",
+    #      "object": "model",
+    #      "created": 1677610602,
+    #      "owned_by": "openai"}
+    # ],
+    data = originData['data']
+    if data is not None:
+        if len(data) > 0:
+            outData = {}
+            for item in data:
+                group = outData[item["owned_by"]]
+                if group is None:
+                    group = []
+                    outData[item["owned_by"]] = group
+                group.append(item["id"])
+            return outData
+    ## mock data
+    outData = {}
+    outData["openAI"] = ["chatgpt-4o-latest", "gpt-4o-2024-08-06"]
+    outData["deepseek"] = ["deepseek/chat", "deepseek/code"]
+    return outData
 
 
 @router.get(
@@ -45,7 +63,7 @@ async def models(user_api_key_dict: UserAPIKeyAuth = Depends(user_api_key_auth))
     tags=["model management"]
 )
 async def endpoints(user_api_key_dict: UserAPIKeyAuth = Depends(user_api_key_auth)):
-    originData = model_list(user_api_key_dict)
+    originData = await model_list(user_api_key_dict)
     ## todo transform data
     print(originData)
     # outData = []
@@ -58,7 +76,10 @@ async def endpoints(user_api_key_dict: UserAPIKeyAuth = Depends(user_api_key_aut
     tags=["model management"]
 )
 async def keys(name):
-    return ""
+    map = {}
+    map.expiresAt = "2034-11-17T06:58:35.462Z"
+    return dumps(map)
+
 
 @router.post(
     "/api/files/images",
