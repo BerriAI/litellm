4d447136c2b288df102aa20686bd01144791b9c9
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-12-17 12:10:24 -0800

    test_voyage_ai_embedding_extra_params

--- tests/llm_translation/test_voyage_ai.py
+++ tests/llm_translation/test_voyage_ai.py
@@ -3,6 +3,7 @@ import os
 import sys
 from datetime import datetime
 from unittest.mock import AsyncMock
+import pytest
 
 sys.path.insert(
     0, os.path.abspath("../..")
@@ -11,6 +12,8 @@ sys.path.insert(
 
 from base_embedding_unit_tests import BaseLLMEmbeddingTest
 import litellm
+from litellm.llms.custom_httpx.http_handler import HTTPHandler
+from unittest.mock import patch, MagicMock
 
 
 class TestVoyageAI(BaseLLMEmbeddingTest):
@@ -24,11 +27,30 @@ class TestVoyageAI(BaseLLMEmbeddingTest):
 
 
 def test_voyage_ai_embedding_extra_params():
-    litellm.set_verbose = True
-    response = litellm.embedding(
-        model="voyage/voyage-3-lite",
-        input=["a"],
-        dimensions=512,
-        input_type="document",
-    )
-    print(response)
+    try:
+
+        client = HTTPHandler()
+        litellm.set_verbose = True
+
+        with patch.object(client, "post") as mock_client:
+            response = litellm.embedding(
+                model="voyage/voyage-3-lite",
+                input=["a"],
+                dimensions=512,
+                input_type="document",
+                client=client,
+            )
+
+            mock_client.assert_called_once()
+            json_data = json.loads(mock_client.call_args.kwargs["data"])
+
+            print("request data to voyage ai", json.dumps(json_data, indent=4))
+
+            # Assert the request parameters
+            assert json_data["input"] == ["a"]
+            assert json_data["model"] == "voyage-3-lite"
+            assert json_data["output_dimension"] == 512
+            assert json_data["input_type"] == "document"
+
+    except Exception as e:
+        pytest.fail(f"Error occurred: {e}")
