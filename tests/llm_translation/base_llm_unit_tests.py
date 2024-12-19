cbdf9040065e2d968421b6a1f4dc0b7b921fe1da
Krrish Dholakia <krrishdholakia@gmail.com>
2024-12-19 10:23:21 -0800

    test: mark prompt caching as a flaky test

--- tests/llm_translation/base_llm_unit_tests.py
+++ tests/llm_translation/base_llm_unit_tests.py
@@ -329,6 +329,7 @@ class BaseLLMChatTest(ABC):
         )
         assert response is not None
 
+    @pytest.mark.flaky(retries=4, delay=1)
     def test_prompt_caching(self):
         litellm.set_verbose = True
         from litellm.utils import supports_prompt_caching
