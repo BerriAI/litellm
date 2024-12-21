7488033ea969c9381411f3586627a9b6358de0d1
Krrish Dholakia <krrishdholakia@gmail.com>
2024-12-20 18:06:59 -0800

    test: fix test

--- tests/logging_callback_tests/test_otel_logging.py
+++ tests/logging_callback_tests/test_otel_logging.py
@@ -270,6 +270,7 @@ def validate_redacted_message_span_attributes(span):
         "metadata.user_api_key_alias",
         "metadata.user_api_key_user_id",
         "metadata.user_api_key_org_id",
+        "metadata.user_api_key_end_user_id",
     ]
 
     _all_attributes = set(
