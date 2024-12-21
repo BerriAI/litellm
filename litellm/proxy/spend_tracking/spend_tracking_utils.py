419bd512e53ea7e619df3ebcd3ca315ee7b535b6
Krrish Dholakia <krrishdholakia@gmail.com>
2024-12-20 18:30:03 -0800

    fix(spend_tracking_utils.py): fix if check

--- litellm/proxy/spend_tracking/spend_tracking_utils.py
+++ litellm/proxy/spend_tracking/spend_tracking_utils.py
@@ -42,8 +42,8 @@ def get_logging_payload(
         kwargs = {}
     if (
         response_obj is None
-        or not isinstance(response_obj, BaseModel)
-        or not isinstance(response_obj, dict)
+        and not isinstance(response_obj, BaseModel)
+        and not isinstance(response_obj, dict)
     ):
         response_obj = {}
     # standardize this function to be used across, s3, dynamoDB, langfuse logging
