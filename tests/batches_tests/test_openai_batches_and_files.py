f30015eedfc5cea7c63507210ad4581cc2a932f1
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-12-23 13:54:42 -0800

    test test_async_create_batch

--- tests/batches_tests/test_openai_batches_and_files.py
+++ tests/batches_tests/test_openai_batches_and_files.py
@@ -69,6 +69,80 @@ def load_vertex_ai_credentials():
     print("created gcs path service account=", os.environ["GCS_PATH_SERVICE_ACCOUNT"])
 
 
+@pytest.mark.parametrize("provider", ["openai"])  # , "azure"
+def test_create_batch(provider):
+    """
+    1. Create File for Batch completion
+    2. Create Batch Request
+    3. Retrieve the specific batch
+    """
+    if provider == "azure":
+        # Don't have anymore Azure Quota
+        return
+    file_name = "openai_batch_completions.jsonl"
+    _current_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(_current_dir, file_name)
+
+    file_obj = litellm.create_file(
+        file=open(file_path, "rb"),
+        purpose="batch",
+        custom_llm_provider=provider,
+    )
+    print("Response from creating file=", file_obj)
+
+    batch_input_file_id = file_obj.id
+    assert (
+        batch_input_file_id is not None
+    ), "Failed to create file, expected a non null file_id but got {batch_input_file_id}"
+
+    time.sleep(5)
+    create_batch_response = litellm.create_batch(
+        completion_window="24h",
+        endpoint="/v1/chat/completions",
+        input_file_id=batch_input_file_id,
+        custom_llm_provider=provider,
+        metadata={"key1": "value1", "key2": "value2"},
+    )
+
+    print("response from litellm.create_batch=", create_batch_response)
+
+    assert (
+        create_batch_response.id is not None
+    ), f"Failed to create batch, expected a non null batch_id but got {create_batch_response.id}"
+    assert (
+        create_batch_response.endpoint == "/v1/chat/completions"
+        or create_batch_response.endpoint == "/chat/completions"
+    ), f"Failed to create batch, expected endpoint to be /v1/chat/completions but got {create_batch_response.endpoint}"
+    assert (
+        create_batch_response.input_file_id == batch_input_file_id
+    ), f"Failed to create batch, expected input_file_id to be {batch_input_file_id} but got {create_batch_response.input_file_id}"
+
+    retrieved_batch = litellm.retrieve_batch(
+        batch_id=create_batch_response.id, custom_llm_provider=provider
+    )
+    print("retrieved batch=", retrieved_batch)
+    # just assert that we retrieved a non None batch
+
+    assert retrieved_batch.id == create_batch_response.id
+
+    # list all batches
+    list_batches = litellm.list_batches(custom_llm_provider=provider, limit=2)
+    print("list_batches=", list_batches)
+
+    file_content = litellm.file_content(
+        file_id=batch_input_file_id, custom_llm_provider=provider
+    )
+
+    result = file_content.content
+
+    result_file_name = "batch_job_results_furniture.jsonl"
+
+    with open(result_file_name, "wb") as file:
+        file.write(result)
+
+    pass
+
+
 class TestCustomLogger(CustomLogger):
     def __init__(self):
         super().__init__()
@@ -84,93 +158,6 @@ class TestCustomLogger(CustomLogger):
         self.standard_logging_object = kwargs["standard_logging_object"]
 
 
-@pytest.mark.parametrize("provider", ["openai"])  # , "azure"
-@pytest.mark.asyncio
-async def test_create_batch(provider):
-    """
-    1. Create File for Batch completion
-    2. Create Batch Request
-    3. Retrieve the specific batch
-    """
-    custom_logger = TestCustomLogger()
-    litellm.callbacks = [custom_logger, "datadog"]
-
-    if provider == "azure":
-        # Don't have anymore Azure Quota
-        return
-    file_name = "openai_batch_completions.jsonl"
-    _current_dir = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(_current_dir, file_name)
-
-    file_obj = await litellm.acreate_file(
-        file=open(file_path, "rb"),
-        purpose="batch",
-        custom_llm_provider=provider,
-    )
-    print("Response from creating file=", file_obj)
-
-    batch_input_file_id = file_obj.id
-    assert (
-        batch_input_file_id is not None
-    ), "Failed to create file, expected a non null file_id but got {batch_input_file_id}"
-
-    await asyncio.sleep(1)
-    create_batch_response = await litellm.acreate_batch(
-        completion_window="24h",
-        endpoint="/v1/chat/completions",
-        input_file_id=batch_input_file_id,
-        custom_llm_provider=provider,
-        metadata={"key1": "value1", "key2": "value2"},
-    )
-
-    print("response from litellm.create_batch=", create_batch_response)
-    await asyncio.sleep(6)
-
-    # Assert that the create batch event is logged on CustomLogger
-    assert custom_logger.standard_logging_object is not None
-    print(
-        "standard_logging_object=",
-        json.dumps(custom_logger.standard_logging_object, indent=4),
-    )
-    assert (
-        create_batch_response.id is not None
-    ), f"Failed to create batch, expected a non null batch_id but got {create_batch_response.id}"
-    assert (
-        create_batch_response.endpoint == "/v1/chat/completions"
-        or create_batch_response.endpoint == "/chat/completions"
-    ), f"Failed to create batch, expected endpoint to be /v1/chat/completions but got {create_batch_response.endpoint}"
-    assert (
-        create_batch_response.input_file_id == batch_input_file_id
-    ), f"Failed to create batch, expected input_file_id to be {batch_input_file_id} but got {create_batch_response.input_file_id}"
-
-    retrieved_batch = await litellm.aretrieve_batch(
-        batch_id=create_batch_response.id, custom_llm_provider=provider
-    )
-    print("retrieved batch=", retrieved_batch)
-    # just assert that we retrieved a non None batch
-
-    assert retrieved_batch.id == create_batch_response.id
-
-    # list all batches
-    list_batches = await litellm.alist_batches(custom_llm_provider=provider, limit=2)
-    print("list_batches=", list_batches)
-
-    file_content = await litellm.afile_content(
-        file_id=batch_input_file_id, custom_llm_provider=provider
-    )
-
-    result = file_content.content
-
-    result_file_name = "batch_job_results_furniture.jsonl"
-
-    with open(result_file_name, "wb") as file:
-        file.write(result)
-
-    await asyncio.sleep(60)
-
-    pass
-
-
 @pytest.mark.parametrize("provider", ["openai"])  #  "azure"
 @pytest.mark.asyncio()
 @pytest.mark.flaky(retries=3, delay=1)
@@ -184,6 +171,9 @@ async def test_async_create_batch(provider):
     if provider == "azure":
         # Don't have anymore Azure Quota
         return
+
+    custom_logger = TestCustomLogger()
+    litellm.callbacks = [custom_logger, "datadog"]
 
     file_name = "openai_batch_completions.jsonl"
     _current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -222,7 +212,9 @@ async def test_async_create_batch(provider):
         create_batch_response.input_file_id == batch_input_file_id
     ), f"Failed to create batch, expected input_file_id to be {batch_input_file_id} but got {create_batch_response.input_file_id}"
 
-    await asyncio.sleep(1)
+    await asyncio.sleep(6)
+    # Assert that the create batch event is logged on CustomLogger
+    assert custom_logger.standard_logging_object is not None
 
     retrieved_batch = await litellm.aretrieve_batch(
         batch_id=create_batch_response.id, custom_llm_provider=provider
