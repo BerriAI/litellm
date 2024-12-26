f7316f517a5abc77bddc58a3dc1aadb17b3d35db
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-12-26 08:58:47 -0800

    (Feat) Add logging for `POST v1/fine_tuning/jobs`  (#7426)
    * init commit ft jobs logging
    
    * add ft logging
    
    * add logging for FineTuningJob
    
    * simple FT Job create test

--- litellm/utils.py
+++ litellm/utils.py
@@ -1234,6 +1234,7 @@ def _is_async_request(
         or kwargs.get("arerank", False) is True
         or kwargs.get("_arealtime", False) is True
         or kwargs.get("acreate_batch", False) is True
+        or kwargs.get("acreate_fine_tuning_job", False) is True
         or is_pass_through is True
     ):
         return True
