3e83841c5bc780991f275846264dadf8ccf381b1
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-12-30 17:31:59 -0800

    use pagerduty as _custom_logger_compatible_callbacks_literal

--- litellm/__init__.py
+++ litellm/__init__.py
@@ -72,6 +72,7 @@ _custom_logger_compatible_callbacks_literal = Literal[
     "argilla",
     "mlflow",
     "langfuse",
+    "pagerduty",
 ]
 logged_real_time_event_types: Optional[Union[List[str], Literal["*"]]] = None
 _known_custom_logger_compatible_callbacks: List = list(
