8aaded33c98358b06d71463acf229cf19ab3cdd4
Krish Dholakia <krrishdholakia@gmail.com>
2025-01-01 07:41:50 -0800

    Prometheus - custom metrics support + other improvements  (#7489)
    * fix(prometheus.py): refactor litellm_input_tokens_metric to use label factory
    
    makes adding new metrics easier
    
    * feat(prometheus.py): add 'request_model' to 'litellm_input_tokens_metric'
    
    * refactor(prometheus.py): refactor 'litellm_output_tokens_metric' to use label factory
    
    makes adding new metrics easier
    
    * feat(prometheus.py): emit requested model in 'litellm_output_tokens_metric'
    
    * feat(prometheus.py): support tracking success events with custom metrics
    
    * refactor(prometheus.py): refactor '_set_latency_metrics' to just use the initially created enum values dictionary
    
    reduces scope for missing values
    
    * feat(prometheus.py): refactor all tags to support custom metadata tags
    
    enables metadata tags to be used across for e2e tracking
    
    * fix(prometheus.py): fix requested model on success event enum_values
    
    * test: fix test
    
    * test: fix test
    
    * test: handle filenotfound error
    
    * docs(prometheus.md): add new values to prometheus
    
    * docs(prometheus.md): document adding custom metrics on prometheus
    
    * bump: version 1.56.5 → 1.56.6

--- litellm/types/integrations/prometheus.py
+++ litellm/types/integrations/prometheus.py
@@ -164,6 +164,28 @@ class PrometheusMetricLabels(Enum):
         UserAPIKeyLabelNames.TAG.value,
     ]
 
+    litellm_input_tokens_metric = [
+        UserAPIKeyLabelNames.END_USER.value,
+        UserAPIKeyLabelNames.API_KEY_HASH.value,
+        UserAPIKeyLabelNames.API_KEY_ALIAS.value,
+        UserAPIKeyLabelNames.v1_LITELLM_MODEL_NAME.value,
+        UserAPIKeyLabelNames.TEAM.value,
+        UserAPIKeyLabelNames.TEAM_ALIAS.value,
+        UserAPIKeyLabelNames.USER.value,
+        UserAPIKeyLabelNames.REQUESTED_MODEL.value,
+    ]
+
+    litellm_output_tokens_metric = [
+        UserAPIKeyLabelNames.END_USER.value,
+        UserAPIKeyLabelNames.API_KEY_HASH.value,
+        UserAPIKeyLabelNames.API_KEY_ALIAS.value,
+        UserAPIKeyLabelNames.v1_LITELLM_MODEL_NAME.value,
+        UserAPIKeyLabelNames.TEAM.value,
+        UserAPIKeyLabelNames.TEAM_ALIAS.value,
+        UserAPIKeyLabelNames.USER.value,
+        UserAPIKeyLabelNames.REQUESTED_MODEL.value,
+    ]
+
     litellm_deployment_successful_fallbacks = [
         UserAPIKeyLabelNames.REQUESTED_MODEL.value,
         UserAPIKeyLabelNames.FALLBACK_MODEL.value,
