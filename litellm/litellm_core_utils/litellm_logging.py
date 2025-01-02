8727af69309779c384cd9dbcb8b1b5a2556d950a
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-01-01 20:21:01 -0800

    (fix) GCS bucket logger - apply `truncate_standard_logging_payload_content` to `standard_logging_payload` and ensure GCS flushes queue on fails (#7500)
    * use truncate_standard_logging_payload_content
    
    * update truncate_standard_logging_payload_content
    
    * update dd logger
    
    * update gcs async_send_batch
    
    * fix code check
    
    * test_datadog_payload_content_truncation
    
    * fix code quality

--- litellm/litellm_core_utils/litellm_logging.py
+++ litellm/litellm_core_utils/litellm_logging.py
@@ -3026,6 +3026,8 @@ def get_standard_logging_object_payload(
             ),
         )
 
+        truncate_standard_logging_payload_content(payload)
+
         return payload
     except Exception as e:
         verbose_logger.exception(
@@ -3040,20 +3042,26 @@ def truncate_standard_logging_payload_content(
     """
     Truncate error strings and message content in logging payload
 
-    Some loggers like DataDog have a limit on the size of the payload. (1MB)
+    Most logging integrations - DataDog / GCS Bucket / have a limit on the size of the payload. ~around(1MB)
 
     This function truncates the error string and the message content if they exceed a certain length.
     """
-    MAX_STR_LENGTH = 10_000
+    try:
+        MAX_STR_LENGTH = 10_000
 
-    # Truncate fields that might exceed max length
-    fields_to_truncate = ["error_str", "messages", "response"]
-    for field in fields_to_truncate:
-        _truncate_field(
-            standard_logging_object=standard_logging_object,
-            field_name=field,
-            max_length=MAX_STR_LENGTH,
+        # Truncate fields that might exceed max length
+        fields_to_truncate = ["error_str", "messages", "response"]
+        for field in fields_to_truncate:
+            _truncate_field(
+                standard_logging_object=standard_logging_object,
+                field_name=field,
+                max_length=MAX_STR_LENGTH,
+            )
+    except Exception as e:
+        verbose_logger.exception(
+            "Error truncating standard logging payload - {}".format(str(e))
         )
+        return
 
 
 def _truncate_text(text: str, max_length: int) -> str:
