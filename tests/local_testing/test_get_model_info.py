a3e65c9bcb7bb5837cba92084dec757ff6ca9387
Krish Dholakia <krrishdholakia@gmail.com>
2025-01-10 17:49:05 -0800

    LiteLLM Minor Fixes & Improvements (01/10/2025) - p1 (#7670)
    * test(test_get_model_info.py): add unit test confirming router deployment updates global 'get_model_info'
    
    * fix(get_supported_openai_params.py): fix custom llm provider 'get_supported_openai_params'
    
    Fixes https://github.com/BerriAI/litellm/issues/7668
    
    * docs(azure.md): clarify how azure ad token refresh on proxy works
    
    Closes https://github.com/BerriAI/litellm/issues/7665

--- tests/local_testing/test_get_model_info.py
+++ tests/local_testing/test_get_model_info.py
@@ -285,3 +285,28 @@ def test_get_model_info_custom_provider():
     get_model_info(
         model="my-custom-llm/my-fake-model"
     )  # 💥 "Exception: This model isn't mapped yet." in v1.56.10
+
+
+def test_get_model_info_custom_model_router():
+    from litellm import Router
+    from litellm import get_model_info
+
+    litellm._turn_on_debug()
+
+    router = Router(
+        model_list=[
+            {
+                "model_name": "ma-summary",
+                "litellm_params": {
+                    "api_base": "http://ma-mix-llm-serving.cicero.svc.cluster.local/v1",
+                    "input_cost_per_token": 1,
+                    "output_cost_per_token": 1,
+                    "model": "openai/meta-llama/Meta-Llama-3-8B-Instruct",
+                    "model_id": "c20d603e-1166-4e0f-aa65-ed9c476ad4ca",
+                },
+            }
+        ]
+    )
+    info = get_model_info("openai/meta-llama/Meta-Llama-3-8B-Instruct")
+    print("info", info)
+    assert info is not None
