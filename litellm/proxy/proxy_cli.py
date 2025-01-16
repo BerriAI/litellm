6033eb9aba470f2f7ffcba0f6b30818fa4624819
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-01-16 12:02:08 -0800

    remove uvloop dep

--- litellm/proxy/proxy_cli.py
+++ litellm/proxy/proxy_cli.py
@@ -673,7 +673,6 @@ def run_server(  # noqa: PLR0915
                 uvicorn_args["ssl_certfile"] = ssl_certfile_path
             uvicorn.run(
                 **uvicorn_args,
-                loop="uvloop",
                 workers=num_workers,
             )
         elif run_gunicorn is True:
