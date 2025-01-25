ad5a03308d9a309bc72357c75ab4e4455785d342
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-01-25 10:01:35 -0800

    (QA / testing) - Add unit testing for key model access checks  (#7999)
    * fix _model_matches_any_wildcard_pattern_in_list
    
    * fix docstring

--- tests/proxy_unit_tests/test_auth_checks.py
+++ tests/proxy_unit_tests/test_auth_checks.py
@@ -110,7 +110,10 @@ async def test_handle_failed_db_connection():
 
 @pytest.mark.parametrize(
     "model, expect_to_work",
-    [("openai/gpt-4o-mini", True), ("openai/gpt-4o", False)],
+    [
+        ("openai/gpt-4o-mini", True),
+        ("openai/gpt-4o", False),
+    ],
 )
 @pytest.mark.asyncio
 async def test_can_key_call_model(model, expect_to_work):
@@ -210,6 +213,82 @@ async def test_can_team_call_model(model, expect_to_work):
         assert model_in_access_group(**args)
     else:
         assert not model_in_access_group(**args)
+
+
+@pytest.mark.parametrize(
+    "key_models, model, expect_to_work",
+    [
+        (["openai/*"], "openai/gpt-4o", True),
+        (["openai/*"], "openai/gpt-4o-mini", True),
+        (["openai/*"], "openaiz/gpt-4o-mini", False),
+        (["bedrock/*"], "bedrock/anthropic.claude-3-5-sonnet-20240620", True),
+        (["bedrock/*"], "bedrockz/anthropic.claude-3-5-sonnet-20240620", False),
+        (["bedrock/us.*"], "bedrock/us.amazon.nova-micro-v1:0", True),
+    ],
+)
+@pytest.mark.asyncio
+async def test_can_key_call_model_wildcard_access(key_models, model, expect_to_work):
+    from litellm.proxy.auth.auth_checks import can_key_call_model
+    from fastapi import HTTPException
+
+    llm_model_list = [
+        {
+            "model_name": "openai/*",
+            "litellm_params": {
+                "model": "openai/*",
+                "api_key": "test-api-key",
+            },
+            "model_info": {
+                "id": "e6e7006f83029df40ebc02ddd068890253f4cd3092bcb203d3d8e6f6f606f30f",
+                "db_model": False,
+            },
+        },
+        {
+            "model_name": "bedrock/*",
+            "litellm_params": {
+                "model": "bedrock/*",
+                "api_key": "test-api-key",
+            },
+            "model_info": {
+                "id": "e6e7006f83029df40ebc02ddd068890253f4cd3092bcb203d3d8e6f6f606f30f",
+                "db_model": False,
+            },
+        },
+        {
+            "model_name": "openai/gpt-4o",
+            "litellm_params": {
+                "model": "openai/gpt-4o",
+                "api_key": "test-api-key",
+            },
+            "model_info": {
+                "id": "0cfcd87f2cb12a783a466888d05c6c89df66db23e01cecd75ec0b83aed73c9ad",
+                "db_model": False,
+            },
+        },
+    ]
+    router = litellm.Router(model_list=llm_model_list)
+
+    user_api_key_object = UserAPIKeyAuth(
+        models=key_models,
+    )
+
+    if expect_to_work:
+        await can_key_call_model(
+            model=model,
+            llm_model_list=llm_model_list,
+            valid_token=user_api_key_object,
+            llm_router=router,
+        )
+    else:
+        with pytest.raises(Exception) as e:
+            await can_key_call_model(
+                model=model,
+                llm_model_list=llm_model_list,
+                valid_token=user_api_key_object,
+                llm_router=router,
+            )
+
+            print(e)
 
 
 @pytest.mark.asyncio
