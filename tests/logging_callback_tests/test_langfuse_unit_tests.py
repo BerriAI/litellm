e1a5fdec949e8223fd3210a3caf3ab240b15c17b
Krish Dholakia <krrishdholakia@gmail.com>
2025-01-25 16:50:57 -0800

    Litellm dev 01 25 2025 p2 (#8003)
    * fix(base_utils.py): supported nested json schema passed in for anthropic calls
    
    * refactor(base_utils.py): refactor ref parsing to prevent infinite loop
    
    * test(test_openai_endpoints.py): refactor anthropic test to use bedrock
    
    * fix(langfuse_prompt_management.py): add unit test for sync langfuse calls
    
    Resolves https://github.com/BerriAI/litellm/issues/7938#issuecomment-2613293757

--- tests/logging_callback_tests/test_langfuse_unit_tests.py
+++ tests/logging_callback_tests/test_langfuse_unit_tests.py
@@ -14,7 +14,7 @@ from litellm.integrations.langfuse.langfuse import (
 from litellm.integrations.langfuse.langfuse_handler import LangFuseHandler
 from litellm.litellm_core_utils.litellm_logging import DynamicLoggingCache
 from unittest.mock import Mock, patch
-
+from respx import MockRouter
 from litellm.types.utils import (
     StandardLoggingPayload,
     StandardLoggingModelInformation,
@@ -292,3 +292,31 @@ def test_get_langfuse_tags():
     mock_payload["request_tags"] = []
     result = global_langfuse_logger._get_langfuse_tags(mock_payload)
     assert result == []
+
+
+def test_langfuse_e2e_sync(monkeypatch):
+    from litellm import completion
+    import litellm
+    import respx
+    import httpx
+    import time
+
+    litellm._turn_on_debug()
+    monkeypatch.setattr(litellm, "success_callback", ["langfuse"])
+
+    with respx.mock:
+        # Mock Langfuse
+        # Mock any Langfuse endpoint
+        langfuse_mock = respx.post(
+            "https://*.cloud.langfuse.com/api/public/ingestion"
+        ).mock(return_value=httpx.Response(200))
+        completion(
+            model="openai/my-fake-endpoint",
+            messages=[{"role": "user", "content": "hello from litellm"}],
+            stream=False,
+            mock_response="Hello from litellm 2",
+        )
+
+        time.sleep(3)
+
+        assert langfuse_mock.called
