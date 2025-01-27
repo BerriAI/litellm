55e299c9fc4a997d33c4759fb185758bfe4f6a8b
Aymeric <aymeric.roucher@gmail.com>
2025-01-27 17:25:19 +0100

    Add smolagents

--- docs/my-website/sidebars.js
+++ docs/my-website/sidebars.js
@@ -436,6 +436,7 @@ const sidebars = {
             slug: "/project",
           },
           items: [
+            "projects/🤗 smolagents",
             "projects/Docq.AI",
             "projects/OpenInterpreter",
             "projects/dbally",
