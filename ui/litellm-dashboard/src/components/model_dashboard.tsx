d503f1cf8648b89609b402bd92d5a5fa8f08099b
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-01-28 22:10:38 -0800

    ui fix entering api keys

--- ui/litellm-dashboard/src/components/model_dashboard.tsx
+++ ui/litellm-dashboard/src/components/model_dashboard.tsx
@@ -1918,6 +1918,152 @@ const ModelDashboard: React.FC<ModelDashboardProps> = ({
                         </Text>
                       </Col>
                     </Row>
+                                            {/* All the provider-specific fields */}
+                                            {dynamicProviderForm !== undefined &&
+                          dynamicProviderForm.fields.length > 0 && (
+                            <DynamicFields
+                              fields={dynamicProviderForm.fields}
+                              selectedProvider={dynamicProviderForm.name}
+                            />
+                          )}
+                        
+                        {selectedProvider != Providers.Bedrock &&
+                          selectedProvider != Providers.Vertex_AI &&
+                          selectedProvider != Providers.Ollama &&
+                          (dynamicProviderForm === undefined ||
+                            dynamicProviderForm.fields.length == 0) && (
+                            <Form.Item
+                              rules={[{ required: true, message: "Required" }]}
+                              label="API Key"
+                              name="api_key"
+                              tooltip="LLM API Credentials"
+                            >
+                              <TextInput placeholder="sk-" type="password" />
+                            </Form.Item>
+                          )}
+                        {selectedProvider == Providers.OpenAI && (
+                          <Form.Item label="Organization ID" name="organization">
+                            <TextInput placeholder="[OPTIONAL] my-unique-org" />
+                          </Form.Item>
+                        )}
+                        {selectedProvider == Providers.Vertex_AI && (
+                          <Form.Item
+                            rules={[{ required: true, message: "Required" }]}
+                            label="Vertex Project"
+                            name="vertex_project"
+                          >
+                            <TextInput placeholder="adroit-cadet-1234.." />
+                          </Form.Item>
+                        )}
+                        {selectedProvider == Providers.Vertex_AI && (
+                          <Form.Item
+                            rules={[{ required: true, message: "Required" }]}
+                            label="Vertex Location"
+                            name="vertex_location"
+                          >
+                            <TextInput placeholder="us-east-1" />
+                          </Form.Item>
+                        )}
+                        {selectedProvider == Providers.Vertex_AI && (
+                          <Form.Item
+                            rules={[{ required: true, message: "Required" }]}
+                            label="Vertex Credentials"
+                            name="vertex_credentials"
+                            className="mb-0"
+                          >
+                            <Upload {...props}>
+                              <Button2 icon={<UploadOutlined />}>
+                                Click to Upload
+                              </Button2>
+                            </Upload>
+                          </Form.Item>
+                        )}
+                        {selectedProvider == Providers.Vertex_AI && (
+                          <Row>
+                            <Col span={10}></Col>
+                            <Col span={10}>
+                              <Text className="mb-3 mt-1">
+                                Give litellm a gcp service account(.json file), so it
+                                can make the relevant calls
+                              </Text>
+                            </Col>
+                          </Row>
+                        )}
+                        {(selectedProvider == Providers.Azure ||
+                          selectedProvider == Providers.OpenAI_Compatible) && (
+                          <Form.Item
+                            rules={[{ required: true, message: "Required" }]}
+                            label="API Base"
+                            name="api_base"
+                          >
+                            <TextInput placeholder="https://..." />
+                          </Form.Item>
+                        )}
+                        {selectedProvider == Providers.Azure && (
+                          <Form.Item
+                            label="API Version"
+                            name="api_version"
+                            tooltip="By default litellm will use the latest version. If you want to use a different version, you can specify it here"
+                          >
+                            <TextInput placeholder="2023-07-01-preview" />
+                          </Form.Item>
+                        )}
+                        {selectedProvider == Providers.Azure && (
+                          <div>
+                            <Form.Item
+                              label="Base Model"
+                              name="base_model"
+                              className="mb-0"
+                            >
+                              <TextInput placeholder="azure/gpt-3.5-turbo" />
+                            </Form.Item>
+                            <Row>
+                              <Col span={10}></Col>
+                              <Col span={10}>
+                                <Text className="mb-2">
+                                  The actual model your azure deployment uses. Used
+                                  for accurate cost tracking. Select name from{" "}
+                                  <Link
+                                    href="https://github.com/BerriAI/litellm/blob/main/model_prices_and_context_window.json"
+                                    target="_blank"
+                                  >
+                                    here
+                                  </Link>
+                                </Text>
+                              </Col>
+                            </Row>
+                          </div>
+                        )}
+                        {selectedProvider == Providers.Bedrock && (
+                          <Form.Item
+                            rules={[{ required: true, message: "Required" }]}
+                            label="AWS Access Key ID"
+                            name="aws_access_key_id"
+                            tooltip="You can provide the raw key or the environment variable (e.g. `os.environ/MY_SECRET_KEY`)."
+                          >
+                            <TextInput placeholder="" />
+                          </Form.Item>
+                        )}
+                        {selectedProvider == Providers.Bedrock && (
+                          <Form.Item
+                            rules={[{ required: true, message: "Required" }]}
+                            label="AWS Secret Access Key"
+                            name="aws_secret_access_key"
+                            tooltip="You can provide the raw key or the environment variable (e.g. `os.environ/MY_SECRET_KEY`)."
+                          >
+                            <TextInput placeholder="" />
+                          </Form.Item>
+                        )}
+                        {selectedProvider == Providers.Bedrock && (
+                          <Form.Item
+                            rules={[{ required: true, message: "Required" }]}
+                            label="AWS Region Name"
+                            name="aws_region_name"
+                            tooltip="You can provide the raw key or the environment variable (e.g. `os.environ/MY_SECRET_KEY`)."
+                          >
+                            <TextInput placeholder="us-east-1" />
+                          </Form.Item>
+                        )}
                         <Form.Item
                           label="LiteLLM Params"
                           name="litellm_extra_params"
