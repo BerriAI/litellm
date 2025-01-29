87b21b38e71833037963d412ade9a3e7a465ffd8
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-01-28 18:05:00 -0800

    completed LoggingCallbackManager

--- litellm/litellm_core_utils/logging_callback_manager.py
+++ litellm/litellm_core_utils/logging_callback_manager.py
@@ -21,18 +21,17 @@ class LoggingCallbackManager:
         """
         Add a input callback to litellm.input_callback
         """
-        if isinstance(callback, str):
-            self._add_string_callback_to_list(
-                callback=callback, parent_list=litellm.input_callback
-            )
-        pass
+        self._safe_add_callback_to_list(
+            callback=callback, parent_list=litellm.input_callback
+        )
 
     def add_service_callback(self, callback: Union[CustomLogger, str, Callable]):
         """
         Add a service callback to litellm.service_callback
         """
-
-        pass
+        self._safe_add_callback_to_list(
+            callback=callback, parent_list=litellm.service_callback
+        )
 
     def add_callback(self, callback: Union[CustomLogger, str, Callable]):
         """
@@ -40,45 +39,41 @@ class LoggingCallbackManager:
 
         Ensures no duplicates are added.
         """
-        pass
+        self._safe_add_callback_to_list(
+            callback=callback, parent_list=litellm.callbacks  # type: ignore
+        )
 
     def add_sync_success_callback(self, callback: Union[CustomLogger, str, Callable]):
         """
         Add a success callback to `litellm.success_callback`
         """
-        pass
+        self._safe_add_callback_to_list(
+            callback=callback, parent_list=litellm.success_callback
+        )
 
     def add_sync_failure_callback(self, callback: Union[CustomLogger, str, Callable]):
         """
         Add a failure callback to `litellm.failure_callback`
         """
-        pass
+        self._safe_add_callback_to_list(
+            callback=callback, parent_list=litellm.failure_callback
+        )
 
     def add_async_success_callback(self, callback: Union[CustomLogger, Callable, str]):
         """
         Add a success callback to litellm._async_success_callback
         """
-        if isinstance(callback, str):
-            self._add_string_callback_to_list(
-                callback=callback, parent_list=litellm._async_success_callback
-            )
-        elif isinstance(callback, Callable):
-            self._add_callback_function_to_list(
-                callback=callback, parent_list=litellm._async_success_callback
-            )
+        self._safe_add_callback_to_list(
+            callback=callback, parent_list=litellm._async_success_callback
+        )
 
     def add_async_failure_callback(self, callback: Union[CustomLogger, Callable, str]):
         """
         Add a failure callback to litellm._async_failure_callback
         """
-        if isinstance(callback, str):
-            self._add_string_callback_to_list(
-                callback=callback, parent_list=litellm._async_failure_callback
-            )
-        elif isinstance(callback, Callable):
-            self._add_callback_function_to_list(
-                callback=callback, parent_list=litellm._async_failure_callback
-            )
+        self._safe_add_callback_to_list(
+            callback=callback, parent_list=litellm._async_failure_callback
+        )
 
     def add_success_callback_sync_and_async(self, callback: Union[CustomLogger, str]):
         """
@@ -138,6 +133,30 @@ class LoggingCallbackManager:
             # service
             self.add_service_callback(custom_logger)
 
+    def _safe_add_callback_to_list(
+        self,
+        callback: Union[CustomLogger, Callable, str],
+        parent_list: List[Union[CustomLogger, Callable, str]],
+    ):
+        """
+        Safe add a callback to a list, if the callback is already in the list, do not add it again.
+
+        Ensures no duplicates are added for `str`, `Callable`, and `CustomLogger` callbacks.
+        """
+        if isinstance(callback, str):
+            self._add_string_callback_to_list(
+                callback=callback, parent_list=parent_list
+            )
+        elif isinstance(callback, Callable):
+            self._add_callback_function_to_list(
+                callback=callback, parent_list=parent_list
+            )
+        elif isinstance(callback, CustomLogger):
+            self._add_custom_logger_to_list(
+                custom_logger=callback,
+                parent_list=parent_list,
+            )
+
     def _add_callback_function_to_list(
         self, callback: Callable, parent_list: List[Union[CustomLogger, Callable, str]]
     ):
@@ -151,6 +170,27 @@ class LoggingCallbackManager:
             verbose_logger.debug(
                 f"Callback function {callback.__name__} already exists in {parent_list}, not adding again.."
             )
+
+    def _add_custom_logger_to_list(
+        self,
+        custom_logger: CustomLogger,
+        parent_list: List[Union[CustomLogger, Callable, str]],
+    ):
+        """
+        Add a custom logger to a list, if another instance of the same custom logger exists in the list, do not add it again.
+        """
+        # Check if an instance of the same class already exists in the list
+        logger_class = custom_logger.__class__
+        for existing_logger in parent_list:
+            if isinstance(existing_logger, CustomLogger) and isinstance(
+                existing_logger, logger_class
+            ):
+                verbose_logger.debug(
+                    f"Custom logger of type {logger_class.__name__} already exists in {parent_list}, not adding again.."
+                )
+                return
+
+        parent_list.append(custom_logger)
 
     def _reset_all_callbacks(self):
         """
