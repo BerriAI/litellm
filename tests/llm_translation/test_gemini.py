de207eb9a281089f3b1484398e8319ccebc9015d
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-01-29 20:55:36 -0800

    run ci/cd again

--- tests/llm_translation/test_gemini.py
+++ tests/llm_translation/test_gemini.py
@@ -5,7 +5,7 @@ import pytest
 
 sys.path.insert(
     0, os.path.abspath("../..")
-)  # Adds the parent directory to the system path
+)  # Adds the parent directory to the system paths
 
 from base_llm_unit_tests import BaseLLMChatTest
 from litellm.llms.vertex_ai.context_caching.transformation import (
