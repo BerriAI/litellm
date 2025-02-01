f3a95b16398afcb281d177bf30939be7fb4d9723
Krrish Dholakia <krrishdholakia@gmail.com>
2025-02-01 08:32:42 -0800

    test: fix test

--- litellm/proxy/management_endpoints/team_endpoints.py
+++ litellm/proxy/management_endpoints/team_endpoints.py
@@ -70,6 +70,7 @@ router = APIRouter()
 def _is_user_team_admin(
     user_api_key_dict: UserAPIKeyAuth, team_obj: LiteLLM_TeamTable
 ) -> bool:
+
     for member in team_obj.members_with_roles:
         if member.user_id is not None and member.user_id == user_api_key_dict.user_id:
             return True
@@ -605,7 +606,10 @@ def team_member_add_duplication_check(
     def _check_member_duplication(member: Member):
         if member.user_id in [m.user_id for m in existing_team_row.members_with_roles]:
             raise HTTPException(
-                status_code=400, detail={"error": "User already in team"}
+                status_code=400,
+                detail={
+                    "error": f"User={member.user_id} already in team. Existing members={existing_team_row.members_with_roles}"
+                },
             )
 
     if isinstance(data.member, Member):
