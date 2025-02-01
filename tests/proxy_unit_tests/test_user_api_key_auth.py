845076f89a19342eddf7935dbaa2f0912cb2f3cc
Krrish Dholakia <krrishdholakia@gmail.com>
2025-01-31 21:24:35 -0800

    test: fix tests post refactor

--- tests/proxy_unit_tests/test_user_api_key_auth.py
+++ tests/proxy_unit_tests/test_user_api_key_auth.py
@@ -882,7 +882,7 @@ def test_user_api_key_auth_end_user_str():
 
 
 def test_can_rbac_role_call_model():
-    from litellm.proxy.auth.user_api_key_auth import can_rbac_role_call_model
+    from litellm.proxy.auth.handle_jwt import JWTAuthManager
     from litellm.proxy._types import RoleBasedPermissions
 
     roles_based_permissions = [
@@ -896,21 +896,21 @@ def test_can_rbac_role_call_model():
         ),
     ]
 
-    assert can_rbac_role_call_model(
+    assert JWTAuthManager.can_rbac_role_call_model(
         rbac_role=LitellmUserRoles.INTERNAL_USER,
         general_settings={"role_permissions": roles_based_permissions},
         model="gpt-4",
     )
 
     with pytest.raises(HTTPException):
-        can_rbac_role_call_model(
+        JWTAuthManager.can_rbac_role_call_model(
             rbac_role=LitellmUserRoles.INTERNAL_USER,
             general_settings={"role_permissions": roles_based_permissions},
             model="gpt-4o",
         )
 
     with pytest.raises(HTTPException):
-        can_rbac_role_call_model(
+        JWTAuthManager.can_rbac_role_call_model(
             rbac_role=LitellmUserRoles.PROXY_ADMIN,
             general_settings={"role_permissions": roles_based_permissions},
             model="gpt-4o",
@@ -918,15 +918,15 @@ def test_can_rbac_role_call_model():
 
 
 def test_can_rbac_role_call_model_no_role_permissions():
-    from litellm.proxy.auth.user_api_key_auth import can_rbac_role_call_model
+    from litellm.proxy.auth.handle_jwt import JWTAuthManager
 
-    assert can_rbac_role_call_model(
+    assert JWTAuthManager.can_rbac_role_call_model(
         rbac_role=LitellmUserRoles.INTERNAL_USER,
         general_settings={},
         model="gpt-4",
     )
 
-    assert can_rbac_role_call_model(
+    assert JWTAuthManager.can_rbac_role_call_model(
         rbac_role=LitellmUserRoles.PROXY_ADMIN,
         general_settings={"role_permissions": []},
         model="anthropic-claude",
