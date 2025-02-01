0d9c6c4199b09a4f6329c29278c910d35ab3034d
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-01-31 21:07:22 -0800

    new release

--- litellm/__init__.py
+++ litellm/__init__.py
@@ -69,10 +69,10 @@ from enum import Enum
 litellm_mode = os.getenv("LITELLM_MODE", "DEV")  # "PRODUCTION", "DEV"
 if litellm_mode == "DEV":
     dotenv.load_dotenv()
-###############################################
+################################################
 if set_verbose == True:
     _turn_on_debug()
-###############################################
+################################################
 ### Callbacks /Logging / Success / Failure Handlers #####
 CALLBACK_TYPES = Union[str, Callable, CustomLogger]
 input_callback: List[CALLBACK_TYPES] = []
