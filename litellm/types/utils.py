5907b36f36bea0c93ab23ff8ba73635e2d9bce6b
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-02-06 08:28:12 -0800

    add assembly ai as a known provider

--- litellm/types/utils.py
+++ litellm/types/utils.py
@@ -1870,6 +1870,7 @@ class LlmProviders(str, Enum):
     LANGFUSE = "langfuse"
     HUMANLOOP = "humanloop"
     TOPAZ = "topaz"
+    ASSEMBLYAI = "assemblyai"
 
 
 # Create a set of all provider values for quick lookup
