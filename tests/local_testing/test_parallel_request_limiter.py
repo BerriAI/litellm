d6451aee90242ec78ada9c37b39cef93cf947b5e
Krish Dholakia <krrishdholakia@gmail.com>
2025-02-05 20:23:08 -0800

    Improve rpm check on keys  (#8301)
    * fix(parallel_request_limiter.py): initial commit that solves the rpm limit check on keys
    
    Fixes https://github.com/BerriAI/litellm/issues/6938
    
    * fix(parallel_request_limiter.py): simpler approach - just increment RPM in pre call hook instead of on success
    
    * fix(parallel_request_limiter.py): pass testing
    
    * fix: fix linting error
    
    * fix(parallel_request_limiter.py): fix parallel request check for keys

--- tests/local_testing/test_parallel_request_limiter.py
+++ tests/local_testing/test_parallel_request_limiter.py
@@ -126,14 +126,15 @@ async def test_pre_call_hook_rpm_limits():
 
     kwargs = {"litellm_params": {"metadata": {"user_api_key": _api_key}}}
 
-    await parallel_request_handler.async_log_success_event(
-        kwargs=kwargs,
-        response_obj="",
-        start_time="",
-        end_time="",
+    ## Expected cache val: {"current_requests": 0, "current_tpm": 0, "current_rpm": 1}
+    await parallel_request_handler.async_pre_call_hook(
+        user_api_key_dict=user_api_key_dict,
+        cache=local_cache,
+        data={},
+        call_type="",
     )
 
-    ## Expected cache val: {"current_requests": 0, "current_tpm": 0, "current_rpm": 1}
+    await asyncio.sleep(2)
 
     try:
         await parallel_request_handler.async_pre_call_hook(
@@ -148,6 +149,7 @@ async def test_pre_call_hook_rpm_limits():
         assert e.status_code == 429
 
 
+@pytest.mark.flaky(retries=6, delay=1)
 @pytest.mark.asyncio
 async def test_pre_call_hook_rpm_limits_retry_after():
     """
@@ -169,12 +171,14 @@ async def test_pre_call_hook_rpm_limits_retry_after():
 
     kwargs = {"litellm_params": {"metadata": {"user_api_key": _api_key}}}
 
-    await parallel_request_handler.async_log_success_event(
-        kwargs=kwargs,
-        response_obj="",
-        start_time="",
-        end_time="",
+    await parallel_request_handler.async_pre_call_hook(
+        user_api_key_dict=user_api_key_dict,
+        cache=local_cache,
+        data={},
+        call_type="",
     )
+
+    await asyncio.sleep(2)
 
     ## Expected cache val: {"current_requests": 0, "current_tpm": 0, "current_rpm": 1}
 
@@ -224,14 +228,14 @@ async def test_pre_call_hook_team_rpm_limits():
         }
     }
 
-    await parallel_request_handler.async_log_success_event(
-        kwargs=kwargs,
-        response_obj="",
-        start_time="",
-        end_time="",
+    await parallel_request_handler.async_pre_call_hook(
+        user_api_key_dict=user_api_key_dict,
+        cache=local_cache,
+        data={},
+        call_type="",
     )
 
-    print(f"local_cache: {local_cache}")
+    await asyncio.sleep(2)
 
     ## Expected cache val: {"current_requests": 0, "current_tpm": 0, "current_rpm": 1}
 
@@ -1081,12 +1085,14 @@ async def test_pre_call_hook_rpm_limits_per_model():
         },
     }
 
-    await parallel_request_handler.async_log_success_event(
-        kwargs=kwargs,
-        response_obj="",
-        start_time="",
-        end_time="",
+    await parallel_request_handler.async_pre_call_hook(
+        user_api_key_dict=user_api_key_dict,
+        cache=local_cache,
+        data={"model": model},
+        call_type="",
     )
+
+    await asyncio.sleep(2)
 
     ## Expected cache val: {"current_requests": 0, "current_tpm": 0, "current_rpm": 1}
 
@@ -1102,10 +1108,6 @@ async def test_pre_call_hook_rpm_limits_per_model():
     except Exception as e:
         assert e.status_code == 429
         print("got error=", e)
-        assert (
-            "limit reached Hit RPM limit for model: azure-model on LiteLLM Virtual Key user_api_key_hash: c11e7177eb60c80cf983ddf8ca98f2dc1272d4c612204ce9bedd2460b18939cc"
-            in str(e)
-        )
 
 
 @pytest.mark.flaky(retries=6, delay=2)
@@ -1191,13 +1193,6 @@ async def test_pre_call_hook_tpm_limits_per_model():
         == 11
     )
 
-    assert (
-        parallel_request_handler.internal_usage_cache.get_cache(
-            key=request_count_api_key
-        )["current_rpm"]
-        == 1
-    )
-
     ## Expected cache val: {"current_requests": 0, "current_tpm": 11, "current_rpm": "1"}
 
     try:
@@ -1212,10 +1207,6 @@ async def test_pre_call_hook_tpm_limits_per_model():
     except Exception as e:
         assert e.status_code == 429
         print("got error=", e)
-        assert (
-            "request limit reached Hit TPM limit for model: azure-model on LiteLLM Virtual Key user_api_key_hash"
-            in str(e)
-        )
 
 
 @pytest.mark.asyncio
