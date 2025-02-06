f44dbccc161798e862ed9df27d2d24cd57ce023a
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-02-06 09:39:57 -0800

    add e2e test assembly

--- tests/store_model_in_db_tests/test_adding_passthrough_model.py
+++ tests/store_model_in_db_tests/test_adding_passthrough_model.py
@@ -18,7 +18,9 @@ import os
 
 TEST_MASTER_KEY = "sk-1234"
 PROXY_BASE_URL = "http://0.0.0.0:4000"
-TEST_BASE_URL = f"{PROXY_BASE_URL}/assemblyai"
+US_BASE_URL = f"{PROXY_BASE_URL}/assemblyai"
+EU_BASE_URL = f"{PROXY_BASE_URL}/eu.assemblyai"
+ASSEMBLYAI_API_KEY_ENV_VAR = "TEST_SPECIAL_ASSEMBLYAI_API_KEY"
 
 
 def test_e2e_assemblyai_passthrough():
@@ -27,10 +29,28 @@ def test_e2e_assemblyai_passthrough():
     wait 20 seconds
     make request
     """
-    add_assembly_ai_model_to_db()
+    add_assembly_ai_model_to_db(api_base="https://api.assemblyai.com")
     virtual_key = create_virtual_key()
     # make request
-    make_assemblyai_basic_transcribe_request(virtual_key=virtual_key)
+    make_assemblyai_basic_transcribe_request(
+        virtual_key=virtual_key, assemblyai_base_url=US_BASE_URL
+    )
+
+    pass
+
+
+def test_e2e_assemblyai_passthrough_eu():
+    """
+    Test adding a pass through assemblyai model + api key + api base to the db
+    wait 20 seconds
+    make request
+    """
+    add_assembly_ai_model_to_db(api_base="https://api.eu.assemblyai.com")
+    virtual_key = create_virtual_key()
+    # make request
+    make_assemblyai_basic_transcribe_request(
+        virtual_key=virtual_key, assemblyai_base_url=EU_BASE_URL
+    )
 
     pass
 
@@ -48,7 +68,9 @@ def create_virtual_key():
     return response.json()["token"]
 
 
-def add_assembly_ai_model_to_db():
+def add_assembly_ai_model_to_db(
+    api_base: str,
+):
     """
     Add the assemblyai model to the db - makes a http request to the /model/new endpoint on PROXY_BASE_URL
     """
@@ -60,8 +82,8 @@ def add_assembly_ai_model_to_db():
             "litellm_params": {
                 "model": "assemblyai/*",
                 "custom_llm_provider": "assemblyai",
-                "api_key": os.getenv("ASSEMBLYAI_API_KEY"),
-                "api_base": "https://api.assemblyai.com",
+                "api_key": os.getenv(ASSEMBLYAI_API_KEY_ENV_VAR),
+                "api_base": api_base,
                 "use_in_pass_through": True,
             },
             "model_info": {},
@@ -71,12 +93,14 @@ def add_assembly_ai_model_to_db():
     pass
 
 
-def make_assemblyai_basic_transcribe_request(virtual_key: str):
+def make_assemblyai_basic_transcribe_request(
+    virtual_key: str, assemblyai_base_url: str
+):
     print("making basic transcribe request to assemblyai passthrough")
 
     # Replace with your API key
     aai.settings.api_key = f"Bearer {virtual_key}"
-    aai.settings.base_url = TEST_BASE_URL
+    aai.settings.base_url = assemblyai_base_url
 
     # URL of the file to transcribe
     FILE_URL = "https://assembly.ai/wildfires.mp3"
