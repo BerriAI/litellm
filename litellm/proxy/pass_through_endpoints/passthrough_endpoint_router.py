aa62b68535113f6e063e91888eb4afa4a7884c09
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-02-06 08:30:12 -0800

    fix PassthroughEndpointRouter

--- litellm/proxy/pass_through_endpoints/passthrough_endpoint_router.py
+++ litellm/proxy/pass_through_endpoints/passthrough_endpoint_router.py
@@ -13,19 +13,30 @@ class PassthroughEndpointRouter:
     def set_pass_through_credentials(
         self,
         custom_llm_provider: str,
-        api_base: str,
-        api_key: str,
-        region_name: str,
+        api_base: Optional[str],
+        api_key: Optional[str],
     ):
+        """
+        Set credentials for a pass-through endpoint. Used when a user adds a pass-through LLM endpoint on the UI.
+
+        Args:
+            custom_llm_provider: The provider of the pass-through endpoint
+            api_base: The base URL of the pass-through endpoint
+            api_key: The API key for the pass-through endpoint
+        """
         credential_name = self._get_credential_name_for_provider(
             custom_llm_provider=custom_llm_provider,
-            region_name=region_name,
+            region_name=self._get_region_name_from_api_base(
+                api_base=api_base, custom_llm_provider=custom_llm_provider
+            ),
         )
+        if api_key is None:
+            raise ValueError("api_key is required for setting pass-through credentials")
         self.credentials[credential_name] = api_key
 
     def get_credentials(
         self,
-        custom_llm_provider: Optional[str],
+        custom_llm_provider: str,
         region_name: Optional[str],
     ):
         credential_name = self._get_credential_name_for_provider(
@@ -42,22 +53,33 @@ class PassthroughEndpointRouter:
             )
             return os.environ[_env_variable_name]
 
-    @staticmethod
     def _get_credential_name_for_provider(
-        custom_llm_provider: Optional[str],
+        self,
+        custom_llm_provider: str,
         region_name: Optional[str],
     ) -> str:
-        if custom_llm_provider is None:
-            raise ValueError(
-                "custom_llm_provider is required for setting pass-through credentials"
-            )
         if region_name is None:
             return f"{custom_llm_provider.upper()}_API_KEY"
         return f"{custom_llm_provider.upper()}_{region_name.upper()}_API_KEY"
 
+    def _get_region_name_from_api_base(
+        self,
+        custom_llm_provider: str,
+        api_base: Optional[str],
+    ) -> Optional[str]:
+        """
+        Get the region name from the API base.
+
+        Each provider might have a different way of specifying the region in the API base - this is where you can use conditional logic to handle that.
+        """
+        if custom_llm_provider == "assemblyai":
+            if api_base and "eu" in api_base:
+                return "eu"
+        return None
+
     @staticmethod
     def _get_default_env_variable_name_passthrough_endpoint(
-        custom_llm_provider: Optional[str],
+        custom_llm_provider: str,
     ) -> str:
         if custom_llm_provider is None:
             raise ValueError(
