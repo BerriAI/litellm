8e0736d5ad4608d48083f009d847806f4d3a99f6
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-02-05 18:58:55 -0800

    (Refactor) - migrate bedrock invoke to `BaseLLMHTTPHandler` class (#8290)
    * initial transform for invoke
    
    * invoke transform_response
    
    * working - able to make request
    
    * working get_complete_url
    
    * working - invoke now runs on llm_http_handler
    
    * fix unused imports
    
    * track litellm overhead ms
    
    * working stream request
    
    * sign_request transform
    
    * sign_request update
    
    * use has_async_custom_stream_wrapper property
    
    * use get_async_custom_stream_wrapper in base llm http handler
    
    * fix make_call in invoke handler
    
    * fix invoke with streaming get_async_custom_stream_wrapper
    
    * working bedrock async streaming with invoke
    
    * fix make call handler for bedrock
    
    * test_all_model_configs
    
    * fix test_bedrock_custom_prompt_template
    
    * sync streaming for bedrock invoke
    
    * fix _add_stream_param_to_request_body
    
    * test_async_text_completion_bedrock
    
    * fix transform_request
    
    * fix get_supported_openai_params
    
    * fix test supports tool choice
    
    * fix test_supports_tool_choice
    
    * add unit test coverage for bedrock invoke transform
    
    * fix location of transformation files
    
    * update import loc
    
    * fix bedrock invoke unit tests
    
    * fix import for max completion tokens

---
+++ litellm/llms/bedrock/chat/invoke_transformations/anthropic_claude3_transformation.py
@@ -0,0 +1,85 @@
+import types
+from typing import List, Optional
+
+
+class AmazonAnthropicClaude3Config:
+    """
+    Reference:
+        https://us-west-2.console.aws.amazon.com/bedrock/home?region=us-west-2#/providers?model=claude
+        https://docs.anthropic.com/claude/docs/models-overview#model-comparison
+
+    Supported Params for the Amazon / Anthropic Claude 3 models:
+
+    - `max_tokens` Required (integer) max tokens. Default is 4096
+    - `anthropic_version` Required (string) version of anthropic for bedrock - e.g. "bedrock-2023-05-31"
+    - `system` Optional (string) the system prompt, conversion from openai format to this is handled in factory.py
+    - `temperature` Optional (float) The amount of randomness injected into the response
+    - `top_p` Optional (float) Use nucleus sampling.
+    - `top_k` Optional (int) Only sample from the top K options for each subsequent token
+    - `stop_sequences` Optional (List[str]) Custom text sequences that cause the model to stop generating
+    """
+
+    max_tokens: Optional[int] = 4096  # Opus, Sonnet, and Haiku default
+    anthropic_version: Optional[str] = "bedrock-2023-05-31"
+    system: Optional[str] = None
+    temperature: Optional[float] = None
+    top_p: Optional[float] = None
+    top_k: Optional[int] = None
+    stop_sequences: Optional[List[str]] = None
+
+    def __init__(
+        self,
+        max_tokens: Optional[int] = None,
+        anthropic_version: Optional[str] = None,
+    ) -> None:
+        locals_ = locals()
+        for key, value in locals_.items():
+            if key != "self" and value is not None:
+                setattr(self.__class__, key, value)
+
+    @classmethod
+    def get_config(cls):
+        return {
+            k: v
+            for k, v in cls.__dict__.items()
+            if not k.startswith("__")
+            and not isinstance(
+                v,
+                (
+                    types.FunctionType,
+                    types.BuiltinFunctionType,
+                    classmethod,
+                    staticmethod,
+                ),
+            )
+            and v is not None
+        }
+
+    def get_supported_openai_params(self):
+        return [
+            "max_tokens",
+            "max_completion_tokens",
+            "tools",
+            "tool_choice",
+            "stream",
+            "stop",
+            "temperature",
+            "top_p",
+            "extra_headers",
+        ]
+
+    def map_openai_params(self, non_default_params: dict, optional_params: dict):
+        for param, value in non_default_params.items():
+            if param == "max_tokens" or param == "max_completion_tokens":
+                optional_params["max_tokens"] = value
+            if param == "tools":
+                optional_params["tools"] = value
+            if param == "stream":
+                optional_params["stream"] = value
+            if param == "stop":
+                optional_params["stop_sequences"] = value
+            if param == "temperature":
+                optional_params["temperature"] = value
+            if param == "top_p":
+                optional_params["top_p"] = value
+        return optional_params
