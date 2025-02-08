dfbbf0bde88249e5e58130c33b693be4fbe226c1
Krish Dholakia <krrishdholakia@gmail.com>
2025-02-07 16:20:28 -0800

    fix: dictionary changed size during iteration error (#8327) (#8341)
    Co-authored-by: Joey Feldberg <joeyfeldberg@users.noreply.github.com>
    Co-authored-by: Joey Feldberg <12495578+joeyfeldberg@users.noreply.github.com>

--- litellm/llms/bedrock/chat/converse_transformation.py
+++ litellm/llms/bedrock/chat/converse_transformation.py
@@ -63,7 +63,7 @@ class AmazonConverseConfig(BaseConfig):
         topP: Optional[int] = None,
         topK: Optional[int] = None,
     ) -> None:
-        locals_ = locals()
+        locals_ = locals().copy()
         for key, value in locals_.items():
             if key != "self" and value is not None:
                 setattr(self.__class__, key, value)
