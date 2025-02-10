5408611fe76bb5ae78140c430230fec9fcec0aa2
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-02-10 14:57:58 -0800

    remove stub

--- litellm/stubs/redis/asyncio/cluster.pyi
+++
@@ -1,8 +0,0 @@
-"""
-Custom stub for redis.asyncio.cluster to add missing attributes for RedisCluster and ClusterPipeline.
-"""
-
-from redis.asyncio.client import Redis
-
-class RedisCluster(Redis):
-    pass
