0b2a453cb105121de9bdd483f0de95772ddc6c3e
Krrish Dholakia <krrishdholakia@gmail.com>
2025-02-17 17:54:18 -0800

    fix(custom_validate.py): working jwt custom validate

--- litellm/proxy/custom_validate.py
+++ litellm/proxy/custom_validate.py
@@ -1,2 +1,5 @@
-def my_custom_validate(token: str) -> bool:
-    return False
+from typing import Literal
+
+
+def my_custom_validate(token: str) -> Literal[True]:
+    raise Exception("Custom validate failed")
