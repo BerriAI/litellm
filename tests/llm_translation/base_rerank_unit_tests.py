bc190f201ec29d38009feece7b8c277f36092785
Krrish Dholakia <krrishdholakia@gmail.com>
2025-02-20 15:32:49 -0800

    fix(cost_calculator.py): bedrock/common_utils.py
    get base model from model w/ arn -> handles rerank model

--- tests/llm_translation/base_rerank_unit_tests.py
+++ tests/llm_translation/base_rerank_unit_tests.py
@@ -80,6 +80,8 @@ class BaseLLMRerankTest(ABC):
     @pytest.mark.parametrize("sync_mode", [True, False])
     async def test_basic_rerank(self, sync_mode):
         litellm.set_verbose = True
+        os.environ["LITELLM_LOCAL_MODEL_COST_MAP"] = "True"
+        litellm.model_cost = litellm.get_model_cost_map(url="")
         rerank_call_args = self.get_base_rerank_call_args()
         custom_llm_provider = self.get_custom_llm_provider()
         if sync_mode is True:
