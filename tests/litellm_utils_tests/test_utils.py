b5a171c0a97a8f7b6d1a3943af05333ac9afb309
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-02-22 17:42:31 -0800

    test_models_by_provider

--- tests/litellm_utils_tests/test_utils.py
+++ tests/litellm_utils_tests/test_utils.py
@@ -1176,6 +1176,7 @@ def test_models_by_provider():
         elif (
             v["litellm_provider"] == "sagemaker"
             or v["litellm_provider"] == "bedrock_converse"
+            or v["litellm_provider"] == "jina_ai"
         ):
             continue
         else:
