f5e1d178fd4a210b76c229df726f2e83cd8b2603
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-03-01 10:37:33 -0800

    fix dd tracing null tracer bug

--- litellm/litellm_core_utils/dd_tracing.py
+++ litellm/litellm_core_utils/dd_tracing.py
@@ -9,57 +9,53 @@ from contextlib import contextmanager
 from litellm.secret_managers.main import get_secret_bool
 
 
+class NullSpan:
+    """A no-op span implementation."""
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        pass
+
+    def finish(self):
+        pass
+
+
+@contextmanager
+def null_tracer(name, **kwargs):
+    """Context manager that yields a no-op span."""
+    yield NullSpan()
+
+
+class NullTracer:
+    """A no-op tracer implementation."""
+
+    def trace(self, name, **kwargs):
+        return NullSpan()
+
+    def wrap(self, name=None, **kwargs):
+        def decorator(f):
+            return f
+
+        return decorator
+
+
 def _should_use_dd_tracer():
-    """
-    Returns True if `USE_DDTRACE` is set to True in .env
-    """
+    """Returns True if `USE_DDTRACE` is set to True in .env"""
     return get_secret_bool("USE_DDTRACE", False) is True
 
 
-has_ddtrace = False
-try:
-    from ddtrace import tracer as dd_tracer
+# Initialize tracer
+should_use_dd_tracer = _should_use_dd_tracer()
+tracer = None
 
-    if _should_use_dd_tracer():
-        has_ddtrace = True
-except ImportError:
-    has_ddtrace = False
+if should_use_dd_tracer:
+    try:
+        from ddtrace import tracer as dd_tracer
 
-    @contextmanager
-    def null_tracer(name, **kwargs):
-        class NullSpan:
-            def __enter__(self):
-                return self
-
-            def __exit__(self, *args):
-                pass
-
-            def finish(self):
-                pass
-
-        yield NullSpan()
-
-    class NullTracer:
-        def trace(self, name, **kwargs):
-            class NullSpan:
-                def __enter__(self):
-                    return self
-
-                def __exit__(self, *args):
-                    pass
-
-                def finish(self):
-                    pass
-
-            return NullSpan()
-
-        def wrap(self, name=None, **kwargs):
-            def decorator(f):
-                return f
-
-            return decorator
-
-    dd_tracer = NullTracer()
-
-# Export the tracer instance
-tracer = dd_tracer
+        tracer = dd_tracer
+    except ImportError:
+        tracer = null_tracer
+else:
+    tracer = null_tracer
