adabe147215dcd76852d6f72fbef4da9a1c23a9b
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-03-01 13:39:45 -0800

    (Bug fix) - don't log messages in `model_parameters` in StandardLoggingPayload (#8932)
    * define model param helper
    
    * use ModelParamHelper
    
    * get_standard_logging_model_parameters
    
    * fix code quality
    
    * get_standard_logging_model_parameters
    
    * StandardLoggingPayload
    
    * test_get_kwargs_for_cache_key
    
    * test_langsmith_key_based_logging
    
    * fix code qa
    
    * fix linting

--- litellm/litellm_core_utils/dd_tracing.py
+++ litellm/litellm_core_utils/dd_tracing.py
@@ -5,8 +5,14 @@ If the ddtrace package is not installed, the tracer will be a no-op.
 """
 
 from contextlib import contextmanager
+from typing import TYPE_CHECKING, Any, Union
 
 from litellm.secret_managers.main import get_secret_bool
+
+if TYPE_CHECKING:
+    from ddtrace.tracer import Tracer as DD_TRACER
+else:
+    DD_TRACER = Any
 
 
 class NullSpan:
@@ -53,12 +59,13 @@ def _should_use_dd_tracer():
 
 # Initialize tracer
 should_use_dd_tracer = _should_use_dd_tracer()
-tracer = None
-
+tracer: Union[NullTracer, DD_TRACER] = NullTracer()
+# We need to ensure tracer is never None and always has the required methods
 if should_use_dd_tracer:
     try:
         from ddtrace import tracer as dd_tracer
 
+        # Define the type to match what's expected by the code using this module
         tracer = dd_tracer
     except ImportError:
         tracer = NullTracer()
