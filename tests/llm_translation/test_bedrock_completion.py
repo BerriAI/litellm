cb520af41d9ce706fd7c4e63f28d65261c2354d8
Krish Dholakia <krrishdholakia@gmail.com>
2025-02-28 18:47:07 -0800

    converse_transformation: pass 'description' if set in response_format (#8907)
    * test(test_bedrock_completion.py): e2e test ensuring tool description is passed in
    
    * fix(converse_transformation.py): pass description, if set
    
    * fix(transformation.py): Fixes https://github.com/BerriAI/litellm/issues/8767#issuecomment-2689887663

--- tests/llm_translation/test_bedrock_completion.py
+++ tests/llm_translation/test_bedrock_completion.py
@@ -2715,3 +2715,42 @@ def test_bedrock_top_k_param(model, expected_params):
             assert data["top_k"] == 2
         else:
             assert data["additionalModelRequestFields"] == expected_params
+
+
+def test_bedrock_description_param():
+    from litellm import completion
+    from litellm.llms.custom_httpx.http_handler import HTTPHandler
+
+    client = HTTPHandler()
+
+    with patch.object(client, "post") as mock_post:
+        try:
+            response = completion(
+                model="bedrock/us.amazon.nova-pro-v1:0",
+                messages=[
+                    {"role": "user", "content": "What is the meaning of this poem?"}
+                ],
+                response_format={
+                    "type": "json_schema",
+                    "json_schema": {
+                        "name": "meaning_reasoning",
+                        "description": "Find the meaning inside a poem",
+                        "schema": {
+                            "type": "object",
+                            "properties": {"meaning": {"type": "string"}},
+                        },
+                    },
+                },
+                client=client,
+            )
+        except Exception as e:
+            print(e)
+        mock_post.assert_called_once()
+
+        request_body = json.loads(mock_post.call_args.kwargs["data"])
+        request_body_str = json.dumps(request_body, indent=4, default=str)
+        print("request_body=", request_body_str)
+
+        assert (
+            "Find the meaning inside a poem" in request_body_str
+        )  # assert description is passed
