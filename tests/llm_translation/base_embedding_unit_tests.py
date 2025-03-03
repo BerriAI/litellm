80b87f9db66c6cbfa7c894509002629c80c1e3a9
Utkash Dubey <utkashd@gmail.com>
2025-03-03 04:16:12 -0800

    changes

--- tests/llm_translation/base_embedding_unit_tests.py
+++ tests/llm_translation/base_embedding_unit_tests.py
@@ -84,8 +84,7 @@ class BaseLLMEmbeddingTest(ABC):
         litellm.set_verbose = True
         from litellm.utils import supports_embedding_image_input
 
-        os.environ["LITELLM_LOCAL_MODEL_COST_MAP"] = "True"
-        litellm.model_cost = litellm.get_model_cost_map(url="")
+        litellm.model_cost = litellm.get_locally_cached_model_cost_map()
 
         base_embedding_call_args = self.get_base_embedding_call_args()
         if not supports_embedding_image_input(base_embedding_call_args["model"], None):
