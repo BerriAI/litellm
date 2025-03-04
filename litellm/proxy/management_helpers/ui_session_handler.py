505987d15a0505c96e5da9f263c2eae104de7b45
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-03-03 18:38:34 -0800

    fix ui session handling

--- litellm/proxy/management_helpers/ui_session_handler.py
+++ litellm/proxy/management_helpers/ui_session_handler.py
@@ -13,6 +13,8 @@ class UISessionHandler:
             key=UISessionHandler._generate_token_name(),
             value=jwt_token,
             secure=True,
+            httponly=True,
+            samesite="strict",
         )
         return redirect_response
 
