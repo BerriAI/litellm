26f089c1ca2507fe107cbba49a4b806fa73e66f2
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-03-04 15:04:55 -0800

    fix ui session handler

--- litellm/proxy/management_helpers/ui_session_handler.py
+++ litellm/proxy/management_helpers/ui_session_handler.py
@@ -3,13 +3,15 @@ from datetime import datetime, timedelta, timezone
 
 from fastapi.responses import RedirectResponse
 
+from litellm.proxy._types import LiteLLM_JWTAuth, LitellmUserRoles
+
 
 class UISessionHandler:
 
     @staticmethod
     def build_authenticated_ui_jwt_token(
         user_id: str,
-        user_role: str,
+        user_role: LitellmUserRoles,
         premium_user: bool,
         disabled_non_admin_personal_key_creation: bool,
     ) -> str:
@@ -26,21 +28,31 @@ class UISessionHandler:
             raise ValueError("Master key is not set")
 
         expiration = datetime.now(timezone.utc) + timedelta(hours=24)
-        jwt_token = jwt.encode(  # type: ignore
-            {
-                "user_id": user_id,
-                "user_email": None,
-                "user_role": user_role,  # this is the path without sso - we can assume only admins will use this
-                "login_method": "username_password",
-                "premium_user": premium_user,
-                "auth_header_name": general_settings.get(
-                    "litellm_key_header_name", "Authorization"
-                ),
-                "iss": "litellm-proxy",  # Issuer - identifies this as an internal token
-                "aud": "litellm-ui",  # Audience - identifies this as a UI token
-                "exp": expiration,
-                "disabled_non_admin_personal_key_creation": disabled_non_admin_personal_key_creation,
-            },
+        initial_payload = {
+            "user_id": user_id,
+            "user_email": None,
+            "user_role": user_role,  # this is the path without sso - we can assume only admins will use this
+            "login_method": "username_password",
+            "premium_user": premium_user,
+            "auth_header_name": general_settings.get(
+                "litellm_key_header_name", "Authorization"
+            ),
+            "iss": "litellm-proxy",  # Issuer - identifies this as an internal token
+            "aud": "litellm-ui",  # Audience - identifies this as a UI token
+            "exp": expiration,
+            "disabled_non_admin_personal_key_creation": disabled_non_admin_personal_key_creation,
+        }
+
+        if (
+            user_role == LitellmUserRoles.PROXY_ADMIN
+            or user_role == LitellmUserRoles.PROXY_ADMIN_VIEW_ONLY
+        ):
+            initial_payload["scope"] = [
+                LiteLLM_JWTAuth().admin_jwt_scope,
+            ]
+
+        jwt_token = jwt.encode(
+            initial_payload,
             master_key,
             algorithm="HS256",
         )
