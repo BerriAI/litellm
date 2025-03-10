201fe03e141d7c6cd5c235bd066bec68e10da6ee
Krrish Dholakia <krrishdholakia@gmail.com>
2025-03-09 19:35:16 -0700

    fix(transformation.py): fix openrouter streaming with reasoning content
    Fixes https://github.com/BerriAI/litellm/issues/8193#issuecomment-270892962

--- litellm/llms/openrouter/chat/transformation.py
+++ litellm/llms/openrouter/chat/transformation.py
@@ -6,11 +6,13 @@ Calls done in OpenAI/openai.py as OpenRouter is openai-compatible.
 Docs: https://openrouter.ai/docs/parameters
 """
 
-from typing import Union
+from typing import Any, AsyncIterator, Iterator, Optional, Union
 
 import httpx
 
+from litellm.llms.base_llm.base_model_iterator import BaseModelResponseIterator
 from litellm.llms.base_llm.chat.transformation import BaseLLMException
+from litellm.types.utils import ModelResponse, ModelResponseStream
 
 from ...openai.chat.gpt_transformation import OpenAIGPTConfig
 from ..common_utils import OpenRouterException
@@ -53,3 +55,34 @@ class OpenrouterConfig(OpenAIGPTConfig):
             status_code=status_code,
             headers=headers,
         )
+
+    def get_model_response_iterator(
+        self,
+        streaming_response: Union[Iterator[str], AsyncIterator[str], ModelResponse],
+        sync_stream: bool,
+        json_mode: Optional[bool] = False,
+    ) -> Any:
+        return OpenRouterChatCompletionStreamingHandler(
+            streaming_response=streaming_response,
+            sync_stream=sync_stream,
+            json_mode=json_mode,
+        )
+
+
+class OpenRouterChatCompletionStreamingHandler(BaseModelResponseIterator):
+
+    def chunk_parser(self, chunk: dict) -> ModelResponseStream:
+        try:
+            new_choices = []
+            for choice in chunk["choices"]:
+                choice["delta"]["reasoning_content"] = choice["delta"].get("reasoning")
+                new_choices.append(choice)
+            return ModelResponseStream(
+                id=chunk["id"],
+                object="chat.completion.chunk",
+                created=chunk["created"],
+                model=chunk["model"],
+                choices=new_choices,
+            )
+        except Exception as e:
+            raise e
