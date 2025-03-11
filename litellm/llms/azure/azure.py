b58edb7fa1b6989856dcc25e84997d2f40118e38
Krrish Dholakia <krrishdholakia@gmail.com>
2025-03-11 12:24:08 -0700

    test(test_azure_common_utils.py): add unit testing for common azure client params function

--- litellm/llms/azure/azure.py
+++ litellm/llms/azure/azure.py
@@ -33,6 +33,7 @@ from .common_utils import (
     AzureOpenAIError,
     get_azure_ad_token_from_oidc,
     process_azure_headers,
+    select_azure_base_url_or_endpoint,
 )
 
 
@@ -96,18 +97,6 @@ class AzureOpenAIAssistantsAPIConfig:
                         status_code=400,
                     )
         return optional_params
-
-
-def select_azure_base_url_or_endpoint(azure_client_params: dict):
-    azure_endpoint = azure_client_params.get("azure_endpoint", None)
-    if azure_endpoint is not None:
-        # see : https://github.com/openai/openai-python/blob/3d61ed42aba652b547029095a7eb269ad4e1e957/src/openai/lib/azure.py#L192
-        if "/openai/deployments" in azure_endpoint:
-            # this is base_url, not an azure_endpoint
-            azure_client_params["base_url"] = azure_endpoint
-            azure_client_params.pop("azure_endpoint")
-
-    return azure_client_params
 
 
 def _check_dynamic_azure_params(
