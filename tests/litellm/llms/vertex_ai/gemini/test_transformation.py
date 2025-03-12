d3b804a1cc2e092d2692a5dd8bf2d520e7c13116
Vedran Vidović <vvidovic@gmail.com>
2025-03-12 14:10:32 +0100

    Optional `labels` field in Vertex AI request
    If the client sets the `labels` field in the request to the LiteLLM:
    - pass the `labels` field to the Vertex AI backend
    
    If the client sets the `metadata` field in the request to the LiteLLM:
    - if the `labels` field is not set, fill it with `metadata` key/value
      pairs for all string values

---
+++ tests/litellm/llms/vertex_ai/gemini/test_transformation.py
@@ -0,0 +1,119 @@
+import os
+import sys
+
+import pytest
+
+sys.path.insert(
+    0, os.path.abspath("../../../../..")
+)  # Adds the parent directory to the system path
+from litellm.llms.vertex_ai.gemini import transformation
+from litellm.types.llms import openai
+from litellm.types import completion
+from litellm.types.llms.vertex_ai import RequestBody
+
+@pytest.mark.asyncio
+async def test__transform_request_body_labels():
+    """
+    Test that Vertex AI requests use the optional Vertex AI
+    "labels" parameters sent by client.
+    """
+
+    # Set up the test parameters
+    model = "vertex_ai/gemini-1.5-pro"
+    messages = [
+        {"role": "user", "content": "hi"},
+        {"role": "assistant", "content": "Hello! How can I assist you today?"},
+        {"role": "user", "content": "hi"},
+    ]
+    optional_params = {
+        "labels": {"lparam1": "lvalue1", "lparam2": "lvalue2"}
+    }
+    litellm_params = {}
+    transform_request_params = {
+        "messages": messages,
+        "model": model,
+        "optional_params": optional_params,
+        "custom_llm_provider": "vertex_ai",
+        "litellm_params": litellm_params,
+        "cached_content": None,
+    }
+
+    rb: RequestBody = transformation._transform_request_body(**transform_request_params)
+
+    # Check URL
+    assert rb["contents"] == [{'parts': [{'text': 'hi'}], 'role': 'user'}, {'parts': [{'text': 'Hello! How can I assist you today?'}], 'role': 'model'}, {'parts': [{'text': 'hi'}], 'role': 'user'}]
+    assert "labels" in rb and rb["labels"] == {"lparam1": "lvalue1", "lparam2": "lvalue2"}
+
+@pytest.mark.asyncio
+async def test__transform_request_body_metadata():
+    """
+    Test that Vertex AI requests use the optional Open AI
+    "metadata" parameters sent by client.
+    """
+
+    # Set up the test parameters
+    model = "vertex_ai/gemini-1.5-pro"
+    messages = [
+        {"role": "user", "content": "hi"},
+        {"role": "assistant", "content": "Hello! How can I assist you today?"},
+        {"role": "user", "content": "hi"},
+    ]
+    optional_params = {}
+    litellm_params = {
+        "metadata": {
+            "requester_metadata": {"rparam1": "rvalue1", "rparam2": "rvalue2"}
+        }
+    }
+    transform_request_params = {
+        "messages": messages,
+        "model": model,
+        "optional_params": optional_params,
+        "custom_llm_provider": "vertex_ai",
+        "litellm_params": litellm_params,
+        "cached_content": None,
+    }
+
+    rb: RequestBody = transformation._transform_request_body(**transform_request_params)
+
+    # Check URL
+    assert rb["contents"] == [{'parts': [{'text': 'hi'}], 'role': 'user'}, {'parts': [{'text': 'Hello! How can I assist you today?'}], 'role': 'model'}, {'parts': [{'text': 'hi'}], 'role': 'user'}]
+    assert "labels" in rb and rb["labels"] == {"rparam1": "rvalue1", "rparam2": "rvalue2"}
+
+@pytest.mark.asyncio
+async def test__transform_request_body_labels_and_metadata():
+    """
+    Test that Vertex AI requests use the optional Vertex AI
+    "labels" parameters sent by client and that the "metadata"
+    optional Open AI parameters are ignored if the client uses
+    "labels" parameters.
+    """
+
+    # Set up the test parameters
+    model = "vertex_ai/gemini-1.5-pro"
+    messages = [
+        {"role": "user", "content": "hi"},
+        {"role": "assistant", "content": "Hello! How can I assist you today?"},
+        {"role": "user", "content": "hi"},
+    ]
+    optional_params = {
+        "labels": {"lparam1": "lvalue1", "lparam2": "lvalue2"}
+    }
+    litellm_params = {
+        "metadata": {
+            "requester_metadata": {"rparam1": "rvalue1", "rparam2": "rvalue2"}
+        }
+    }
+    transform_request_params = {
+        "messages": messages,
+        "model": model,
+        "optional_params": optional_params,
+        "custom_llm_provider": "vertex_ai",
+        "litellm_params": litellm_params,
+        "cached_content": None,
+    }
+
+    rb: RequestBody = transformation._transform_request_body(**transform_request_params)
+
+    # Check URL
+    assert rb["contents"] == [{'parts': [{'text': 'hi'}], 'role': 'user'}, {'parts': [{'text': 'Hello! How can I assist you today?'}], 'role': 'model'}, {'parts': [{'text': 'hi'}], 'role': 'user'}]
+    assert "labels" in rb and rb["labels"] == {"lparam1": "lvalue1", "lparam2": "lvalue2"}
