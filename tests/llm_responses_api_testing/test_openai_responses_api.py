52b43f672b7cca227d1f137455f85adbfc9c200d
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-03-11 17:35:43 -0700

    working test_basic_openai_responses_api

--- tests/llm_responses_api_testing/test_openai_responses_api.py
+++ tests/llm_responses_api_testing/test_openai_responses_api.py
@@ -1,13 +1,18 @@
 import os
 import sys
+import pytest
 
 sys.path.insert(0, os.path.abspath("../.."))
 import litellm
+import json
 
 
-def test_basic_openai_responses_api():
-    response = litellm.responses(
+@pytest.mark.asyncio
+async def test_basic_openai_responses_api():
+    litellm._turn_on_debug()
+    response = await litellm.aresponses(
         model="gpt-4o", input="Tell me a three sentence bedtime story about a unicorn."
     )
+    print("litellm response=", json.dumps(response, indent=4, default=str))
 
     # validate_responses_api_response()
