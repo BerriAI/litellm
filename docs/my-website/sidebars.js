550b63c7b503b4648f5a4fa5f0798840c1984f9b
Mark <markolofsen@gmail.com>
2025-03-16 16:51:35 +0800

    docs: add Pydantic2 documentation

--- docs/my-website/sidebars.js
+++ docs/my-website/sidebars.js
@@ -490,6 +490,7 @@ const sidebars = {
             "projects/LiteLLM Proxy",
             "projects/llm_cord",
             "projects/pgai",
+            "projects/Pydantic2",
           ],
         },
         "proxy/pii_masking",
