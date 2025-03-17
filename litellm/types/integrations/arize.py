b4fd4015bee9aa7a6a6c47dbd4054a800f144e09
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-03-17 14:41:27 -0700

    add space_id to ArizeConfig

--- litellm/types/integrations/arize.py
+++ litellm/types/integrations/arize.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Literal, Any
+from typing import TYPE_CHECKING, Any, Literal, Optional
 
 from pydantic import BaseModel
 
@@ -6,9 +6,11 @@ if TYPE_CHECKING:
     Protocol = Literal["otlp_grpc", "otlp_http"]
 else:
     Protocol = Any
-    
+
+
 class ArizeConfig(BaseModel):
     space_key: str
+    space_id: Optional[str]
     api_key: str
     protocol: Protocol
     endpoint: str
