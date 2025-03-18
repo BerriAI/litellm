bd122f631e854d7ff415a45460b4d9ecf60f2929
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-03-18 16:54:31 -0700

    fix arize config

--- litellm/types/integrations/arize.py
+++ litellm/types/integrations/arize.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Literal, Any
+from typing import TYPE_CHECKING, Any, Literal, Optional
 
 from pydantic import BaseModel
 
@@ -6,9 +6,10 @@ if TYPE_CHECKING:
     Protocol = Literal["otlp_grpc", "otlp_http"]
 else:
     Protocol = Any
-    
+
+
 class ArizeConfig(BaseModel):
-    space_key: str
-    api_key: str
+    space_key: Optional[str] = None
+    api_key: Optional[str] = None
     protocol: Protocol
     endpoint: str
