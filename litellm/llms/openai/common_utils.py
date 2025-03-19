04fedae5abd033f1c959a3673ad4d65f5fa0c643
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-03-18 17:59:46 -0700

    fix common utils

--- litellm/llms/openai/common_utils.py
+++ litellm/llms/openai/common_utils.py
@@ -163,7 +163,7 @@ class BaseOpenAILLM:
     @staticmethod
     def get_openai_client_initialization_param_fields(
         client_type: Literal["openai", "azure"]
-    ) -> list[str]:
+    ) -> List[str]:
         """Returns a list of fields that are used to initialize the OpenAI client"""
         import inspect
 
