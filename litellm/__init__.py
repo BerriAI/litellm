64f48beff75a01a61a0bb39e13d2d1d8c1066911
Hugues Chocart <chocart.hugues@icloud.com>
2025-03-20 19:02:26 +0100

    Update __init__.py

--- litellm/__init__.py
+++ litellm/__init__.py
@@ -86,6 +86,7 @@ service_callback: List[CALLBACK_TYPES] = []
 logging_callback_manager = LoggingCallbackManager()
 _custom_logger_compatible_callbacks_literal = Literal[
     "lago",
+    "lunary",
     "openmeter",
     "logfire",
     "literalai",
