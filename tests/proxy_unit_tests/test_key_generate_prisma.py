ddc9d4fbd371134b71051e4feb86c8f246674fff
Krrish Dholakia <krrishdholakia@gmail.com>
2025-03-27 14:46:40 -0700

    fix: print db url

--- tests/proxy_unit_tests/test_key_generate_prisma.py
+++ tests/proxy_unit_tests/test_key_generate_prisma.py
@@ -3286,6 +3286,7 @@ async def test_aadmin_only_routes(prisma_client):
     only an admin should be able to access admin only routes
     """
     litellm.set_verbose = True
+    print(f"os.getenv('DATABASE_URL')={os.getenv('DATABASE_URL')}")
     setattr(litellm.proxy.proxy_server, "prisma_client", prisma_client)
     setattr(litellm.proxy.proxy_server, "master_key", "sk-1234")
     await litellm.proxy.proxy_server.prisma_client.connect()
