c11293aa6ba18955ffdf8b694623fef97e35c49a
Krrish Dholakia <krrishdholakia@gmail.com>
2025-03-26 22:52:28 -0700

    test: update test

--- tests/local_testing/test_get_model_info.py
+++ tests/local_testing/test_get_model_info.py
@@ -516,6 +516,8 @@ def test_aaamodel_prices_and_context_window_json_is_valid():
                             "/v1/images/variations",
                             "/v1/images/edits",
                             "/v1/batch",
+                            "/v1/audio/transcriptions",
+                            "/v1/audio/speech",
                         ],
                     },
                 },
