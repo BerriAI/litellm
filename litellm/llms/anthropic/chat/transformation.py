e1aefb960c02c3b2a7c66392a30e10e9a2a3d8e5
Krish Dholakia <krrishdholakia@gmail.com>
2025-03-28 15:57:40 -0700

    Fix anthropic thinking + response_format (#9594)
    * fix(anthropic/chat/transformation.py): Don't set tool choice on response_format conversion when thinking is enabled
    
    Not allowed by Anthropic
    
    Fixes https://github.com/BerriAI/litellm/issues/8901
    
    * refactor: move test to base anthropic chat tests
    
    ensures consistent behaviour across vertex/anthropic/bedrock
    
    * fix(anthropic/chat/transformation.py): if thinking token is specified and max tokens is not - ensure max token to anthropic is higher than thinking tokens
    
    * feat(converse_transformation.py): correctly handle thinking + response format on Bedrock Converse
    
    Fixes https://github.com/BerriAI/litellm/issues/8901
    
    * fix(converse_transformation.py): correctly handle adding max tokens
    
    * test: handle service unavailable error

--- litellm/llms/anthropic/chat/transformation.py
+++ litellm/llms/anthropic/chat/transformation.py
@@ -300,6 +300,15 @@ class AnthropicConfig(BaseConfig):
         model: str,
         drop_params: bool,
     ) -> dict:
+
+        is_thinking_enabled = self.is_thinking_enabled(
+            non_default_params=non_default_params
+        )
+
+        ## handle thinking tokens
+        self.update_optional_params_with_thinking_tokens(
+            non_default_params=non_default_params, optional_params=optional_params
+        )
         for param, value in non_default_params.items():
             if param == "max_tokens":
                 optional_params["max_tokens"] = value
@@ -349,19 +358,23 @@ class AnthropicConfig(BaseConfig):
                 - Remember that the model will pass the input to the tool, so the name of the tool and description should be from the model’s perspective.
                 """
 
-                _tool_choice = {"name": RESPONSE_FORMAT_TOOL_NAME, "type": "tool"}
+                if not is_thinking_enabled:
+                    _tool_choice = {"name": RESPONSE_FORMAT_TOOL_NAME, "type": "tool"}
+                    optional_params["tool_choice"] = _tool_choice
+
                 _tool = self._create_json_tool_call_for_response_format(
                     json_schema=json_schema,
                 )
                 optional_params = self._add_tools_to_optional_params(
                     optional_params=optional_params, tools=[_tool]
                 )
-                optional_params["tool_choice"] = _tool_choice
+
                 optional_params["json_mode"] = True
             if param == "user":
                 optional_params["metadata"] = {"user_id": value}
             if param == "thinking":
                 optional_params["thinking"] = value
+
         return optional_params
 
     def _create_json_tool_call_for_response_format(
