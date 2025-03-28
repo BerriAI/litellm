df7063fe62403a1abec7ab94633b9880c46ede33
Krrish Dholakia <krrishdholakia@gmail.com>
2025-03-27 23:25:46 -0700

    fix(proxy_server.py): Fix https://github.com/BerriAI/litellm/issues/9576

--- litellm/proxy/proxy_server.py
+++ litellm/proxy/proxy_server.py
@@ -511,7 +511,7 @@ async def proxy_startup_event(app: FastAPI):
 
     ### LOAD MASTER KEY ###
     # check if master key set in environment - load from there
-    master_key = get_secret("LITELLM_MASTER_KEY", None)  # type: ignore
+    # master_key = get_secret("LITELLM_MASTER_KEY", None)  # type: ignore
     # check if DATABASE_URL in environment - load from there
     if prisma_client is None:
         _db_url: Optional[str] = get_secret("DATABASE_URL", None)  # type: ignore
@@ -1974,6 +1974,7 @@ class ProxyConfig:
 
             if master_key and master_key.startswith("os.environ/"):
                 master_key = get_secret(master_key)  # type: ignore
+
                 if not isinstance(master_key, str):
                     raise Exception(
                         "Master key must be a string. Current type - {}".format(
