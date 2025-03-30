4c79670ce9442e4a7e98f33fba0137ef04654d54
Krrish Dholakia <krrishdholakia@gmail.com>
2025-03-29 23:14:31 -0700

    fix: fix linting errors

--- litellm/llms/custom_httpx/llm_http_handler.py
+++ litellm/llms/custom_httpx/llm_http_handler.py
@@ -619,6 +619,7 @@ class BaseLLMHTTPHandler:
 
         api_base = provider_config.get_complete_url(
             api_base=api_base,
+            api_key=api_key,
             model=model,
             optional_params=optional_params,
             litellm_params=litellm_params,
@@ -892,6 +893,7 @@ class BaseLLMHTTPHandler:
 
         complete_url = provider_config.get_complete_url(
             api_base=api_base,
+            api_key=api_key,
             model=model,
             optional_params=optional_params,
             litellm_params=litellm_params,
