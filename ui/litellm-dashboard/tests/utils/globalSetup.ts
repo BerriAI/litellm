68e82ecba8e4da7a43011d405a8c28f9b4c60da2
omnisilica <oxmaverix@gmail.com>
2025-03-31 23:16:04 -0400

    Transferred e2e from 'litellm/ui/litellm-dashboard/src/' to 'litellm/tests/proxy_admin_ui_tests'

--- ui/litellm-dashboard/tests/utils/globalSetup.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-
-export default async function globalSetup() {
-  dotenv.config({
-    //path should be relative to playwright.config.ts
-    path: "./../../../.env",
-  });
-}
