c8755e20ed52974529309a48dbc38cf8f6d18bf5
mjnitz02 <matt.nitzken@datarobot.com>
2025-04-02 13:09:25 -0400

    Add support for DataRobot as a router in LiteLLM

--- litellm/__init__.py
+++ litellm/__init__.py
@@ -188,6 +188,7 @@ maritalk_key: Optional[str] = None
 ai21_key: Optional[str] = None
 ollama_key: Optional[str] = None
 openrouter_key: Optional[str] = None
+datarobot_key: Optional[str] = None
 predibase_key: Optional[str] = None
 huggingface_key: Optional[str] = None
 vertex_project: Optional[str] = None
@@ -399,6 +400,7 @@ mistral_chat_models: List = []
 text_completion_codestral_models: List = []
 anthropic_models: List = []
 openrouter_models: List = []
+datarobot_models: List = []
 vertex_language_models: List = []
 vertex_vision_models: List = []
 vertex_chat_models: List = []
@@ -503,6 +505,8 @@ def add_known_models():
             empower_models.append(key)
         elif value.get("litellm_provider") == "openrouter":
             openrouter_models.append(key)
+        elif value.get("litellm_provider") == "datarobot":
+            datarobot_models.append(key)
         elif value.get("litellm_provider") == "vertex_ai-text-models":
             vertex_text_models.append(key)
         elif value.get("litellm_provider") == "vertex_ai-code-text-models":
@@ -649,6 +653,7 @@ model_list = (
     + anthropic_models
     + replicate_models
     + openrouter_models
+    + datarobot_models
     + huggingface_models
     + vertex_chat_models
     + vertex_text_models
@@ -709,6 +714,7 @@ models_by_provider: dict = {
     "together_ai": together_ai_models,
     "baseten": baseten_models,
     "openrouter": openrouter_models,
+    "datarobot": datarobot_models,
     "vertex_ai": vertex_chat_models
     + vertex_text_models
     + vertex_anthropic_models
@@ -857,6 +863,7 @@ from .llms.huggingface.embedding.transformation import HuggingFaceEmbeddingConfi
 from .llms.oobabooga.chat.transformation import OobaboogaConfig
 from .llms.maritalk import MaritalkConfig
 from .llms.openrouter.chat.transformation import OpenrouterConfig
+from .llms.datarobot.chat.transformation import DataRobotConfig
 from .llms.anthropic.chat.transformation import AnthropicConfig
 from .llms.anthropic.common_utils import AnthropicModelInfo
 from .llms.groq.stt.transformation import GroqSTTConfig
--- litellm/constants.py
+++ litellm/constants.py
@@ -174,6 +174,7 @@ LITELLM_CHAT_PROVIDERS = [
     "replicate",
     "huggingface",
     "together_ai",
+    "datarobot",
     "openrouter",
     "vertex_ai",
     "vertex_ai_beta",
