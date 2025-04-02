b6a6b38c83ee2d41dc1868f551c71f532b8ffc8b
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-04-01 19:03:50 -0700

    get_supported_openai_params for o-1 series models

--- litellm/llms/openai/chat/o_series_transformation.py
+++ litellm/llms/openai/chat/o_series_transformation.py
@@ -71,28 +71,6 @@ class OpenAIOSeriesConfig(OpenAIGPTConfig):
             verbose_logger.debug(
                 f"Unable to infer model provider for model={model}, defaulting to openai for o1 supported param check"
             )
-            custom_llm_provider = "openai"
-
-        _supports_function_calling = supports_function_calling(
-            model, custom_llm_provider
-        )
-        _supports_response_schema = supports_response_schema(model, custom_llm_provider)
-        _supports_parallel_tool_calls = supports_parallel_function_calling(
-            model, custom_llm_provider
-        )
-
-        if not _supports_function_calling:
-            non_supported_params.append("tools")
-            non_supported_params.append("tool_choice")
-            non_supported_params.append("function_call")
-            non_supported_params.append("functions")
-
-        if not _supports_parallel_tool_calls:
-            non_supported_params.append("parallel_tool_calls")
-
-        if not _supports_response_schema:
-            non_supported_params.append("response_format")
-
         return [
             param for param in all_openai_params if param not in non_supported_params
         ]
