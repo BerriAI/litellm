30b690114037b08cd898f20583453c70685021d7
mjnitz02 <matt.nitzken@datarobot.com>
2025-04-03 10:52:05 -0400

    Capitalize bearer

--- litellm/main.py
+++ litellm/main.py
@@ -2329,7 +2329,7 @@ def completion(  # type: ignore # noqa: PLR0915
             )
 
             datarobot_headers = {
-                "Authorization": f"bearer {api_key}",
+                "Authorization": f"Bearer {api_key}",
             }
 
             _headers = headers or litellm.headers
