eb75dacda239d34dc02b0f5340c7911ed9217c5c
Krish Dholakia <krrishdholakia@gmail.com>
2025-04-02 22:36:40 -0700

    (v0) Unified file id - allow calling multiple providers with same file id (#9718)
    * feat(files_endpoints.py): initial commit adding 'target_model_names' support
    
    allow developer to specify all the models they want to call with the file
    
    * feat(files_endpoints.py): return unified files endpoint
    
    * test(test_files_endpoints.py): add validation test - if invalid purpose submitted
    
    * feat: more updates
    
    * feat: initial working commit of unified file id translation
    
    * fix: additional fixes
    
    * fix(router.py): remove model replace logic in jsonl on acreate_file
    
    enables file upload to work for chat completion requests as well
    
    * fix(files_endpoints.py): remove whitespace around model name
    
    * fix(azure/handler.py): return acreate_file with correct response type
    
    * fix: fix linting errors
    
    * test: fix mock test to run on github actions
    
    * fix: fix ruff errors
    
    * fix: fix file too large error
    
    * fix(utils.py): remove redundant var
    
    * test: modify test to work on github actions
    
    * test: update tests
    
    * test: more debug logs to understand ci/cd issue
    
    * test: fix test for respx
    
    * test: skip mock respx test
    
    fails on ci/cd - not clear why

--- litellm/main.py
+++ litellm/main.py
@@ -110,7 +110,10 @@ from .litellm_core_utils.fallback_utils import (
     async_completion_with_fallbacks,
     completion_with_fallbacks,
 )
-from .litellm_core_utils.prompt_templates.common_utils import get_completion_messages
+from .litellm_core_utils.prompt_templates.common_utils import (
+    get_completion_messages,
+    update_messages_with_model_file_ids,
+)
 from .litellm_core_utils.prompt_templates.factory import (
     custom_prompt,
     function_call_prompt,
@@ -953,7 +956,6 @@ def completion(  # type: ignore # noqa: PLR0915
     non_default_params = get_non_default_completion_params(kwargs=kwargs)
     litellm_params = {}  # used to prevent unbound var errors
     ## PROMPT MANAGEMENT HOOKS ##
-
     if isinstance(litellm_logging_obj, LiteLLMLoggingObj) and prompt_id is not None:
         (
             model,
@@ -1067,6 +1069,15 @@ def completion(  # type: ignore # noqa: PLR0915
                 custom_prompt_dict[model]["bos_token"] = bos_token
             if eos_token:
                 custom_prompt_dict[model]["eos_token"] = eos_token
+
+        if kwargs.get("model_file_id_mapping"):
+            messages = update_messages_with_model_file_ids(
+                messages=messages,
+                model_id=kwargs.get("model_info", {}).get("id", None),
+                model_file_id_mapping=cast(
+                    Dict[str, Dict[str, str]], kwargs.get("model_file_id_mapping")
+                ),
+            )
 
         provider_config: Optional[BaseConfig] = None
         if custom_llm_provider is not None and custom_llm_provider in [
