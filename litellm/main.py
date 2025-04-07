c581c5b1ce72e5c979a42b11063d781e800d21eb
mjnitz02 <matt.nitzken@datarobot.com>
2025-04-07 15:31:17 -0400

    Lowercase

--- litellm/main.py
+++ litellm/main.py
@@ -2336,7 +2336,7 @@ def completion(  # type: ignore # noqa: PLR0915
             )
 
             datarobot_headers = {
-                "Authorization": f"Bearer {api_key}",
+                "Authorization": f"bearer {api_key}",
             }
 
             _headers = headers or litellm.headers
