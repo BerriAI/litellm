d8f47fc9e5c90ec7d4daca68d708c18fe1293b5f
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-04-07 20:16:15 -0700

    databricks/databricks-meta-llama-3-3-70b-instruct

--- tests/local_testing/test_completion_cost.py
+++ tests/local_testing/test_completion_cost.py
@@ -1266,13 +1266,13 @@ def test_completion_cost_prompt_caching(model, custom_llm_provider):
 @pytest.mark.parametrize(
     "model",
     [
-        "databricks/databricks-meta-llama-3.2-3b-instruct",
-        "databricks/databricks-meta-llama-3.3-70b-instruct",
+        "databricks/databricks-meta-llama-3-3-70b-instruct",
         "databricks/databricks-dbrx-instruct",
         # "databricks/databricks-mixtral-8x7b-instruct",
     ],
 )
 def test_completion_cost_databricks(model):
+    litellm._turn_on_debug()
     os.environ["LITELLM_LOCAL_MODEL_COST_MAP"] = "True"
     litellm.model_cost = litellm.get_model_cost_map(url="")
     model, messages = model, [{"role": "user", "content": "What is 2+2?"}]
