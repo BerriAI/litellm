4c85e13226df83bc2c8ad20ea304d30a4683e07e
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-04-10 16:59:14 -0700

    [Feat] Emit Key, Team Budget metrics on a cron job schedule  (#9528)
    * _initialize_remaining_budget_metrics
    
    * initialize_budget_metrics_cron_job
    
    * initialize_budget_metrics_cron_job
    
    * initialize_budget_metrics_cron_job
    
    * test_initialize_budget_metrics_cron_job
    
    * LITELLM_PROXY_ADMIN_NAME
    
    * fix code qa checks
    
    * test_initialize_budget_metrics_cron_job
    
    * test_initialize_budget_metrics_cron_job
    
    * pod lock manager allow dynamic cron job ID
    
    * fix pod lock manager
    
    * require cronjobid for PodLockManager
    
    * fix DB_SPEND_UPDATE_JOB_NAME acquire / release lock
    
    * add comment on prometheus logger
    
    * add debug statements for emitting key, team budget metrics
    
    * test_pod_lock_manager.py
    
    * test_initialize_budget_metrics_cron_job
    
    * initialize_budget_metrics_cron_job
    
    * initialize_remaining_budget_metrics
    
    * remove outdated test

--- litellm/proxy/proxy_server.py
+++ litellm/proxy/proxy_server.py
@@ -803,9 +803,9 @@ model_max_budget_limiter = _PROXY_VirtualKeyModelMaxBudgetLimiter(
     dual_cache=user_api_key_cache
 )
 litellm.logging_callback_manager.add_litellm_callback(model_max_budget_limiter)
-redis_usage_cache: Optional[
-    RedisCache
-] = None  # redis cache used for tracking spend, tpm/rpm limits
+redis_usage_cache: Optional[RedisCache] = (
+    None  # redis cache used for tracking spend, tpm/rpm limits
+)
 user_custom_auth = None
 user_custom_key_generate = None
 user_custom_sso = None
@@ -1131,9 +1131,9 @@ async def update_cache(  # noqa: PLR0915
         _id = "team_id:{}".format(team_id)
         try:
             # Fetch the existing cost for the given user
-            existing_spend_obj: Optional[
-                LiteLLM_TeamTable
-            ] = await user_api_key_cache.async_get_cache(key=_id)
+            existing_spend_obj: Optional[LiteLLM_TeamTable] = (
+                await user_api_key_cache.async_get_cache(key=_id)
+            )
             if existing_spend_obj is None:
                 # do nothing if team not in api key cache
                 return
@@ -2812,9 +2812,9 @@ async def initialize(  # noqa: PLR0915
         user_api_base = api_base
         dynamic_config[user_model]["api_base"] = api_base
     if api_version:
-        os.environ[
-            "AZURE_API_VERSION"
-        ] = api_version  # set this for azure - litellm can read this from the env
+        os.environ["AZURE_API_VERSION"] = (
+            api_version  # set this for azure - litellm can read this from the env
+        )
     if max_tokens:  # model-specific param
         dynamic_config[user_model]["max_tokens"] = max_tokens
     if temperature:  # model-specific param
@@ -3190,6 +3190,11 @@ class ProxyStartupEvent:
                     timezone=ZoneInfo("America/Los_Angeles"),  # Pacific Time
                 )
                 await proxy_logging_obj.slack_alerting_instance.send_fallback_stats_from_prometheus()
+
+        if litellm.prometheus_initialize_budget_metrics is True:
+            from litellm.integrations.prometheus import PrometheusLogger
+
+            PrometheusLogger.initialize_budget_metrics_cron_job(scheduler=scheduler)
 
         scheduler.start()
 
@@ -7753,9 +7758,9 @@ async def get_config_list(
                             hasattr(sub_field_info, "description")
                             and sub_field_info.description is not None
                         ):
-                            nested_fields[
-                                idx
-                            ].field_description = sub_field_info.description
+                            nested_fields[idx].field_description = (
+                                sub_field_info.description
+                            )
                         idx += 1
 
                     _stored_in_db = None
