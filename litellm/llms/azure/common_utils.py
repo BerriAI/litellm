7fa265e081cba2dc3c73bcc77fd1b96179d34588
Marc Abramowitz <abramowi@adobe.com>
2025-04-15 17:31:18 -0700

    Fix typo: Entrata -> Entra in code (#9922)
    * Fix typo: Entrata -> Entra
    
    * Fix a few more

--- litellm/llms/azure/common_utils.py
+++ litellm/llms/azure/common_utils.py
@@ -61,7 +61,7 @@ def process_azure_headers(headers: Union[httpx.Headers, dict]) -> dict:
     return {**llm_response_headers, **openai_headers}
 
 
-def get_azure_ad_token_from_entrata_id(
+def get_azure_ad_token_from_entra_id(
     tenant_id: str,
     client_id: str,
     client_secret: str,
@@ -81,7 +81,7 @@ def get_azure_ad_token_from_entrata_id(
     """
     from azure.identity import ClientSecretCredential, get_bearer_token_provider
 
-    verbose_logger.debug("Getting Azure AD Token from Entrata ID")
+    verbose_logger.debug("Getting Azure AD Token from Entra ID")
 
     if tenant_id.startswith("os.environ/"):
         _tenant_id = get_secret_str(tenant_id)
@@ -324,9 +324,9 @@ class BaseAzureLLM(BaseOpenAILLM):
         timeout = litellm_params.get("timeout")
         if not api_key and tenant_id and client_id and client_secret:
             verbose_logger.debug(
-                "Using Azure AD Token Provider from Entrata ID for Azure Auth"
+                "Using Azure AD Token Provider from Entra ID for Azure Auth"
             )
-            azure_ad_token_provider = get_azure_ad_token_from_entrata_id(
+            azure_ad_token_provider = get_azure_ad_token_from_entra_id(
                 tenant_id=tenant_id,
                 client_id=client_id,
                 client_secret=client_secret,
