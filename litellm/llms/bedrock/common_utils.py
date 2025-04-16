36630e19f924c641cc81a4a362ee58abe07f7422
Krrish Dholakia <krrishdholakia@gmail.com>
2025-04-16 08:00:39 -0700

    fix(bedrock/common_utils.py): add us-west-1 to us regions

--- litellm/llms/bedrock/common_utils.py
+++ litellm/llms/bedrock/common_utils.py
@@ -51,8 +51,8 @@ class AmazonBedrockGlobalConfig:
             "ap-northeast-1",  # Asia Pacific (Tokyo)
             "ap-northeast-2",  # Asia Pacific (Seoul)
             "ap-northeast-3",  # Asia Pacific (Osaka)
-            "ap-south-1",      # Asia Pacific (Mumbai)
-            "ap-south-2",      # Asia Pacific (Hyderabad)
+            "ap-south-1",  # Asia Pacific (Mumbai)
+            "ap-south-2",  # Asia Pacific (Hyderabad)
             "ap-southeast-1",  # Asia Pacific (Singapore)
             "ap-southeast-2",  # Asia Pacific (Sydney)
         ]
@@ -65,14 +65,14 @@ class AmazonBedrockGlobalConfig:
         Source: https://www.aws-services.info/bedrock.html
         """
         return [
-            "eu-west-1",      # Europe (Ireland)
-            "eu-west-2",      # Europe (London)
-            "eu-west-3",      # Europe (Paris)
-            "eu-central-1",   # Europe (Frankfurt)
-            "eu-central-2",   # Europe (Zurich)
-            "eu-south-1",     # Europe (Milan)
-            "eu-south-2",     # Europe (Spain)
-            "eu-north-1",     # Europe (Stockholm)
+            "eu-west-1",  # Europe (Ireland)
+            "eu-west-2",  # Europe (London)
+            "eu-west-3",  # Europe (Paris)
+            "eu-central-1",  # Europe (Frankfurt)
+            "eu-central-2",  # Europe (Zurich)
+            "eu-south-1",  # Europe (Milan)
+            "eu-south-2",  # Europe (Spain)
+            "eu-north-1",  # Europe (Stockholm)
         ]
 
     def get_ca_regions(self) -> List[str]:
@@ -83,9 +83,10 @@ class AmazonBedrockGlobalConfig:
         Source: https://www.aws-services.info/bedrock.html
         """
         return [
-            "us-east-1",      # US East (N. Virginia)
-            "us-east-2",      # US East (Ohio)
-            "us-west-2",      # US West (Oregon)
+            "us-east-1",  # US East (N. Virginia)
+            "us-east-2",  # US East (Ohio)
+            "us-west-1",  # US West (N. California)
+            "us-west-2",  # US West (Oregon)
             "us-gov-east-1",  # AWS GovCloud (US-East)
             "us-gov-west-1",  # AWS GovCloud (US-West)
         ]
