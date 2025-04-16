71e79ea6bded5bef30e8d131589913a44cc87e37
Krrish Dholakia <krrishdholakia@gmail.com>
2025-04-15 19:49:45 -0700

    refactor(groq.py): initial commit migrating groq to base_llm_http_handler

--- tests/local_testing/test_stream_chunk_builder.py
+++ tests/local_testing/test_stream_chunk_builder.py
@@ -927,11 +927,13 @@ def execute_completion(opts: dict):
     for i, part in enumerate(response_gen):
         partial_streaming_chunks.append(part)
     assembly = litellm.stream_chunk_builder(partial_streaming_chunks)
-    print(assembly.choices[0].message.tool_calls)
+    print(f"assembly.choices[0].message.tool_calls: {assembly.choices[0].message.tool_calls}")
     assert len(assembly.choices[0].message.tool_calls) == 3, (
         assembly.choices[0].message.tool_calls[0].function.arguments[0]
     )
     print(assembly.choices[0].message.tool_calls)
+    for tool_call in assembly.choices[0].message.tool_calls:
+        json.loads(tool_call.function.arguments) # assert valid json - https://github.com/BerriAI/litellm/issues/10034
 
 
 def test_grok_bug(load_env):
