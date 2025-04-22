6c958419823765a68b1735c3b65308f563747abb
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-04-21 19:46:22 -0700

    [Feat] Pass through endpoints - ensure `PassthroughStandardLoggingPayload` is logged and contains method, url, request/response body (#10194)
    * ensure passthrough_logging_payload is filled in kwargs
    
    * test_assistants_passthrough_logging
    
    * test_assistants_passthrough_logging
    
    * test_assistants_passthrough_logging
    
    * test_threads_passthrough_logging
    
    * test _init_kwargs_for_pass_through_endpoint
    
    * _init_kwargs_for_pass_through_endpoint

--- litellm/proxy/pass_through_endpoints/llm_provider_handlers/anthropic_passthrough_logging_handler.py
+++ litellm/proxy/pass_through_endpoints/llm_provider_handlers/anthropic_passthrough_logging_handler.py
@@ -13,7 +13,9 @@ from litellm.llms.anthropic.chat.handler import (
 from litellm.llms.anthropic.chat.transformation import AnthropicConfig
 from litellm.proxy._types import PassThroughEndpointLoggingTypedDict
 from litellm.proxy.auth.auth_utils import get_end_user_id_from_request_body
-from litellm.proxy.pass_through_endpoints.types import PassthroughStandardLoggingPayload
+from litellm.types.passthrough_endpoints.pass_through_endpoints import (
+    PassthroughStandardLoggingPayload,
+)
 from litellm.types.utils import ModelResponse, TextCompletionResponse
 
 if TYPE_CHECKING:
@@ -122,9 +124,9 @@ class AnthropicPassthroughLoggingHandler:
             litellm_model_response.id = logging_obj.litellm_call_id
             litellm_model_response.model = model
             logging_obj.model_call_details["model"] = model
-            logging_obj.model_call_details[
-                "custom_llm_provider"
-            ] = litellm.LlmProviders.ANTHROPIC.value
+            logging_obj.model_call_details["custom_llm_provider"] = (
+                litellm.LlmProviders.ANTHROPIC.value
+            )
             return kwargs
         except Exception as e:
             verbose_proxy_logger.exception(
