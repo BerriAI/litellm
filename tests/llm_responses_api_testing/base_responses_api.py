168adb0c8c431b5604aad453b66ced01c6540c33
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-04-22 13:05:43 -0700

    test_basic_openai_responses_delete_endpoint

--- tests/llm_responses_api_testing/base_responses_api.py
+++ tests/llm_responses_api_testing/base_responses_api.py
@@ -189,7 +189,7 @@ class BaseResponsesAPITest(ABC):
 
 
 
-    @pytest.mark.parametrize("sync_mode", [True, False])
+    @pytest.mark.parametrize("sync_mode", [False, True])
     @pytest.mark.asyncio
     async def test_basic_openai_responses_delete_endpoint(self, sync_mode):
         litellm._turn_on_debug()
@@ -208,16 +208,19 @@ class BaseResponsesAPITest(ABC):
                 )
             else:
                 raise ValueError("response is not a ResponsesAPIResponse")
-        # else:
-        #     response = await litellm.aresponses(
-        #         input="Basic ping", max_output_tokens=20,
-        #         **base_completion_call_args
-        #     )
+        else:
+            response = await litellm.aresponses(
+                input="Basic ping", max_output_tokens=20,
+                **base_completion_call_args
+            )
 
-        #     # async delete the response
-        #     await litellm.adelete_responses(
-        #         response_id=response.id,
-        #     )
+            # async delete the response
+            if isinstance(response, ResponsesAPIResponse):
+                await litellm.adelete_responses(
+                    response_id=response.id,
+                )
+            else:
+                raise ValueError("response is not a ResponsesAPIResponse")
     
 
     # @pytest.mark.parametrize("sync_mode", [True, False])
