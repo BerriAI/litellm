04f64a1032c854bc179cb657a8e42189a88125db
Yujie Zhu <yujie.zhu@bitdeer.com>
2025-04-23 13:45:16 +0800

    remove useless mapping

--- litellm/utils.py
+++ litellm/utils.py
@@ -2739,7 +2739,6 @@ def get_optional_params_embeddings(  # noqa: PLR0915
             non_default_params=non_default_params,
             optional_params={},
             model=model,
-             drop_params=drop_params if drop_params is not None else False,
         )
         final_params = {**optional_params, **kwargs}
         return final_params
