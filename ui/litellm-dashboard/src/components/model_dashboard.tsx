f30479557e79c31bed72eae5f53cfc748715a832
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-04-26 16:36:07 -0700

    fix remove Column Selector, it seems to not be working

--- ui/litellm-dashboard/src/components/model_dashboard.tsx
+++ ui/litellm-dashboard/src/components/model_dashboard.tsx
@@ -1148,7 +1148,7 @@ const ModelDashboard: React.FC<ModelDashboardProps> = ({
                           </div>
                           
                           {/* Column Selector will be rendered here */}
-                          <div className="relative" ref={dropdownRef}>
+                          {/* <div className="relative" ref={dropdownRef}>
                             <button
                               onClick={() => setIsDropdownOpen(!isDropdownOpen)}
                               className="flex items-center gap-2 px-3 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
@@ -1184,7 +1184,7 @@ const ModelDashboard: React.FC<ModelDashboardProps> = ({
                                 </div>
                               </div>
                             )}
-                          </div>
+                          </div> */}
                         </div>
 
                         {/* Results Count */}
