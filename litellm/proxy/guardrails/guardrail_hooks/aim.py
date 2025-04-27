1d8f40629387daf8d8870f45ed010b383a5e37c4
drorbaron <dror@aim.security>
2025-04-27 15:57:21 +0300

    support anonymize and deanonymize

--- litellm/proxy/guardrails/guardrail_hooks/aim.py
+++ litellm/proxy/guardrails/guardrail_hooks/aim.py
@@ -56,6 +56,7 @@ class AimGuardrail(CustomGuardrail):
         self.ws_api_base = self.api_base.replace("http://", "ws://").replace(
             "https://", "wss://"
         )
+        self.dlp_entities: list[dict] = []
         super().__init__(**kwargs)
 
     async def async_pre_call_hook(
@@ -76,10 +77,9 @@ class AimGuardrail(CustomGuardrail):
     ) -> Union[Exception, str, dict, None]:
         verbose_proxy_logger.debug("Inside AIM Pre-Call Hook")
 
-        await self.call_aim_guardrail(
+        return await self.call_aim_guardrail(
             data, hook="pre_call", key_alias=user_api_key_dict.key_alias
         )
-        return data
 
     async def async_moderation_hook(
         self,
@@ -103,7 +103,7 @@ class AimGuardrail(CustomGuardrail):
 
     async def call_aim_guardrail(
         self, data: dict, hook: str, key_alias: Optional[str]
-    ) -> None:
+    ) -> dict:
         user_email = data.get("metadata", {}).get("headers", {}).get("x-aim-user-email")
         call_id = data.get("litellm_call_id")
         headers = self._build_aim_headers(
@@ -113,31 +113,76 @@ class AimGuardrail(CustomGuardrail):
             litellm_call_id=call_id,
         )
         response = await self.async_handler.post(
-            f"{self.api_base}/detect/openai",
+            f"{self.api_base}/detect/openai/v2",
             headers=headers,
             json={"messages": data.get("messages", [])},
         )
         response.raise_for_status()
         res = response.json()
-        detected = res["detected"]
+        required_action = res.get("required_action") and res["required_action"].get("prompt_policy_action", None)
+        if required_action is None:
+            verbose_proxy_logger.warning("Aim: No required action specified")
+            return data
+        match required_action:
+            case "monitor_action":
+                verbose_proxy_logger.info("Aim: monitor action")
+            case "block_action":
+                self._handle_block_action(res["analysis_result"])
+            case "anonymize_action":
+                verbose_proxy_logger.info("Aim: anonymize action")
+                return self._handle_anonymize_action(res, data)
+            case "engage_action":
+                verbose_proxy_logger.info("Aim: engage action")
+            case _:
+                verbose_proxy_logger.error("Aim: unknown action")
+        return data
+
+    def _handle_block_action(self, analysis_result: Any) -> None:
+        detected = analysis_result["detected"]
         verbose_proxy_logger.info(
             "Aim: detected: {detected}, enabled policies: {policies}".format(
                 detected=detected,
-                policies=list(res["details"].keys()),
+                policies=list(analysis_result["details"].keys()),
             ),
         )
         if detected:
-            raise HTTPException(status_code=400, detail=res["detection_message"])
+            raise HTTPException(status_code=400, detail=analysis_result["detection_message"])
+
+    def _handle_anonymize_action(self, res: Any, data: dict) -> dict:
+        try:
+            redaction_result = res["redaction_result"]
+            if not redaction_result:
+                return data
+            analysis_result = res["analysis_result"]
+            if analysis_result and analysis_result.get("session_entities"):
+                self.dlp_entities = analysis_result.get("session_entities")
+            data["messages"] = [
+                    {
+                        "role": redaction_result["redacted_new_message"]["role"],
+                        "content": redaction_result["redacted_new_message"]["content"],
+                    }
+                ] + [
+                    {
+                        "role": message["role"],
+                        "content": message["content"],
+                    }
+                    for message in redaction_result["all_redacted_messages"]
+                ]
+            return data
+        except Exception as e:
+            verbose_proxy_logger.error(f"Aim: Error while redacting: {e}")
+            return data # todo do we want to fallback to this or raise?
+
 
     async def call_aim_guardrail_on_output(
         self, request_data: dict, output: str, hook: str, key_alias: Optional[str]
-    ) -> Optional[str]:
+    ) -> Optional[dict]:
         user_email = (
             request_data.get("metadata", {}).get("headers", {}).get("x-aim-user-email")
         )
         call_id = request_data.get("litellm_call_id")
         response = await self.async_handler.post(
-            f"{self.api_base}/detect/output",
+            f"{self.api_base}/detect/output/v2",
             headers=self._build_aim_headers(
                 hook=hook,
                 key_alias=key_alias,
@@ -148,16 +193,44 @@ class AimGuardrail(CustomGuardrail):
         )
         response.raise_for_status()
         res = response.json()
-        detected = res["detected"]
+        required_action = res.get("required_action") and res["required_action"].get("prompt_policy_action", None)
+        if required_action is None:
+            verbose_proxy_logger.warning("Aim: No required action specified")
+            return self._deanonymize_output(res, output)
+        match required_action:
+            case "monitor_action":
+                verbose_proxy_logger.info("Aim: monitor action")
+            case "block_action":
+                return self._handle_block_action_on_output(res["analysis_result"])
+            case "anonymize_action":
+                verbose_proxy_logger.info("Aim: anonymize action")
+            case "engage_action":
+                verbose_proxy_logger.info("Aim: engage action")
+            case _:
+                verbose_proxy_logger.error("Aim: unknown action")
+        return self._deanonymize_output(res, res)
+
+    def _handle_block_action_on_output(self, analysis_result: Any):
+        analysis_result = analysis_result
+        detected = analysis_result["detected"]
         verbose_proxy_logger.info(
             "Aim: detected: {detected}, enabled policies: {policies}".format(
                 detected=detected,
-                policies=list(res["details"].keys()),
+                policies=list(analysis_result["details"].keys()),
             ),
         )
         if detected:
-            return res["detection_message"]
+            return {"detection_message": analysis_result["detection_message"]}
         return None
+
+    def _deanonymize_output(self, res: Any, output: str) -> dict | None:
+        try:
+            for entity in self.dlp_entities:
+                output = output.replace(f"[{entity["name"]}]", entity["content"])
+            return {"redacted_output": output}
+        except Exception as e:
+            verbose_proxy_logger.error(f"Aim: Error while redacting output: {e}")
+            return None
 
     def _build_aim_headers(
         self,
@@ -204,11 +277,13 @@ class AimGuardrail(CustomGuardrail):
             and isinstance(response.choices[0], Choices)
         ):
             content = response.choices[0].message.content or ""
-            detection = await self.call_aim_guardrail_on_output(
+            aim_output_guardrail_result = await self.call_aim_guardrail_on_output(
                 data, content, hook="output", key_alias=user_api_key_dict.key_alias
             )
-            if detection:
-                raise HTTPException(status_code=400, detail=detection)
+            if aim_output_guardrail_result and aim_output_guardrail_result.get("detection_message"):
+                raise HTTPException(status_code=400, detail=aim_output_guardrail_result.get("detection_message"))
+            if aim_output_guardrail_result and aim_output_guardrail_result.get("redacted_output"):
+                response.choices[0].message.content = aim_output_guardrail_result.get("redacted_output")
 
     async def async_post_call_streaming_iterator_hook(
         self,
