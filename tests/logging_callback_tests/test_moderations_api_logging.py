8449279f01a3023fdfa11e0947b78c24c6ed5589
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-04-28 15:47:37 -0700

    Revert "test_moderations_api_logging"
    This reverts commit 5a9001f04c592b063f016d2bb854416255ec6cdf.

--- tests/logging_callback_tests/test_moderations_api_logging.py
+++ tests/logging_callback_tests/test_moderations_api_logging.py
@@ -13,6 +13,8 @@ import os
 import time
 import json
 
+# this file is to test litellm/proxy
+
 sys.path.insert(
     0, os.path.abspath("../..")
 )  # Adds the parent directory to the system path
@@ -51,9 +53,9 @@ async def test_moderations_api_logging(model):
     custom_logger = TestCustomLogger()
     litellm.logging_callback_manager.add_litellm_callback(custom_logger)
 
-    input_content = "Hello, how are you?"
+
     response = await litellm.amoderation(
-        input=input_content,
+        input="Hello, how are you?",
         model=model,
     )
 
@@ -69,9 +71,6 @@ async def test_moderations_api_logging(model):
     assert standard_logging_payload["status"] == "success"
     assert standard_logging_payload["custom_llm_provider"] == litellm.LlmProviders.OPENAI.value
 
-
-    # assert the logged input == input
-    assert standard_logging_payload["messages"][0]["content"] == input_content
 
     # assert the logged response == response user received client side 
     assert dict(standard_logging_payload["response"]) == response.model_dump()
