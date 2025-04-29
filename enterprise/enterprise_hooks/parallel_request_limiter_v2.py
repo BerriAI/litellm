9c0b23520a76fb33e25717ddef2172fa8bd4337c
Krrish Dholakia <krrishdholakia@gmail.com>
2025-04-28 20:52:14 -0700

    fix: initial commit of v2 parallel request limiter hook
    enables multi-instance rate limiting to work

---
+++ enterprise/enterprise_hooks/parallel_request_limiter_v2.py
@@ -0,0 +1,5 @@
+"""
+V2 Implementation of Parallel Requests, TPM, RPM Limiting on the proxy
+
+Designed to work on a multi-instance setup, where multiple instances are writing to redis simultaneously
+"""
