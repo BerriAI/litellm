2d4a7d5b281b4bbad1bf73d471a0acc7d362cf70
mjnitz02 <matt.nitzken@datarobot.com>
2025-05-02 11:37:01 -0400

    Migrate everything to OpenAI LIke

--- litellm/llms/datarobot/chat/transformation.py
+++ litellm/llms/datarobot/chat/transformation.py
@@ -4,50 +4,31 @@ Support for OpenAI's `/v1/chat/completions` endpoint.
 Calls done in OpenAI/openai.py as DataRobot is openai-compatible.
 """
 
-from typing import Optional, Union, Tuple
-
-import httpx
-
-from litellm.llms.base_llm.chat.transformation import BaseLLMException
+from typing import Optional, Tuple
 from litellm.secret_managers.main import get_secret_str
-from ..common_utils import DataRobotException
 from ...openai_like.chat.transformation import OpenAILikeChatConfig
 
 
 class DataRobotConfig(OpenAILikeChatConfig):
-    def _get_openai_compatible_provider_info(
-        self,
-        api_base: Optional[str],
-        api_key: Optional[str],
-    ) -> Tuple[Optional[str], Optional[str]]:
-        api_base = api_base or get_secret_str("DATAROBOT_API_BASE")  # type: ignore
-        dynamic_api_key = (
-            api_key or get_secret_str("DATAROBOT_API_KEY") or ""
-        )  # vllm does not require an api key
-        return api_base, dynamic_api_key
+    @staticmethod
+    def _resolve_api_key(api_key: Optional[str] = None) -> str:
+        """Attempt to ensure that the API key is set, preferring the user-provided key
+        over the secret manager key (``DATAROBOT_API_KEY``).
 
-    def get_error_class(
-        self, error_message: str, status_code: int, headers: Union[dict, httpx.Headers]
-    ) -> BaseLLMException:
-        return DataRobotException(
-            message=error_message,
-            status_code=status_code,
-            headers=headers,
-        )
-
-    def get_complete_url(
-        self,
-        api_base: Optional[str],
-        api_key: Optional[str],
-        model: str,
-        optional_params: dict,
-        litellm_params: dict,
-        stream: Optional[bool] = None,
-    ) -> str:
+        If both are None, a fake API key is returned for testing.
         """
-        Ensure that the url references a deployment or the DataRobot LLM Gateway
+        return api_key or get_secret_str("DATAROBOT_API_KEY") or "fake-api-key"
 
+    @staticmethod
+    def _resolve_api_base(api_base: Optional[str] = None) -> Optional[str]:
+        """Attempt to ensure that the API base is set, preferring the user-provided key
+        over the secret manager key (``DATAROBOT_API_BASE``).
+
+        If both are None, a default Llamafile server URL is returned.
+        See: https://github.com/Mozilla-Ocho/llamafile/blob/bd1bbe9aabb1ee12dbdcafa8936db443c571eb9d/README.md#L61
         """
+        api_base = api_base or get_secret_str("DATAROBOT_API_BASE")
+
         if api_base is None:
             api_base = "https://app.datarobot.com"
 
@@ -61,4 +42,20 @@ class DataRobotConfig(OpenAILikeChatConfig):
         if not api_base.endswith("/"):
             api_base += "/"
 
-        return str(api_base)
+        return api_base # type: ignore
+
+    def _get_openai_compatible_provider_info(
+        self,
+        api_base: Optional[str],
+        api_key: Optional[str]
+    ) -> Tuple[Optional[str], Optional[str]]:
+        """Attempts to ensure that the API base and key are set, preferring user-provided values,
+        before falling back to secret manager values (``DATAROBOT_API_BASE`` and ``DATAROBOT_API_KEY``
+        respectively).
+
+        If an API key cannot be resolved via either method, a fake key is returned.
+        """
+        api_base = DataRobotConfig._resolve_api_base(api_base)
+        dynamic_api_key = DataRobotConfig._resolve_api_key(api_key)
+
+        return api_base, dynamic_api_key
