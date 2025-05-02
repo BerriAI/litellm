77b7468a2581b0f260b00e0d04547b7fe1a73971
mjnitz02 <matt.nitzken@datarobot.com>
2025-05-02 10:20:47 -0400

    Migrate to OpenAILike

--- litellm/llms/datarobot/chat/transformation.py
+++ litellm/llms/datarobot/chat/transformation.py
@@ -4,16 +4,28 @@ Support for OpenAI's `/v1/chat/completions` endpoint.
 Calls done in OpenAI/openai.py as DataRobot is openai-compatible.
 """
 
-from typing import Optional, Union
+from typing import Optional, Union, Tuple
 
 import httpx
 
 from litellm.llms.base_llm.chat.transformation import BaseLLMException
+from litellm.secret_managers.main import get_secret_str
 from ..common_utils import DataRobotException
-from ...openai.chat.gpt_transformation import OpenAIGPTConfig
+from ...openai_like.chat.transformation import OpenAILikeChatConfig
 
 
-class DataRobotConfig(OpenAIGPTConfig):
+class DataRobotConfig(OpenAILikeChatConfig):
+    def _get_openai_compatible_provider_info(
+        self,
+        api_base: Optional[str],
+        api_key: Optional[str],
+    ) -> Tuple[Optional[str], Optional[str]]:
+        api_base = api_base or get_secret_str("DATAROBOT_API_BASE")  # type: ignore
+        dynamic_api_key = (
+            api_key or get_secret_str("DATAROBOT_API_KEY") or ""
+        )  # vllm does not require an api key
+        return api_base, dynamic_api_key
+
     def get_error_class(
         self, error_message: str, status_code: int, headers: Union[dict, httpx.Headers]
     ) -> BaseLLMException:
