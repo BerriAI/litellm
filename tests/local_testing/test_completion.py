de7870cb54e1c29e48b3380ced9701760f3d0efb
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-05-01 18:36:55 -0700

    Add `llamafile` as a provider (#10203) (#10482)
    * Update docs for OpenAI compatible providers, add Llamafile docs, include Llamafile in the sidebar
    
    * Add Llamafile as an LlmProviders enum
    
    * Add llamafile as a OpenAI compatible provider (in the list of compatible providers)
    
    * Add Llamafile chat config and tests
    
    * Wire up Llamafile
    
    Co-authored-by: Peter Wilson <peter@mozilla.ai>

--- tests/local_testing/test_completion.py
+++ tests/local_testing/test_completion.py
@@ -1477,7 +1477,7 @@ HF Tests we should pass
 
 
 @pytest.mark.parametrize(
-    "provider", ["openai", "hosted_vllm", "lm_studio"]
+    "provider", ["openai", "hosted_vllm", "lm_studio", "llamafile"]
 )  # "vertex_ai",
 @pytest.mark.asyncio
 async def test_openai_compatible_custom_api_base(provider):
@@ -1519,6 +1519,7 @@ async def test_openai_compatible_custom_api_base(provider):
     [
         "openai",
         "hosted_vllm",
+        "llamafile",
     ],
 )  # "vertex_ai",
 @pytest.mark.asyncio
