6347cbf2843f273075713d4b325f7a0b394987c2
Mayank Solanki <bizmiku@gmail.com>
2025-05-07 16:46:05 +0530

    async success implemented

---
+++ litellm/integrations/deepeval/utils.py
@@ -0,0 +1,8 @@
+from datetime import datetime, timezone
+
+def to_zod_compatible_iso(dt: datetime) -> str:
+    return (
+        dt.astimezone(timezone.utc)
+        .isoformat(timespec="milliseconds")
+        .replace("+00:00", "Z")
+    )
