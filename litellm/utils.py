0c723c9e56495a537806d46bb46feaff31837b91
Tom Matthews <tom@unitary.ai>
2025-05-07 18:24:53 +0100

    Add support for nscale provider

--- litellm/utils.py
+++ litellm/utils.py
@@ -6383,6 +6383,8 @@ class ProviderConfigManager:
             return litellm.LiteLLMProxyChatConfig()
         elif litellm.LlmProviders.OPENAI == provider:
             return litellm.OpenAIGPTConfig()
+        elif litellm.LlmProviders.NSCALE == provider:
+            return litellm.NscaleConfig()
         return None
 
     @staticmethod
