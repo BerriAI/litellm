18ab8487dc9ac2537ecdafc3a37a64d88b920773
Krrish Dholakia <krrishdholakia@gmail.com>
2025-05-06 22:15:33 -0700

    test: update model in test

--- tests/local_testing/test_streaming.py
+++ tests/local_testing/test_streaming.py
@@ -2014,7 +2014,7 @@ def test_openai_chat_completion_complete_response_call():
     "model",
     [
         "gpt-3.5-turbo",
-        "azure/chatgpt-v-3",
+        "azure/gpt-4o-new-test",
         "claude-3-haiku-20240307",
         "o1-preview",
         "o1",
