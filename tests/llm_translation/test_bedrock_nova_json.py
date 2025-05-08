bfc82a7a6489c9847e98d6a5497fc4791289456a
Krrish Dholakia <krrishdholakia@gmail.com>
2025-05-08 16:17:12 -0700

    test(base_llm_unit_tests.py): return '<thinking>' tag in response content

--- tests/llm_translation/test_bedrock_nova_json.py
+++ tests/llm_translation/test_bedrock_nova_json.py
@@ -26,9 +26,9 @@ class TestBedrockNovaJson(BaseLLMChatTest):
         """Test that tool calls with no arguments is translated correctly. Relevant issue: https://github.com/BerriAI/litellm/issues/6833"""
         pass
 
-    @pytest.fixture(autouse=True)
-    def skip_non_json_tests(self, request):
-        if not "json" in request.function.__name__.lower():
-            pytest.skip(
-                f"Skipping non-JSON test: {request.function.__name__} does not contain 'json'"
-            )
+    # @pytest.fixture(autouse=True)
+    # def skip_non_json_tests(self, request):
+    #     if not "json" in request.function.__name__.lower():
+    #         pytest.skip(
+    #             f"Skipping non-JSON test: {request.function.__name__} does not contain 'json'"
+    #         )
