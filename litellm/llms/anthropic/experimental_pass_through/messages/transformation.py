b8edb01c6a77115d5a75556ac1b23aa0837f3ea1
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-05-08 15:09:18 -0700

    fix: add transform_anthropic_messages_request

--- litellm/llms/anthropic/experimental_pass_through/messages/transformation.py
+++ litellm/llms/anthropic/experimental_pass_through/messages/transformation.py
@@ -1,8 +1,13 @@
-from typing import Optional
+from typing import Dict, List, Optional
 
 from litellm.llms.base_llm.anthropic_messages.transformation import (
     BaseAnthropicMessagesConfig,
 )
+from litellm.types.llms.anthropic import (
+    AnthropicMessageRequestBase,
+    AnthropicMessagesRequest,
+)
+from litellm.types.router import GenericLiteLLMParams
 
 DEFAULT_ANTHROPIC_API_BASE = "https://api.anthropic.com"
 DEFAULT_ANTHROPIC_API_VERSION = "2023-06-01"
@@ -45,3 +50,22 @@ class AnthropicMessagesConfig(BaseAnthropicMessagesConfig):
         if "content-type" not in headers:
             headers["content-type"] = "application/json"
         return headers
+
+    def transform_anthropic_messages_request(
+        self,
+        model: str,
+        messages: List[Dict],
+        anthropic_messages_optional_request_params: Dict,
+        litellm_params: GenericLiteLLMParams,
+        headers: dict,
+    ) -> Dict:
+        """
+        No transformation is needed for Anthropic messages
+
+
+        This takes in a request in the Anthropic /v1/messages API spec -> transforms it to /v1/messages API spec (i.e) no transformation is needed
+        """
+        anthropic_messages_request: AnthropicMessagesRequest = AnthropicMessagesRequest(
+            **anthropic_messages_optional_request_params,
+        )
+        return dict(anthropic_messages_request)
