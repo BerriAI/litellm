d13117992c40d29ab4a4bb2acad5757634026916
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-05-10 17:26:12 -0700

    fix: test_init_custom_logger_compatible_class_as_callback

--- tests/logging_callback_tests/test_unit_tests_init_callbacks.py
+++ tests/logging_callback_tests/test_unit_tests_init_callbacks.py
@@ -44,6 +44,7 @@ from litellm.integrations.humanloop import HumanloopLogger
 from litellm.proxy.hooks.dynamic_rate_limiter import _PROXY_DynamicRateLimitHandler
 from litellm_enterprise.enterprise_callbacks.generic_api_callback import GenericAPILogger
 from litellm_enterprise.enterprise_callbacks.send_emails.resend_email import ResendEmailLogger
+from litellm_enterprise.enterprise_callbacks.send_emails.smtp_email import SMTPEmailLogger
 from unittest.mock import patch
 
 # clear prometheus collectors / registry
@@ -83,6 +84,7 @@ callback_class_str_to_classType = {
     "bedrock_vector_store": BedrockVectorStore,
     "generic_api": GenericAPILogger,
     "resend_email": ResendEmailLogger,
+    "smtp_email": SMTPEmailLogger,
 }
 
 expected_env_vars = {
