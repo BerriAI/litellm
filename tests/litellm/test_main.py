9250b971cbe32abdb073a6606b34ccdda2fde70a
yanrujing <rujing.yan@protagonist-ai.com>
2025-05-13 16:05:59 +0800

    chore: chore

--- tests/litellm/test_main.py
+++ tests/litellm/test_main.py
@@ -136,7 +136,7 @@ def test_completion_missing_role(openai_api_response):
         "gemini/gemini-1.5-flash",
         "bedrock/anthropic.claude-3-5-sonnet-20240620-v1:0",
         "bedrock/invoke/anthropic.claude-3-5-sonnet-20240620-v1:0",
-        "anthropic/claude-3-5-sonnet"
+        "anthropic/claude-3-5-sonnet",
     ],
 )
 @pytest.mark.parametrize("sync_mode", [True, False])
