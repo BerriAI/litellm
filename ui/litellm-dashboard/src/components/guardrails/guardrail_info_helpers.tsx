2aba5e6dcdded94f4defcfd0992bb7f7bbec37ea
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-05-14 12:08:19 -0700

    fixes guardrails

--- ui/litellm-dashboard/src/components/guardrails/guardrail_info_helpers.tsx
+++ ui/litellm-dashboard/src/components/guardrails/guardrail_info_helpers.tsx
@@ -1,21 +1,8 @@
 export enum GuardrailProviders {
     PresidioPII = "Presidio PII",
-    Aporia = "Aporia",
-    AimSecurity = "Aim Security",
-    Bedrock = "Amazon Bedrock",
-    GuardrailsAI = "Guardrails.ai",
-    LakeraAI = "Lakera AI",
-    Custom = "Custom Guardrail",
-    PromptInjection = "Prompt Injection Detection",
 }
 
 export const guardrail_provider_map: Record<string, string> = {
-    Aporia: "aporia",
-    AimSecurity: "aim",
-    Bedrock: "bedrock",
-    GuardrailsAI: "guardrails_ai",
-    LakeraAI: "lakera",
-    PromptInjection: "detect_prompt_injection",
     PresidioPII: "presidio",
 };
 
@@ -27,13 +14,7 @@ export const provider_specific_fields: Record<string, string[]> = {
 const asset_logos_folder = '../ui/assets/logos/';
 
 export const guardrailLogoMap: Record<string, string> = {
-    [GuardrailProviders.Aporia]: `${asset_logos_folder}aporia.svg`,
-    [GuardrailProviders.AimSecurity]: `${asset_logos_folder}aim.svg`,
-    [GuardrailProviders.Bedrock]: `${asset_logos_folder}bedrock.svg`,
-    [GuardrailProviders.GuardrailsAI]: `${asset_logos_folder}guardrails_ai.svg`,
-    [GuardrailProviders.LakeraAI]: `${asset_logos_folder}lakera.svg`,
-    [GuardrailProviders.PromptInjection]: `${asset_logos_folder}prompt_injection.svg`,
-    [GuardrailProviders.PresidioPII]: `${asset_logos_folder}presidio.svg`
+    [GuardrailProviders.PresidioPII]: `${asset_logos_folder}presidio.png`
 };
 
 export const getGuardrailLogoAndName = (guardrailValue: string): { logo: string, displayName: string } => {
