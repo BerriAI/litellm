5b7b1d6cbc9523fdba9705c6e6eb2ec0db4ec588
Krrish Dholakia <krrishdholakia@gmail.com>
2025-05-15 15:47:15 -0700

    fix(types/utils.py): add '__contains__' support for chatcompletiondeltatool call
    Fixes https://github.com/BerriAI/litellm/issues/7099

--- litellm/types/utils.py
+++ litellm/types/utils.py
@@ -441,6 +441,22 @@ class ChatCompletionDeltaToolCall(OpenAIObject):
     type: Optional[str] = None
     index: int
 
+    def __contains__(self, key):
+        # Define custom behavior for the 'in' operator
+        return hasattr(self, key)
+
+    def get(self, key, default=None):
+        # Custom .get() method to access attributes with a default value if the attribute doesn't exist
+        return getattr(self, key, default)
+
+    def __getitem__(self, key):
+        # Allow dictionary-style access to attributes
+        return getattr(self, key)
+
+    def __setitem__(self, key, value):
+        # Allow dictionary-style assignment of attributes
+        setattr(self, key, value)
+
 
 class HiddenParams(OpenAIObject):
     original_response: Optional[Union[str, Any]] = None
--- tests/litellm/types/test_types_utils.py
+++ tests/litellm/types/test_types_utils.py
@@ -17,3 +17,19 @@ def test_hidden_params_response_ms():
     setattr(hidden_params, "_response_ms", 100)
     hidden_params_dict = hidden_params.model_dump()
     assert hidden_params_dict.get("_response_ms") == 100
+
+
+def test_chat_completion_delta_tool_call():
+    from litellm.types.utils import ChatCompletionDeltaToolCall, Function
+
+    tool = ChatCompletionDeltaToolCall(
+        id="call_m87w",
+        function=Function(
+            arguments='{"location": "San Francisco", "unit": "imperial"}',
+            name="get_current_weather",
+        ),
+        type="function",
+        index=0,
+    )
+
+    assert "function" in tool
