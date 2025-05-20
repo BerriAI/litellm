fdc4cccf3ba773899af14758ebcf6beee0a69a16
Krrish Dholakia <krrishdholakia@gmail.com>
2025-05-19 19:52:27 -0700

    refactor(parallel_request_limiter_v2.py): Migrate multi instance rate limiting to OSS
    Closes https://github.com/BerriAI/litellm/issues/10052

--- litellm/proxy/hooks/__init__.py
+++ litellm/proxy/hooks/__init__.py
@@ -1,9 +1,11 @@
+import os
 from typing import Literal, Union
 
 from . import *
 from .cache_control_check import _PROXY_CacheControlCheck
 from .max_budget_limiter import _PROXY_MaxBudgetLimiter
 from .parallel_request_limiter import _PROXY_MaxParallelRequestsHandler
+from .parallel_request_limiter_v2 import _PROXY_MaxParallelRequestsHandler_v2
 
 ### CHECK IF ENTERPRISE HOOKS ARE AVAILABLE ###
 
@@ -18,6 +20,11 @@ PROXY_HOOKS = {
     "parallel_request_limiter": _PROXY_MaxParallelRequestsHandler,
     "cache_control_check": _PROXY_CacheControlCheck,
 }
+
+## FEATURE FLAG HOOKS ##
+if os.getenv("EXPERIMENTAL_MULTI_INSTANCE_RATE_LIMITING", "false").lower() == "true":
+    PROXY_HOOKS["max_parallel_requests"] = _PROXY_MaxParallelRequestsHandler_v2
+
 
 ### update PROXY_HOOKS with ENTERPRISE_PROXY_HOOKS ###
 
