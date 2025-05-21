b55c8dc44829b21df53beefd26c2749f495c0bfa
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-05-21 12:57:23 -0700

    tets: test_openai_image_edit_litellm_sdk

--- tests/image_gen_tests/test_image_edits.py
+++ tests/image_gen_tests/test_image_edits.py
@@ -5,21 +5,23 @@ import os
 import sys
 import traceback
 import pytest
+import base64
 
 sys.path.insert(
     0, os.path.abspath("../..")
 )  # Adds the parent directory to the system path
 
-
+import litellm
 from litellm.utils import ImageResponse
 
 @pytest.mark.parametrize("sync_mode", [True])
 @pytest.mark.asyncio
 async def test_openai_image_edit_litellm_sdk(sync_mode):
     from litellm import image_edit, aimage_edit
+    litellm._turn_on_debug()
 
     prompt = """
-    Create a studio ghibli style image that combines all the reference images
+    Create a studio ghibli style image that combines all the reference images. Make sure the person looks like a CTO.
     """
     image=[
         open("ishaan_github.png", "rb"),
