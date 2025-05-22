197c608078ee44f367bb7359837d0d7a69a992a2
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-05-22 13:33:54 -0700

    [Feat] Add claude-4 model family  (#11060)
    * add new claude-sonnet-4-2025051
    
    * feat: add bedrock claude-4 models
    
    * add bedrock claude-4 models
    
    * add vertx_ai/claude-sonnet-4
    
    * fix provider=bedrock_converse
    
    * feat: ensure thinking is supported for claude-4 model family

--- litellm/__init__.py
+++ litellm/__init__.py
@@ -354,6 +354,8 @@ project = None
 config_path = None
 vertex_ai_safety_settings: Optional[dict] = None
 BEDROCK_CONVERSE_MODELS = [
+    "anthropic.claude-opus-4-20250514-v1:0",
+    "anthropic.claude-sonnet-4-20250514-v1:0",
     "anthropic.claude-3-7-sonnet-20250219-v1:0",
     "anthropic.claude-3-5-haiku-20241022-v1:0",
     "anthropic.claude-3-5-sonnet-20241022-v2:0",
