9b938cf6cf58db7fcf7e9a78b3cbba870553ddca
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-05-24 07:31:39 -0700

    feat: add image edits on litellm router

--- tests/image_gen_tests/test_image_edits.py
+++ tests/image_gen_tests/test_image_edits.py
@@ -21,7 +21,7 @@ TEST_IMAGES = [
     open(os.path.join(pwd, "litellm_site.png"), "rb"),
 ]
 
-@pytest.mark.parametrize("sync_mode", [True])
+@pytest.mark.parametrize("sync_mode", [True, False])
 @pytest.mark.flaky(retries=3, delay=2)
 @pytest.mark.asyncio
 async def test_openai_image_edit_litellm_sdk(sync_mode):
@@ -44,6 +44,46 @@ async def test_openai_image_edit_litellm_sdk(sync_mode):
                 model="gpt-image-1",
                 image=TEST_IMAGES,
             )
+        print("result from image edit", result)
+
+        # Validate the response meets expected schema
+        ImageResponse.model_validate(result)
+        
+        if isinstance(result, ImageResponse):
+            image_base64 = result.data[0].b64_json
+            image_bytes = base64.b64decode(image_base64)
+
+            # Save the image to a file
+            with open("test_image_edit.png", "wb") as f:
+                f.write(image_bytes)
+    except litellm.ContentPolicyViolationError as e:
+        pass
+
+
+
+@pytest.mark.flaky(retries=3, delay=2)
+@pytest.mark.asyncio
+async def test_openai_image_edit_litellm_router():
+    litellm._turn_on_debug()
+    try:
+        prompt = """
+        Create a studio ghibli style image that combines all the reference images. Make sure the person looks like a CTO.
+        """
+        router = litellm.Router(
+            model_list=[
+                {
+                    "model_name": "gpt-image-1",
+                    "litellm_params": {
+                        "model": "gpt-image-1",
+                    },
+                }
+            ]
+        )
+        result = await router.aimage_edit(
+            prompt=prompt,
+            model="gpt-image-1",
+            image=TEST_IMAGES,
+        )
         print("result from image edit", result)
 
         # Validate the response meets expected schema
