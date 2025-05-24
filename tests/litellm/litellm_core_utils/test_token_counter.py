3a5da77f79e85f7fbf622615121213c8b8208193
Krrish Dholakia <krrishdholakia@gmail.com>
2025-05-24 16:59:34 -0700

    test: update import path

--- tests/litellm/litellm_core_utils/test_token_counter.py
+++ tests/litellm/litellm_core_utils/test_token_counter.py
@@ -23,7 +23,8 @@ import litellm
 from litellm import create_pretrained_tokenizer, decode, encode, get_modified_max_tokens
 from litellm import token_counter as token_counter_old
 from litellm.litellm_core_utils.token_counter import token_counter as token_counter_new
-from tests.large_text import text
+
+from ...large_text import text
 
 
 def token_counter_both_assert_same(**args):
