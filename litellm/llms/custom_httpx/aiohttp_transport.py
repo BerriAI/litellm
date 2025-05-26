9a608e37492a8c598d0e66061664968344aea5ec
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-05-26 10:44:11 -0700

    [Fix] Rollback to httpx==0.27.0  (#11146)
    * fix - fix - use old httpx version
    
    * fix - fix - use old httpx version
    
    * fix: test
    
    * fix: comment

--- litellm/llms/custom_httpx/aiohttp_transport.py
+++ litellm/llms/custom_httpx/aiohttp_transport.py
@@ -1,11 +1,71 @@
 import asyncio
+import contextlib
+import typing
 from typing import Callable, Union
 
+import aiohttp
 import httpx
-from aiohttp.client import ClientSession
-from httpx_aiohttp import AiohttpTransport
+from aiohttp.client import ClientResponse, ClientSession
 
 from litellm._logging import verbose_logger
+
+AIOHTTP_EXC_MAP = {
+    aiohttp.ServerTimeoutError: httpx.TimeoutException,
+    aiohttp.ConnectionTimeoutError: httpx.ConnectTimeout,
+    aiohttp.SocketTimeoutError: httpx.ReadTimeout,
+    aiohttp.ClientConnectorError: httpx.ConnectError,
+    aiohttp.ClientPayloadError: httpx.ReadError,
+    aiohttp.ClientProxyConnectionError: httpx.ProxyError,
+}
+
+
+@contextlib.contextmanager
+def map_aiohttp_exceptions() -> typing.Iterator[None]:
+    try:
+        yield
+    except Exception as exc:
+        mapped_exc = None
+
+        for from_exc, to_exc in AIOHTTP_EXC_MAP.items():
+            if not isinstance(exc, from_exc):  # type: ignore
+                continue
+            if mapped_exc is None or issubclass(to_exc, mapped_exc):
+                mapped_exc = to_exc
+
+        if mapped_exc is None:  # pragma: no cover
+            raise
+
+        message = str(exc)
+        raise mapped_exc(message) from exc
+
+
+class AiohttpResponseStream(httpx.AsyncByteStream):
+    CHUNK_SIZE = 1024 * 16
+
+    def __init__(self, aiohttp_response: ClientResponse) -> None:
+        self._aiohttp_response = aiohttp_response
+
+    async def __aiter__(self) -> typing.AsyncIterator[bytes]:
+        with map_aiohttp_exceptions():
+            async for chunk in self._aiohttp_response.content.iter_chunked(
+                self.CHUNK_SIZE
+            ):
+                yield chunk
+
+    async def aclose(self) -> None:
+        with map_aiohttp_exceptions():
+            await self._aiohttp_response.__aexit__(None, None, None)
+
+
+class AiohttpTransport(httpx.AsyncBaseTransport):
+    def __init__(
+        self, client: ClientSession | typing.Callable[[], ClientSession]
+    ) -> None:
+        self.client = client
+
+    async def aclose(self) -> None:
+        if isinstance(self.client, ClientSession):
+            await self.client.close()
 
 
 class LiteLLMAiohttpTransport(AiohttpTransport):
