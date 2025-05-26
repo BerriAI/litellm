2d8b9608d83fd55694e3cb1ea5934ff839c9ed8a
Akim Tsvigun <akimtsvigun@gmail.com>
2025-05-26 22:18:50 +0200

    Reviewer's comments resolved

--- litellm/__init__.py
+++ litellm/__init__.py
@@ -1070,7 +1070,7 @@ from .llms.azure.chat.o_series_transformation import AzureOpenAIO1Config
 from .llms.watsonx.completion.transformation import IBMWatsonXAIConfig
 from .llms.watsonx.chat.transformation import IBMWatsonXChatConfig
 from .llms.watsonx.embed.transformation import IBMWatsonXEmbeddingConfig
-from .llms.nebius.chat import NebiusConfig
+from .llms.nebius.chat.transformation import NebiusConfig
 from .main import *  # type: ignore
 from .integrations import *
 from .exceptions import (
