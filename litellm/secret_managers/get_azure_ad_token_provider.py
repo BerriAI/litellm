85525c5c6bf5ec0baef33b88d5c75ff5feea0bb1
Niko Izsak <izsak.niko@gmail.com>
2025-05-28 18:33:13 +0200

    Azure OIDC provider improvements + OIDC audience bug fix  (#10054)
    * added support for custom scope in get_azure_ad_token_provider
    
    * if AZURE_FEDERATED_TOKEN_FILE not set, use azure_token_provider to retrive token with the oidc audiances as scope
    
    * fix bug where oidc audience that contains "/" won't be extract correctly
    
    * added tests for get_secret with oidc
    
    * moved tests to litellm tests folder
    
    * tes file naming aligned with source code
    
    * renamed test_main because it caused issue in the test in github workflow
    
    * updated docs
    
    * moved docs to the end of file
    
    * fix aws region in example config
    
    * renamed test file
    
    * added support for custom scope in get_azure_ad_token_provider
    
    * if AZURE_FEDERATED_TOKEN_FILE not set, use azure_token_provider to retrive token with the oidc audiances as scope
    
    * fix bug where oidc audience that contains "/" won't be extract correctly
    
    * added tests for get_secret with oidc
    
    * moved tests to litellm tests folder
    
    * tes file naming aligned with source code
    
    * renamed test_main because it caused issue in the test in github workflow
    
    * updated docs
    
    * moved docs to the end of file
    
    * fix aws region in example config
    
    * renamed test file
    
    * fix merge conflict resolution error
    
    * added support for custom scope in get_azure_ad_token_provider
    
    * if AZURE_FEDERATED_TOKEN_FILE not set, use azure_token_provider to retrive token with the oidc audiances as scope
    
    * fix bug where oidc audience that contains "/" won't be extract correctly
    
    * added tests for get_secret with oidc
    
    * moved tests to litellm tests folder
    
    * tes file naming aligned with source code
    
    * renamed test_main because it caused issue in the test in github workflow
    
    * updated docs
    
    * moved docs to the end of file
    
    * fix aws region in example config
    
    * renamed test file
    
    * added tests for get_secret with oidc
    
    * moved tests to litellm tests folder
    
    * tes file naming aligned with source code
    
    * renamed test_main because it caused issue in the test in github workflow
    
    * updated docs
    
    * moved docs to the end of file
    
    * renamed test file
    
    * fix merge conflict resolution error

--- litellm/secret_managers/get_azure_ad_token_provider.py
+++ litellm/secret_managers/get_azure_ad_token_provider.py
@@ -6,7 +6,7 @@ from litellm.types.secret_managers.get_azure_ad_token_provider import (
 )
 
 
-def get_azure_ad_token_provider() -> Callable[[], str]:
+def get_azure_ad_token_provider(azure_scope: Optional[str] = None) -> Callable[[], str]:
     """
     Get Azure AD token provider based on Service Principal with Secret workflow.
 
@@ -14,6 +14,11 @@ def get_azure_ad_token_provider() -> Callable[[], str]:
     See Also:
         https://learn.microsoft.com/en-us/python/api/overview/azure/identity-readme?view=azure-python#service-principal-with-secret;
         https://learn.microsoft.com/en-us/python/api/azure-identity/azure.identity.clientsecretcredential?view=azure-python.
+
+    Args:
+        azure_scope (str, optional): The Azure scope to request token for.
+                                    Defaults to environment variable AZURE_SCOPE or
+                                    "https://cognitiveservices.azure.com/.default".
 
     Returns:
         Callable that returns a temporary authentication token.
@@ -26,9 +31,9 @@ def get_azure_ad_token_provider() -> Callable[[], str]:
         get_bearer_token_provider,
     )
 
-    azure_scope = os.environ.get(
-        "AZURE_SCOPE", "https://cognitiveservices.azure.com/.default"
-    )
+    if azure_scope is None:
+        azure_scope = os.environ.get("AZURE_SCOPE", "https://cognitiveservices.azure.com/.default")
+
     cred: Union[AzureCredentialType, str] = AzureCredentialType(
         os.environ.get("AZURE_CREDENTIAL", AzureCredentialType.ClientSecretCredential)
     )
