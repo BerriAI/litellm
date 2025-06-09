9dfc6e638734628ef2acb1f9cc1e7bc02148240d
gustavo.nunez <gustavo.nunez@externos.w2m.com>
2025-06-09 10:28:59 +0200

    Pass custom_llm_provider_to_embeddings

--- litellm/main.py
+++ litellm/main.py
@@ -3368,7 +3368,10 @@ async def aembedding(*args, **kwargs) -> EmbeddingResponse:
         func_with_context = partial(ctx.run, func)
 
         _, custom_llm_provider, _, _ = get_llm_provider(
-            model=model, api_base=kwargs.get("api_base", None)
+            model=model,
+            custom_llm_provider=kwargs.get("custom_llm_provider", None),
+            api_base=kwargs.get("api_base", None),
+            api_key=kwargs.get("api_key", None),
         )
 
         # Await normally
