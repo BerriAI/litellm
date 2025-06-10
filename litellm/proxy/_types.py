07006e6ec018777e44be5d43c51e0a4c1e507d13
Krrish Dholakia <krrishdholakia@gmail.com>
2025-06-09 19:36:00 -0700

    feat(internal_user_endpoints.py): assign user to orgs on user creation
    allows user to be a member of orgs on creation - work to enable default orgs on UI

--- litellm/proxy/_types.py
+++ litellm/proxy/_types.py
@@ -911,6 +911,7 @@ class NewUserRequest(GenerateRequestBase):
     )
     send_invite_email: Optional[bool] = None
     sso_user_id: Optional[str] = None
+    organizations: Optional[List[str]] = None
 
 
 class NewUserResponse(GenerateKeyResponse):
@@ -1342,7 +1343,6 @@ class LiteLLM_BudgetTable(LiteLLMPydanticObjectBase):
 class LiteLLM_BudgetTableFull(LiteLLM_BudgetTable):
     """Represents all params for a LiteLLM_BudgetTable record"""
 
-    budget_id: str
     created_at: datetime
 
 
@@ -2519,7 +2519,10 @@ class OrgMemberAddRequest(LiteLLMPydanticObjectBase):
         member_data = data.get("member")
         if isinstance(member_data, list):
             # If member is a list of dictionaries, convert each dictionary to a Member object
-            members = [OrgMember(**item) for item in member_data]
+            if all(isinstance(item, dict) for item in member_data):
+                members = [OrgMember(**item) for item in member_data]
+            else:
+                members = [item for item in member_data]
             # Replace member_data with the list of Member objects
             data["member"] = members
         elif isinstance(member_data, dict):
--- litellm/proxy/management_endpoints/organization_endpoints.py
+++ litellm/proxy/management_endpoints/organization_endpoints.py
@@ -516,7 +516,7 @@ async def deprecated_info_organization(data: OrganizationRequest):
 @management_endpoint_wrapper
 async def organization_member_add(
     data: OrganizationMemberAddRequest,
-    http_request: Request,
+    http_request: Optional[Request] = None,
     user_api_key_dict: UserAPIKeyAuth = Depends(user_api_key_auth),
 ) -> OrganizationAddMemberResponse:
     """
