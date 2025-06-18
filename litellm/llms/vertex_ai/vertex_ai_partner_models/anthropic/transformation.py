2740c8d77d8612525b92ad84085dcd5245b80ba1
X4tar <32390515+X4tar@users.noreply.github.com>
2025-06-18 13:35:31 +0800

    Fix vertex ai claude thinking params (#11796)
    * fix:  vertex_ai/claude-sonnet-4 thinking params can not be accepted
    
    * CHORE: add unit test
    
    ---------
    
    Co-authored-by: wick.hu <wick.hu@momenta.ai>

--- litellm/llms/vertex_ai/vertex_ai_partner_models/anthropic/transformation.py
+++ litellm/llms/vertex_ai/vertex_ai_partner_models/anthropic/transformation.py
@@ -46,6 +46,9 @@ class VertexAIAnthropicConfig(AnthropicConfig):
 
     Note: Please make sure to modify the default parameters as required for your use case.
     """
+    @property
+    def custom_llm_provider(self) -> Optional[str]:
+        return "vertex_ai"
 
     def transform_request(
         self,
