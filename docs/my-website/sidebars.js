0f849ae891d3232e854014d8778a3863f0c079b6
Krrish Dholakia <krrishdholakia@gmail.com>
2025-06-21 16:50:03 -0700

    docs: rename doc

--- docs/my-website/sidebars.js
+++ docs/my-website/sidebars.js
@@ -519,7 +519,7 @@ const sidebars = {
         "tutorials/openweb_ui",
         "tutorials/openai_codex",
         "tutorials/anthropic_file_usage",
-        "tutorials/sso_self_serve",
+        "tutorials/default_team_self_serve",
         "tutorials/msft_sso",
         "tutorials/prompt_caching",
         "tutorials/tag_management",
