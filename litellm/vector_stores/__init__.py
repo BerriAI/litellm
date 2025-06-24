5e6a23efbcf352cc65ebbd09c34f1f91e75219b7
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-06-24 14:09:56 -0700

    add search, asearch vector stores

---
+++ litellm/vector_stores/__init__.py
@@ -0,0 +1,3 @@
+from .main import asearch, search
+
+__all__ = ["search", "asearch"] 
---
+++ litellm/vector_stores/main.py
@@ -0,0 +1,15 @@
+"""
+LiteLLM SDK Functions for Creating and Searching Vector Stores
+"""
+def search():
+    """
+    Search a vector store
+    """
+    pass
+
+
+async def asearch():
+    """
+    Async version of search
+    """
+    pass
