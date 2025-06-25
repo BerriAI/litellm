5cf92351e8d78bb26ec1d140fbcec6d4f9f2ba61
Krish Dholakia <krrishdholakia@gmail.com>
2025-06-25 15:19:05 -0700

    Revert "Add Elasticsearch Logging Tutorial (#11761)"
    This reverts commit 51c1c7bd36f2b5c5b499400332f9ad27fce75ed0.

--- docs/my-website/sidebars.js
+++ docs/my-website/sidebars.js
@@ -525,7 +525,6 @@ const sidebars = {
         "tutorials/prompt_caching",
         "tutorials/tag_management",
         'tutorials/litellm_proxy_aporia',
-        "tutorials/elasticsearch_logging",
         "tutorials/gemini_realtime_with_audio",
         "tutorials/claude_responses_api",
         {
