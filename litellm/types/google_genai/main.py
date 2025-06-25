161e8006c26bbc41665a18b8b688c6b35bef5ee1
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-06-25 14:17:42 -0700

    fix - required imports

--- litellm/types/google_genai/main.py
+++ litellm/types/google_genai/main.py
@@ -1,19 +1,39 @@
 # Import types from the Google GenAI SDK
-from typing import Optional
+from typing import Any, Optional, TypedDict
 
-from google.genai import types
-
-ContentListUnion = types.ContentListUnion
-ContentListUnionDict = types.ContentListUnionDict
-GenerateContentConfigOrDict = types.GenerateContentConfigOrDict
-GenerateContentResponse = types.GenerateContentResponse
-
-########################################################
-# Request Type
-########################################################
-GenerateContentContentListUnionDict = types.ContentListUnionDict
-GenerateContentConfigDict = types.GenerateContentConfigDict
+from pydantic import BaseModel
 
 
-class GenerateContentRequestDict(types._GenerateContentParametersDict, total=False):
-    generationConfig: Optional[GenerateContentConfigDict]
+# Define fallback base class
+class _FallbackRequestDict(TypedDict):
+    pass
+
+try:
+    from google.genai import types
+
+    ContentListUnion = types.ContentListUnion
+    ContentListUnionDict = types.ContentListUnionDict
+    GenerateContentConfigOrDict = types.GenerateContentConfigOrDict
+    GenerateContentResponse = types.GenerateContentResponse
+
+    ########################################################
+    # Request Type
+    ########################################################
+    GenerateContentContentListUnionDict = types.ContentListUnionDict
+    GenerateContentConfigDict = types.GenerateContentConfigDict
+    GenerateContentRequestParametersDict = types._GenerateContentParametersDict
+
+    # When google-genai is available, inherit from their base clas
+
+except ImportError:
+    # If google-genai is not installed, we need to define the types manually
+    ContentListUnion = Any
+    ContentListUnionDict = Any
+    GenerateContentConfigOrDict = Any
+    GenerateContentResponse = Any
+    GenerateContentContentListUnionDict = Any
+    GenerateContentConfigDict = Any
+    GenerateContentRequestParametersDict = _FallbackRequestDict
+
+class GenerateContentRequestDict(GenerateContentRequestParametersDict):  # type: ignore
+    generationConfig: Optional[Any]
