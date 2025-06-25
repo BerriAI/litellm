a712958ca36ba3f756e4f6a3ed7b71f892d73910
ChihWei Chen <chihweiwork@gmail.com>
2025-06-25 13:43:05 +0000

    refactor: rewrite get_formatted_prompt.py to support acompletion parsing

--- litellm/litellm_core_utils/llm_response_utils/get_formatted_prompt.py
+++ litellm/litellm_core_utils/llm_response_utils/get_formatted_prompt.py
@@ -1,49 +1,43 @@
+from litellm._logging import verbose_proxy_logge
 from typing import List, Literal
-
 
 def get_formatted_prompt(
     data: dict,
     call_type: Literal[
         "completion",
+        "acompletion",
         "embedding",
         "image_generation",
         "audio_transcription",
         "moderation",
         "text_completion",
-    ],
-) -> str:
+    ]) -> str:
+
     """
     Extracts the prompt from the input data based on the call type.
-
     Returns a string.
     """
-    prompt = ""
-    if call_type == "completion":
-        for message in data["messages"]:
-            if message.get("content", None) is not None:
-                content = message.get("content")
-                if isinstance(content, str):
-                    prompt += message["content"]
-                elif isinstance(content, List):
-                    for c in content:
-                        if c["type"] == "text":
-                            prompt += c["text"]
-            if "tool_calls" in message:
-                for tool_call in message["tool_calls"]:
-                    if "function" in tool_call:
-                        function_arguments = tool_call["function"]["arguments"]
-                        prompt += function_arguments
-    elif call_type == "text_completion":
-        prompt = data["prompt"]
-    elif call_type == "embedding" or call_type == "moderation":
-        if isinstance(data["input"], str):
-            prompt = data["input"]
-        elif isinstance(data["input"], list):
-            for m in data["input"]:
-                prompt += m
-    elif call_type == "image_generation":
-        prompt = data["prompt"]
-    elif call_type == "audio_transcription":
-        if "prompt" in data:
-            prompt = data["prompt"]
-    return prompt
+    if call_type in {"completion", "acompletion"}:
+        return _extract_messages(data.get("messages", []))
+    elif call_type in {"text_completion"}:
+        return "".join(data.get('prompt', []))
+    elif call_type in {"image_generation", "audio_transcription"}:
+        if (prompt:=data.get('prompt', None)):
+            return prompt
+    elif call_type in {"embedding", "moderation"}:
+        input_data = data.get("input", "")
+        if isinstance(input_data, str):
+            return input_data
+        elif isinstance(input_data, list):
+            return "".join(map(str, input_data))
+        return ""
+    else:
+        verbose_proxy_logger.warning(f"Undefined call_type: {call_type}")
+        return ""
+
+def _extract_messages(messages: List[dict]) -> str:
+    result = ""
+    for message in messages:
+        content = message.get('content', None)
+        if content == None:
+            continue
