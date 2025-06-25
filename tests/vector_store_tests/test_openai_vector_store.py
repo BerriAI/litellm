d6cc3847808dabbee472943f9977a1be25ab99fe
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-06-24 20:46:48 -0700

    [Feat] OpenAI/Azure OpenAI - Add support for creating vector stores on LiteLLM (#12021)
    * add create/acreate vector store
    
    * add azure config
    
    * add _base_validate_azure_environment
    
    * fix base test
    
    * add get_base_create_vector_store_args
    
    * use base llm for headers responses api
    
    * add _get_base_azure_url
    
    * fix AzureOpenAIVectorStoreConfig
    
    * TestAzureOpenAIVectorStore
    
    * fix azure openai vector store
    
    * fix test comment
    
    * fix unused imports
    
    * test_validate_environment_azure_api_key_within_secret_str
    
    * test_azure_transformation.py

--- tests/vector_store_tests/test_openai_vector_store.py
+++ tests/vector_store_tests/test_openai_vector_store.py
@@ -9,3 +9,12 @@ class TestOpenAIVectorStore(BaseVectorStoreTest):
             "vector_store_id": "vs_685b14b1a1b88191bc27e04f1917fddd",
             "custom_llm_provider": "openai",
         }
+    
+
+    def get_base_create_vector_store_args(self) -> dict:
+        """
+        This is a real vector store on OpenAI
+        """
+        return {
+            "custom_llm_provider": "openai",
+        }
