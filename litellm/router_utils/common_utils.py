1a7fd1d1c7172f7904c270b356d0631ff6114c4b
Krish Dholakia <krrishdholakia@gmail.com>
2025-06-25 21:54:13 -0700

    Litellm dev 06 25 2025 p2 (#12049)
    * test(test_router.py): initial unit test confirming router.afile_content uses dynamic api key / api base
    
    * fix(managed_files.py): filter deployments for only those within file id mapping
    
    ensure call works - only route to models where the file was written
    
    * fix(proxy_server.py): fix loading in model ids from config, if config id is int
    
    * fix(router.py): return all model file id mappings on create_file
    
    if multiple deployments - this ensures all the file id mappings are bubbled up
    
    Fixes issue when trying to use loadbalanced deployments - only 1 file id mapping was being stored
    
    * feat(router_utils/common_utils.py): filter models by team id when selecting for routing
    
    Prevents team only models from being used by other teams
    
    * fix(common_utils.py): additional fixes around filtering team-based models
    
    * fix(batches_endpoints/endpoints): support list batches with target model names specified
    
    * fix(common_utils.py): more testing for team deployment filters

--- litellm/router_utils/common_utils.py
+++ litellm/router_utils/common_utils.py
@@ -1,6 +1,10 @@
 import hashlib
 import json
+<<<<<<< litellm_dev_06_25_2025_p2
+from typing import TYPE_CHECKING, Dict, List, Optional, Union
+=======
 from typing import TYPE_CHECKING, Dict, List, Union
+>>>>>>> main
 
 if TYPE_CHECKING:
     from litellm.types.llms.openai import OpenAIFileObject
@@ -38,3 +42,41 @@ def add_model_file_id_mappings(
         for model_id, file_id in healthy_deployments.items():
             model_file_id_mapping[model_id] = file_id
     return model_file_id_mapping
+<<<<<<< litellm_dev_06_25_2025_p2
+
+
+def filter_team_based_models(
+    healthy_deployments: Union[List[Dict], Dict],
+    request_kwargs: Optional[Dict] = None,
+) -> Union[List[Dict], Dict]:
+    """
+    If a model has a team_id
+
+    Only use if request is from that team
+    """
+    if request_kwargs is None:
+        return healthy_deployments
+
+    metadata = request_kwargs.get("metadata") or {}
+    litellm_metadata = request_kwargs.get("litellm_metadata") or {}
+    request_team_id = metadata.get("user_api_key_team_id") or litellm_metadata.get(
+        "user_api_key_team_id"
+    )
+    ids_to_remove = []
+    if isinstance(healthy_deployments, dict):
+        return healthy_deployments
+    for deployment in healthy_deployments:
+        _model_info = deployment.get("model_info") or {}
+        model_team_id = _model_info.get("team_id")
+        if model_team_id is None:
+            continue
+        if model_team_id != request_team_id:
+            ids_to_remove.append(deployment.get("model_info", {}).get("id"))
+
+    return [
+        deployment
+        for deployment in healthy_deployments
+        if deployment.get("model_info", {}).get("id") not in ids_to_remove
+    ]
+=======
+>>>>>>> main
