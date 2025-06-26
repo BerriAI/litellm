efd8ed7de823503e3b282250702f92ed65a0dcdf
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-06-26 13:18:07 -0700

    add google gen ai adapter

---
+++ litellm/google_genai/adapters/__init__.py
@@ -0,0 +1,7 @@
+"""
+Google GenAI Adapters for transforming between generate_content and completion formats
+"""
+
+from .transformation import GoogleGenAIAdapter
+
+__all__ = ["GoogleGenAIAdapter"] 
