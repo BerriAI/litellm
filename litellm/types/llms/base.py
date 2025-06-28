a38bacd91ddfa5f662c9864ba0b5cffb34780762
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-06-28 14:08:13 -0700

    use 1 file for KeyManagementSystem

--- litellm/types/llms/base.py
+++ litellm/types/llms/base.py
@@ -1,4 +1,27 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
+
+
+class LiteLLMPydanticObjectBase(BaseModel):
+    """
+    Implements default functions, all pydantic objects should have.
+    """
+
+    def json(self, **kwargs):  # type: ignore
+        try:
+            return self.model_dump(**kwargs)  # noqa
+        except Exception:
+            # if using pydantic v1
+            return self.dict(**kwargs)
+
+    def fields_set(self):
+        try:
+            return self.model_fields_set  # noqa
+        except Exception:
+            # if using pydantic v1
+            return self.__fields_set__
+
+    model_config = ConfigDict(protected_namespaces=())
+
 
 
 class BaseLiteLLMOpenAIResponseObject(BaseModel):
