7ef590df84d0a6ad452fa6dfe88c5f42394b4bfb
Zayd <zaydsimjee@gmail.com>
2025-07-01 18:07:38 -0700

    Passes through extra_ properties on "custom" llm provider (#12185)
    * Passes through headers on "custom" llm provider
    
    * add test
    
    * adds extra_body support for custom llm providers

--- litellm/main.py
+++ litellm/main.py
@@ -3221,6 +3221,7 @@ def completion(  # type: ignore # noqa: PLR0915
             prompt = " ".join([message["content"] for message in messages])  # type: ignore
             resp = litellm.module_level_client.post(
                 url,
+                headers=headers,
                 json={
                     "model": model,
                     "params": {
@@ -3230,6 +3231,7 @@ def completion(  # type: ignore # noqa: PLR0915
                         "top_p": top_p,
                         "top_k": kwargs.get("top_k"),
                     },
+                    **kwargs.get("extra_body", {}),
                 },
             )
             response_json = resp.json()
