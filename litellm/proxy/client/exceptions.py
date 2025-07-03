995d8d3c4df0509608c60c4578ff0cd4354b65e3
Nick Albion <nicholas.albion@lendigroup.com.au>
2025-07-04 00:28:29 +1000

    GitHub workflow uses python 3.12

--- litellm/proxy/client/exceptions.py
+++ litellm/proxy/client/exceptions.py
@@ -5,7 +5,7 @@ from typing import Union
 class UnauthorizedError(Exception):
     """Exception raised when the API returns a 401 Unauthorized response."""
 
-    def __init__(self, orig_exception: Union[requests.exceptions.HTTPError, str]):
+    def __init__(self, orig_exception: requests.exceptions.HTTPError | str):
         self.orig_exception = orig_exception
         super().__init__(str(orig_exception))
 
@@ -13,6 +13,6 @@ class UnauthorizedError(Exception):
 class NotFoundError(Exception):
     """Exception raised when the API returns a 404 Not Found response or indicates a resource was not found."""
 
-    def __init__(self, orig_exception: Union[requests.exceptions.HTTPError, str]):
+    def __init__(self, orig_exception: requests.exceptions.HTTPError | str):
         self.orig_exception = orig_exception
         super().__init__(str(orig_exception))
