2fc3eec6c9aabd8ea8a3c651a9d108fcb1bb9743
Krrish Dholakia <krrishdholakia@gmail.com>
2025-07-02 17:53:21 -0700

    test(test_azure_text_moderation.py): add unit test for responses

--- tests/test_litellm/proxy/guardrails/guardrail_hooks/azure/test_azure_text_moderation.py
+++ tests/test_litellm/proxy/guardrails/guardrail_hooks/azure/test_azure_text_moderation.py
@@ -48,3 +48,40 @@ async def test_azure_text_moderation_guardrail_pre_call_hook():
 
         mock_async_make_request.assert_called_once()
         assert mock_async_make_request.call_args.kwargs["text"] == "I hate you!"
+
+
+@pytest.mark.asyncio
+async def test_azure_text_moderation_guardrail_post_call_success_hook():
+
+    azure_text_moderation_guardrail = AzureContentSafetyTextModerationGuardrail(
+        guardrail_name="azure_text_moderation",
+        api_key="azure_text_moderation_api_key",
+        api_base="azure_text_moderation_api_base",
+    )
+    with patch.object(
+        azure_text_moderation_guardrail, "async_make_request"
+    ) as mock_async_make_request:
+        mock_async_make_request.return_value = {
+            "blocklistsMatch": [],
+            "categoriesAnalysis": [
+                {"category": "Hate", "severity": 2},
+            ],
+        }
+        with pytest.raises(HTTPException):
+            result = await azure_text_moderation_guardrail.async_post_call_success_hook(
+                data={},
+                user_api_key_dict=UserAPIKeyAuth(
+                    api_key="azure_text_moderation_api_key"
+                ),
+                response=ModelResponse(
+                    choices=[
+                        Choices(
+                            index=0,
+                            message=Message(content="I hate you!"),
+                        )
+                    ]
+                ),
+            )
+
+        mock_async_make_request.assert_called_once()
+        mock_async_make_request.call_args.kwargs["text"] == "I hate you!"
