d3e5cb8ed249b51c5f3a0f133b20997453d3f37a
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-07-08 14:48:53 -0700

    fix _get_http_exception_for_blocked_guardrail

--- litellm/proxy/guardrails/guardrail_hooks/bedrock_guardrails.py
+++ litellm/proxy/guardrails/guardrail_hooks/bedrock_guardrails.py
@@ -287,7 +287,6 @@ class BedrockGuardrail(CustomGuardrail, BaseAWSLLM):
             for output in outputs:
                 if output.get("text"):
                     bedrock_guardrail_output_text += output.get("text") or ""
-                    bedrock_guardrail_output_text += "\n"
                 
         
         return HTTPException(
