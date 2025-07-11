8a892ed1d1d89c2c6577266660468b4f47b420a2
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-07-11 16:15:39 -0700

    allow using standard_callback_dynamic_params to disable callbacks

--- litellm/types/utils.py
+++ litellm/types/utils.py
@@ -2086,6 +2086,7 @@ class StandardCallbackDynamicParams(TypedDict, total=False):
 
     # Logging settings
     turn_off_message_logging: Optional[bool]  # when true will not log messages
+    litellm_disabled_callbacks: Optional[List[str]]
 
 
 all_litellm_params = [
