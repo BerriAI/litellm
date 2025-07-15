8af17819e1f1a9342f2c7aa73750d9123f5cdbd7
Krrish Dholakia <krrishdholakia@gmail.com>
2025-07-14 20:59:54 -0700

    fix(prisma_migration.py): don't use subprocess

--- litellm/proxy/prisma_migration.py
+++ litellm/proxy/prisma_migration.py
@@ -2,15 +2,15 @@
 ## Script to apply initial prisma migration on Docker setup
 
 import os
-import subprocess
 import sys
 
 sys.path.insert(
     0, os.path.abspath("./")
 )  # Adds the parent directory to the system path
 
-result = subprocess.run(
-    ["litellm", "--use_prisma_migrate", "--skip_server_startup"],
-    capture_output=True,
-    text=True,
+from litellm.proxy.proxy_cli import run_server
+
+run_server(
+    use_prisma_migrate=True,
+    skip_server_startup=True,
 )
