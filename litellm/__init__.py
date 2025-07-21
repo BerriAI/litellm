dd48c71461e88f2eb60f84672f4ae13fbe11e046
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-07-21 14:22:18 -0700

    fix code qa check

--- litellm/__init__.py
+++ litellm/__init__.py
@@ -504,6 +504,7 @@ dashscope_models: List = []
 moonshot_models: List = []
 v0_models: List = []
 lambda_ai_models: List = []
+recraft_models: List = []
 
 def is_bedrock_pricing_only_model(key: str) -> bool:
     """
@@ -686,6 +687,8 @@ def add_known_models():
             v0_models.append(key)
         elif value.get("litellm_provider") == "lambda_ai":
             lambda_ai_models.append(key)
+        elif value.get("litellm_provider") == "recraft":
+            recraft_models.append(key)
 
 
 add_known_models()
@@ -772,6 +775,7 @@ model_list = (
     + moonshot_models
     + v0_models
     + lambda_ai_models
+    + recraft_models
 )
 
 model_list_set = set(model_list)
@@ -841,6 +845,7 @@ models_by_provider: dict = {
     "moonshot": moonshot_models,
     "v0": v0_models,
     "lambda_ai": lambda_ai_models,
+    "recraft": recraft_models,
 }
 
 # mapping for those models which have larger equivalents
