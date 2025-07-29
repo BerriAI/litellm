5dbf1149976bddbf39f938c6c4d0d16a83142e54
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-07-29 12:46:55 -0700

    Revert "fix get_content_from_model_response"
    This reverts commit 3c3891d4a9a5a586923686ad00b941720ce4fbad.

--- litellm/litellm_core_utils/prompt_templates/common_utils.py
+++ litellm/litellm_core_utils/prompt_templates/common_utils.py
@@ -204,9 +204,9 @@ def get_content_from_model_response(response: Union[ModelResponse, dict]) -> str
     for choice in new_response.choices:
         if isinstance(choice, Choices):
             content += choice.message.content if choice.message.content else ""
-            if hasattr(choice.message, "function_call") and choice.message.function_call:
+            if choice.message.function_call:
                 content += choice.message.function_call.model_dump_json()
-            if hasattr(choice.message, "tool_calls") and choice.message.tool_calls:
+            if choice.message.tool_calls:
                 for tc in choice.message.tool_calls:
                     content += tc.model_dump_json()
         elif isinstance(choice, StreamingChoices):
