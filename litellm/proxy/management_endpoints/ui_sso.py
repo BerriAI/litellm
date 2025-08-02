217651f2d68111ac894b725f46360c84b65c01ef
Cole McIntosh <colemcintosh6@gmail.com>
2025-08-02 10:08:32 -0600

    Fix unused import: remove secrets module

--- litellm/proxy/management_endpoints/ui_sso.py
+++ litellm/proxy/management_endpoints/ui_sso.py
@@ -10,7 +10,6 @@ Has all /sso/* routes
 
 import asyncio
 import os
-import secrets
 import uuid
 from copy import deepcopy
 from typing import TYPE_CHECKING, Any, Dict, List, Optional, Tuple, Union, cast
