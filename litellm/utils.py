63220dbe0ddcae27a4f7c804365a12e9f93104f3
Krish Dholakia <krrishdholakia@gmail.com>
2025-08-04 23:44:02 -0700

    fix(main.py): handle tool being a pydantic object (#13274)
    * fix(main.py): handle tool being a pydantic object
    
    Fixes https://github.com/BerriAI/litellm/issues/13064
    
    * fix(prompt_templates/common_utils.py): fix unpack defs deepcopy issue
    
    Fixes https://github.com/BerriAI/litellm/issues/13151
    
    * fix(utils.py): handle tools is none

--- litellm/utils.py
+++ litellm/utils.py
@@ -6604,6 +6604,19 @@ def validate_and_fix_openai_messages(messages: List):
         new_messages.append(cleaned_message)
     return validate_chat_completion_user_messages(messages=new_messages)
 
+def validate_and_fix_openai_tools(tools: Optional[List]) -> Optional[List[dict]]: 
+    """
+    Ensure tools is List[dict] and not List[BaseModel]
+    """
+    new_tools = []
+    if tools is None:
+        return tools
+    for tool in tools:
+        if isinstance(tool, BaseModel):
+            new_tools.append(tool.model_dump())
+        elif isinstance(tool, dict):
+            new_tools.append(tool)
+    return new_tools
 
 def cleanup_none_field_in_message(message: AllMessageValues):
     """
