1415b0b1bbe6b069027dcdb10abf7df4fca0550a
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-08-06 23:19:59 -0700

    test responses API

--- test_responses_api.py
+++ test_responses_api.py
@@ -1,8 +1,8 @@
 import base64
 from openai import OpenAI
-
+import time
 client = OpenAI(
-    base_url="http://0.0.0.0:12635",
+    base_url="http://0.0.0.0:4001",
     api_key="sk-1234"
 )
 
@@ -25,7 +25,7 @@ response = client.responses.create(
         {
             "role": "user",
             "content": [
-                { "type": "input_text", "text": "what's in this image?" },
+                { "type": "input_text", "text": "what color is the image"},
                 {
                     "type": "input_image",
                     "image_url": f"data:image/jpeg;base64,{base64_image}",
@@ -36,4 +36,18 @@ response = client.responses.create(
 )
 
 
+
 print(response.output_text)
+print("response1 id===", response.id)
+print("sleeping for 20 seconds...")
+time.sleep(20)
+print("making follow up request for existing id")
+response2 = client.responses.create(
+    model="bedrock/us.anthropic.claude-3-5-sonnet-20241022-v2:0",
+    previous_response_id=response.id,
+    input="ok, and what objects are in the image?"
+)
+
+print(response2.output_text)
+
+
