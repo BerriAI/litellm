70ed4e41f18d234a845a0cc2c3193c0433712068
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-08-12 09:33:21 -0700

    fixes google_count_tokens

--- litellm/proxy/google_endpoints/endpoints.py
+++ litellm/proxy/google_endpoints/endpoints.py
@@ -149,7 +149,6 @@ async def google_stream_generate_content(
 @router.post("/models/{model_name}:countTokens", dependencies=[Depends(user_api_key_auth)])
 async def google_count_tokens(request: Request, model_name: str):
     """
-    Not Implemented, this is a placeholder for the google genai countTokens endpoint.
     ```json
     return {
         "totalTokens": 31,
