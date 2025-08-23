91872b787c71c45b74dbabfebd2762d5090a8029
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2025-08-23 11:59:10 -0700

    remove conftest

--- tests/router_unit_tests/conftest.py
+++ tests/router_unit_tests/conftest.py
@@ -13,58 +13,58 @@ import litellm
 
 import asyncio
 
-@pytest.fixture(scope="session")
-def event_loop():
-    try:
-        loop = asyncio.get_running_loop()
-    except RuntimeError:
-        loop = asyncio.new_event_loop()
-    yield loop
-    loop.close()
+# @pytest.fixture(scope="session")
+# def event_loop():
+#     try:
+#         loop = asyncio.get_running_loop()
+#     except RuntimeError:
+#         loop = asyncio.new_event_loop()
+#     yield loop
+#     loop.close()
 
 
-@pytest.fixture(scope="function", autouse=True)
-def setup_and_teardown(event_loop):  # Add event_loop as a dependency
-    curr_dir = os.getcwd()
-    sys.path.insert(0, os.path.abspath("../.."))
+# @pytest.fixture(scope="function", autouse=True)
+# def setup_and_teardown(event_loop):  # Add event_loop as a dependency
+#     curr_dir = os.getcwd()
+#     sys.path.insert(0, os.path.abspath("../.."))
 
-    import litellm
-    from litellm import Router
+#     import litellm
+#     from litellm import Router
 
-    from litellm.litellm_core_utils.logging_worker import GLOBAL_LOGGING_WORKER
-    # flush all logs
-    print("conftest.py clearing queue of logs.....")
-    asyncio.run(GLOBAL_LOGGING_WORKER.clear_queue())
-    print("conftest.py queue cleared........")
+#     from litellm.litellm_core_utils.logging_worker import GLOBAL_LOGGING_WORKER
+#     # flush all logs
+#     print("conftest.py clearing queue of logs.....")
+#     asyncio.run(GLOBAL_LOGGING_WORKER.clear_queue())
+#     print("conftest.py queue cleared........")
 
-    importlib.reload(litellm)
+#     importlib.reload(litellm)
 
-    # Set the event loop from the fixture
-    asyncio.set_event_loop(event_loop)
+#     # Set the event loop from the fixture
+#     asyncio.set_event_loop(event_loop)
 
-    print(litellm)
-    yield
+#     print(litellm)
+#     yield
 
-    # Clean up any pending tasks
-    pending = asyncio.all_tasks(event_loop)
-    for task in pending:
-        task.cancel()
+#     # Clean up any pending tasks
+#     pending = asyncio.all_tasks(event_loop)
+#     for task in pending:
+#         task.cancel()
 
-    # Run the event loop until all tasks are cancelled
-    if pending:
-        event_loop.run_until_complete(asyncio.gather(*pending, return_exceptions=True))
+#     # Run the event loop until all tasks are cancelled
+#     if pending:
+#         event_loop.run_until_complete(asyncio.gather(*pending, return_exceptions=True))
 
 
-def pytest_collection_modifyitems(config, items):
-    # Separate tests in 'test_amazing_proxy_custom_logger.py' and other tests
-    custom_logger_tests = [
-        item for item in items if "custom_logger" in item.parent.name
-    ]
-    other_tests = [item for item in items if "custom_logger" not in item.parent.name]
+# def pytest_collection_modifyitems(config, items):
+#     # Separate tests in 'test_amazing_proxy_custom_logger.py' and other tests
+#     custom_logger_tests = [
+#         item for item in items if "custom_logger" in item.parent.name
+#     ]
+#     other_tests = [item for item in items if "custom_logger" not in item.parent.name]
 
-    # Sort tests based on their names
-    custom_logger_tests.sort(key=lambda x: x.name)
-    other_tests.sort(key=lambda x: x.name)
+#     # Sort tests based on their names
+#     custom_logger_tests.sort(key=lambda x: x.name)
+#     other_tests.sort(key=lambda x: x.name)
 
-    # Reorder the items list
-    items[:] = custom_logger_tests + other_tests
+#     # Reorder the items list
+#     items[:] = custom_logger_tests + other_tests
