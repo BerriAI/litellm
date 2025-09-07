07bd1951232a1b0bed674ab5fafb5f0414eb6d79
Bobronium <appkiller16@gmail.com>
2025-09-07 18:52:25 +0200

    Use getattr instead of hasattr in several places
    Not an exhaustive change
    
    (cherry picked from commit 77a9a1668f93029d93ac5e87396f3df0b934c09e)

--- litellm/caching/caching_handler.py
+++ litellm/caching/caching_handler.py
@@ -202,10 +202,11 @@ class LLMCachingHandler:
                         **kwargs
                     )
                     if (
-                        isinstance(cached_result, BaseModel)
-                        or isinstance(cached_result, CustomStreamWrapper)
-                    ) and hasattr(cached_result, "_hidden_params"):
-                        cached_result._hidden_params["cache_key"] = cache_key  # type: ignore
+                        isinstance(cached_result, (BaseModel, CustomStreamWrapper))
+                        and (_hidden_params := getattr(cached_result, "_hidden_params", None))
+                        is not None
+                    ):
+                        _hidden_params["cache_key"] = cache_key  # type: ignore
                     return CachingHandlerResponse(cached_result=cached_result)
                 elif (
                     call_type == CallTypes.aembedding.value
@@ -309,10 +310,11 @@ class LLMCachingHandler:
                         **kwargs
                     )
                     if (
-                        isinstance(cached_result, BaseModel)
-                        or isinstance(cached_result, CustomStreamWrapper)
-                    ) and hasattr(cached_result, "_hidden_params"):
-                        cached_result._hidden_params["cache_key"] = cache_key  # type: ignore
+                        isinstance(cached_result, (BaseModel, CustomStreamWrapper))
+                    ) and (
+                        _hidden_params := getattr(cached_result, "_hidden_params", None)
+                    ) is not None:
+                        _hidden_params["cache_key"] = cache_key  # type: ignore
                     return CachingHandlerResponse(cached_result=cached_result)
         return CachingHandlerResponse(cached_result=cached_result)
 
@@ -708,9 +710,9 @@ class LLMCachingHandler:
             )
 
         if (
-            hasattr(cached_result, "_hidden_params")
-            and cached_result._hidden_params is not None
-            and isinstance(cached_result._hidden_params, dict)
+            (_hidden_params := getattr(cached_result, "_hidden_params", None))
+            and _hidden_params is not None
+            and isinstance(_hidden_params, dict)
         ):
             cached_result._hidden_params["cache_hit"] = True
         return cached_result
