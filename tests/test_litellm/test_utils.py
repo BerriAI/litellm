00313a9df919396bfd0ceccac065f784443342c0
Krrish Dholakia <krrishdholakia@gmail.com>
2025-09-16 19:17:44 -0700

    fix(test_utils.py): fix test

--- tests/test_litellm/test_utils.py
+++ tests/test_litellm/test_utils.py
@@ -678,8 +678,8 @@ def test_aaamodel_prices_and_context_window_json_is_valid():
         },
     }
 
-    # prod_json = "./model_prices_and_context_window.json"
-    prod_json = "../../model_prices_and_context_window.json"
+    prod_json = "./model_prices_and_context_window.json"
+    # prod_json = "../../model_prices_and_context_window.json"
     with open(prod_json, "r") as model_prices_file:
         actual_json = json.load(model_prices_file)
     assert isinstance(actual_json, dict)
