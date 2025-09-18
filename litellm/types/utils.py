ae92404d05b8e224b6632edfe5a5ef335061ea42
Eddie Richter <eddie.richter@amd.com>
2025-09-18 13:06:09 -0600

    Initial addition of Lemonade provider.

--- litellm/types/utils.py
+++ litellm/types/utils.py
@@ -2428,6 +2428,7 @@ class LlmProviders(str, Enum):
     DOTPROMPT = "dotprompt"
     WANDB = "wandb"
     OVHCLOUD = "ovhcloud"
+    LEMONADE = "lemonade"
 
 
 # Create a set of all provider values for quick lookup
