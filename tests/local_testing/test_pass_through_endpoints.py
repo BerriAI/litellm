a1b9eebcbf3e1d1dc11aafbbc1facf6441b319dc
daily-kim <dh.kim@kaist.ac.kr>
2025-09-21 10:44:47 +0000

    fix: update authorization header to use 'Bearer' instead of 'bearer'

--- tests/local_testing/test_pass_through_endpoints.py
+++ tests/local_testing/test_pass_through_endpoints.py
@@ -117,7 +117,7 @@ async def test_pass_through_endpoint_rerank(client):
         {
             "path": "/v1/rerank",
             "target": "https://api.cohere.com/v1/rerank",
-            "headers": {"Authorization": f"bearer {_cohere_api_key}"},
+            "headers": {"Authorization": f"Bearer {_cohere_api_key}"},
         }
     ]
 
@@ -193,7 +193,7 @@ async def test_pass_through_endpoint_rpm_limit(
             "path": "/v1/rerank",
             "target": "https://api.cohere.com/v1/rerank",
             "auth": auth,
-            "headers": {"Authorization": f"bearer {_cohere_api_key}"},
+            "headers": {"Authorization": f"Bearer {_cohere_api_key}"},
         }
     ]
 
@@ -293,7 +293,7 @@ async def test_pass_through_endpoint_sequential_rpm_limit(
             "path": "/v1/rerank",
             "target": "https://api.cohere.com/v1/rerank",
             "auth": auth,
-            "headers": {"Authorization": f"bearer {_cohere_api_key}"},
+            "headers": {"Authorization": f"Bearer {_cohere_api_key}"},
         }
     ]
 
