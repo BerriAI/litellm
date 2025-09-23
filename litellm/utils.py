12a3233ddd579cc921f0acea5d8c4ab2f2914f3a
Alex Shoop <alexander.shoop@datarobot.com>
2025-09-24 01:43:11 +0900

    put back cache import utils

--- litellm/utils.py
+++ litellm/utils.py
@@ -59,6 +59,12 @@ import litellm.litellm_core_utils.json_validation_rule
 import litellm.llms
 import litellm.llms.gemini
 from litellm._uuid import uuid
+# Import cached imports utilities
+from litellm.litellm_core_utils.cached_imports import (
+    get_coroutine_checker,
+    get_litellm_logging_class,
+    get_set_callbacks,
+)
 from litellm.caching._internal_lru_cache import lru_cache_wrapper
 from litellm.caching.caching import DualCache
 from litellm.caching.caching_handler import CachingHandlerResponse, LLMCachingHandler
