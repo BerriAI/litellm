72b918f57b40ef66e1066d36893934cd46e6456c
Eddie Richter <eddie.richter@amd.com>
2025-09-23 17:07:34 -0600

    Adding max_tokens to constructor of LemonadeChatConfig

--- litellm/llms/lemonade/chat/transformation.py
+++ litellm/llms/lemonade/chat/transformation.py
@@ -42,6 +42,7 @@ class LemonadeChatConfig(OpenAILikeChatConfig):
         functions: Optional[list] = None,
         logit_bias: Optional[dict] = None,
         max_completion_tokens: Optional[int] = None,
+        max_tokens: Optional[int] = None,
         n: Optional[int] = None,
         presence_penalty: Optional[int] = None,
         stop: Optional[Union[str, list]] = None,
