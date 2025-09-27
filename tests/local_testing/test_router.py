82788853d14b03d8c96a90e3cd81d18f77b7ccd7
Ishaan Jaffer <ishaanjaffer0324@gmail.com>
2025-09-27 12:27:34 -0700

    test_router_azure_acompletion

--- tests/local_testing/test_router.py
+++ tests/local_testing/test_router.py
@@ -612,7 +612,7 @@ def test_router_azure_acompletion():
             {
                 "model_name": "gpt-3.5-turbo",  # openai model name
                 "litellm_params": {  # params for litellm completion/embedding call
-                    "model": "azure/gpt-4o-new-test",
+                    "model": "azure/gpt-4.1-nano",
                     "api_key": old_api_key,
                     "api_version": os.getenv("AZURE_API_VERSION"),
                     "api_base": os.getenv("AZURE_API_BASE"),
