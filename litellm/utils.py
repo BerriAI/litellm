9e3a21230305baca72177dcea9eaa5f19bd1ad43
Eddie Richter <eddie.richter@amd.com>
2025-09-30 09:29:13 -0600

    Removing get_model_info from Lemonade provider. Implemented get_models which gets hooked into get_valid_models litellm utility. Also, added a simple cost calculator implementation for Lemonade so calling cost_calculator.completion_cost() doesn't return an error when a model is not found in the model_cost json.

--- litellm/utils.py
+++ litellm/utils.py
@@ -4775,8 +4775,6 @@ def _get_model_info_helper(  # noqa: PLR0915
             custom_llm_provider == "ollama" or custom_llm_provider == "ollama_chat"
         ) and not _is_potential_model_name_in_model_cost(potential_model_names):
             return litellm.OllamaConfig().get_model_info(model)
-        elif (custom_llm_provider == "lemonade" and not _is_potential_model_name_in_model_cost(potential_model_names)):
-            return litellm.LemonadeChatConfig().get_model_info(model)
         else:
             """
             Check if: (in order of specificity)
@@ -7319,6 +7317,8 @@ class ProviderConfigManager:
             )
 
             return VLLMModelInfo()
+        elif LlmProviders.LEMONADE == provider:
+            return litellm.LemonadeChatConfig()
         return None
 
     @staticmethod
