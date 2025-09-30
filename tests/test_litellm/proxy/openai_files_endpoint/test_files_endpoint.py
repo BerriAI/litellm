f7636283fa1444a967234525a7212afae888d085
Henry Wang <phwhx@hotmail.com>
2025-09-30 16:05:17 +0800

    fix test issues from pr review

--- tests/test_litellm/proxy/openai_files_endpoint/test_files_endpoint.py
+++ tests/test_litellm/proxy/openai_files_endpoint/test_files_endpoint.py
@@ -134,14 +134,14 @@ def test_mock_create_audio_file(mocker: MockerFixture, monkeypatch, llm_router: 
                 custom_llm_provider="azure",
                 model="azure/chatgpt-v-2",
                 api_key="azure_api_key",
-                file=file_data,
+                file=file_data[1],
                 purpose=purpose_data,
             )
             await litellm.files.main.create_file(
                 custom_llm_provider="openai",
                 model="openai/gpt-3.5-turbo",
                 api_key="openai_api_key",
-                file=file_data,
+                file=file_data[1],
                 purpose=purpose_data,
             )
             # Return a dummy response object as needed by the test
