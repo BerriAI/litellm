461721b86de0f0a00bd5b5b350f227118efaefe9
andrew <elkin.andr@gmail.com>
2025-10-09 10:28:48 +0300

    snowflake support PAT, account_id and embeddings

--- litellm/__init__.py
+++ litellm/__init__.py
@@ -1285,6 +1285,7 @@ from .llms.vercel_ai_gateway.chat.transformation import VercelAIGatewayConfig
 from .llms.ovhcloud.chat.transformation import OVHCloudChatConfig
 from .llms.ovhcloud.embedding.transformation import OVHCloudEmbeddingConfig
 from .llms.lemonade.chat.transformation import LemonadeChatConfig
+from .llms.snowflake.embedding.transformation import SnowflakeEmbeddingConfig
 from .main import *  # type: ignore
 from .integrations import *
 from .llms.custom_httpx.async_client_cleanup import close_litellm_async_clients
