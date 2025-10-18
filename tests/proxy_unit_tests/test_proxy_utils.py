c4a4364b53bfcf7ef48a3219163d684994cc645e
Ishaan Jaffer <ishaanjaffer0324@gmail.com>
2025-10-18 15:38:55 -0700

    test_provider_specific_header_in_request

--- tests/proxy_unit_tests/test_proxy_utils.py
+++ tests/proxy_unit_tests/test_proxy_utils.py
@@ -1796,6 +1796,38 @@ def test_provider_specific_header_multi_provider():
     }
 
 
+
+@pytest.mark.parametrize(
+    "custom_llm_provider, expected_result",
+    [("anthropic", {"anthropic-beta": "test"}), ("bedrock", {"anthropic-beta": "test"}), ("vertex_ai", {"anthropic-beta": "test"})],
+)
+def test_provider_specific_header_in_request(custom_llm_provider, expected_result):
+    from litellm.types.utils import ProviderSpecificHeader
+    from litellm.llms.custom_httpx.http_handler import HTTPHandler
+    from unittest.mock import patch
+
+    litellm.set_verbose = True
+    client = HTTPHandler()
+    with patch.object(client, "post", return_value=MagicMock()) as mock_post:
+        try:
+            resp = litellm.completion(
+                model="anthropic/claude-3-5-sonnet-v2@20241022",
+                messages=[{"role": "user", "content": "Hello world"}],
+                provider_specific_header=ProviderSpecificHeader(
+                    custom_llm_provider="anthropic",
+                    extra_headers={"anthropic-beta": "test"},
+                ),
+                client=client,
+            )
+        except Exception as e:
+            print(f"Error: {e}")
+
+        mock_post.assert_called_once()
+        print(mock_post.call_args.kwargs["headers"])
+        assert "anthropic-beta" in mock_post.call_args.kwargs["headers"]
+
+
+
 from litellm.proxy._types import LiteLLM_UserTable
 
 
