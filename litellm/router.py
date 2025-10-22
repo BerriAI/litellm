17063a76226d29d540937c364b6bffbf80744e09
Abdullah-Majid-ZOE <abdullah.majid@joinzoe.com>
2025-10-22 14:20:57 +0100

    use deepcopy in _acompletion call so kwargs aren't mutated between requests

--- litellm/router.py
+++ litellm/router.py
@@ -1294,7 +1294,7 @@ class Router:
 
             input_kwargs = {
                 **data,
-                "messages": messages,
+                "messages": copy.deepcopy(messages),
                 "caching": self.cache_responses,
                 "client": model_client,
                 **kwargs,
