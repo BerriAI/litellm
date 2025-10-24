e80a46645a3acaefa0e003bb1dc4db7f011ac63b
Krrish Dholakia <krrishdholakia@gmail.com>
2025-10-23 18:46:52 -0700

    docs(docs/): document new azure ai vector store search

--- docs/my-website/sidebars.js
+++ docs/my-website/sidebars.js
@@ -398,6 +398,7 @@ const sidebars = {
           type: "category",
           label: "/vector_stores",
           items: [
+            "vector_stores/create",
             "vector_stores/search",
           ]
         },
@@ -448,6 +449,7 @@ const sidebars = {
             "providers/azure_ocr",
             "providers/azure_ai_speech",
             "providers/azure_ai_img",
+            "providers/azure_ai_vector_stores",
           ]
         },
         {
