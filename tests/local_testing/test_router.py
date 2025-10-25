ab0fc0a30de724587b329fa0683c314342631ac4
Ishaan Jaffer <ishaanjaffer0324@gmail.com>
2025-10-25 15:11:21 -0700

    test_aimg_gen_on_router

--- tests/local_testing/test_router.py
+++ tests/local_testing/test_router.py
@@ -1119,16 +1119,7 @@ async def test_aimg_gen_on_router():
                 "litellm_params": {
                     "model": "dall-e-3",
                 },
-            },
-            {
-                "model_name": "dall-e-3",
-                "litellm_params": {
-                    "model": "azure/dall-e-3-test",
-                    "api_version": "2023-12-01-preview",
-                    "api_base": os.getenv("AZURE_SWEDEN_API_BASE"),
-                    "api_key": os.getenv("AZURE_SWEDEN_API_KEY"),
-                },
-            },
+            }
         ]
         router = Router(model_list=model_list, num_retries=3)
         response = await router.aimage_generation(
