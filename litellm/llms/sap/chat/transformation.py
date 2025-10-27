86ea471aed9d9ac1dfd81adcbfb225698e954375
Vasilisa Parshikova <vasilisa.parshikova@sap.com>
2025-10-27 22:11:43 +0400

    fix(sap): fix chat transformation and list of supported params
    Signed-off-by: Vasilisa Parshikova <vasilisa.parshikova@sap.com>

--- litellm/llms/sap/chat/transformation.py
+++ litellm/llms/sap/chat/transformation.py
@@ -64,7 +64,7 @@ class GenAIHubOrchestrationConfig(OpenAIGPTConfig):
         return super().get_config()
 
     def get_supported_openai_params(self, model):
-        return [
+        params = [
             "frequency_penalty",
             "logit_bias",
             "logprobs",
@@ -89,6 +89,18 @@ class GenAIHubOrchestrationConfig(OpenAIGPTConfig):
             "response_format",
             "timeout",
         ]
+        if (
+            model.startswith('anthropic')
+            or model.startswith("amazon")
+            or model.startswith("cohere")
+            or model.startswith("alephalpha")
+            or model == "gpt-4"
+        ):
+            return [p for p in params if p != "response_format"]
+
+        return params
+
+
 
     def _transform_request(
         self,
@@ -114,13 +126,19 @@ class GenAIHubOrchestrationConfig(OpenAIGPTConfig):
 
         tools_ = optional_params.pop("tools", [])
         tools = [validate_dict(tool, ChatCompletionTool) for tool in tools_]
-
-        response_format_ = model_params.pop("response_format", {"type": "text"})
-        if response_format_["type"] == "json_schema":
-            response_format = validate_dict(response_format_, ResponseFormatJSONSchema)
+        if tools != []:
+            tools = {"tools": tools}
         else:
-            response_format = validate_dict(response_format_, ResponseFormat)
+            tools = {}
 
+        response_format = model_params.pop("response_format", {})
+        resp_type = response_format.get("type", None)
+        if resp_type:
+            if resp_type== "json_schema":
+                response_format = validate_dict(response_format, ResponseFormatJSONSchema)
+            else:
+                response_format = validate_dict(response_format, ResponseFormat)
+            response_format = {"response_format": response_format}
         stream = model_params.pop("stream", False)
         stream_config = {}
         if stream or "stream_options" in model_params:
@@ -137,8 +155,8 @@ class GenAIHubOrchestrationConfig(OpenAIGPTConfig):
                     "prompt_templating": {
                         "prompt": {
                             "template": template,
-                            "tools": tools,
-                            "response_format": response_format,
+                            **tools,
+                            **response_format
                         },
                         "model": {
                             "name": model,
@@ -150,17 +168,6 @@ class GenAIHubOrchestrationConfig(OpenAIGPTConfig):
                 "stream": stream_config,
             }
         }
-        if (
-            model.startswith('anthropic')
-            or model.startswith("amazon")
-            or model.startswith("cohere")
-            or model.startswith("alephalpha")
-            or model == "gpt-4"
-        ):
-            config["config"]["modules"]["prompt_templating"]["prompt"].pop("response_format")
-            config["config"]["modules"]["prompt_templating"]["prompt"]["template"].append(
-                {"role":"system",
-                 'content':json.dumps(response_format, ensure_ascii=False)})
 
         return config
 
