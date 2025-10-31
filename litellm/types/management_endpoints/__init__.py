be8a5af260c734a305128a489850a9951ec763e4
Ishaan Jaffer <ishaanjaffer0324@gmail.com>
2025-10-31 09:50:35 -0700

    add ROUTER_SETTINGS_FIELDS

---
+++ litellm/types/management_endpoints/__init__.py
@@ -0,0 +1,8 @@
+"""
+Types for management endpoints
+"""
+
+from .router_settings_endpoints import ROUTER_SETTINGS_FIELDS, RouterSettingsField
+
+__all__ = ["ROUTER_SETTINGS_FIELDS", "RouterSettingsField"]
+
