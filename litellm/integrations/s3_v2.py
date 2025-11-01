30aabf70b63b941fc25a378c0bd24040f888bba6
deepanshu <deepanshu.lulla@hq.bill.com>
2025-11-01 12:07:50 -0400

    s3 use key prefix

--- litellm/integrations/s3_v2.py
+++ litellm/integrations/s3_v2.py
@@ -49,7 +49,7 @@ class S3Logger(CustomBatchLogger, BaseAWSLLM):
         s3_batch_size: Optional[int] = DEFAULT_S3_BATCH_SIZE,
         s3_config=None,
         s3_use_team_prefix: bool = False,
-        s3_strip_base64_files: bool = False
+        s3_strip_base64_files: bool = False,
         **kwargs,
     ):
         try:
