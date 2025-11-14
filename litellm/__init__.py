32885087c2f760ecb920dea5bdcd700e0b9a8638
Ishaan Jaffer <ishaanjaffer0324@gmail.com>
2025-11-13 16:25:27 -0800

    add runwayml_models

--- litellm/__init__.py
+++ litellm/__init__.py
@@ -500,6 +500,7 @@ watsonx_models: Set = set()
 gemini_models: Set = set()
 xai_models: Set = set()
 deepseek_models: Set = set()
+runwayml_models: Set = set()
 azure_ai_models: Set = set()
 jina_ai_models: Set = set()
 voyage_models: Set = set()
@@ -687,6 +688,8 @@ def add_known_models():
             fal_ai_models.add(key)
         elif value.get("litellm_provider") == "deepseek":
             deepseek_models.add(key)
+        elif value.get("litellm_provider") == "runwayml":
+            runwayml_models.add(key)
         elif value.get("litellm_provider") == "meta_llama":
             llama_models.add(key)
         elif value.get("litellm_provider") == "nscale":
@@ -830,6 +833,7 @@ model_list = list(
     | deepinfra_models
     | perplexity_models
     | set(maritalk_models)
+    | runwayml_models
     | vertex_language_models
     | watsonx_models
     | gemini_models
@@ -921,6 +925,7 @@ models_by_provider: dict = {
     "xai": xai_models,
     "fal_ai": fal_ai_models,
     "deepseek": deepseek_models,
+    "runwayml": runwayml_models,
     "mistral": mistral_chat_models,
     "azure_ai": azure_ai_models,
     "voyage": voyage_models,
