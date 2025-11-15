40a858bcee8f5367380af909d37669202eaef47e
Emerson Gomes <emerson.gomes@thalesgroup.com>
2025-11-14 18:10:47 -0600

    Add Vertex Kimi-K2-Thinking

--- litellm/__init__.py
+++ litellm/__init__.py
@@ -485,6 +485,7 @@ vertex_ai_ai21_models: Set = set()
 vertex_mistral_models: Set = set()
 vertex_openai_models: Set = set()
 vertex_minimax_models: Set = set()
+vertex_moonshot_models: Set = set()
 ai21_models: Set = set()
 ai21_chat_models: Set = set()
 nlp_cloud_models: Set = set()
@@ -649,6 +650,9 @@ def add_known_models():
         elif value.get("litellm_provider") == "vertex_ai-minimax_models":
             key = key.replace("vertex_ai/", "")
             vertex_minimax_models.add(key)
+        elif value.get("litellm_provider") == "vertex_ai-moonshot_models":
+            key = key.replace("vertex_ai/", "")
+            vertex_moonshot_models.add(key)
         elif value.get("litellm_provider") == "ai21":
             if value.get("mode") == "chat":
                 ai21_chat_models.add(key)
@@ -908,7 +912,8 @@ models_by_provider: dict = {
     | vertex_vision_models
     | vertex_language_models
     | vertex_deepseek_models
-    | vertex_minimax_models,
+    | vertex_minimax_models
+    | vertex_moonshot_models,
     "ai21": ai21_models,
     "bedrock": bedrock_models | bedrock_converse_models,
     "petals": petals_models,
