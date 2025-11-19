1557e2d4ceb72b7b53fad1efa0e4e35240755fa9
Ido Lavi <ido@noma.security>
2025-11-19 16:59:38 +0200

    Use auth key name if there are no app id in in headers or in extra_data

--- litellm/proxy/guardrails/guardrail_hooks/noma/noma.py
+++ litellm/proxy/guardrails/guardrail_hooks/noma/noma.py
@@ -799,7 +799,7 @@ class NomaGuardrail(CustomGuardrail):
                     or request_data.get("metadata", {})
                     .get("headers", {})
                     .get("x-noma-application-id")
-                    or self.application_id,
+                    or (user_auth.key_name if user_auth.key_name else self.application_id),
                     "ipAddress": request_data.get("metadata", {}).get(
                         "requester_ip_address", None
                     ),
