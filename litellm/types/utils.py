135c3cc2625a317db51c7fea73acb8ca5efac4a4
Daniele Scasciafratte <mte90net@gmail.com>
2025-11-24 09:58:55 +0100

    Remove redundant model assignments in utils.py
    Removed redundant assignments of the 'model' variable.

--- litellm/types/utils.py
+++ litellm/types/utils.py
@@ -1394,7 +1394,6 @@ class ModelResponse(ModelResponseBase):
             created = int(time.time())
         else:
             created = created
-        model = model
         if usage is not None:
             if isinstance(usage, dict):
                 usage = Usage(**usage)
@@ -1508,7 +1507,6 @@ class EmbeddingResponse(OpenAIObject):
         if _response_headers:
             self._response_headers = _response_headers
 
-        model = model
         super().__init__(model=model, object=object, data=data, usage=usage)  # type: ignore
 
         if hidden_params:
@@ -1657,7 +1655,6 @@ class TextCompletionResponse(OpenAIObject):
         else:
             created = created
 
-        model = model
         if usage:
             usage = usage
         else:
