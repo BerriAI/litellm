773c4d08b420c439bc9ba1fed6e6916a3180ec53
yuneng-jiang <yuneng.jiang@gmail.com>
2025-12-13 13:04:28 -0800

    Merge Agent Usage with A2A Cost Tracking

--- docs/my-website/sidebars.js
+++ docs/my-website/sidebars.js
@@ -313,7 +313,6 @@ const sidebars = {
             "proxy/tag_budgets",
             "proxy/customers",
             "proxy/customer_usage",
-            "proxy/agent_usage",
             "proxy/dynamic_rate_limit",
             "proxy/rate_limit_tiers",
             "proxy/temporary_budget_increase",
