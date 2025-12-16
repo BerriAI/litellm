f80628502d8c3c57d609a1766535f72a71918b6a
Ishaan Jaffer <ishaanjaffer0324@gmail.com>
2025-12-17 02:27:08 +0400

    init docs

--- docs/my-website/sidebars.js
+++ docs/my-website/sidebars.js
@@ -472,6 +472,7 @@ const sidebars = {
           "generateContent",
           "apply_guardrail",
           "bedrock_invoke",
+          "interactions",
         {
           type: "category",
           label: "/images",
