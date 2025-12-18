d7514c0976708f33eab72503e668f34104364c77
yuneng-jiang <yuneng.jiang@gmail.com>
2025-12-17 16:01:59 -0800

    Max budget alerts working

--- litellm/proxy/_types.py
+++ litellm/proxy/_types.py
@@ -2482,6 +2482,7 @@ class CallInfo(LiteLLMPydanticObjectBase):
 class WebhookEvent(CallInfo):
     event: Literal[
         "budget_crossed",
+        "max_budget_alert",
         "soft_budget_crossed",
         "threshold_crossed",
         "projected_limit_exceeded",
