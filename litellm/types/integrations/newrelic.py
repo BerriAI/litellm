d427061bb2263346bb05db1d58c603b025ada669
Josh Bonczkowski <jbonczkowski@newrelic.com>
2025-12-29 16:37:26 -0500

    Mised file.

---
+++ litellm/types/integrations/newrelic.py
@@ -0,0 +1,11 @@
+from typing import Optional
+
+from litellm.types.integrations.custom_logger import StandardCustomLoggerInitParams
+
+
+class NewRelicInitParams(StandardCustomLoggerInitParams):
+    """
+    Params for initializing a New Relic logger on litellm
+    """
+
+    pass
