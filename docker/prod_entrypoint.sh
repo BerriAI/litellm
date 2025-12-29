747876837fd31668e08318df1913d30fae3015e5
Josh Bonczkowski <jbonczkowski@newrelic.com>
2025-12-29 12:18:41 -0500

    Enabling New Relic agent to be wired when docker container starts if an environment variable is set.

--- docker/prod_entrypoint.sh
+++ docker/prod_entrypoint.sh
@@ -3,6 +3,8 @@
 if [ "$USE_DDTRACE" = "true" ]; then
     export DD_TRACE_OPENAI_ENABLED="False"
     exec ddtrace-run litellm "$@"
+elif [ "$USE_NEWRELIC" = "true" ]; then
+    exec newrelic-admin run-python litellm "$@"
 else
     exec litellm "$@"
 fi
