8be3712e829ca28f739ea68780fbeb8e64d2f52c
Sameer Kankute <sameer@berri.ai>
2026-01-16 12:52:13 +0530

    Add docs for message sanitisation

--- docs/my-website/sidebars.js
+++ docs/my-website/sidebars.js
@@ -822,6 +822,7 @@ const sidebars = {
         "completion/knowledgebase",
         "guides/code_interpreter",
         "completion/message_trimming",
+        "completion/message_sanitization",
         "completion/model_alias",
         "completion/mock_requests",
         "completion/predict_outputs",
