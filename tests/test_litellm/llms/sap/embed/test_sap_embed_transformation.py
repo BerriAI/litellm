4201c9cc532c979e11ec9231c0f62e3776c65ec4
Vasilisa Parshikova <vasilisa.parshikova@sap.com>
2026-01-17 01:11:14 +0400

    fix embed test

--- tests/test_litellm/llms/sap/embed/test_sap_embed_transformation.py
+++ tests/test_litellm/llms/sap/embed/test_sap_embed_transformation.py
@@ -1,9 +1,20 @@
+from unittest.mock import patch, PropertyMock
+
 import pytest
 
 from litellm.llms.sap.embed.transformation import GenAIHubEmbeddingConfig
 
+@pytest.fixture
+def fake_token_creator():
+    return lambda: "Bearer FAKE_TOKEN", "https://api.ai.moke-sap.com", "fake-group"
+
+
+@pytest.fixture
+def fake_deployment_url():
+    return "https://api.ai.moke-sap.com/v2/inference/deployments/mokeid"
+
 @pytest.mark.asyncio
-async def test_basic_config_transform():
+async def test_basic_config_transform(fake_token_creator, fake_deployment_url):
     expected_dict = {
         'config': {
             'modules': {
@@ -21,10 +32,18 @@ async def test_basic_config_transform():
             'type': 'text'
         }
     }
-    body = GenAIHubEmbeddingConfig().transform_embedding_request(
-        model="text-embedding-3-small",
-        input="Hi",
-        optional_params={},
-        headers={}
-    )
-    assert body == expected_dict
+    with patch(
+            "litellm.llms.sap.embed.transformation.GenAIHubEmbeddingConfig.deployment_url",
+            new_callable=PropertyMock,
+            return_value=fake_deployment_url,
+    ), patch(
+        "litellm.llms.sap.embed.transformation.get_token_creator",
+        return_value=fake_token_creator,
+    ):
+        body = GenAIHubEmbeddingConfig().transform_embedding_request(
+            model="text-embedding-3-small",
+            input="Hi",
+            optional_params={},
+            headers={}
+        )
+        assert body == expected_dict
