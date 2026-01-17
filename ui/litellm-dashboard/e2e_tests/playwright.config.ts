c4f254d9e47b2fe7f39ade1b769a453dde9a1dc2
yuneng-jiang <yuneng.jiang@gmail.com>
2026-01-17 14:04:55 -0800

    Adding debugging for e2e ui test

--- ui/litellm-dashboard/e2e_tests/playwright.config.ts
+++ ui/litellm-dashboard/e2e_tests/playwright.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
+    screenshot: "only-on-failure",
   },
 
   /* Configure projects for major browsers */
