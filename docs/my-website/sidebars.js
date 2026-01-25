937ccf1977a07cd25de3b4e9f063fdaf1cbfc86e
yuneng-jiang <yuneng.jiang@gmail.com>
2026-01-24 16:23:46 -0800

    UI Keys Teams Router Settings docs

--- docs/my-website/sidebars.js
+++ docs/my-website/sidebars.js
@@ -879,6 +879,7 @@ const sidebars = {
         "scheduler",
         "proxy/auto_routing",
         "proxy/load_balancing",
+        "proxy/keys_teams_router_settings",
         "proxy/provider_budget_routing",
         "proxy/reliability",
         "proxy/fallback_management",
