49cf326a77a78329e352a62e1f8b0946abc8cc2e
Dominic Fallows <dominic.fallows@ii.co.uk>
2026-01-30 15:14:13 +0000

    Rebuild ui dashboard

--- litellm/proxy/_experimental/out/_next/static/chunks/pages/_app-b92e2a0a71f93668.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2888],{42733:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return u(21038)}])}},function(n){var _=function(_){return n(n.s=_)};n.O(0,[9774,179],function(){return _(42733),_(45653)}),_N_E=n.O()}]);
--- litellm/proxy/_experimental/out/_next/static/chunks/pages/_error-85b817c4d94ba0d2.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4820],{60313:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(8785)}])}},function(n){n.O(0,[2888,9774,179],function(){return n(n.s=60313)}),_N_E=n.O()}]);
