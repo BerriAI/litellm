bb363f03074d9feafac2a5d49e328a0dd27b2d22
Sameer Kankute <sameer@berri.ai>
2026-02-02 17:49:18 +0530

    Fix: test_bedrock_optional_params_embeddings_dimension

--- tests/test_litellm/test_utils.py
+++ tests/test_litellm/test_utils.py
@@ -749,7 +749,7 @@ def test_aaamodel_prices_and_context_window_json_is_valid():
         },
     }
 
-    prod_json = "litellm/model_prices_and_context_window.json"
+    prod_json = "./model_prices_and_context_window.json"
     # prod_json = "../../model_prices_and_context_window.json"
     with open(prod_json, "r") as model_prices_file:
         actual_json = json.load(model_prices_file)
