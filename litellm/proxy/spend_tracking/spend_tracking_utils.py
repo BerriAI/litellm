e3451b3ba54911d55cd2b85348e0d0c801a81119
RussellLuo <luopeng.he@gmail.com>
2026-02-27 00:07:12 +0800

    Some tweaks and add more tests

--- litellm/proxy/spend_tracking/spend_tracking_utils.py
+++ litellm/proxy/spend_tracking/spend_tracking_utils.py
@@ -771,6 +771,7 @@ def _transform_anthropic_request_to_openai_format(request_body: dict) -> dict:
         Transformed request body in OpenAI-compatible format
     """
     # Transform system message if present
+    request_body = dict(request_body)
     system_content = request_body.pop("system", None)
     if system_content:
         request_body["messages"] = [
@@ -784,11 +785,12 @@ def _transform_anthropic_request_to_openai_format(request_body: dict) -> dict:
                 "type": "function",
                 "function": {
                     "name": t["name"],
-                    "description": t["description"],
-                    "parameters": t["input_schema"]
+                    "description": t.get("description", ""),
+                    "parameters": t.get("input_schema", {})
                 }
             }
             for t in request_body["tools"]
+            if "input_schema" in t  # Skip non-standard tools (computer_use, bash, etc.)
         ]
 
     return request_body
