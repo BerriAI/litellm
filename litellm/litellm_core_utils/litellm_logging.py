756490ea6da3d64913bd68f7360fecb300a8d516
giulio-leone <giulio-leone@users.noreply.github.com>
2026-03-01 19:44:58 +0100

    fix(logging): skip non-numeric header values instead of storing str fallback

--- litellm/litellm_core_utils/litellm_logging.py
+++ litellm/litellm_core_utils/litellm_logging.py
@@ -4880,7 +4880,7 @@ class StandardLoggingPayloadSetup:
                 try:
                     _value = int(_value)
                 except (ValueError, TypeError):
-                    _value = str(_value)
+                    continue
             else:
                 _value = str(_value)
 
