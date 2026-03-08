6847a75408fff7a3003cdbc0202a8e96498f0950
Ishaan Jaffer <ishaanjaffer0324@gmail.com>
2026-03-07 16:57:24 -0800

    fix(mistral): upgrade http:// image URLs to https:// before sending to API
    Mistral rejects http:// image URLs with 422 Unprocessable Entity.
    Upgrade them to https:// transparently in the message transformation
    so users don't need to worry about the protocol.

--- tests/llm_translation/base_llm_unit_tests.py
+++ tests/llm_translation/base_llm_unit_tests.py
@@ -910,10 +910,6 @@ class BaseLLMChatTest(ABC):
             )
         except litellm.InternalServerError:
             pytest.skip("Model is overloaded")
-        except litellm.BadRequestError:
-            if "http://" in image_url:
-                pytest.skip("Model does not support http:// image URLs")
-            raise
 
         assert response is not None
 
