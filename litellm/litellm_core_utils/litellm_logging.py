41098a86a8b65425b099e07064e7f01fbbf9282e
Harshit28j <Harshitjain0562@gmail.com>
2026-03-12 08:02:32 +0530

    fix: missing brace

--- litellm/litellm_core_utils/litellm_logging.py
+++ litellm/litellm_core_utils/litellm_logging.py
@@ -2917,10 +2917,13 @@ class Logging(LiteLLMLoggingBaseClass):
                             start_time=start_time,
                             end_time=end_time,
                             print_verbose=print_verbose,
+                            callback_func=callback,
+                        )
                     if (
                         isinstance(callback, CustomLogger)
                         and is_sync_request
-                        and self.call_type != CallTypes.pass_through.value  # pass-through endpoints call async_log_failure_event
+                        and self.call_type
+                        != CallTypes.pass_through.value  # pass-through endpoints call async_log_failure_event
                     ):  # custom logger class
                         callback.log_failure_event(
                             start_time=start_time,
