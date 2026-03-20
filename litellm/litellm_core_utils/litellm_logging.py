df6aacea183aa27d205261bd60fa3160305d9aee
naaa760 <neh6a683@gmail.com>
2026-03-20 15:48:09 +0530

    update

--- litellm/litellm_core_utils/litellm_logging.py
+++ litellm/litellm_core_utils/litellm_logging.py
@@ -1971,6 +1971,8 @@ class Logging(LiteLLMLoggingBaseClass):
             event_type="sync_success"
         ):  # prevent double logging
             return
+        if kwargs.get("response_cost") is not None:
+            self.model_call_details["response_cost"] = kwargs["response_cost"]
         start_time, end_time, result = self._success_handler_helper_fn(
             start_time=start_time,
             end_time=end_time,
@@ -2442,6 +2444,9 @@ class Logging(LiteLLMLoggingBaseClass):
             event_type="async_success"
         ):  # prevent double logging
             return
+
+        if kwargs.get("response_cost") is not None:
+            self.model_call_details["response_cost"] = kwargs["response_cost"]
 
         ## CALCULATE COST FOR BATCH JOBS
         if self.call_type == CallTypes.aretrieve_batch.value and isinstance(
