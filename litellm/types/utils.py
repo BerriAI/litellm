8ac3a1256801485372bcc72e63829c1e58d592b3
Josh <joshhbyron@gmail.com>
2026-03-22 10:34:00 -0400

    Add user_api_key_org_alias to StandardLoggingUserAPIKeyMetadata

--- litellm/types/utils.py
+++ litellm/types/utils.py
@@ -2486,6 +2486,7 @@ class StandardLoggingUserAPIKeyMetadata(TypedDict):
     user_api_key_max_budget: Optional[float]
     user_api_key_budget_reset_at: Optional[str]
     user_api_key_org_id: Optional[str]
+    user_api_key_org_alias: Optional[str]
     user_api_key_team_id: Optional[str]
     user_api_key_project_id: Optional[str]
     user_api_key_user_id: Optional[str]
