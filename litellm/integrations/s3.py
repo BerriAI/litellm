12bbd109ce0ea993ab9c6e142a6b7efd54cdc7f2
Raashish Aggarwal <94279692+raashish1601@users.noreply.github.com>
2026-03-26 22:03:27 +0530

    Clarify S3 filename length constant

--- litellm/integrations/s3.py
+++ litellm/integrations/s3.py
@@ -9,7 +9,7 @@ import litellm
 from litellm._logging import print_verbose, verbose_logger
 from litellm.types.utils import StandardLoggingPayload
 
-MAX_S3_FILENAME_COMPONENT_LENGTH = 250
+MAX_S3_FILENAME_STEM_LENGTH = 250
 
 
 class S3Logger:
@@ -188,9 +188,9 @@ def get_s3_object_key(
     start_time: datetime,
     s3_file_name: str,
 ) -> str:
-    if len(s3_file_name) > MAX_S3_FILENAME_COMPONENT_LENGTH:
+    if len(s3_file_name) > MAX_S3_FILENAME_STEM_LENGTH:
         digest = hashlib.sha256(s3_file_name.encode("utf-8")).hexdigest()[:16]
-        prefix_length = MAX_S3_FILENAME_COMPONENT_LENGTH - len(digest) - 1
+        prefix_length = MAX_S3_FILENAME_STEM_LENGTH - len(digest) - 1
         s3_file_name = f"{s3_file_name[:prefix_length]}_{digest}"
 
     s3_object_key = (
