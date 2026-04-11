556891fdf620542104c26f9e5068bcc7d8d47d1f
Ishaan Jaffer <ishaan@berri.ai>
2026-04-11 12:32:04 -0700

    style: black format entire codebase (24.10.0)

--- litellm/constants.py
+++ litellm/constants.py
@@ -1421,7 +1421,7 @@ APSCHEDULER_REPLACE_EXISTING = os.getenv(
     "1",
 ]  # always replace existing jobs
 
-# The number of tag entries are higher than number of user, team entries. This leads to a higher QPS. 
+# The number of tag entries are higher than number of user, team entries. This leads to a higher QPS.
 # This will run tag spcific tasks at a later time to smooth QPS
 DAILY_TAG_SPEND_BATCH_MULTIPLIER = 2.3
 
