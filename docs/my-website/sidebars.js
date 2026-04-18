52934ae9ed405bcf5ecc554945fa4387dc3826c3
clyang <clyang@gmail.com>
2026-04-18 22:09:44 +0800

    Add skills gateway category back to sidebar

--- docs/my-website/sidebars.js
+++ docs/my-website/sidebars.js
@@ -340,6 +340,13 @@ const sidebars = {
                 },
               ],
             },
+            {
+              type: "category",
+              label: "Skills Gateway",
+              items: [
+                "skills_gateway",
+              ],
+            },
           ],
         },
         {
