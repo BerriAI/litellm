45474568ad97ab80b9f8fd424940e6110f71298e
Sameer Kankute <sameer@berri.ai>
2026-04-28 17:30:41 +0530

    Fix tests

--- tests/test_litellm/test_utils.py
+++ tests/test_litellm/test_utils.py
@@ -855,6 +855,7 @@ def test_aaamodel_prices_and_context_window_json_is_valid():
                 "supports_max_reasoning_effort": {"type": "boolean"},
                 "supports_service_tier": {"type": "boolean"},
                 "supports_preset": {"type": "boolean"},
+                    "supports_output_config": {"type": "boolean"},
                 "tool_use_system_prompt_tokens": {"type": "number"},
                 "tpm": {"type": "number"},
                 "provider_specific_entry": {"type": "object"},
