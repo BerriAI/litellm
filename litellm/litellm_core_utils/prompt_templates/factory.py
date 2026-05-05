4464035291f82afe213e996898b0425a4172ab4c
Krrish Dholakia <krrish+github@berri.ai>
2026-05-04 18:12:34 -0700

    fix: test commit

--- litellm/litellm_core_utils/prompt_templates/factory.py
+++ litellm/litellm_core_utils/prompt_templates/factory.py
@@ -1681,6 +1681,8 @@ def convert_to_anthropic_tool_result(
 ) -> AnthropicMessagesToolResultParam:
     """
     OpenAI message with a tool result looks like:
+
+
     {
         "tool_call_id": "tool_1",
         "role": "tool",
