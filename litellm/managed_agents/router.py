cf454e38c2d9852827fd69ef9d66574bc15e513a
Ishaan Jaffer <ishaanjaffer0324@gmail.com>
2026-05-07 10:07:54 -0700

    fix(v2 managed agents): wire up sessions/messages/events sub-routers

--- litellm/managed_agents/router.py
+++ litellm/managed_agents/router.py
@@ -6,20 +6,13 @@ for inclusion in the main FastAPI app.
 from fastapi import APIRouter
 
 from litellm.managed_agents.endpoints.agents import router as agents_router
-
-# TODO: enable when litellm/managed_agents/endpoints/sessions.py ships
-# from litellm.managed_agents.endpoints.sessions import router as sessions_router
-# TODO: enable when litellm/managed_agents/endpoints/messages.py ships
-# from litellm.managed_agents.endpoints.messages import router as messages_router
-# TODO: enable when litellm/managed_agents/endpoints/events.py ships
-# from litellm.managed_agents.endpoints.events import router as events_router
+from litellm.managed_agents.endpoints.events import router as events_router
+from litellm.managed_agents.endpoints.messages import router as messages_router
+from litellm.managed_agents.endpoints.sessions import router as sessions_router
 
 managed_agents_router = APIRouter(tags=["managed_agents_v2"])
 
 managed_agents_router.include_router(agents_router)
-# TODO: enable when litellm/managed_agents/endpoints/sessions.py ships
-# managed_agents_router.include_router(sessions_router)
-# TODO: enable when litellm/managed_agents/endpoints/messages.py ships
-# managed_agents_router.include_router(messages_router)
-# TODO: enable when litellm/managed_agents/endpoints/events.py ships
-# managed_agents_router.include_router(events_router)
+managed_agents_router.include_router(sessions_router)
+managed_agents_router.include_router(messages_router)
+managed_agents_router.include_router(events_router)
