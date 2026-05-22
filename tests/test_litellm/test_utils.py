2165dc8141a5d6ccc27d3cb9065dbdc238791eec
mateo-berri <277851410+mateo-berri@users.noreply.github.com>
2026-05-22 17:06:28 +0000

    test: allow regional_processing_uplift_multiplier_{eu,us} in model_prices schema

--- tests/test_litellm/test_utils.py
+++ tests/test_litellm/test_utils.py
@@ -736,6 +736,8 @@ def test_aaamodel_prices_and_context_window_json_is_valid():
                 "output_cost_per_token_priority": {"type": "number"},
                 "output_cost_per_token_above_200k_tokens_priority": {"type": "number"},
                 "output_cost_per_token_above_272k_tokens_priority": {"type": "number"},
+                "regional_processing_uplift_multiplier_eu": {"type": "number"},
+                "regional_processing_uplift_multiplier_us": {"type": "number"},
                 "input_cost_per_pixel": {"type": "number"},
                 "input_cost_per_query": {"type": "number"},
                 "input_cost_per_request": {"type": "number"},
