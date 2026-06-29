69d8d741f10c346d67266d9c3a295fe85a0954e8
Luke Sorvik <lsorvik@digitalocean.com>
2026-06-29 15:19:48 -0700

    trying to fix empty tool call in stream

--- litellm/llms/anthropic/experimental_pass_through/adapters/transformation.py
+++ litellm/llms/anthropic/experimental_pass_through/adapters/transformation.py
@@ -1432,7 +1432,6 @@ class LiteLLMAnthropicMessagesAdapter:
             if choice.delta.content is not None and len(choice.delta.content) > 0:
                 text += choice.delta.content
             if choice.delta.tool_calls is not None and len(choice.delta.tool_calls) > 0:
-                partial_json = ""
                 for tool in choice.delta.tool_calls:
                     if (
                         tool.function is not None
