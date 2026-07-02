d4d31b17efdbc8751e497a3b22fc3d88d2f8ce30
Fabrizio Siciliano <fa.siciliano@reply.de>
2026-07-02 12:46:36 +0200

    fixed missing resource key

--- terraform/litellm/aws/alb.tf
+++ terraform/litellm/aws/alb.tf
@@ -137,7 +137,7 @@ resource "aws_lb_listener" "https" {
   port              = 443
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
-  certificate_arn   = aws_acm_certificate.cert.arn
+  certificate_arn   = aws_acm_certificate.cert[0].arn
 
   default_action {
     type             = "forward"
