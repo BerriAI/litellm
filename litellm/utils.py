e3f7c1f23a5c7ddd5f265e3b37331ad17043bf4c
Bartok9 <danielrpike9@gmail.com>
2026-07-16 19:08:43 -0400

    fix(utils): report missing HEROKU_API_KEY in validate_environment
    Heroku chat uses HEROKU_API_KEY; validate_environment previously
    skipped the provider and looked always configured.

--- litellm/utils.py
+++ litellm/utils.py
@@ -6149,6 +6149,11 @@ def validate_environment(
                 keys_in_environment = True
             else:
                 missing_keys.append("MOONSHOT_API_KEY")
+        elif custom_llm_provider == "heroku":
+            if "HEROKU_API_KEY" in os.environ:
+                keys_in_environment = True
+            else:
+                missing_keys.append("HEROKU_API_KEY")
     else:
         ## openai - chatcompletion + text completion
         if (
