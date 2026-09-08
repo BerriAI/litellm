32ad504c193e20947ff370edf7dbb2bce0d3be27
Sricharan Anugandula <anugandulasricharan99@gmail.com>
2026-09-09 00:56:20 +0530

    fix formatting

--- litellm/proxy/auth/user_api_key_auth.py
+++ litellm/proxy/auth/user_api_key_auth.py
@@ -1916,7 +1916,6 @@ async def _user_api_key_auth_builder(
             valid_token.end_user_rpm_limit = end_user_params.get("end_user_rpm_limit")
             valid_token.allowed_model_region = end_user_params.get("allowed_model_region")
 
-       
         if valid_token is not None:
             valid_token = update_valid_token_with_end_user_params(
                 valid_token=valid_token, end_user_params=end_user_params
