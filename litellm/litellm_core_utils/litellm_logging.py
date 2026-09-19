0c116d9122f4fde6a925614ef551fdaed5c2176b
onatozmenn <onatozmen44@gmail.com>
2026-09-19 23:22:14 +0300

    fix(logging): fall through to cost calculator on None hidden response_cost

--- litellm/litellm_core_utils/litellm_logging.py
+++ litellm/litellm_core_utils/litellm_logging.py
@@ -2396,7 +2396,7 @@ class Logging(LiteLLMLoggingBaseClass):
 
         if self.model_call_details.get("cache_hit") is True:
             self.model_call_details["response_cost"] = 0.0
-        elif "response_cost" in hidden_params:
+        elif hidden_params.get("response_cost") is not None:
             self.model_call_details["response_cost"] = hidden_params["response_cost"]
             self._record_zero_cost_diagnostic(logging_result, hidden_params["response_cost"])
         elif (existing_cost := self.model_call_details.get("response_cost")) is not None and existing_cost != 0:
