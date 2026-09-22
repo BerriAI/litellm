cf2f311d59904e042e5bf151b01d7e8b91db9066
leecoder <leecoder@aol.com>
2026-09-22 14:55:36 +0900

    Revert "feat(macos-usage): add gateway quota menu bar app"
    This reverts commit affdef5b1edc8f8c2c8fbfaef389e6677b30efc8.

--- apps/macos-usage/Package.swift
+++
@@ -1,9 +0,0 @@
-// swift-tools-version: 6.0
-import PackageDescription
-
-let package = Package(
-    name: "LiteLLMUsageMenuBar",
-    platforms: [.macOS(.v13)],
-    products: [.executable(name: "LiteLLMUsageMenuBar", targets: ["LiteLLMUsageMenuBar"])],
-    targets: [.executableTarget(name: "LiteLLMUsageMenuBar")]
-)
